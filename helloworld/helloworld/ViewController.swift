7602ab7de2aa727d531a8e81c9557a9f5e732ad3
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-06-10 00:27:57 +0200

    Added lesson 1

---
+++ helloworld/helloworld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  helloworld
+//
+//  Created by Domenico Solazzo on 6/10/14.
+//  Copyright (c) 2014 Domenico Solazzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
