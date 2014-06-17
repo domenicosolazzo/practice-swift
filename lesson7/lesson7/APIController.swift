390963515d8bc7c9b36a085d41f1fc1e22f7a53e
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-06-17 20:26:17 +0200

    Cleaning up the APIController

--- lesson7/lesson7/APIController.swift
+++ lesson7/lesson7/APIController.swift
@@ -5,23 +5,8 @@ protocol APIControllerProtocol {
 }
 
 class APIController: NSObject {
-    var data: NSMutableData = NSMutableData()
     var delegate: APIControllerProtocol?
     
-    func connection(didReceiveResponse: NSURLConnection!, didReceiveResponse response: NSURLResponse!) {
-        self.data = NSMutableData()
-    }
-    
-    func connection(connection: NSURLConnection!, didReceiveData data: NSData!) {
-        self.data.appendData(data)
-    }
-    
-    func connectionDidFinishLoading(connection: NSURLConnection!) {
-        var err: NSError
-        var jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
-        
-        delegate?.didRecieveAPIResults(jsonResult)
-    }
     
     func searchItunesFor(searchTerm: String){
         var itunesSearchTerm = searchTerm.stringByReplacingOccurrencesOfString(" ", withString: "+", options: NSStringCompareOptions.CaseInsensitiveSearch, range:nil)
@@ -29,10 +14,8 @@ class APIController: NSObject {
         var urlPath = "https://itunes.apple.com/search?term=\(escapedItunesSearchTerm)&media=software"
         var url: NSURL = NSURL(string: urlPath)
         var request: NSURLRequest = NSURLRequest(URL: url)
-        var connection = NSURLConnection(request: request, delegate:self, startImmediately:false)
         
-        println("Start ITUNES search at \(urlPath)")
-        connection.start()
+        
     }
     
     
