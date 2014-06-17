203de7a9a319dba22645e6f2f1747e75845442b0
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-06-17 20:46:13 +0200

    Getting the data using sendAsynchronousRequest

--- lesson7/lesson7/APIController.swift
+++ lesson7/lesson7/APIController.swift
@@ -4,9 +4,12 @@ protocol APIControllerProtocol {
     func didRecieveAPIResults(results: NSDictionary)
 }
 
-class APIController: NSObject {
+class APIController {
     var delegate: APIControllerProtocol?
     
+    init(delegate: APIControllerProtocol?) {
+        self.delegate = delegate
+    }
     
     func searchItunesFor(searchTerm: String){
         var itunesSearchTerm = searchTerm.stringByReplacingOccurrencesOfString(" ", withString: "+", options: NSStringCompareOptions.CaseInsensitiveSearch, range:nil)
@@ -15,8 +18,24 @@ class APIController: NSObject {
         var url: NSURL = NSURL(string: urlPath)
         var request: NSURLRequest = NSURLRequest(URL: url)
         
-        
+        // Use SendAsynchronous
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: {(response: NSURLResponse!,data: NSData!,error: NSError!) -> Void in
+            if error? {
+                println("ERROR: (error.localizedDescription)")
+            }
+            else {
+                var error: NSError?
+                let jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &error) as NSDictionary
+                // Now send the JSON result to our delegate object
+                if error? {
+                    println("HTTP Error: (error?.localizedDescription)")
+                }
+                else {
+                    println("Results recieved")
+                    self.delegate?.didRecieveAPIResults(jsonResult)
+                }
+            }
+        })
     }
-    
     
 }
