5c77a8f983c711b51dd5e43b83477a4f0b9b03dd
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-06-17 20:22:31 +0200

    Lesson7: Multiple views

---
+++ lesson7/lesson7Tests/lesson7Tests.swift
@@ -0,0 +1,35 @@
+//
+//  lesson7Tests.swift
+//  lesson7Tests
+//
+//  Created by Domenico Solazzo on 6/17/14.
+//  Copyright (c) 2014 Domenico Solazzo. All rights reserved.
+//
+
+import XCTest
+
+class lesson7Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
