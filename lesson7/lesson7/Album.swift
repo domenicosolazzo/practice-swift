3654eb78399e35e27cfd5d48a6674a2a85290023
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-06-17 20:52:26 +0200

    Added Album model

---
+++ lesson7/lesson7/Album.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+class Album{
+    var title: String?
+    var price: String?
+    var thumbnailImageURL: String?
+    var largeImageURL: String?
+    var itemURL: String?
+    var artistURL: String?
+    
+    init(name: String!, price: String!, thumbnailImageURL: String!, largeImageURL: String!, itemURL: String!, artistURL: String!) {
+        self.title = name
+        self.price = price
+        self.thumbnailImageURL = thumbnailImageURL
+        self.largeImageURL = largeImageURL
+        self.itemURL = itemURL
+        self.artistURL = artistURL
+    }
+
+}
