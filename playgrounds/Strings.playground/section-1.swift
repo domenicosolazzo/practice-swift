e43b86fbd7e38d7b0c4c23ca025dd4fc10212314
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-06-22 10:42:18 +0200

    Added constants, initialization and concatenation of strings

--- playgrounds/Strings.playground/section-1.swift
+++ playgrounds/Strings.playground/section-1.swift
@@ -2,8 +2,38 @@
 
 import Cocoa
 
+/*
+======== A string ========
+*/
 var str = "Hello, playground"
 
-// Immutable string
+/*
+======== Immutable string ========
+*/
 let immutableString = "This is immutable"
+
+/*
+======== Create a character constant ========
+*/
+let euroSymbolConstant: Character = "€"
+
+/* 
+======== Create a character value ========
+*/
+var euroSymbol: Character = "€"
+
+
+/* 
+======== Initializing an empty string ========
+*/
+var emptyString = String()
+// Or..
+emptyString = ""
+
+/*
+======== Concatenating strings ========
+*/
+var str1 = "Hello"
+var str2 = "World"
+var finalResult = (str1 + str2)
 
