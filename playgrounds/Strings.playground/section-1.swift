6a69c36ca3e4b296cd2fa87d64611ce903bf417d
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-06-22 10:48:25 +0200

    String interpolation, counting characters and string equality

--- playgrounds/Strings.playground/section-1.swift
+++ playgrounds/Strings.playground/section-1.swift
@@ -37,3 +37,27 @@ var str1 = "Hello"
 var str2 = "World"
 var finalResult = (str1 + str2)
 
+/* 
+======== String interpolation ========
+*/
+let name: String  = "Domenico"
+var interpolation = "My name is \(name)"
+
+/*
+======== Counting characters ========
+*/
+var count = (countElements("Domenico"))
+
+/*
+======== String equality ========
+*/
+var equality1 = "Domenico"
+var equality2 = "Domenico"
+
+if(equality1 == equality2){
+    var equal = "These two strings are equal"
+}
+
+
+
+
