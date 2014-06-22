b932f2f1bbd2b10fad245ba07354ecccd9957c28
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-06-22 15:33:05 +0200

    Added Optionals playground (FOptionals, Forced unwrapping, Nil and if statements

---
+++ playgrounds/Optionals.playground/section-1.swift
@@ -0,0 +1,30 @@
+// Optionals: Use optionals when a value may be absent
+import Cocoa
+
+let possibleNumber = "123"
+// This is converted to Int? (Optional int) because not every string can be
+// converted to Int
+let number = possibleNumber.toInt()
+
+/*
+====== IF statements ======
+*/
+if number{
+    // If the optional contains a value, the if statement will be true
+    println("The number exists")
+}else{
+    println("The number does not exist")
+}
+
+/*
+====== Forced unwrapping ======
+*/
+// You use the exlamation mark when you know that the optional has a value
+// If the optional does not contain any value, it will throw a runtime-error
+var unwrappedNumber = number!
+
+/*
+====== Nil ======
+*/
+var optional: String? = nil // Set the optional to a valueless state
+
