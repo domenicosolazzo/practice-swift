fbc991b28a653dd43556d55d010352837c8cbcf0
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-06-22 18:33:39 +0200

    More about dictionary

--- playgrounds/Dictionary.playground/section-1.swift
+++ playgrounds/Dictionary.playground/section-1.swift
@@ -13,3 +13,41 @@ dict = ["First":"1", "Second":"2"]
 */
 var count = dict.count
 
+/*
+====== Updating an element ======
+*/
+dict["First"] = "First"
+// Alternative syntax
+dict.updateValue("Updated", forKey: "First")
+
+/*
+====== Remove an element ======
+*/
+dict["First"] = nil
+// Alternative syntax
+dict.removeValueForKey("Second")
+
+/*
+====== Iterating over a dictionary ======
+*/
+var cities = ["France":"Paris", "Italy":"Rome", "German":"Berlin"]
+println("Dictionary")
+for (country, capital) in cities{
+    var tempCountry = country
+    var tempCapital = capital
+    println("\(country) - \(capital)")
+}
+
+// Iterating over the dictionary keys
+println("Dictionary keys")
+for country in cities.keys{
+    var tempCountry = country
+    println(tempCountry)
+}
+
+// Iterating over the dictionary values
+println("Dictionary values")
+for capital in cities.values{
+    var tempCapital = capital
+    println(tempCapital)
+}
