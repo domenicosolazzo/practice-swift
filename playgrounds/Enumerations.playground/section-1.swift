6b4c59e883f0ecbfb07beada9438aea02952d30f
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-06-25 23:32:55 +0200

    More about enumerations

--- playgrounds/Enumerations.playground/section-1.swift
+++ playgrounds/Enumerations.playground/section-1.swift
@@ -8,3 +8,22 @@ enum Planet{
 }
 
 var earth = Planet.Earth
+
+enum Barcode{
+    case UPCA(Int, Int, Int)
+    case QRCode(String)
+}
+
+var productBarCode = Barcode.UPCA(20, 20, 100)
+
+/**
+======= RAW Values =======
+*/
+enum ASCIIControlCharacter: Character {
+    case Tab = "\t"
+    case LineFeed = "\n"
+    case CarriageReturn = "\r"
+}
+
+var char = ASCIIControlCharacter.Tab.toRaw()
+println(char)
