64b38c7aff0d298d160c317537197ccd595f100c
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-06-27 22:00:13 +0200

    First simple subscript

--- playgrounds/Subscript.playground/section-1.swift
+++ playgrounds/Subscript.playground/section-1.swift
@@ -1,3 +1,26 @@
 // Subscript
+/*
+Classes, structures, and enumerations can define subscripts, which are
+shortcuts for accessing the member elements of a collection, list, or
+sequence. You use subscripts to set and retrieve values by index without
+needing separate methods for setting and retrieval. 
+
+For example, you access
+elements in an Array instance as someArray[index] and elements in a
+Dictionary instance as someDictionary[key].
+
+*/
 import Cocoa
+
+struct TimesTable{
+    let multiplier: Int
+    
+    subscript(index:Int) -> Int{
+        return multiplier * index
+    }
+}
+
+var timesTableInstance = TimesTable(multiplier: 3)
+var subscriptResult = timesTableInstance[6] // Expecting 18
+println(subscriptResult)
 
