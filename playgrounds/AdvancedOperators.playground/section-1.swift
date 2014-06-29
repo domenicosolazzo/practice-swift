f3c4d019a61db59a14d190cc0aa5bf822479a205
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-06-29 19:26:36 +0200

    Advanced Operators: Bitwise OR operator

--- playgrounds/AdvancedOperators.playground/section-1.swift
+++ playgrounds/AdvancedOperators.playground/section-1.swift
@@ -19,3 +19,10 @@ let invertedBits = ~initialBits // 11110000
 let firstSixBits: UInt8 = 0b11111100
 let lastSixBits:UInt8 = 0b00111111
 let middleFourBits = firstSixBits & lastSixBits // 00111100
+
+/*
+=== Bitwise OR operator (|) ===
+*/
+let someBits: UInt8 = 0b10110010
+let moreBits: UInt8 = 0b01011110
+let combinedbits = someBits | moreBits // 11111110
