23150de07446077a4ac349e2b87ab3a689045472
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-06-29 19:22:34 +0200

    Playground: Advanced operators

---
+++ playgrounds/AdvancedOperators.playground/section-1.swift
@@ -0,0 +1,15 @@
+// Playground: Advanced operators
+import Cocoa
+
+/**
+====== Bitwise Operators ======
+
+Bitwise operators enable you to manipulate the individual raw data bits withing a data structure
+*/
+
+/*
+=== Bitwise NOT operator (~) ===
+*/
+let initialBits: UInt8 = 0b00001111
+let invertedBits = ~initialBits // 11110000
+
