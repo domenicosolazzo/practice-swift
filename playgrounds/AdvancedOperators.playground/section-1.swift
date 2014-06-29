152fc9b991a8d2a007667c3f5d80d0507588eae6
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-06-29 20:02:32 +0200

    Advanced Operators: Bitwise Left/Right shift operator

--- playgrounds/AdvancedOperators.playground/section-1.swift
+++ playgrounds/AdvancedOperators.playground/section-1.swift
@@ -33,3 +33,19 @@ let combinedbits = someBits | moreBits // 11111110
 let firstBits:UInt8 = 0b00010100
 let otherBits: UInt8 = 0b00000101
 let outputBits = firstBits ^ otherBits // 00010001
+
+/*
+=== Bitwise Left and Right shift operator (<<) (>>) ===
+*/
+let shiftBits:Uint8 =4 //00000100 in binary
+shiftBits << 1 // 00001000
+shiftBits << 2 // 00010000
+shiftBits << 5 // 10000000
+shiftBits << 6 // 00000000
+shiftBits >> 2 // 00000001
+
+let pink: UInt32 = 0xCC6699
+let redComponent: UInt32 = (pink & 0xFF0000) >> 16 // redComponent is 0xCC or 204
+let greenComponent: UInt32 = (pink & 0x00FF00) >> 8 // greenComponent is 0x66, or 102
+let blueComponent:UInt32 = pink & 0x0000FF  // blueComponent is 0x99 or 153
+
