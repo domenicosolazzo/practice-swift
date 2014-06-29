7b904ceacff3f66a99228d49fd01a579e1b7aded
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-06-29 19:24:37 +0200

    Advanced Operators: Bitwise AND operator

--- playgrounds/AdvancedOperators.playground/section-1.swift
+++ playgrounds/AdvancedOperators.playground/section-1.swift
@@ -13,3 +13,9 @@ Bitwise operators enable you to manipulate the individual raw data bits withing 
 let initialBits: UInt8 = 0b00001111
 let invertedBits = ~initialBits // 11110000
 
+/*
+=== Bitwise AND operator (&) ===
+*/
+let firstSixBits: UInt8 = 0b11111100
+let lastSixBits:UInt8 = 0b00111111
+let middleFourBits = firstSixBits & lastSixBits // 00111100
