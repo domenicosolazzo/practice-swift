5776801a442d2005553c1cd00b39f52e556e3033
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-06-29 13:52:41 +0200

    Added some examples for the generics. Added both version of Stack

--- playgrounds/Generics.playground/section-1.swift
+++ playgrounds/Generics.playground/section-1.swift
@@ -10,8 +10,49 @@ abstracted manner.
 */
 import Cocoa
 
+// This function can work with any type
 func swapTwoValues<T>(inout a:T, inout b:T){
     let temporaryA = a
     a = b
     b = temporaryA
+}
+var firstInt = 3
+var secondInt = 103
+
+// Int example
+swapTwoValues(&firstInt, &secondInt)
+println("\(firstInt)" + " - \(secondInt)" )
+
+// String example
+var firstString = "First"
+var secondString = "Second"
+swapTwoValues(&firstString, &secondString)
+println(firstString + " - " + secondString)
+
+
+// Not-generic version of a Stack
+struct InStack{
+    var items = Int[]()
+    
+    mutating func push(i:Int){
+        items.append(i)
+    }
+    
+    mutating func pull() -> Int{
+        return items.removeLast()
+    }
+}
+
+// Generic version of Stack
+struct GenericInStack<T>{
+    var items = T[]()
+    
+    mutating func push(i:T){
+        items.append(i)
+    }
+    
+    mutating func pull() -> T{
+        return items.removeLast()
+    }
+
 }
