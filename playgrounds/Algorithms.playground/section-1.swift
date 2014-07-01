9de4bcab726225fb924dfc3a1054e16001eb6809
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-01 23:11:45 +0200

    Algorithm playground: Selection sort

--- playgrounds/Algorithms.playground/section-1.swift
+++ playgrounds/Algorithms.playground/section-1.swift
@@ -24,13 +24,45 @@ func bubbleSort(inputArray: Int[]){
 }
 
 // Generate random numbers 
-for r in 0..10{
-    inputArray.append(((Int(arc4random()))%100))
+func generateRandomValues(){
+   for r in 0..10{
+      inputArray.append(((Int(arc4random()))%100))
+   }
 }
-
+generateRandomValues()
 // call bubblesort
 println("Before Bubble sort")
 inputArray
 bubbleSort(inputArray)
 println("After Bubble sort")
+inputArray
+
+println("")
+/**
+==== Selection sort ====
+*/
+inputArray = Int[]()
+
+var min: Int = 0
+
+func selectionSort(inputArray: Int[]){
+    println(inputArray)
+    for var index:Int = 0; index < inputArray.count-1; ++index{
+        min = index
+        for(var j:Int = index+1; j < inputArray.count-1; ++j){
+            if inputArray[j] < inputArray[min]{
+                println("Found new min: \(inputArray[j]). Old min: \(inputArray[min])")
+                println(inputArray)
+                min = j
+            }
+        }
+        swapNumbers(index, min)
+    }
+}
+
+generateRandomValues()
+println("Before Selection Sort")
+inputArray
+selectionSort(inputArray)
+println("After Selection Sort")
 inputArray
