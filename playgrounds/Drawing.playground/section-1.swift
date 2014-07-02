de530a891b17e069bfa246d766f201f4b518b8fe
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-02 19:23:51 +0200

    Drawing: Create a rectangle

--- playgrounds/Drawing.playground/section-1.swift
+++ playgrounds/Drawing.playground/section-1.swift
@@ -2,4 +2,13 @@
 // Source: http://goo.gl/MYRQJl
 
 import Cocoa
+import XCPlayground
 
+/**
+==== Create a Rectangle ====
+*/
+var x = 0
+var y = 0
+var width = 300
+var height = 300
+var frameRect = NSRect(x: x,y: y,width: width, height: height)
