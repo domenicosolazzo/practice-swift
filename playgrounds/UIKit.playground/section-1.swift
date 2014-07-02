acf2b05fccfa64e09cd15046835d0ec29b4b9e18
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-02 20:39:43 +0200

    UIKit: Set title for a UIButton

--- playgrounds/UIKit.playground/section-1.swift
+++ playgrounds/UIKit.playground/section-1.swift
@@ -10,3 +10,5 @@ var v = UIView(frame: CGRectMake(0,0, 200,200))
 ==== Button ====
 */
 var b = UIButton(frame: CGRectMake(0,0,100,30))
+// Set the title
+b.setTitle("Domenico Button", forState: UIControlState.Normal)
