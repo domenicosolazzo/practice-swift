00c2ec3fbde252b44930130999d7fc8755574071
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-02 20:43:13 +0200

    UIKit: Show the view

--- playgrounds/UIKit.playground/section-1.swift
+++ playgrounds/UIKit.playground/section-1.swift
@@ -11,9 +11,12 @@ var v = UIView(frame: CGRectMake(0,0, 200,200))
 */
 var b = UIButton(frame: CGRectMake(0,0,100,30))
 // Set the title
-b.setTitle("Domenico Button", forState: UIControlState.Normal)
+b.setTitle("Dome", forState: UIControlState.Normal)
 // Set the title color
 b.setTitleColor(UIColor.blueColor(), forState: UIControlState.Normal)
 
 // Add the button to the subview
 v.addSubview(b)
+
+// Show the view
+v
