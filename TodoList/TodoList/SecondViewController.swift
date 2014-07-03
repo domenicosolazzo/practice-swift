26ed649acde5e2bbd0ff28656751260eb2330de6
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-03 23:14:13 +0200

    Added TodoList app

---
+++ TodoList/TodoList/SecondViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SecondViewController.swift
+//  TodoList
+
+import UIKit
+
+class SecondViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
