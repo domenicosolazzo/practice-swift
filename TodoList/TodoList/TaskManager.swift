9ee052ced416d1c6fda33c63af3288046d29849f
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-03 23:21:09 +0200

    AddTask Function for the TaskManager

--- TodoList/TodoList/TaskManager.swift
+++ TodoList/TodoList/TaskManager.swift
@@ -3,11 +3,16 @@ import UIKit
 var taskManager = TaskManager()
 
 // Task structure
-struct task{
+struct Task{
     var name = "Un-Named"
     var desc = "Un-Described"
 }
 
 class TaskManager: NSObject {
+    var tasks = Task[]()
     
+    // Add a task
+    func addTask(name:String, desc:String){
+        tasks.append(Task(name:name, desc:desc))
+    }
 }
