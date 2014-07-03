eddc234259b55b2a551ba33d863c34c5c76296c1
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-03 23:18:17 +0200

    Added task structure

--- TodoList/TodoList/TaskManager.swift
+++ TodoList/TodoList/TaskManager.swift
@@ -1,5 +1,13 @@
 import UIKit
 
+var taskManager = TaskManager()
+
+// Task structure
+struct task{
+    var name = "Un-Named"
+    var desc = "Un-Described"
+}
+
 class TaskManager: NSObject {
-   
+    
 }
