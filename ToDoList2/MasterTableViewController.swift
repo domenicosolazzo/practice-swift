3f942710a93e8419675eec26ec8b0a7b886a340f
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-10 21:53:20 +0200

    Added identifier for both Segue

--- ToDoList2/MasterTableViewController.swift
+++ ToDoList2/MasterTableViewController.swift
@@ -100,14 +100,16 @@ class MasterTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // #pragma mark - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
         // Get the new view controller using [segue destinationViewController].
         // Pass the selected object to the new view controller.
+        
+        
     }
-    */
+    
 
 }
