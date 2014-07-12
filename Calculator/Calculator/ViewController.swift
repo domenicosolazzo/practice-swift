ce510ec95ad524813e3a8d82bea38e298d8f01f5
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-12 22:41:34 +0200

    Added UI for the calculator

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+    @IBOutlet var resultTextView: UITextView = UITextView()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func MemoryOperationPressed(sender: AnyObject) {
+    }
 
+    @IBAction func pressedOperation(sender: AnyObject) {
+    }
+    @IBAction func numberPressed(sender: AnyObject) {
+    }
 }
 
