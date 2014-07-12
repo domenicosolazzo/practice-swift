b47daad63cc3d19283e6e53af0df73e29ef62ee1
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-13 00:48:42 +0200

    Added function for resetting the result

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -11,23 +11,33 @@ import UIKit
 class ViewController: UIViewController {
                             
     @IBOutlet var resultTextView: UITextView = UITextView()
+    
+    var operation: String = ""
+    var firstOperand: Int = 0
+    var secondOperand: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func prefixPressed(sender: UIButton) {
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func resetResult(sender: UIButton) {
+        resultTextView.text = ""
+    }
     @IBAction func MemoryOperationPressed(sender: UIButton) {
     }
 
     @IBAction func pressedOperation(sender: UIButton) {
-        if(sender.titleLabel.text == "C"){
-            resultTextView.text = ""
-        }
+
+        var op = sender.titleLabel.text
+        operation = op
         
     }
     @IBAction func numberPressed(sender: UIButton) {
