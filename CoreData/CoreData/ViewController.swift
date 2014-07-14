13161964cb466a4cc9631f41eb5d07a5e61818e4
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-14 22:50:42 +0200

    Added IBOutlets and IBActions

--- CoreData/CoreData/ViewController.swift
+++ CoreData/CoreData/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+    @IBOutlet var txtUsername: UITextField = UITextField()
+    @IBOutlet var txtPassword: UITextField = UITextField()
+    
+    @IBAction func btnLoad(sender: UIButton) {
+        
+    }
+    
+    @IBAction func btnSave(sender: UIButton) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
