cb739343b352d1cb42c3d6e237ad20489355c686
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-16 22:05:24 +0200

    Finding the document directory for the current user

--- CoreData2/CoreData2/ViewController.swift
+++ CoreData2/CoreData2/ViewController.swift
@@ -1,12 +1,6 @@
-//
-//  ViewController.swift
-//  CoreData2
-//
-//  Created by Domenico Solazzo on 7/16/14.
-//  Copyright (c) 2014 Domenico Solazzo. All rights reserved.
-//
-
 import UIKit
+
+let documentFileFolder = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
 
 class ViewController: UIViewController {
                             
