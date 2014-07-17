5c9db28184280637899b9e2a099e7a7bcd8d6382
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-17 21:15:54 +0200

    Added IBOutlets and IBActions

--- CoreData3/CoreData3/ViewController.swift
+++ CoreData3/CoreData3/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+    @IBOutlet var txtUsername: UITextField
+    @IBOutlet var txtLastname: UITextField
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnLoad(sender: UIButton) {
+        
+    }
+    @IBAction func btnSave(sender: UIButton) {
+        
+    }
 
 }
 
