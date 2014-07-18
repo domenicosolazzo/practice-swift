0f674a240a418c65fdebd02d64d37f733eca1bd5
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-18 08:16:34 +0200

    Added some methods to theto the Users class

--- CoreData3/CoreData3/Users.swift
+++ CoreData3/CoreData3/Users.swift
@@ -7,4 +7,15 @@ class Users: NSManagedObject {
     // NSManaged: It tells the compiler that these properties will be manage from CoreData
     @NSManaged var username:String
     @NSManaged var password:String
+    
+    // ToString method
+    func ToString() -> String{
+        return "The username is \(self.username) and the password is \(self.password)"
+    }
+    
+    // Add prefix to the username
+    func AddPrefix(prefix:String){
+        self.username = "\(prefix)-(\self.username)"
+    }
+    
 }
