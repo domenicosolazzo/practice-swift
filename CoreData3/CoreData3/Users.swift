f148cbb1fe53e83683ed8af304d811b5f9dec887
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-18 08:12:49 +0200

    Make the Users class available to Objective-C

--- CoreData3/CoreData3/Users.swift
+++ CoreData3/CoreData3/Users.swift
@@ -2,6 +2,7 @@ import UIKit
 // You need to import CoreData
 import CoreData
 
+@objc(Users) // <= Make the class available to Objective-C as Users
 class Users: NSManagedObject {
     // NSManaged: It tells the compiler that these properties will be manage from CoreData
     @NSManaged var username:String
