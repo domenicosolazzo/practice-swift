384449e8e42961192ff8b64ada9306c11cd5aabe
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-19 12:38:28 +0200

    Added new project: UserLocserLocation

---
+++ UserLocation/UserLocation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UserLocation
+//
+//  Created by Domenico Solazzo on 7/19/14.
+//  Copyright (c) 2014 Domenico Solazzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
