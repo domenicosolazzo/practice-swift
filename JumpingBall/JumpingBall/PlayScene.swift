135589b30d10f7dd9abe6b30f2cffa6488ed6c56
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-19 19:10:58 +0200

    Refactoring...

--- JumpingBall/JumpingBall/PlayScene.swift
+++ JumpingBall/JumpingBall/PlayScene.swift
@@ -1,16 +1,19 @@
 import SpriteKit
 
-// Sprite node for the bar
-let runningBar = SKSpriteNode(imageNamed: "bar")
 
 class PlayScene: SKScene{
+    // Sprite node for the bar
+    let runningBar = SKSpriteNode(imageNamed: "bar")
+
     override func didMoveToView(view: SKView!) {
         self.backgroundColor = UIColor(hex: 0x809DFF)
         
         // Set the anchor point of the running bar at (0, 0.5)
         self.runningBar.anchorPoint = CGPointMake(0, 0.5)
         // Set the bar at the bottom of the screen
-        self.runningBar.position = CGPointMake(CGRectGetMinX(self.frame), CGRectGetMinY(self.frame))
+        self.runningBar.position = CGPointMake(
+            CGRectGetMinX(self.frame),
+            CGRectGetMinY(self.frame))
         
     }
     
