4e4eeece467b82f57769dd6ae4453eedce6a8331
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-19 18:11:17 +0200

    Added SpriteNode for the por the play button

--- JumpingBall/JumpingBall/GameScene.swift
+++ JumpingBall/JumpingBall/GameScene.swift
@@ -1,6 +1,9 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    // Play button node
+    let playButton = SKSpriteNode(imageNamed: "play")
+    
     override func didMoveToView(view: SKView) {
         
     }
