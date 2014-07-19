a3aeff13f9190b3a7400a58880bf81ada199ef89
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-19 19:06:32 +0200

    Add an anchor point for the running bar

--- JumpingBall/JumpingBall/PlayScene.swift
+++ JumpingBall/JumpingBall/PlayScene.swift
@@ -6,6 +6,9 @@ let runningBar = SKSpriteNode(imageNamed: "bar")
 class PlayScene: SKScene{
     override func didMoveToView(view: SKView!) {
         self.backgroundColor = UIColor(hex: 0x809DFF)
+        
+        // Set the anchor point of the running bar at (0, 0.5)
+        self.runningBar.anchorPoint = CGPointMake(0, 0.5)   
     }
     
     /// Update the scene: It will run every single frame
