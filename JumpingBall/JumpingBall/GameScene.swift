58b20f4759177d84f2055346c6ef1b04b3436f16
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-19 18:14:53 +0200

    Position the play button in the middle of the frame

--- JumpingBall/JumpingBall/GameScene.swift
+++ JumpingBall/JumpingBall/GameScene.swift
@@ -5,7 +5,9 @@ class GameScene: SKScene {
     let playButton = SKSpriteNode(imageNamed: "play")
     
     override func didMoveToView(view: SKView) {
-        
+        // Position the play button in the middle of the screen
+        // It position the button in the middle of the frame.
+        self.playButton.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame))
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
