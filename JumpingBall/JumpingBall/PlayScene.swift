5a2d129b26fce11484ec88cc9fdac54e72ac9d76
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-19 18:55:24 +0200

    Added a SpriteNode for the bar in the Play Scene

--- JumpingBall/JumpingBall/PlayScene.swift
+++ JumpingBall/JumpingBall/PlayScene.swift
@@ -1,5 +1,8 @@
 import SpriteKit
 
+// Sprite node for the bar
+let runningBar = SKSpriteNode(imageNamed: "bar")
+
 class PlayScene: SKScene{
     override func didMoveToView(view: SKView!) {
         self.backgroundColor = UIColor(hex: 0x809DFF)
