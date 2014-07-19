3b58121570501ed4f8fd327e69a6b63abc82a6b6
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-19 19:08:39 +0200

    Set the bar at the bottom of the screen

--- JumpingBall/JumpingBall/PlayScene.swift
+++ JumpingBall/JumpingBall/PlayScene.swift
@@ -8,7 +8,10 @@ class PlayScene: SKScene{
         self.backgroundColor = UIColor(hex: 0x809DFF)
         
         // Set the anchor point of the running bar at (0, 0.5)
-        self.runningBar.anchorPoint = CGPointMake(0, 0.5)   
+        self.runningBar.anchorPoint = CGPointMake(0, 0.5)
+        // Set the bar at the bottom of the screen
+        self.runningBar.position = CGPointMake(CGRectGetMinX(self.frame), CGRectGetMinY(self.frame))
+        
     }
     
     /// Update the scene: It will run every single frame
