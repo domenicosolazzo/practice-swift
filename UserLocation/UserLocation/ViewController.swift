3c9d34244b5906f66d926990dd055bed1afc6fce
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-19 12:42:40 +0200

    Added the IBOutlets for the MapView

--- UserLocation/UserLocation/ViewController.swift
+++ UserLocation/UserLocation/ViewController.swift
@@ -2,6 +2,7 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+    @IBOutlet var theMap: MKMapView
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
