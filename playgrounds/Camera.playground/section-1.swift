7e5985eb82c4e69084f8e6efc60ba09e59f97fd6
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-20 18:31:40 +0200

    Showing the view

--- playgrounds/Camera.playground/section-1.swift
+++ playgrounds/Camera.playground/section-1.swift
@@ -10,3 +10,6 @@ var view = NSView(frame: NSRect(x:0, y:0, width:640, height:480))
 // Creating an AVCaptureSession
 var session = AVCaptureSession()
 
+
+// Show the view
+XCPShowView("camera", view)
