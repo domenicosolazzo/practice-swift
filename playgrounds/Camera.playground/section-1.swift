7ec4824b1c3e8d4cb206f67fe6e8ec6d49b70a95
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-20 18:36:27 +0200

    Setting the session preset

--- playgrounds/Camera.playground/section-1.swift
+++ playgrounds/Camera.playground/section-1.swift
@@ -10,6 +10,9 @@ var view = NSView(frame: NSRect(x:0, y:0, width:640, height:480))
 // Creating an AVCaptureSession
 var session = AVCaptureSession()
 
+// SessionPreset: A constant value indicating the quality level or bitrate of the output.
+session.sessionPreset = AVCaptureSessionPreset640x480
+
 
 // Show the view
 XCPShowView("camera", view)
