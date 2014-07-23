ac6cf86117fd49c572537cdca2d132249d244905
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-23 21:54:27 +0200

    Added _previousTime, _currentTime, _deltaTime. Fixed typo

--- playgrounds/SpriteKit.playground/section-1.swift
+++ playgrounds/SpriteKit.playground/section-1.swift
@@ -4,11 +4,14 @@ import SpriteKit
 import XCPlayground
 
 class MyScene:SKScene{
-
+    var _previousTime = 0.0
+    var _currentTime = 0.0
+    var _deltaTime = 0.0
+    
     override func didMoveToView(view: SKView!) {
         var sprite = SKSpriteNode(color:NSColor.greenColor(), size: CGSize(width:50.0, height:50.0))
         sprite.name = "block"
-        sprite.position = GCPoint(x:50.0, y:50.0)
+        sprite.position = CGPoint(x:50.0, y:50.0)
         self.addChild(sprite)
     }
     
