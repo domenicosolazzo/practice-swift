f617003e1297a49a6c25d9b2cfdf118adf761162
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-23 21:52:13 +0200

    Added sprite position

--- playgrounds/SpriteKit.playground/section-1.swift
+++ playgrounds/SpriteKit.playground/section-1.swift
@@ -8,6 +8,8 @@ class MyScene:SKScene{
     override func didMoveToView(view: SKView!) {
         var sprite = SKSpriteNode(color:NSColor.greenColor(), size: CGSize(width:50.0, height:50.0))
         sprite.name = "block"
-        
+        sprite.position = GCPoint(x:50.0, y:50.0)
+        self.addChild(sprite)
     }
+    
 }
