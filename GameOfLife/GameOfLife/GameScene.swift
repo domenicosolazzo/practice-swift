f7d261b012e80665536d24fe5a621ddaaa803dbd
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-25 21:18:07 +0200

    Added grid background

--- GameOfLife/GameOfLife/GameScene.swift
+++ GameOfLife/GameOfLife/GameScene.swift
@@ -8,11 +8,21 @@ class GameScene: SKScene {
     let _gridLowerLeftCorner: CGPoint = CGPoint(x: 158, y:10)
     
     override func didMoveToView(view: SKView) {
+        /// Add main background
         let background = SKSpriteNode(imageNamed: "background.pmg")
         background.anchorPoint = CGPoint(x:0, y:0)
         background.size = self.size
         background.zPosition = -2
         self.addChild(background)
+        
+        // Add main grid background
+        let gridBackground = SKSpriteNode(imageNamed: "grid.png")
+        gridBackground.size = CGSize(width: _gridWidth, height:_gridHeight)
+        gridBackground.zPosition = -1
+        gridBackground.anchorPoint = CGPoint(x:0, y:0)
+        gridBackground.position = _gridLowerLeftCorner
+        self.addChild(gridBackground)
+        
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
