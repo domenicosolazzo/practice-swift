7cea5aaea6953c9217fdad0fbe219555bfbf3096
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-25 21:00:54 +0200

    Removing some old code

--- GameOfLife/GameOfLife/GameScene.swift
+++ GameOfLife/GameOfLife/GameScene.swift
@@ -6,34 +6,13 @@ class GameScene: SKScene {
     let _numRows = 8
     let _numCols = 10
     let _gridLowerLeftCorner: CGPoint = CGPoint(x: 158, y:10)
+    
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
         
-        self.addChild(myLabel)
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        /* Called when a touch begins */
         
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
     }
    
     override func update(currentTime: CFTimeInterval) {
