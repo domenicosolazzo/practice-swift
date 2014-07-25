4bb656b0e32c92203e1867b8a9f159fee63dd329
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-25 21:02:43 +0200

    Added a new sprite (background.png) and added as child

--- GameOfLife/GameOfLife/GameScene.swift
+++ GameOfLife/GameOfLife/GameScene.swift
@@ -8,7 +8,11 @@ class GameScene: SKScene {
     let _gridLowerLeftCorner: CGPoint = CGPoint(x: 158, y:10)
     
     override func didMoveToView(view: SKView) {
-        
+        let background = SKSpriteNode(imageNamed: "background.pmg")
+        background.anchorPoint = CGPoint(x:0, y:0)
+        background.size = self.size
+        background.zPosition = -2
+        self.addChild(background)
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
