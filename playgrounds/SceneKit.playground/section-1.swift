7ba95b1824592616542b7d1f7070007423815c5e
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-27 11:48:25 +0200

    Added a new scene

--- playgrounds/SceneKit.playground/section-1.swift
+++ playgrounds/SceneKit.playground/section-1.swift
@@ -7,3 +7,5 @@ import XCPlayground // for the live preview
 
 // create a scene view with an empty scene
 var sceneView = SCNView(frame:CGRect(x:0, y:0, width:300, height:300))
+// New scene
+var scene = SCNScene()
