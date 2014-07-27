d0d49b1d71e8b7a896b9202e447f39c5438e58bc
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-27 09:05:50 +0200

    Added isAlive property

--- GameOfLife/GameOfLife/Tile.swift
+++ GameOfLife/GameOfLife/Tile.swift
@@ -2,5 +2,13 @@ import UIKit
 import SpriteKit
 
 class Tile: SKSpriteNode {
-   
+    
+    // Property isAlive
+    var isAlive:Bool = false{
+       didSet{
+          self.hidden = !isAlive
+       }
+    }
+    
+    
 }
