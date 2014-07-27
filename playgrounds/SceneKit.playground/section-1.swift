b341c95ce8ce3823acdd72a378c9cc18cc65b5fc
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-27 11:46:46 +0200

    Imported some libraries (Cocoa, SceneKit, QuartzCore and XCPlayground

--- playgrounds/SceneKit.playground/section-1.swift
+++ playgrounds/SceneKit.playground/section-1.swift
@@ -1,5 +1,8 @@
 // Playground: SceneKit
 
-import UIKit
+import Cocoa
+import SceneKit
+import QuartzCore // for the basic animation
+import XCPlayground // for the live preview
 
 var str = "Hello, playground"
