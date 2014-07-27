48669b50e2d76ad44a5660a0315f31b9ef15c9c7
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-27 21:16:52 +0200

    Added configuration for the label

--- SpaceInvader/SpaceInvader/GameOverScene.swift
+++ SpaceInvader/SpaceInvader/GameOverScene.swift
@@ -15,5 +15,15 @@ class GameOverScene: SKScene {
         }else{
             message = "Game Over"
         }
+        
+        // Create a label initialized with a font name
+        // P.S. Some useful fonts can be found at http://iosfonts.com
+        var label:SKLabelNode = SKLabelNode(fontNamed: "HelveticaNeue")
+        label.text = message
+        // Add the font color
+        label.fontColor = SKColor.whiteColor()
+        // Add the position
+        label.position = CGPointMake(self.size.width / 2, self.size.height / 2)
+        
     }
 }
