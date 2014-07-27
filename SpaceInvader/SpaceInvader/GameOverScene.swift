964656d20f1958c6d30178ee99564368a1d020fd
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-27 21:11:24 +0200

    Selecting the message to show in the GameOver scene

--- SpaceInvader/SpaceInvader/GameOverScene.swift
+++ SpaceInvader/SpaceInvader/GameOverScene.swift
@@ -4,6 +4,16 @@ import SpriteKit
 class GameOverScene: SKScene {
    
     init(size:CGSize, won:Bool){
-        super.init(size)
+        super.init(size: size)
+        
+        // Create a new message
+        var message:NSString = NSString()
+        
+        // Select the message based on the won variable
+        if won{
+            message = "You WON!"
+        }else{
+            message = "Game Over"
+        }
     }
 }
