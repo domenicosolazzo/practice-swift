f9799e8f02708bfc45cb93694cfdb53f0596c787
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-27 14:00:30 +0200

    Removed some old code and comments

--- SpaceInvader/SpaceInvader/AppDelegate.swift
+++ SpaceInvader/SpaceInvader/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  SpaceInvader
-//
-//  Created by Domenico Solazzo on 7/27/14.
-//  Copyright (c) 2014 Domenico Solazzo. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
--- SpaceInvader/SpaceInvader/GameViewController.swift
+++ SpaceInvader/SpaceInvader/GameViewController.swift
@@ -1,11 +1,3 @@
-//
-//  GameViewController.swift
-//  SpaceInvader
-//
-//  Created by Domenico Solazzo on 7/27/14.
-//  Copyright (c) 2014 Domenico Solazzo. All rights reserved.
-//
-
 import UIKit
 import SpriteKit
 
