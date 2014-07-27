11306a50227eae3ad00459e36d931ae24742640a
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-27 09:06:55 +0200

    Number of living neighbors

--- GameOfLife/GameOfLife/Tile.swift
+++ GameOfLife/GameOfLife/Tile.swift
@@ -2,6 +2,8 @@ import UIKit
 import SpriteKit
 
 class Tile: SKSpriteNode {
+    // Number of living neighbors
+    var numLivingNeighbors = 0
     
     // Property isAlive
     var isAlive:Bool = false{
