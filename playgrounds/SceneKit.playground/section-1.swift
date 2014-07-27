891727ca3b7e2a9c80c844e18b62648211fabc89
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-07-27 11:48:00 +0200

    Added a new scene

--- playgrounds/SceneKit.playground/section-1.swift
+++ playgrounds/SceneKit.playground/section-1.swift
@@ -5,4 +5,5 @@ import SceneKit
 import QuartzCore // for the basic animation
 import XCPlayground // for the live preview
 
-var str = "Hello, playground"
+// create a scene view with an empty scene
+var sceneView = SCNView(frame:CGRect(x:0, y:0, width:300, height:300))
