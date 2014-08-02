60889e15f421d1ecd983ccb1d87cf59f5481a0cf
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-08-02 22:07:21 +0200

    Check if we need to remove this object from the scene

--- UberJump/GameNodeObject.swift
+++ UberJump/GameNodeObject.swift
@@ -7,4 +7,12 @@ class GameNodeObject: SKNode {
         return false
     }
     
+    
+    // Called every frame to see if the game object should be removed from the scene
+    func checkNodeRemoval(playerY:CGFloat){
+        // Checks to see if the player node has traveled more than 300 points beyond this node
+        if playerY > self.position.y + CGFloat(300){
+            self.removeFromParent()
+        }
+    }
 }
