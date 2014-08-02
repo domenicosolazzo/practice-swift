e4fc1efefaf22abaf0018a8469f996d848567fd9
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-08-02 22:05:34 +0200

    Function for detecting the collision of an object with the player

--- UberJump/GameNodeObject.swift
+++ UberJump/GameNodeObject.swift
@@ -2,4 +2,9 @@ import SpriteKit
 
 class GameNodeObject: SKNode {
    
+    // Called when a player physics body collides with the game object's physics body
+    func collisionWithPlayer(player:SKNode) -> Bool{
+        return false
+    }
+    
 }
