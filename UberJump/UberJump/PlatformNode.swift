1001b192b878dd8b128a53e72b1c45c49eab4311
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-08-03 00:14:06 +0200

    Implementing collisionWithPlayer for the Platform node

--- UberJump/UberJump/PlatformNode.swift
+++ UberJump/UberJump/PlatformNode.swift
@@ -9,10 +9,24 @@ enum PlatformTypes{
 
 class PlatformNode: GameObjectNode {
     // Platform type
-    var starType:PlatformTypes = PlatformTypes.PLATFORM_NORMAL
+    var platformType:PlatformTypes = PlatformTypes.PLATFORM_NORMAL
     
     override func collisionWithPlayer(player: SKNode) -> Bool {
     
-        return true
+        /*
+            The player node should only bounce if it hits a platform while falling,
+            which is indicated by a negative dy value in its velocity. This check also ensures the player node doesn’t
+            collide with platforms while moving up the screen
+        */
+        if player.physicsBody.velocity.dy < 0{
+            // You give the player node a vertical boost to make it bounce off the platform
+            player.physicsBody.velocity = CGVectorMake(player.physicsBody.velocity.dx, CGFloat(250))
+        }
+        
+        if platformType == PlatformTypes.PLATFORM_BREAK{
+            self.removeFromParent()
+        }
+        
+        return false
     }
 }
