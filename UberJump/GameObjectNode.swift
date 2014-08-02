c4e7649e12d4c3fec57e76c06f5eeaf711d6422d
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-08-02 22:08:10 +0200

    Refactoring

--- UberJump/GameObjectNode.swift
+++ UberJump/GameObjectNode.swift
@@ -1,6 +1,6 @@
 import SpriteKit
 
-class GameNodeObject: SKNode {
+class GameObjectNode: SKNode {
    
     // Called when a player physics body collides with the game object's physics body
     func collisionWithPlayer(player:SKNode) -> Bool{
