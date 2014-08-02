b2b09d60c03f92b5bbb7c2481eeafb663f6adedf
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-08-02 22:16:15 +0200

    Implemented collisionWithPlayer with the StarNode

--- UberJump/UberJump/StarNode.swift
+++ UberJump/UberJump/StarNode.swift
@@ -1,3 +1,15 @@
+import SpriteKit
 class StarNode: GameObjectNode {
    
+    override func collisionWithPlayer(player: SKNode) -> Bool {
+        
+        // Boost the player up
+        player.physicsBody.velocity = CGVectorMake(player.physicsBody.velocity.dx, CGFloat(400))
+        
+        // Remove the star from the scene
+        self.removeFromParent()
+        
+        // The HUD needs updating to show the new stars and score
+        return true
+    }
 }
