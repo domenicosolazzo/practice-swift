e60fee210182c7ffaba1e5a1c21f827d340010b3
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-09-20 16:49:03 +0200

    Added background Added images

--- UberJump/UberJump/GameScene.swift
+++ UberJump/UberJump/GameScene.swift
@@ -11,6 +11,7 @@ import SpriteKit
 class GameScene: SKScene {
     override init(size: CGSize) {
         super.init(size: size)
+        self.initialization()
     }
     
     required init(coder aDecoder: NSCoder) {
@@ -20,11 +21,28 @@ class GameScene: SKScene {
     
     func initialization(){
         self.backgroundColor = SKColor.whiteColor()
+        
+        var backgroundNode:SKNode  = self.createBackgroundNode()
+        self.addChild(backgroundNode)
     }
     override func didMoveToView(view: SKView) {
         
     }
-    
+    func createBackgroundNode() -> SKNode{
+        // Create node 1
+        var backgroundNode:SKNode = SKNode()
+        
+        for count in 0...20{
+            var backgroundImage = NSString(format:"Background%02d", count+1) as String
+            var spriteNode:SKSpriteNode = SKSpriteNode(imageNamed: backgroundImage)
+            spriteNode.anchorPoint = CGPointMake(CGFloat(0.5), CGFloat(0))
+            spriteNode.position = CGPointMake(CGFloat(160), CGFloat(64 * count))
+            
+            backgroundNode.addChild(spriteNode)
+            
+        }
+        return backgroundNode
+    }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
--- UberJump/UberJump/GameViewController.swift
+++ UberJump/UberJump/GameViewController.swift
@@ -30,6 +30,7 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
         if let scene = GameScene.unarchiveFromFile("GameScene") as? GameScene {
             // Configure the view.
             let skView = self.view as SKView
