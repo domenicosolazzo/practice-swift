ec90716025e4d24f7c4a3da1e17d8ef78ff0680a
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-09-21 17:57:03 +0200

    Implemented collisionWithPlayer(GameObjectNode)

--- UberJump/UberJump/GameObjectNode.swift
+++ UberJump/UberJump/GameObjectNode.swift
@@ -4,7 +4,7 @@ import SpriteKit
 class GameObjectNode: SKNode {
    
     func collisionWithPlayer(player:SKNode) -> Bool{
-    
+        return false
     }
     
     func checkNodeRemoval(playerY:CGFloat){
