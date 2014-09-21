283fe1e32c02de2681ed2df55c962b52f503c7ef
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-09-21 17:59:20 +0200

    Implemented checkNodeRemoval (GameObjectNode)

--- UberJump/UberJump/GameObjectNode.swift
+++ UberJump/UberJump/GameObjectNode.swift
@@ -8,6 +8,9 @@ class GameObjectNode: SKNode {
     }
     
     func checkNodeRemoval(playerY:CGFloat){
-    
+        /// checks to see if the player node has traveled more than 300 points beyond this node. 
+        if (playerY > self.position.y + CGFloat(300)) {
+            self.removeFromParent()
+        }
     }
 }
