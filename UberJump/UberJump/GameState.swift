79f5a900afde0101db2fd30c7980423e9e831969
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-09-24 07:48:14 +0200

    Added GameState Object

---
+++ UberJump/UberJump/GameState.swift
@@ -0,0 +1,13 @@
+//
+//  GameState.swift
+//  UberJump
+//
+//  Created by Domenico Solazzo on 9/24/14.
+//  Copyright (c) 2014 Domenico Solazzo. All rights reserved.
+//
+
+import UIKit
+
+class GameState: NSObject {
+   
+}
