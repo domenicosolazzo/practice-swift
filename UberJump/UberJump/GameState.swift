5278ea654efb9ae1735fcd6cd82df66d4afa1e32
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-09-24 08:08:14 +0200

    Load the game state

--- UberJump/UberJump/GameState.swift
+++ UberJump/UberJump/GameState.swift
@@ -13,4 +13,25 @@ class GameState: NSObject {
         return Singleton.instance
     }
     
+    override init(){
+        // Init
+        _score = 0
+        _highScore = 0
+        _stars = 0
+        
+        // Load game state
+        var defaults:NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        var highScore = defaults.objectForKey("highScore")
+        
+        if let y = highScore as? Int {
+            _highScore = y
+        }
+        
+        var stars = defaults.objectForKey("stars")
+        if let y = stars as? Int{
+            _stars = y
+        }
+    }
+
+    
 }
