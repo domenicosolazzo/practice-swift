d240b89245b0b4759b24f33659e5a2676751b2d6
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-09-27 20:48:24 +0200

    Enumeration list

--- SwiftTetris/SwiftTetris/Block.swift
+++ SwiftTetris/SwiftTetris/Block.swift
@@ -4,6 +4,8 @@ import SpriteKit
 let NumberOfColors: UInt32 = 6
 
 /// Classes, structures and enums which implement Printable are capable of generating human-readable strings when debugging or printing their value to the console.
-enum BlockColor: UInt32, Printable {
+enum BlockColor: Int, Printable {
+
+    case Blue = 0, Orange, Purple, Red, Teal, Yellow
 
 }
