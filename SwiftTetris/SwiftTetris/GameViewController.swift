1dd6fb365a18367af22092a033287c01820ae3bb
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-09-27 20:13:51 +0200

    Create the new GameScene in the controller

--- SwiftTetris/SwiftTetris/GameViewController.swift
+++ SwiftTetris/SwiftTetris/GameViewController.swift
@@ -7,6 +7,16 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Configure the view.
+        let skView = view as SKView
+        skView.multipleTouchEnabled = false
+        
+        // Create and configure the scene.
+        var scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .AspectFill
+        
+        // Present the scene.
+        skView.presentScene(scene)
     }
 
     override func prefersStatusBarHidden() -> Bool {
