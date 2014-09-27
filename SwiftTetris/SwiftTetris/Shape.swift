72422e4ab27ed7c198bb2b2a42783adc9f7cf37d
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-09-27 21:30:38 +0200

    description: Required from the printable interface

--- SwiftTetris/SwiftTetris/Shape.swift
+++ SwiftTetris/SwiftTetris/Shape.swift
@@ -5,4 +5,17 @@ import SpriteKit
 enum Orientation:Int, Printable{
     case Zero = 0, Ninety, OneEighty, TwoSeventy
 
+    /// Required fromt the printable interface
+    var description: String {
+        switch self {
+        case .Zero:
+            return "0"
+        case .Ninety:
+            return "90"
+        case .OneEighty:
+            return "180"
+        case .TwoSeventy:
+            return "270"
+        }
+    }
 }
