7793825d5e62501345c6fab9acee85ec1f44903d
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-09-27 19:55:51 +0200

    Added init methods to the GameScene

--- SwiftTetris/SwiftTetris/GameScene.swift
+++ SwiftTetris/SwiftTetris/GameScene.swift
@@ -2,7 +2,16 @@ import SpriteKit
 
 class GameScene: SKScene {
     
-   
+    override init(size: CGSize) {
+        super.init(size:size)
+        
+    }
+    
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
