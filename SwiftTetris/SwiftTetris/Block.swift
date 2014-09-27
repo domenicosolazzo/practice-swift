5ff9038806ac7a9bd2a6666f4ac8606d9e417788
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-09-27 20:47:44 +0200

    Some comments

--- SwiftTetris/SwiftTetris/Block.swift
+++ SwiftTetris/SwiftTetris/Block.swift
@@ -3,6 +3,7 @@ import SpriteKit
 /// How many colors are available
 let NumberOfColors: UInt32 = 6
 
+/// Classes, structures and enums which implement Printable are capable of generating human-readable strings when debugging or printing their value to the console.
 enum BlockColor: UInt32, Printable {
 
 }
