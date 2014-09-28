41ed60caa3b622d68969901124df361047fb9fa4
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-09-28 11:06:04 +0200

    Overridden blockRowColumnPositions and bottomBlocksForOrientations

--- SwiftTetris/SwiftTetris/Shape.swift
+++ SwiftTetris/SwiftTetris/Shape.swift
@@ -170,4 +170,21 @@ class TShape:Shape {
     marks the row/column indicator for the shape
     
     */
+    override var blockRowColumnPositions: [Orientation: Array<(columnDiff: Int, rowDiff: Int)>] {
+        return [
+            Orientation.Zero:       [(1, 0), (0, 1), (1, 1), (2, 1)],
+            Orientation.Ninety:     [(2, 1), (1, 0), (1, 1), (1, 2)],
+            Orientation.OneEighty:  [(1, 2), (0, 1), (1, 1), (2, 1)],
+            Orientation.TwoSeventy: [(0, 1), (1, 0), (1, 1), (1, 2)]
+        ]
+    }
+    
+    override var bottomBlocksForOrientations: [Orientation: Array<Block>] {
+        return [
+            Orientation.Zero:       [blocks[SecondBlockIdx], blocks[ThirdBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.Ninety:     [blocks[FirstBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.OneEighty:  [blocks[FirstBlockIdx], blocks[SecondBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.TwoSeventy: [blocks[FirstBlockIdx], blocks[FourthBlockIdx]]
+        ]
+    }
 }
