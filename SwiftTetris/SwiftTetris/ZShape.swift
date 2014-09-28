845b54c770dc8d66ff868db949e6376e179da93e
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-09-28 11:17:35 +0200

    Refactoring...

---
+++ SwiftTetris/SwiftTetris/ZShape.swift
@@ -0,0 +1,46 @@
+class ZShape:Shape {
+    /*
+    
+    Orientation 0
+    
+    | 0 |
+    | 2 | 1 |
+    | 3 |
+    
+    Orientation 90
+    
+    | 0 | 1 |
+    | 2 | 3 |
+    
+    Orientation 180
+    
+    | 0 |
+    | 2 | 1 |
+    | 3 |
+    
+    Orientation 270
+    
+    | 0 | 1|
+    | 2 | 3 |
+    
+    
+    */
+    
+    override var blockRowColumnPositions: [Orientation: Array<(columnDiff: Int, rowDiff: Int)>] {
+        return [
+            Orientation.Zero:       [(1, 0), (1, 1), (0, 1), (0, 2)],
+            Orientation.Ninety:     [(-1,0), (0, 0), (0, 1), (1, 1)],
+            Orientation.OneEighty:  [(1, 0), (1, 1), (0, 1), (0, 2)],
+            Orientation.TwoSeventy: [(-1,0), (0, 0), (0, 1), (1, 1)]
+        ]
+    }
+    
+    override var bottomBlocksForOrientations: [Orientation: Array<Block>] {
+        return [
+            Orientation.Zero:       [blocks[SecondBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.Ninety:     [blocks[FirstBlockIdx], blocks[ThirdBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.OneEighty:  [blocks[SecondBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.TwoSeventy: [blocks[FirstBlockIdx], blocks[ThirdBlockIdx], blocks[FourthBlockIdx]]
+        ]
+    }
+}
