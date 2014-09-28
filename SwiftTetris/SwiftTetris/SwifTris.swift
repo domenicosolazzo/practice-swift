2ae3e56e32b04093cd6bc40681c9a67978f930b6
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-09-28 18:43:26 +0200

    Refactoring

--- SwiftTetris/SwiftTetris/SwifTris.swift
+++ SwiftTetris/SwiftTetris/SwifTris.swift
@@ -84,4 +84,93 @@ class SwifTris{
     func endGame() {
         delegate?.gameDidEnd(self)
     }
+    
+    
+    /// Dropping a shape is the act of sending it plummeting towards the bottom of the game board.
+    func dropShape() {
+        if let shape = fallingShape {
+            while detectIllegalPlacement() == false {
+                shape.lowerShapeByOneRow()
+            }
+            shape.raiseShapeByOneRow()
+            delegate?.gameShapeDidDrop(self)
+        }
+    }
+    
+    /// It lowers the shape by one row and ends the game if it fails to do so without finding legal placement for it.
+    func letShapeFall() {
+        if let shape = fallingShape {
+            shape.lowerShapeByOneRow()
+            if detectIllegalPlacement() {
+                shape.raiseShapeByOneRow()
+                if detectIllegalPlacement() {
+                    endGame()
+                } else {
+                    settleShape()
+                }
+            } else {
+                delegate?.gameShapeDidMove(self)
+                if detectTouch() {
+                    settleShape()
+                }
+            }
+        }
+    }
+    
+    func rotateShape() {
+        if let shape = fallingShape {
+            shape.rotateClockwise()
+            if detectIllegalPlacement() {
+                shape.rotateCounterClockwise()
+            } else {
+                delegate?.gameShapeDidMove(self)
+            }
+        }
+    }
+    
+    func moveShapeLeft() {
+        if let shape = fallingShape {
+            shape.shiftLeftByOneColumn()
+            if detectIllegalPlacement() {
+                shape.shiftRightByOneColumn()
+                return
+            }
+            delegate?.gameShapeDidMove(self)
+        }
+    }
+    
+    
+    func moveShapeRight() {
+        if let shape = fallingShape {
+            shape.shiftRightByOneColumn()
+            if detectIllegalPlacement() {
+                shape.shiftLeftByOneColumn()
+                return
+            }
+            delegate?.gameShapeDidMove(self)
+        }
+    }
+    
+    /// settleShape() adds the falling shape to the collection of blocks maintained by Swiftris. Once the falling shape's blocks are part of the game board, fallingShape is nullified and the delegate is notified of a new shape settling into the game board.
+    func settleShape() {
+        if let shape = fallingShape {
+            for block in shape.blocks {
+                blockArray[block.column, block.row] = block
+            }
+            fallingShape = nil
+            delegate?.gameShapeDidLand(self)
+        }
+    }
+    
+    func detectTouch() -> Bool {
+        if let shape = fallingShape {
+            for bottomBlock in shape.bottomBlocks {
+                if bottomBlock.row == NumRows - 1 ||
+                    blockArray[bottomBlock.column, bottomBlock.row + 1] != nil {
+                        return true
+                }
+            }
+        }
+        return false
+    }
 }
