37ae70d9bded9b8b4b4524f3af675325278269ca
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-09-28 11:40:32 +0200

    Initialization of the SwifTris class

--- SwiftTetris/SwiftTetris/SwifTris.swift
+++ SwiftTetris/SwiftTetris/SwifTris.swift
@@ -1,11 +1,21 @@
-class SwifTris{
-    let NumColumns = 10
-    let NumRows = 20
+let NumColumns = 10
+let NumRows = 20
     
-    let StartingColumn = 4
-    let StartingRow = 0
+let StartingColumn = 4
+let StartingRow = 0
     
-    let PreviewColumn = 12
-    let PreviewRow = 1
+let PreviewColumn = 12
+let PreviewRow = 1
 
+class SwifTris{
+
+    var blockArray:Array2D<Block>
+    var nextShape:Shape?
+    var fallingShape:Shape?
+    
+    init() {
+        fallingShape = nil
+        nextShape = nil
+        blockArray = Array2D<Block>(columns: NumColumns, rows: NumRows)
+    }
 }
