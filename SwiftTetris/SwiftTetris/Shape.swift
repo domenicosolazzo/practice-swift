70548758ecad5598e69c1968cb6b86c707ecfcf9
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-09-28 11:08:25 +0200

    JShape block

--- SwiftTetris/SwiftTetris/Shape.swift
+++ SwiftTetris/SwiftTetris/Shape.swift
@@ -195,30 +195,29 @@ class LShape:Shape {
     
     Orientation 0
     
-    | 0•|
+    | 0 |
     | 1 |
     | 2 | 3 |
     
     Orientation 90
     
-    •
+    
     | 2 | 1 | 0 |
     | 3 |
     
     Orientation 180
     
-    | 3 | 2•|
+    | 3 | 2 |
     | 1 |
     | 0 |
     
     Orientation 270
     
-    • | 3 |
+        | 3 |
     | 0 | 1 | 2 |
     
-    • marks the row/column indicator for the shape
+    marks the row/column indicator for the shape
     
-    Pivots about `1`
     
     */
     
@@ -237,6 +236,55 @@ class LShape:Shape {
             Orientation.Ninety:     [blocks[FirstBlockIdx], blocks[SecondBlockIdx], blocks[FourthBlockIdx]],
             Orientation.OneEighty:  [blocks[FirstBlockIdx], blocks[FourthBlockIdx]],
             Orientation.TwoSeventy: [blocks[FirstBlockIdx], blocks[SecondBlockIdx], blocks[ThirdBlockIdx]]
+        ]
+    }
+}
+
+class JShape:Shape {
+    /*
+    
+    Orientation 0
+    
+    | 0 |
+    | 1 |
+    | 3 | 2 |
+    
+    Orientation 90
+    
+    | 3 |
+    | 2 | 1 | 0 |
+    
+    Orientation 180
+    
+    | 2 | 3 |
+    | 1 |
+    | 0 |
+    
+    Orientation 270
+    
+    | 0 | 1 | 2 |
+    | 3 |
+    
+    marks the row/column indicator for the shape
+    
+    
+    */
+    
+    override var blockRowColumnPositions: [Orientation: Array<(columnDiff: Int, rowDiff: Int)>] {
+        return [
+            Orientation.Zero:       [(1, 0), (1, 1),  (1, 2),  (0, 2)],
+            Orientation.Ninety:     [(2, 1), (1, 1),  (0, 1),  (0, 0)],
+            Orientation.OneEighty:  [(0, 2), (0, 1),  (0, 0),  (1, 0)],
+            Orientation.TwoSeventy: [(0, 0), (1, 0),  (2, 0),  (2, 1)]
+        ]
+    }
+    
+    override var bottomBlocksForOrientations: [Orientation: Array<Block>] {
+        return [
+            Orientation.Zero:       [blocks[ThirdBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.Ninety:     [blocks[FirstBlockIdx], blocks[SecondBlockIdx], blocks[ThirdBlockIdx]],
+            Orientation.OneEighty:  [blocks[FirstBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.TwoSeventy: [blocks[FirstBlockIdx], blocks[SecondBlockIdx], blocks[FourthBlockIdx]]
         ]
     }
 }
