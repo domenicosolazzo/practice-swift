81dcf1576ee27ab88c32aabad839ae3aaca80c37
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-09-28 18:53:00 +0200

    Refactoring

--- SwiftTetris/SwiftTetris/GameViewController.swift
+++ SwiftTetris/SwiftTetris/GameViewController.swift
@@ -26,15 +26,6 @@ class GameViewController: UIViewController, SwiftrisDelegate {
         // Present the scene.
         skView.presentScene(scene)
         
-        // It adds nextShape to the game layer at the preview location. When that animation completes, we reposition the underlying Shape object at the starting row and starting column before we ask GameScene to move it from the preview location to its starting position. Once that completes, we ask Swiftris for a new shape, begin ticking, and add the newly established upcoming piece to the preview area.
-        scene.addPreviewShapeToScene(swiftris.nextShape!) {
-            self.swiftris.nextShape?.moveTo(StartingColumn, row: StartingRow)
-            self.scene.movePreviewShape(self.swiftris.nextShape!) {
-                let nextShapes = self.swiftris.newShape()
-                self.scene.startTicking()
-                self.scene.addPreviewShapeToScene(nextShapes.nextShape!) {}
-            }
-        }
         
         
     }
@@ -45,8 +36,7 @@ class GameViewController: UIViewController, SwiftrisDelegate {
     
     /// It lowers the falling shape by one row and then asks GameScene to redraw the shape at its new location.
     func didTick() {
-        swiftris.fallingShape?.lowerShapeByOneRow()
-        scene.redrawShape(swiftris.fallingShape!, completion: {})
+        swiftris.letShapeFall()
     }
 
 }
