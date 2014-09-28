648219a7488dc784dd5735c835f87af830c0f5a9
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-09-28 11:39:24 +0200

    The total number of rows and columns on the game board, the location of where each piece starts and the location of where the preview piece belongs

--- SwiftTetris/SwiftTetris/SwifTris.swift
+++ SwiftTetris/SwiftTetris/SwifTris.swift
@@ -1,3 +1,11 @@
 class SwifTris{
+    let NumColumns = 10
+    let NumRows = 20
+    
+    let StartingColumn = 4
+    let StartingRow = 0
+    
+    let PreviewColumn = 12
+    let PreviewRow = 1
 
 }
