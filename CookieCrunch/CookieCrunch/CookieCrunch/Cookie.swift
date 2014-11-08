1a8569b246bf1e6eaafc5056443f5efc4a09038f
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-11-08 18:26:52 +0100

    Added CookieType and empty Cookie Class

---
+++ CookieCrunch/CookieCrunch/CookieCrunch/Cookie.swift
@@ -0,0 +1,15 @@
+//
+//  Cookie.swift
+//  CookieCrunch
+//
+//  Created by Domenico on 11/8/14
+
+import SpriteKit
+
+enum CookieType: Int {
+    case Unknown = 0, Croissant, Cupcake, Danish, Donut, Macaroon, SugarCookie
+}
+
+class Cookie{
+
+}
