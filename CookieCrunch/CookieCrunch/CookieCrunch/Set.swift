3b425ae95172a153fbdf44db67f5cff55ec7b2c6
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-11-08 19:23:57 +0100

    Custom set in swift

---
+++ CookieCrunch/CookieCrunch/CookieCrunch/Set.swift
@@ -0,0 +1,52 @@
+//
+//  Set.swift
+//  CookieCrunch
+//
+//  Created by Domenico on 11/8/14.
+//
+
+struct Set<T: Hashable>: SequenceType, Printable {
+    private var dictionary = Dictionary<T, Bool>()
+    
+    mutating func addElement(newElement: T) {
+        dictionary[newElement] = true
+    }
+    
+    mutating func removeElement(element: T) {
+        dictionary[element] = nil
+    }
+    
+    func containsElement(element: T) -> Bool {
+        return dictionary[element] != nil
+    }
+    
+    func allElements() -> [T] {
+        return Array(dictionary.keys)
+    }
+    
+    var count: Int {
+        return dictionary.count
+    }
+    
+    func unionSet(otherSet: Set<T>) -> Set<T> {
+        var combined = Set<T>()
+        
+        for obj in dictionary.keys {
+            combined.dictionary[obj] = true
+        }
+        
+        for obj in otherSet.dictionary.keys {
+            combined.dictionary[obj] = true
+        }
+        
+        return combined
+    }
+    
+    func generate() -> IndexingGenerator<Array<T>> {
+        return allElements().generate()
+    }
+    
+    var description: String {
+        return dictionary.description
+    }
+}
