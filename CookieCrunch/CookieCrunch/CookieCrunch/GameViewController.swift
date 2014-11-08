b8999d66be1d2d9823e947212d76e54d8019aeaf
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-11-08 18:13:06 +0100

    ViewDidLoad in the GameViewController

--- CookieCrunch/CookieCrunch/CookieCrunch/GameViewController.swift
+++ CookieCrunch/CookieCrunch/CookieCrunch/GameViewController.swift
@@ -30,20 +30,20 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        if let scene = GameScene.unarchiveFromFile("GameScene") as? GameScene {
-            // Configure the view.
-            let skView = self.view as SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
-            
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
-            skView.ignoresSiblingOrder = true
-            
-            /* Set the scale mode to scale to fit the window */
-            scene.scaleMode = .AspectFill
-            
-            skView.presentScene(scene)
-        }
+        // Configure the view.
+        let skView = self.view as SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.multipleTouchEnabled = true
+        
+        /* Sprite Kit applies additional optimizations to improve rendering performance */
+        skView.ignoresSiblingOrder = true
+        
+        /* Set the scale mode to scale to fit the window */
+        var scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .AspectFill
+        
+        skView.presentScene(scene)
     }
 
     override func shouldAutorotate() -> Bool {
