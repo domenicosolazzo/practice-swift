9e1e630f661e1e47a140fb4dfa1540d43f48b3fc
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-11-14 20:23:00 +0100

    Moving animateSwap to GameScene and adding animateInvalidSwap: it slides the cookies to their new positions and then immediately flips them back

--- CookieCrunch/CookieCrunch/CookieCrunch/GameScene.swift
+++ CookieCrunch/CookieCrunch/CookieCrunch/GameScene.swift
@@ -214,4 +214,43 @@ class GameScene: SKScene {
             SKAction.fadeOutWithDuration(0.3),
             SKAction.removeFromParent()]))
     }
+    
+    /// Animating the swap of two cookies
+    func animateSwap(swap: Swap, completion: () -> ()) {
+        let spriteA = swap.cookieA.sprite!
+        let spriteB = swap.cookieB.sprite!
+        
+        spriteA.zPosition = 100
+        spriteB.zPosition = 90
+        
+        let Duration: NSTimeInterval = 0.3
+        
+        let moveA = SKAction.moveTo(spriteB.position, duration: Duration)
+        moveA.timingMode = .EaseOut
+        spriteA.runAction(moveA, completion: completion)
+        
+        let moveB = SKAction.moveTo(spriteA.position, duration: Duration)
+        moveB.timingMode = .EaseOut
+        spriteB.runAction(moveB)
+    }
+    
+    // it slides the cookies to their new positions and then immediately flips them back.
+    func animateInvalidSwap(swap: Swap, completion: () -> ()) {
+        let spriteA = swap.cookieA.sprite!
+        let spriteB = swap.cookieB.sprite!
+        
+        spriteA.zPosition = 100
+        spriteB.zPosition = 90
+        
+        let Duration: NSTimeInterval = 0.2
+        
+        let moveA = SKAction.moveTo(spriteB.position, duration: Duration)
+        moveA.timingMode = .EaseOut
+        
+        let moveB = SKAction.moveTo(spriteA.position, duration: Duration)
+        moveB.timingMode = .EaseOut
+        
+        spriteA.runAction(SKAction.sequence([moveA, moveB]), completion: completion)
+        spriteB.runAction(SKAction.sequence([moveB, moveA]))
+    }
 }
