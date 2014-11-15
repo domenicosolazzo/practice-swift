2c86066ff1b6c75a11be169c1297df44cf86f49b
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-11-15 16:50:56 +0100

    Detect vertical matches

--- CookieCrunch/CookieCrunch/CookieCrunch/Level.swift
+++ CookieCrunch/CookieCrunch/CookieCrunch/Level.swift
@@ -235,4 +235,32 @@ class Level{
         }
         return set
     }
+    
+    private func detectVerticalMatches() -> Set<Chain> {
+        var set = Set<Chain>()
+        
+        for column in 0..<NumColumns {
+            for var row = 0; row < NumRows - 2; {
+                if let cookie = cookies[column, row] {
+                    let matchType = cookie.cookieType
+                    
+                    if cookies[column, row + 1]?.cookieType == matchType &&
+                        cookies[column, row + 2]?.cookieType == matchType {
+                            
+                            let chain = Chain(chainType: .Vertical)
+                            do {
+                                chain.addCookie(cookies[column, row]!)
+                                ++row
+                            }
+                                while row < NumRows && cookies[column, row]?.cookieType == matchType
+                            
+                            set.addElement(chain)
+                            continue
+                    }
+                }
+                ++row
+            }
+        }
+        return set
+    }
 }
