85f4187ef88401ef61e98c1e6905d542f6d86038
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-11-16 11:52:32 +0100

    Animating the new cookies in handleMatches

--- CookieCrunch/CookieCrunch/CookieCrunch/GameViewController.swift
+++ CookieCrunch/CookieCrunch/CookieCrunch/GameViewController.swift
@@ -122,7 +122,10 @@ class GameViewController: UIViewController {
         scene.animateMatchedCookies(chains) {
             let columns = self.level.fillHoles()
             self.scene.animateFallingCookies(columns) {
-                self.view.userInteractionEnabled = true
+                let columns = self.level.topUpCookies()
+                self.scene.animateNewCookies(columns) {
+                    self.view.userInteractionEnabled = true
+                }
             }
         }
     }
