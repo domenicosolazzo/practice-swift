611eeb14ded922c535f7136118d3b3fa4b7dc6f7
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-03-16 21:19:34 +0100

    Some refactoring

--- stanford/cs193p/2015/Calculator/Calculator/CalculatorBrain.swift
+++ stanford/cs193p/2015/Calculator/Calculator/CalculatorBrain.swift
@@ -8,17 +8,30 @@
 
 import Foundation
 
-class CalculatorBrain{
-    enum Op{
+public class CalculatorBrain{
+    private enum Op: Printable{
         case Operand(Double)
         case UnaryOperation(String, Double -> Double)
         case BinaryOperation(String, (Double, Double) -> Double)
+        
+        var description: String{
+            get{
+                switch self{
+                case .Operand(let operand):
+                    return "\(operand)"
+                case .UnaryOperation(let symbol, _):
+                    return symbol
+                case .BinaryOperation(let symbol, _):
+                    return symbol
+                }
+            }
+        }
     }
     
     // Known operations
-    var knownOps = [String:Op]()
+    private var knownOps = [String:Op]()
     
-    var opStack = [Op]()
+    private var opStack = [Op]()
     
     init(){
         knownOps["+"] = Op.BinaryOperation("+", +)
@@ -27,7 +40,8 @@ class CalculatorBrain{
         knownOps["÷"] = Op.BinaryOperation("÷"){$1 / $0}
         knownOps["√"] = Op.UnaryOperation("√", sqrt)
     }
-    private func evaluate(ops:[Op]) -> (result:Double?, remainingOps:[Op]){
+        
+    private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op]){
         if(!ops.isEmpty){
             // Need this step because ops is immutable
             var remainingOps = ops
@@ -37,33 +51,37 @@ class CalculatorBrain{
                 return (operand, remainingOps)
             case .UnaryOperation(_, let operation):
                 let operandEvaluation = evaluate(remainingOps)
-                if let operand = operandEvaluation.result{
+                if let operand = operandEvaluation.result {
                     return (operation(operand), operandEvaluation.remainingOps)
                 }
             case .BinaryOperation(_, let operation):
                 let op1Evaluation = evaluate(remainingOps)
                 if let operand1 = op1Evaluation.result{
-                    op2Evaluation = evaluate(op1Evaluation.remainingOps)
-                    if let operand2 = op2Evaluation.result{
+                    let op2Evaluation = evaluate(op1Evaluation.remainingOps)
+                    if let operand2 =  op2Evaluation.result{
                         return (operation(operand1, operand2), op2Evaluation.remainingOps)
                     }
                 }
-            
+                
+            }
         }
         return (nil, ops)
     }
+    
     func evaluate() -> Double?{
         let (result, remainder) = evaluate(opStack)
         return result
     }
     
-    func pushOperand(operand: Double){
+    func pushOperand(operand: Double) -> Double?{
         opStack.append(Op.Operand(operand))
+        return evaluate()
     }
     
-    func performOperation(symbol:String){
+    func performOperation(symbol:String) -> Double?{
         if let operation = knownOps[symbol]{
             opStack.append(operation)
         }
+        return evaluate()
     }
 }
--- stanford/cs193p/2015/Calculator/Calculator/ViewController.swift
+++ stanford/cs193p/2015/Calculator/Calculator/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var display: UILabel!
 
     var userIsDigitingNumber: Bool = false
-    var brainCalculator: CalculatorBrain
+    var brain =  CalculatorBrain()
     
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
@@ -25,40 +25,27 @@ class ViewController: UIViewController {
         }
     }
     @IBAction func operate(sender: UIButton) {
-        let operation = sender.currentTitle!
         if(userIsDigitingNumber){
             enter()
         }
         
-        switch operation{
-            case "+": performOperation(){$0 + $1}
-            case "−": performOperation(){$1 - $0}
-            case "×":performOperation(){$0 * $1}
-            case "÷":performOperation(){$1 / $0}
-            case "√":performOperation(){sqrt($0)}
-            default: break;
+        if let operation = sender.currentTitle{
+            if let result = brain.performOperation(operation){
+                displayValue = result
+            }else{
+                displayValue = 0
+            }
         }
     }
     
-    func performOperation(operation:(Double, Double) -> Double){
-        if operandStack.count >= 2{
-            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
-            enter()
-        }
-    }
-    
-    func performOperation(operation:(Double) -> Double){
-        if operandStack.count >= 1{
-            displayValue = operation(operandStack.removeLast())
-            enter()
-        }
-    }
-    
-    var operandStack = Array<Double>()
     
     @IBAction func enter() {
-        operandStack.append(displayValue)
         userIsDigitingNumber = false
+        if let result = brain.pushOperand(displayValue){
+            displayValue = result
+        }else{
+            displayValue = 0
+        }
     }
     
     var displayValue: Double{
