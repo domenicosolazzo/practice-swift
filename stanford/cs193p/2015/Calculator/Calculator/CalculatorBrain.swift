5520ac9c57b01a1062d947d58125b1b11edb7235
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-03-16 20:45:25 +0100

    Recursive evaluate for evaluating operands and operations

--- stanford/cs193p/2015/Calculator/Calculator/CalculatorBrain.swift
+++ stanford/cs193p/2015/Calculator/Calculator/CalculatorBrain.swift
@@ -27,6 +27,34 @@ class CalculatorBrain{
         knownOps["÷"] = Op.BinaryOperation("÷"){$1 / $0}
         knownOps["√"] = Op.UnaryOperation("√", sqrt)
     }
+    private func evaluate(ops:[Op]) -> (result:Double?, remainingOps:[Op]){
+        if(!ops.isEmpty){
+            // Need this step because ops is immutable
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+            switch op{
+            case .Operand(let operand):
+                return (operand, remainingOps)
+            case .UnaryOperation(_, let operation):
+                let operandEvaluation = evaluate(remainingOps)
+                if let operand = operandEvaluation.result{
+                    return (operation(operand), operandEvaluation.remainingOps)
+                }
+            case .BinaryOperation(_, let operation):
+                let op1Evaluation = evaluate(remainingOps)
+                if let operand1 = op1Evaluation.result{
+                    op2Evaluation = evaluate(op1Evaluation.remainingOps)
+                    if let operand2 = op2Evaluation.result{
+                        return (operation(operand1, operand2), op2Evaluation.remainingOps)
+                    }
+                }
+            
+        }
+        return (nil, ops)
+    }
+    func evaluate() -> Double?{
+        return nil
+    }
     
     func pushOperand(operand: Double){
         opStack.append(Op.Operand(operand))
