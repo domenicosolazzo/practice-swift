d8a809ac208185d0cd30d31bab2b038b089ee864
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-03-16 19:55:23 +0100

    Adding the model for the Calculator

---
+++ stanford/cs193p/2015/Calculator/Calculator/CalculatorBrain.swift
@@ -0,0 +1,13 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Domenico on 16.03.15.
+//  Copyright (c) 2015 Domenico Solazzo. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain{
+
+}
