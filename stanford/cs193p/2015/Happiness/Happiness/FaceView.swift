a7a62809c99f8dec4cea816a7bf6c081d9170b9f
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-03-19 20:44:46 +0100

    Bezier path for smile

--- stanford/cs193p/2015/Happiness/Happiness/FaceView.swift
+++ stanford/cs193p/2015/Happiness/Happiness/FaceView.swift
@@ -27,7 +27,7 @@ class FaceView: UIView {
         static let FaceRadiusToEyeOffsetRatio: CGFloat = 3
         static let FaceRadiusToEyeSeparationRatio: CGFloat = 1.5
         static let FaceRadiusToMouthWidthRatio: CGFloat = 1
-        static let FaceRadiusToEMouthHeightRatio: CGFloat = 3
+        static let FaceRadiusToMouthHeightRatio: CGFloat = 3
         static let FaceRadiusToMouthOffsetRatio: CGFloat = 3
     }
     
@@ -56,6 +56,25 @@ class FaceView: UIView {
         case .Right: eyeCenter.x += eyeHorizontalSeparation / 2
         }
         var path = UIBezierPath(arcCenter: eyeCenter, radius: eyeRadius, startAngle: 0, endAngle: CGFloat(2*M_PI), clockwise: true)
+        path.lineWidth = lineWidth
+        return path
+    }
+    
+    private func bezierPathForSmile(fractionOfMaxSmile: Double) -> UIBezierPath{
+        let mouthWidth = faceRadius / Scaling.FaceRadiusToMouthWidthRatio
+        let mouthHeight = faceRadius / Scaling.FaceRadiusToMouthHeightRatio
+        let mouthVerticalOffset = faceRadius / Scaling.FaceRadiusToMouthOffsetRatio
+        
+        let smileHeight = CGFloat(max(min(fractionOfMaxSmile, 1), -1)) * mouthHeight
+        
+        let start = CGPoint(x: faceCenter.x - mouthWidth / 2, y: faceCenter.y + mouthVerticalOffset)
+        let end = CGPoint(x: start.x + mouthWidth, y: start.y)
+        let cp1 = CGPoint(x: start.x + mouthWidth / 3, y: start.y + smileHeight)
+        let cp2 = CGPoint(x: end.x - mouthWidth / 3, y: cp1.y)
+        
+        let path = UIBezierPath()
+        path.moveToPoint(start)
+        path.addCurveToPoint(end, controlPoint1: cp1, controlPoint2: cp2)
         path.lineWidth = lineWidth
         return path
     }
