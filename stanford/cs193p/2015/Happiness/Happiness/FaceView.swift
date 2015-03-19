d7b55875ffb05e60fd1ddfc020cacadeaaae6933
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-03-19 19:17:42 +0100

    Adding FaceView

---
+++ stanford/cs193p/2015/Happiness/Happiness/FaceView.swift
@@ -0,0 +1,12 @@
+//
+//  FaceView.swift
+//  Happiness
+//
+//  Created by Domenico on 19.03.15.
+//  Copyright (c) 2015 Domenico Solazzo. All rights reserved.
+//
+import UIKit
+
+class FaceView: UIView {
+
+}
