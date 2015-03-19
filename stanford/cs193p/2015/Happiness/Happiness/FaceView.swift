ef3ddb268bf7ac97e5d0afd1c1e25e5e8d7952a3
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-03-19 19:49:17 +0100

    Connecting the FaceView to the Storyboard

--- stanford/cs193p/2015/Happiness/Happiness/FaceView.swift
+++ stanford/cs193p/2015/Happiness/Happiness/FaceView.swift
@@ -3,7 +3,7 @@
 //  Happiness
 //
 //  Created by Domenico on 19.03.15.
-//  Copyright (c) 2015 Domenico Solazzo. All rights reserved.
+//  Copyright (c) 2015 Domenico Solazzo. All rights reserved.ha
 //
 import UIKit
 
