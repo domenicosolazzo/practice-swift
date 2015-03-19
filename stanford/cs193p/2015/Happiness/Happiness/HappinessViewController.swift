8c5812e56771756af7dea43ea18213b5cad9e8eb
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-03-19 19:01:34 +0100

    Adding HappinessViewController

---
+++ stanford/cs193p/2015/Happiness/Happiness/HappinessViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Domenico on 19.03.15.
+//  Copyright (c) 2015 Domenico Solazzo. All rights reserved.
+//
+
+import Cocoa
+
+class HappinessViewController: UIViewController {
+
+}
