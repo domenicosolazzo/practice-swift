1f8a34fef5edcbad7a96b866d2f7a2634d0c0453
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-03-19 20:31:01 +0100

    Bezier path for drawing the eyes

--- stanford/cs193p/2015/Happiness/Happiness/FaceView.swift
+++ stanford/cs193p/2015/Happiness/Happiness/FaceView.swift
@@ -10,14 +10,18 @@ import UIKit
 class FaceView: UIView {
     var lineWidth: CGFloat = 3 {didSet{ setNeedsDisplay()}}
     var color:UIColor = UIColor.blueColor() {didSet{ setNeedsDisplay()}}
+    var scale: CGFloat = 0.90 {didSet{setNeedsDisplay()}}
+    
+    
     var faceCenter: CGPoint{
         // Convert center from the superview to FaceView
         return convertPoint(center, fromView: superview)
     }
     
     var faceRadius: CGFloat{
-        return min(bounds.size.width, bounds.size.height) / 2
+        return min(bounds.size.width, bounds.size.height) / 2 * scale
     }
+    
     
     override func drawRect(rect: CGRect) {
         let facePath = UIBezierPath(arcCenter: faceCenter, radius: faceRadius, startAngle: CGFloat(0), endAngle: CGFloat(2*M_PI), clockwise: true)
@@ -26,5 +30,21 @@ class FaceView: UIView {
         color.set()
         // Draw
         facePath.stroke()
+    }
+    private enum Eye{case Left, Right}
+    
+    private func bezierPathForEye(whichEye: Eye) -> UIBezierPath{
+        var eyeRadius = faceRadius / Scaling.FaceRadiusToEyeRadiusRatio
+        var eyeVerticalOffset = faceRadius / Scaling.FaceRadiusToEyeOffsetRatio
+        var eyeHorizontalSeparation = faceRadius / Scaling.FaceRadiusToEyeSeparationRatio
+        var eyeCenter = faceCenter
+        eyeCenter.y -= eyeVerticalOffset
+        switch whichEye{
+        case .Left: eyeCenter.x -= eyeHorizontalSeparation / 2
+        case .Right: eyeCenter.x += eyeHorizontalSeparation / 2
+        }
+        var path = UIBezierPath(arcCenter: eyeCenter, radius: eyeRadius, startAngle: 0, endAngle: CGFloat(2*M_PI), clockwise: true)
+        path.lineWidth = lineWidth
+        return path
     }
 }
