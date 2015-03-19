667115f00a02a7907a729b4aaab75eecfdd2759e
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-03-19 20:05:03 +0100

    Creating a UIBezierPath

--- stanford/cs193p/2015/Happiness/Happiness/FaceView.swift
+++ stanford/cs193p/2015/Happiness/Happiness/FaceView.swift
@@ -8,5 +8,23 @@
 import UIKit
 
 class FaceView: UIView {
-
+    var lineWidth: CGFloat = 3 {didSet{ setNeedsDisplay()}}
+    var color:UIColor = UIColor.blueColor() {didSet{ setNeedsDisplay()}}
+    var faceCenter: CGPoint{
+        // Convert center from the superview to FaceView
+        return convertPoint(center, fromView: superview)
+    }
+    
+    var faceRadius: CGFloat{
+        return min(bounds.size.width, bounds.size.height) / 2
+    }
+    
+    override func drawRect(rect: CGRect) {
+        let facePath = UIBezierPath(arcCenter: faceCenter, radius: faceRadius, startAngle: CGFloat(0), endAngle: CGFloat(2*M_PI), clockwise: true)
+        facePath.lineWidth = lineWidth
+        // Set the color
+        color.set()
+        // Draw
+        facePath.stroke()
+    }
 }
