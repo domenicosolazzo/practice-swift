00b8b8118f2e8b100614833ceb57c83912e13cf7
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-03-20 18:36:13 +0100

    Adding IBInspectable for some properties

--- stanford/cs193p/2015/Happiness/Happiness/FaceView.swift
+++ stanford/cs193p/2015/Happiness/Happiness/FaceView.swift
@@ -9,8 +9,11 @@ import UIKit
 
 @IBDesignable
 class FaceView: UIView {
+    @IBInspectable
     var lineWidth: CGFloat = 3 {didSet{ setNeedsDisplay()}}
+    @IBInspectable
     var color:UIColor = UIColor.blueColor() {didSet{ setNeedsDisplay()}}
+    @IBInspectable
     var scale: CGFloat = 0.90 {didSet{setNeedsDisplay()}}
     
     
