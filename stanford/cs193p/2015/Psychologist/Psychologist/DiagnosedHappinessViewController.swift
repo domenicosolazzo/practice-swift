1627c250b51dd00c1e85b003c8ea66f79545e706
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-03-21 18:39:33 +0100

    Implemented prepareForSegue inside the DiagnosedHappinessViewController

--- stanford/cs193p/2015/Psychologist/Psychologist/DiagnosedHappinessViewController.swift
+++ stanford/cs193p/2015/Psychologist/Psychologist/DiagnosedHappinessViewController.swift
@@ -9,5 +9,20 @@
 import UIKit
 
 class DiagnosedHappinessViewController: HappinessViewController{
-    var diagnosticHistory: [Int]()
+    var diagnosticHistory = [Int]()
+    
+    private struct History{
+        static let SegueIdentifier = "Show Diagnostic History"
+    }
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let identifier = segue.identifier{
+            switch identifier{
+            case History.SegueIdentifier:
+                if let tvc = segue.destinationViewController as? TextViewController{
+                    tvc.text = "\(diagnosticHistory)"
+                }
+            default: break
+            }
+        }
+    }
 }
