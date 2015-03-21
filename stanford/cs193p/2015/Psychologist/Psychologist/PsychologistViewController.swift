5409ecc04e53e8efe98c4abc553726c16197890f
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-03-21 13:08:57 +0100

    Clean up

---
+++ stanford/cs193p/2015/Psychologist/Psychologist/PsychologistViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Psychologist
+//
+//  Created by Domenico on 21.03.15.
+//  Copyright (c) 2015 Domenico Solazzo. All rights reserved.
+//
+
+import UIKit
+
+class PsychologistViewController: UIViewController {
+
+    
+
+
+}
+
