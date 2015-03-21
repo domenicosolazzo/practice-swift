2fb5c97ba4f81831efdacd970a5c52da5984f550
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-03-21 18:27:34 +0100

    Adding the textView

--- stanford/cs193p/2015/Psychologist/Psychologist/TextViewController.swift
+++ stanford/cs193p/2015/Psychologist/Psychologist/TextViewController.swift
@@ -10,4 +10,15 @@ import UIKit
 
 class TextViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!{
+        didSet{
+            textView.text = text
+        }
+    }
+    
+    var text: String = ""{
+        didSet{
+            textView?.text = text
+        }
+    }
 }
