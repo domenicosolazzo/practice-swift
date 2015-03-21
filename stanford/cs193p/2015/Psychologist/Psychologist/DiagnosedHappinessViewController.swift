0817b69ee3f3b4c5b3115a87c652c0c4c50798a7
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-03-21 18:54:03 +0100

    Computed property diagnosticHistory using NSUserDefaults

--- stanford/cs193p/2015/Psychologist/Psychologist/DiagnosedHappinessViewController.swift
+++ stanford/cs193p/2015/Psychologist/Psychologist/DiagnosedHappinessViewController.swift
@@ -17,13 +17,18 @@ class DiagnosedHappinessViewController: HappinessViewController{
     }
     
     private let defaults = NSUserDefaults.standardUserDefaults()
-    var diagnosticHistory: Int{
-        get {}
-        set {}
+    var diagnosticHistory: [Int]{
+        get {
+            return defaults.objectForKey(History.DefaultsKey) as? [Int] ?? []
+        }
+        set {
+            defaults.setObject(newValue, forKey: History.DefaultsKey)
+        }
     }
     
     private struct History{
         static let SegueIdentifier = "Show Diagnostic History"
+        static let DefaultsKey = "DiagnosedHappinessViewController.History"
     }
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if let identifier = segue.identifier{
