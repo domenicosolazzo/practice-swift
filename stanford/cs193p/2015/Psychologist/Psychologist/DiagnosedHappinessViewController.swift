675d3be045f9dfd58f8fa541a156873b1a811591
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-03-21 18:32:05 +0100

    Added DiagnosedHappinessViewController

---
+++ stanford/cs193p/2015/Psychologist/Psychologist/DiagnosedHappinessViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DiagnosedHappinessViewController.swift
+//  Psychologist
+//
+//  Created by Domenico on 21.03.15.
+//  Copyright (c) 2015 Domenico Solazzo. All rights reserved.
+//
+
+import UIKit
+
+class DiagnosedHappinessViewController: HappinessViewController{
+
+}
