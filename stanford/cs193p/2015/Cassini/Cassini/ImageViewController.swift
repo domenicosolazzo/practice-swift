458b948460c6c3c523f8733e1bc5043f93bd105e
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-11 13:05:43 +0200

    Added variable for the imageURL; Added function for retrieving the image

--- stanford/cs193p/2015/Cassini/Cassini/ImageViewController.swift
+++ stanford/cs193p/2015/Cassini/Cassini/ImageViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class ImageViewController: UIViewController {
     
+    private var imageURL: NSURL?{
+        didSet{
+            image = nil
+            fetchImage()
+        }
+    }
+    
+    private func fetchImage(){
+        if let url = imageURL{
+            let imageData = NSData(contentsOfFile: url)
+            if imageData != nil{
+                image = UIIMage(data: imageData)
+            }else{
+                image = nil
+            }
+        }
+    }
+    
     private var imageView = UIImageView()
     
     private var image: UIImage?{
