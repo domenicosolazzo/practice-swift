51fdd0e5255ac2b1358fd74725a8f95e0f3ed0be
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-11 13:19:00 +0200

    DemoURL

---
+++ stanford/cs193p/2015/Cassini/Cassini/DemoURL.swift
@@ -0,0 +1,18 @@
+//
+//  DemoURL.swift
+//  Cassini
+//
+//  Created by Domenico on 11.04.15.
+//  License: MIT
+//
+
+import Foundation
+struct DemoURL{
+    static let Stanford = NSURL(string:"http://comm.stanford.edu/wp-content/uploads/2013/01/stanford-campus.png")
+    
+    struct NASA{
+        static let Cassini = NSURL(string:"http://www.jpl.nasa.gov/images/cassini/20090202/pia03883-full.jpg")
+        static let Earth = NSURL(string: "http://www.nasa.gov/sites/default/files/wave_earth_mosaic_3.jpg")
+        static let Saturn = NSURL(string: "http://www.nasa.gov/sites/default/files/saturn_collage.jpg")
+    }
+}
--- stanford/cs193p/2015/Cassini/Cassini/ImageViewController.swift
+++ stanford/cs193p/2015/Cassini/Cassini/ImageViewController.swift
@@ -26,7 +26,7 @@ class ImageViewController: UIViewController {
             // Very bad:it can take long time to retrieve the data
             let imageData = NSData(contentsOfURL: url)
             if imageData != nil{
-                image = UIImage(data: imageData)
+                image = UIImage(data: imageData!)
             }else{
                 image = nil
             }
