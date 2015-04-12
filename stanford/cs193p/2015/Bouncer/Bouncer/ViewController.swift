808c3759f47717fd0273c63aa04786df0906c448
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-12 18:35:54 +0200

    Implementing the ViewController

--- stanford/cs193p/2015/Bouncer/Bouncer/ViewController.swift
+++ stanford/cs193p/2015/Bouncer/Bouncer/ViewController.swift
@@ -10,14 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let bouncer = BouncerBehavior()
+    lazy var animator: UIDynamicAnimator = { UIDynamicAnimator(referenceView: self.view) }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        animator.addBehavior(bouncer)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    var redBlock: UIView?
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        if redBlock == nil {
+            redBlock = addBlock()
+            redBlock?.backgroundColor = UIColor.redColor()
+            bouncer.addBlock(redBlock!)
+        }
+        let motionManager = AppDelegate.Motion.Manager
+        if motionManager.accelerometerAvailable {
+            motionManager.startAccelerometerUpdatesToQueue(NSOperationQueue.mainQueue()) { (data, error) -> Void in
+                self.bouncer.gravity.gravityDirection = CGVector(dx: data.acceleration.x, dy: -data.acceleration.y)
+            }
+        }
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        AppDelegate.Motion.Manager.stopAccelerometerUpdates()
+    }
+    
+    struct Constants {
+        static let BlockSize = CGSize(width: 40, height: 40)
+    }
+    
+    func addBlock() -> UIView {
+        let block = UIView(frame: CGRect(origin: CGPoint.zeroPoint, size: Constants.BlockSize))
+        block.center = CGPoint(x: view.bounds.midX, y: view.bounds.midY)
+        view.addSubview(block)
+        return block
     }
 
 
