cca2880dac80b614f1969d2e4d0f1d48205661e3
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-12 17:50:03 +0200

    Post a new notification

--- stanford/cs193p/2015/Trax/Trax/AppDelegate.swift
+++ stanford/cs193p/2015/Trax/Trax/AppDelegate.swift
@@ -8,13 +8,24 @@
 
 import UIKit
 
+struct GPXURL{
+    static let Notification = "GPX Notification"
+    static let Key = "GPX key"
+}
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
 
-    
+    func application(application: UIApplication, handleOpenURL url: NSURL) -> Bool {
+        // Get the notification center
+        var center = NSNotificationCenter.defaultCenter()
+        // Push the notification
+        var notification = NSNotification(name: GPXURL.Notification, object: self, userInfo: [GPXURL.Key: url])
+        center.postNotification(notification)
+        return true
+    }
 
 
 }
