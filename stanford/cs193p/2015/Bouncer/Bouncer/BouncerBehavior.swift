9dda909355ad976afc8538b637b01492f2c3defe
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-12 18:18:25 +0200

    Adding BouncerBehavior

---
+++ stanford/cs193p/2015/Bouncer/Bouncer/BouncerBehavior.swift
@@ -0,0 +1,59 @@
+//
+//  BouncerBehavior.swift
+//  Bouncer
+//
+//  Created by Domenico on 12.04.15.
+//  Copyright (c) 2015 Domenico Solazzo. All rights reserved.
+//
+
+import UIKit
+
+class BouncerBehavior: UIDynamicBehavior {
+    // Gravity Behaviour
+    var gravity = UIGravityBehavior()
+    
+    // Collider Behaviour
+    lazy var collider: UICollisionBehavior = {
+        let lazilyCreatedCollider = UICollisionBehavior()
+        lazilyCreatedCollider.translatesReferenceBoundsIntoBoundary = true;
+        return lazilyCreatedCollider
+        }()
+    
+    lazy var dropBehavior:UIDynamicItemBehavior = {
+        let lazilyCreatedDropBehavior = UIDynamicItemBehavior()
+        lazilyCreatedDropBehavior.allowsRotation = true
+        // Elasticity
+        lazilyCreatedDropBehavior.elasticity = 0.85
+        // Friction
+        lazilyCreatedDropBehavior.friction = 0 //off
+        // Resistance
+        lazilyCreatedDropBehavior.resistance = 0 //off
+        return lazilyCreatedDropBehavior
+        }()
+    
+    override init(){
+        super.init()
+        addChildBehavior(gravity)
+        addChildBehavior(collider)
+        addChildBehavior(dropBehavior)
+    }
+    
+    func addBarrier(path: UIBezierPath, named name:String){
+        collider.removeBoundaryWithIdentifier(name)
+        collider.addBoundaryWithIdentifier(name, forPath: path)
+    }
+    func addDrop(drop: UIView){
+        dynamicAnimator?.referenceView?.addSubview(drop)
+        gravity.addItem(drop)
+        collider.addItem(drop)
+        dropBehavior.addItem(drop)
+    }
+    
+    func removeDrop(drop: UIView){
+        gravity.removeItem(drop)
+        collider.removeItem(drop)
+        dropBehavior.removeItem(drop)
+        drop.removeFromSuperview()
+    }
+
+}
