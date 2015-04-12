22747ced6e181fe4519543e7aa1ea4413c290623
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-12 18:03:15 +0200

    Receive the notification in the ViewController

--- stanford/cs193p/2015/Trax/Trax/ViewController.swift
+++ stanford/cs193p/2015/Trax/Trax/ViewController.swift
@@ -11,5 +11,20 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let center = NSNotificationCenter.defaultCenter()
+        let queue = NSOperationQueue.mainQueue()
+        
+        let appDelegate = UIApplication.sharedApplication().delegate
+        
+        center.addObserverForName(GPXURL.Notification, object: appDelegate, queue: queue){ notification in
+            if let url = notification?.userInfo?[GPXURL.Key] as? NSURL{
+                self.textView.text = "Received \(url)"
+            }
+        }
+    }
 }
 
