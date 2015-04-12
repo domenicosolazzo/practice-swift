926be44dd2a107c6f6ac38e6c7a2e08d00f94b6c
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-12 12:49:18 +0200

    Adding custom drop behavior avoiding rotation and reducing elasticity

--- stanford/cs193p/2015/DropIt/DropIt/DropBehavior.swift
+++ stanford/cs193p/2015/DropIt/DropIt/DropBehavior.swift
@@ -20,21 +20,33 @@ class DropBehavior: UIDynamicBehavior {
         return lazilyCreatedCollider
         }()
     
+    lazy var dropBehavior:UIDynamicItemBehavior = {
+        let lazilyCreatedDropBehavior = UIDynamicItemBehavior()
+        // Do not allow rotation
+        lazilyCreatedDropBehavior.allowsRotation = false
+        // Elasticity
+        lazilyCreatedDropBehavior.elasticity = 0.75
+        return lazilyCreatedDropBehavior
+    }()
+    
     override init(){
         super.init()
         addChildBehavior(gravity)
         addChildBehavior(collider)
+        addChildBehavior(dropBehavior)
     }
     
     func addDrop(drop: UIView){
         dynamicAnimator?.referenceView?.addSubview(drop)
         gravity.addItem(drop)
         collider.addItem(drop)
+        dropBehavior.addItem(drop)
     }
     
     func removeDrop(drop: UIView){
         gravity.removeItem(drop)
         collider.removeItem(drop)
+        dropBehavior.removeItem(drop)
         drop.removeFromSuperview()
     }
 }
