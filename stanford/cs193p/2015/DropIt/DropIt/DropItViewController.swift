81471333f52420afbe25f18473c79d2aa58f3845
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-12 12:10:26 +0200

    Added dropsPerRow

--- stanford/cs193p/2015/DropIt/DropIt/DropItViewController.swift
+++ stanford/cs193p/2015/DropIt/DropIt/DropItViewController.swift
@@ -11,26 +11,7 @@ import UIKit
 class DropItViewController: UIViewController {
 
     @IBOutlet weak var gameView: UIView!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    var dropsPerRow = 10
 
 }
