4f93db1b9e08d7731a1245e7501e9011cbb3169f
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-12 12:11:23 +0200

    Added computed variable dropSize

--- stanford/cs193p/2015/DropIt/DropIt/DropItViewController.swift
+++ stanford/cs193p/2015/DropIt/DropIt/DropItViewController.swift
@@ -13,5 +13,10 @@ class DropItViewController: UIViewController {
     @IBOutlet weak var gameView: UIView!
     
     var dropsPerRow = 10
+    
+    var dropSize: CGSize{
+        let size = gameView.bounds.size.width / CGFloat(dropsPerRow)
+        return CGSize(width: size, height: size)
+    }
 
 }
