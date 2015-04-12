787cfcc05aadffb875fd367aabef4da67d1e784a
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-12 12:20:44 +0200

    Extension for UIColor

--- stanford/cs193p/2015/DropIt/DropIt/DropItViewController.swift
+++ stanford/cs193p/2015/DropIt/DropIt/DropItViewController.swift
@@ -38,4 +38,18 @@ private extension CGFloat {
     }
 }
 
+private extension UIColor{
+    class var random: UIColor{
+        switch arc4random() % 5{
+        case 0: return UIColor.greenColor()
+        case 1: return UIColor.blueColor()
+        case 2: return UIColor.orangeColor()
+        case 3: return UIColor.redColor()
+        case 4: return UIColor.purpleColor()
+        default: return UIColor.blackColor()
+        }
+    }
+}
+
+
 
