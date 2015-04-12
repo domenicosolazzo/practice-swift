0c6cca6208cfd5bcb2be40db235acd2d8c72c6fb
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-12 17:21:05 +0200

    New project

---
+++ stanford/cs193p/2015/Trax/Trax/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Trax
+//
+//  Created by Domenico on 12.04.15.
+//  Copyright (c) 2015 Domenico Solazzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
