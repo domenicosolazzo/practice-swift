e4c6bbf9e94856c448c04b0d78af8ebb69ec1815
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-18 21:10:59 +0200

    Adding the values for the slider

--- Autolayout/MultipleControls/MultipleControls/ViewController.swift
+++ Autolayout/MultipleControls/MultipleControls/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        sliderValue.text = "50"
     }
 
     override func didReceiveMemoryWarning() {
