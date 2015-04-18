ebdf418252696e909ff4e3f47c60325399ab87d7
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-18 19:58:15 +0200

    Added IBOutlets for the textfields

--- Autolayout/MultipleControls/MultipleControls/ViewController.swift
+++ Autolayout/MultipleControls/MultipleControls/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var numberField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
