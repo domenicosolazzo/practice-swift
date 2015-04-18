16e062a8b85fed931573ce89665231108305cedc
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-18 22:22:05 +0200

    Added an ActionSheet

--- Autolayout/MultipleControls/MultipleControls/ViewController.swift
+++ Autolayout/MultipleControls/MultipleControls/ViewController.swift
@@ -59,6 +59,37 @@ class ViewController: UIViewController {
         }
     }
     @IBAction func buttonPressed(sender: UIButton) {
+        let controller = UIAlertController(title: "Are You Sure?",
+            message:nil, preferredStyle: .ActionSheet)
+        
+        let yesAction = UIAlertAction(title: "Yes, I'm sure!",
+            style: .Destructive, handler: { action in
+                let msg = self.nameField.text.isEmpty
+                    ? "You can breathe easy, everything went OK."
+                    : "You can breathe easy, \(self.nameField.text),"
+                    + " everything went OK."
+                let controller2 = UIAlertController(
+                    title:"Something Was Done",
+                    message: msg, preferredStyle: .Alert)
+                let cancelAction = UIAlertAction(title: "Phew!",
+                    style: .Cancel, handler: nil)
+                controller2.addAction(cancelAction)
+                self.presentViewController(controller2, animated: true,
+                    completion: nil)
+        })
+        
+        let noAction = UIAlertAction(title: "No way!",
+            style: .Cancel, handler: nil)
+        
+        controller.addAction(yesAction)
+        controller.addAction(noAction)
+        
+        if let ppc = controller.popoverPresentationController {
+            ppc.sourceView = sender
+            ppc.sourceRect = sender.bounds
+        }
+        
+        presentViewController(controller, animated: true, completion: nil)
     }
 }
 
