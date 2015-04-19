17aab967ace795b821d87493c66d52d3486f6382
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-19 17:07:36 +0200

    Implemented supportedInterfaceOrientations

--- Autolayout/Orientations/Orientations/ViewController.swift
+++ Autolayout/Orientations/Orientations/ViewController.swift
@@ -20,6 +20,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    override func supportedInterfaceOrientations() -> Int {
+        return Int(UIInterfaceOrientationMask.Portrait.rawValue)
+            | Int(UIInterfaceOrientationMask.LandscapeLeft.rawValue)
+    }
 
 }
 
