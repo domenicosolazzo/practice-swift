8fdaf922e2601ab7b1601d1f4d63acb7df728d24
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-20 18:07:29 +0200

    Added the view controllers

---
+++ ViewSwitcher/ViewSwitcher/BlueViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BlueViewController.swift
+//  ViewSwitcher
+//
+//  Created by Domenico on 20.04.15.
+//  Copyright (c) 2015 Domenico Solazzo. All rights reserved.
+//
+
+import UIKit
+
+class BlueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
