977772eac11eee584663761ea2163576350862a7
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-20 18:38:30 +0200

    Loading the BlueViewController when the application load

--- ViewSwitcher/ViewSwitcher/SwitchingViewController.swift
+++ ViewSwitcher/ViewSwitcher/SwitchingViewController.swift
@@ -16,26 +16,19 @@ class SwitchingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Create an instance of BlueViewController
+        blueViewController = storyboard?.instantiateViewControllerWithIdentifier("Blue")
+        blueViewController.view.frame = view.frame
+        /// Switching the current view using the helper method
+        switchViewController(from: nil, to: blueViewController)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     @IBAction func switchViews(sender: UIBarButtonItem){
         
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
