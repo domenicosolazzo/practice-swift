7dd27ba812313f7af86543e54f0833fe51fc6642
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-21 20:12:58 +0200

    Playing the crunch sound

--- Pickers/Pickers/CustomPickerViewController.swift
+++ Pickers/Pickers/CustomPickerViewController.swift
@@ -35,6 +35,21 @@ class CustomPickerViewController: UIViewController, UIPickerViewDelegate, UIPick
         button.hidden = false
     }
     
+    func playWinSound() {
+        if winSoundID == 0 {
+            let soundURL = NSBundle.mainBundle().URLForResource(
+                "win", withExtension: "wav")! as CFURLRef
+            AudioServicesCreateSystemSoundID(soundURL, &winSoundID)
+        }
+        AudioServicesPlaySystemSound(winSoundID)
+        winLabel.text = "WINNER!"
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW,
+            Int64(1.5 * Double(NSEC_PER_SEC))),
+            dispatch_get_main_queue()) {
+                self.showButton()
+        }
+    }
+    
     @IBOutlet weak var winLabel: UILabel!
     @IBOutlet weak var button: UIButton!
     
@@ -66,7 +81,28 @@ class CustomPickerViewController: UIViewController, UIPickerViewDelegate, UIPick
             }
         }
         
-        winLabel.text = win ? "WINNER!" : " "
+        if crunchSoundID == 0 {
+            let soundURL = NSBundle.mainBundle().URLForResource(
+                "crunch", withExtension: "wav")! as CFURLRef
+            AudioServicesCreateSystemSoundID(soundURL, &crunchSoundID)
+        }
+        AudioServicesPlaySystemSound(crunchSoundID)
+        
+        if win {
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW,
+                Int64(0.5 * Double(NSEC_PER_SEC))),
+                dispatch_get_main_queue()) {
+                    self.playWinSound()
+            }
+        } else {
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW,
+                Int64(0.5 * Double(NSEC_PER_SEC))),
+                dispatch_get_main_queue()) {
+                    self.showButton()
+            }
+        }
+        button.hidden = true
+        winLabel.text = " " // Note the space between the quotes
     }
     
     // MARK:-
