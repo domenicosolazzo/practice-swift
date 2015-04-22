2100b2c45280af89ce76f0402982b6d937b9868b
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-22 19:08:36 +0200

    Added UITableViewCell

---
+++ TableViews/TableCells/TableCells/NameAndColorCell.swift
@@ -0,0 +1,24 @@
+//
+//  NameAndColorCell.swift
+//  TableCells
+//
+//  Created by Domenico on 22.04.15.
+//  Copyright (c) 2015 Domenico Solazzo. All rights reserved.
+//
+
+import UIKit
+
+class NameAndColorCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
