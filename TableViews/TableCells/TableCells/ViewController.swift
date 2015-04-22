8bf9b6bd2c109bc2212fb23692e61f892cca4b45
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-22 19:12:41 +0200

    Created IBOutlet for the TableView

--- TableViews/TableCells/TableCells/ViewController.swift
+++ TableViews/TableCells/TableCells/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
