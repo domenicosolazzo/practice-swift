2a055b19651cbad47b7d6e09ea5775dac9ccd5c8
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-22 19:10:26 +0200

    Implemented the table cell code

--- TableViews/TableCells/TableCells/NameAndColorCell.swift
+++ TableViews/TableCells/TableCells/NameAndColorCell.swift
@@ -10,6 +10,53 @@ import UIKit
 
 class NameAndColorCell: UITableViewCell {
 
+    var name: String = "" {
+        didSet {
+            if (name != oldValue) {
+                nameLabel.text = name
+            }
+        }
+    }
+    var color: String = "" {
+        didSet {
+            if (color != oldValue) {
+                colorLabel.text = color
+            }
+        }
+    }
+    var nameLabel: UILabel!
+    var colorLabel: UILabel!
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        let nameLabelRect = CGRectMake(0, 5, 70, 15)
+        let nameMarker = UILabel(frame: nameLabelRect)
+        nameMarker.textAlignment = NSTextAlignment.Right
+        nameMarker.text = "Name:"
+        nameMarker.font = UIFont.boldSystemFontOfSize(12)
+        contentView.addSubview(nameMarker)
+        
+        let colorLabelRect = CGRectMake(0, 26, 70, 15)
+        let colorMarker = UILabel(frame: colorLabelRect)
+        colorMarker.textAlignment = NSTextAlignment.Right
+        colorMarker.text = "Color:"
+        colorMarker.font = UIFont.boldSystemFontOfSize(12)
+        contentView.addSubview(colorMarker)
+        
+        let nameValueRect = CGRectMake(80, 5, 200, 15)
+        nameLabel = UILabel(frame: nameValueRect)
+        contentView.addSubview(nameLabel)
+        
+        let colorValueRect = CGRectMake(80, 25, 200, 15)
+        colorLabel = UILabel(frame: colorValueRect)
+        contentView.addSubview(colorLabel)
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -20,5 +67,7 @@ class NameAndColorCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    
+  
 
 }
