932ef052a0683c826ede655a86a43bc64262a426
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-23 18:54:03 +0200

    Added variable that can only be read from outside

---
+++ Navigation/Fonts/Fonts/FavoritesList.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesList.swift
+//  Fonts
+//
+//  Created by Domenico on 23.04.15.
+//
+
+import UIKit
+import Foundation
+
+class FavoritesList{
+    
+    // We want external classed to be able to get this variables, but not modifying it.
+    // private(set) 
+    private(set) var favorites: [String]
+}
