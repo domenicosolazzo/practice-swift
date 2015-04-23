b1ee0f65df802da2e3baef7fd941320afbb93eaf
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-23 19:51:14 +0200

    Calculate the cellPointSize

--- Navigation/Fonts/Fonts/FontListViewController.swift
+++ Navigation/Fonts/Fonts/FontListViewController.swift
@@ -6,8 +6,18 @@
 //  Copyright (c) 2015 Domenico Solazzo. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
 class FontListViewController: UITableViewController {
-
+    var fontNames: [String] = []
+    var showsFavorites:Bool = false
+    private var cellPointSize: CGFloat!
+    private let cellIdentifier = "FontName"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let preferredTableViewFont = UIFont.preferredFontForTextStyle(UIFontTextStyleHeadline)
+        cellPointSize = preferredTableViewFont.pointSize
+    }
 }
