4fc950467cfdadff7451d0158f527f88543b2ea0
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-25 20:31:02 +0200

    Helper method

--- CollectionView/DialogViewer/DialogViewer/ViewController.swift
+++ CollectionView/DialogViewer/DialogViewer/ViewController.swift
@@ -17,11 +17,43 @@ class ViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // Mock data
+        sections = [
+            ["header": "First Witch",
+                "content" : "Hey, when will the three of us meet up later?"],
+            ["header" : "Second Witch",
+                "content" : "When everything's straightened out."],
+            ["header" : "Third Witch",
+                "content" : "That'll be just before sunset."],
+            ["header" : "First Witch",
+                "content" : "Where?"],
+            ["header" : "Second Witch",
+                "content" : "The dirt patch."],
+            ["header" : "Third Witch",
+                "content" : "I guess we'll see Mac there."]
+        ]
+        
+        // Register the content cell
+        collectionView.registerClass(ContentCell.self,
+            forCellWithReuseIdentifier: "CONTENT")
+        
+        // Avoid issue with the status bar
+        var contentInset = collectionView.contentInset
+        contentInset.top = 20
+        collectionView.contentInset = contentInset
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // Helper method
+    func wordsInSection(section: Int) -> [String] {
+        let content = sections[section]["content"]
+        let spaces = NSCharacterSet.whitespaceAndNewlineCharacterSet()
+        let words = content?.componentsSeparatedByCharactersInSet(spaces)
+        return words!
     }
 
 
