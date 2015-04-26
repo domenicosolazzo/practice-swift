4087c57f479519de399b245bb39e423a23698d11
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-26 11:23:55 +0200

    Implemented LanguageListController

--- SplitView/Presidents/Presidents/LanguageListController.swift
+++ SplitView/Presidents/Presidents/LanguageListController.swift
@@ -12,4 +12,38 @@ class LanguageListController: UITableViewController {
     weak var detailViewController: DetailViewController? = nil
     private let languageNames: [String] = ["English", "French", "German", "Spanish"]
     private let languageCodes: [String] = ["en", "fr", "de", "es"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        clearsSelectionOnViewWillAppear = false
+        preferredContentSize = CGSizeMake(320, CGFloat(languageCodes.count * 44))
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView,
+        numberOfRowsInSection section: Int) -> Int {
+            
+            return languageCodes.count
+    }
+    
+    override func tableView(tableView: UITableView,
+        cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCellWithIdentifier("Cell",
+                forIndexPath: indexPath) as! UITableViewCell
+            
+            // Configure the cell...
+            cell.textLabel!.text = languageNames[indexPath.row]
+            
+            return cell
+    }
+    
+    override func tableView(tableView: UITableView,
+        didSelectRowAtIndexPath indexPath: NSIndexPath) {
+            detailViewController?.languageString = languageCodes[indexPath.row]
+    }
 }
