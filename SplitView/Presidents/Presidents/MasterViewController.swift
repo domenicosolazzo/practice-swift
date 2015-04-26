df38edd1108649c531a810a3988fef44b2309945
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-26 11:36:40 +0200

    Keep the last language string

--- SplitView/Presidents/Presidents/MasterViewController.swift
+++ SplitView/Presidents/Presidents/MasterViewController.swift
@@ -49,9 +49,13 @@ class MasterViewController: UITableViewController {
             if let indexPath = self.tableView.indexPathForSelectedRow() {
                 let object = presidents[indexPath.row]
                 let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
+                if let oldController = detailViewController {
+                    controller.languageString = oldController.languageString
+                }
                 controller.detailItem = object
                 controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
                 controller.navigationItem.leftItemsSupplementBackButton = true
+                detailViewController = controller
             }
         }
     }
