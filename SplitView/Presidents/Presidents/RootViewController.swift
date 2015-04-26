6c61dafe068c69bd20d4ec8bde3784bfd322accc
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-26 11:52:47 +0200

    Overriden the TraitsCollection inside the RootViewController

--- SplitView/Presidents/Presidents/RootViewController.swift
+++ SplitView/Presidents/Presidents/RootViewController.swift
@@ -9,5 +9,15 @@
 import UIKit
 
 class RootViewController: UINavigationController {
-
+    override func traitCollectionDidChange(previousTraitCollection: UITraitCollection?) {
+        let splitVC = viewControllers[0] as! UIViewController
+        let newTraits = traitCollection
+        if newTraits.horizontalSizeClass == .Compact
+            && newTraits.verticalSizeClass == .Compact {
+                let childTraits = UITraitCollection(horizontalSizeClass: .Regular)
+                setOverrideTraitCollection(childTraits, forChildViewController: splitVC)
+        } else {
+            setOverrideTraitCollection(nil, forChildViewController: splitVC)
+        }
+    }
 }
