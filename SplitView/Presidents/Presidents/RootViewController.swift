0a6b56c579715bf6b88146560c020affa5a047e4
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-26 11:49:12 +0200

    Adding RootViewController

---
+++ SplitView/Presidents/Presidents/RootViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RootViewController.swift
+//  Presidents
+//
+//  Created by Domenico on 26.04.15.
+//  Copyright (c) 2015 Domenico Solazzo. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UINavigationController {
+
+}
