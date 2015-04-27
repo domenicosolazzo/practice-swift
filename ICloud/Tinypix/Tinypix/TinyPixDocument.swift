af480f78e81665a2c0e9819029c974e9e4940b27
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-27 07:55:03 +0200

    Added TinyPixDocument (UIDocument)

---
+++ ICloud/Tinypix/Tinypix/TinyPixDocument.swift
@@ -0,0 +1,13 @@
+//
+//  TinyPixDocument.swift
+//  Tinypix
+//
+//  Created by Domenico on 27.04.15.
+//  Copyright (c) 2015 Domenico Solazzo. All rights reserved.
+//
+
+import UIKit
+
+class TinyPixDocument: UIDocument {
+   
+}
