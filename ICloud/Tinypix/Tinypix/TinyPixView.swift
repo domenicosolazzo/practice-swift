3738f4297c0372d7a3fc8667c78de8b63cec1104
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-28 20:24:47 +0200

    Methods for drawing the grid

--- ICloud/Tinypix/Tinypix/TinyPixView.swift
+++ ICloud/Tinypix/Tinypix/TinyPixView.swift
@@ -44,4 +44,45 @@ class TinyPixView: UIView {
         gridRect = CGRectMake((size.width - space)/2, (size.height - space)/2,
             space, space)
     }
+    
+    //- MARK: Draw
+    
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect)
+    {
+        if (document != nil) {
+            let size = bounds.size
+            if !CGSizeEqualToSize(size, lastSize) {
+                lastSize = size
+                calculateGridForSize(size)
+            }
+            
+            for var row = 0; row < 8; row++ {
+                for var column = 0; column < 8; column++ {
+                    drawBlockAt(row: row, column: column)
+                }
+            }
+        }
+    }
+    
+    private func drawBlockAt(#row: Int, column: Int) {
+        let startX = gridRect.origin.x + gap
+            + (blockSize.width + gap) * (7 - CGFloat(column)) + 1
+        let startY = gridRect.origin.y + gap
+            + (blockSize.height + gap) * CGFloat(row) + 1
+        
+        let blockFrame = CGRectMake(startX, startY,
+            blockSize.width, blockSize.height)
+        let color = document.stateAt(row: row, column: column)
+            ? UIColor.blackColor() : UIColor.whiteColor()
+        
+        color.setFill()
+        tintColor.setStroke()
+        let path = UIBezierPath(rect:blockFrame)
+        path.fill()
+        path.stroke()
+    }
+    
+    
 }
