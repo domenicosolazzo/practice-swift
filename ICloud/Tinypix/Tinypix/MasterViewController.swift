313cde9ecee7f80bec2d6900f7deac5212d40cc8
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-28 19:07:46 +0200

    Added IBOutlet, chosenDocument and file names

--- ICloud/Tinypix/Tinypix/MasterViewController.swift
+++ ICloud/Tinypix/Tinypix/MasterViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class MasterViewController: UITableViewController {
-
+    @IBOutlet var colorControl: UISegmentedControl!
+    private var documentFileNames: [String] = []
+    private var chosenDocument: TinyPixDocument?
     
 }
 
