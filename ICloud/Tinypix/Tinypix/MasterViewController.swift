c3dfef3e4469459686a91c52dd0be8f4c303bf72
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-28 19:12:26 +0200

    Helper methods

--- ICloud/Tinypix/Tinypix/MasterViewController.swift
+++ ICloud/Tinypix/Tinypix/MasterViewController.swift
@@ -13,5 +13,44 @@ class MasterViewController: UITableViewController {
     private var documentFileNames: [String] = []
     private var chosenDocument: TinyPixDocument?
     
+    //- MARK: Helper methods
+    private func urlForFileName(fileName: NSString) -> NSURL {
+        let fm = NSFileManager.defaultManager()
+        let urls = fm.URLsForDirectory(NSSearchPathDirectory.DocumentDirectory,
+            inDomains: NSSearchPathDomainMask.UserDomainMask) as! [NSURL]
+        let directoryURL = urls[0]
+        let fileURL = directoryURL.URLByAppendingPathComponent(fileName as String)
+        return fileURL
+    }
+    
+    private func reloadFiles() {
+        let paths = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory,
+            NSSearchPathDomainMask.UserDomainMask, true) as! [String]
+        let path = paths[0]
+        let fm = NSFileManager.defaultManager()
+        
+        var error:NSError? = nil;
+        let files = fm.contentsOfDirectoryAtPath(path, error: &error) as? [String]
+        if files != nil {
+            let sortedFileNames = sorted(files!) { fileName1, fileName2 in
+                let file1Path = path.stringByAppendingPathComponent(fileName1)
+                let file2Path = path.stringByAppendingPathComponent(fileName2)
+                let attr1 = fm.attributesOfItemAtPath(file1Path, error: nil)
+                let attr2 = fm.attributesOfItemAtPath(file2Path, error: nil)
+                let file1Date = attr1![NSFileCreationDate] as! NSDate
+                let file2Date = attr2![NSFileCreationDate] as! NSDate
+                let result = file1Date.compare(file2Date)
+                return result == NSComparisonResult.OrderedAscending
+            }
+            
+            documentFileNames = sortedFileNames
+            tableView.reloadData()
+        } else {
+            println("Error listing files in directory \(path): \(error)")
+        }
+    }
+    
+    //- MARK: UITableView
+    
 }
 
