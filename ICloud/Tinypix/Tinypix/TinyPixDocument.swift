e479f86483f7e9feecc56dfc4a85edf3a8f9c799
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-28 18:38:17 +0200

    Overriding init

--- ICloud/Tinypix/Tinypix/TinyPixDocument.swift
+++ ICloud/Tinypix/Tinypix/TinyPixDocument.swift
@@ -11,4 +11,9 @@ import UIKit
 class TinyPixDocument: UIDocument {
     // 8x8 bitmap data
     private var bitmap: [UInt8] = []
+    
+    override init(fileURL url: NSURL) {
+        bitmap = [0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80]
+        super.init(fileURL: url)
+    }
 }
