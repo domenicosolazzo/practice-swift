9ec00f5f227dd222345fcd460f34d749ea1e6ea7
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-28 20:04:37 +0200

    Added TinyPixView

---
+++ ICloud/Tinypix/Tinypix/TinyPixView.swift
@@ -0,0 +1,13 @@
+//
+//  TinyPixView.swift
+//  Tinypix
+//
+//  Created by Domenico on 28/04/15.
+//  Copyright (c) 2015 Domenico Solazzo. All rights reserved.
+//
+
+import UIKit
+
+class TinyPixView: UIView {
+
+}
