19e482be8e6489be2980eeec8d7819b8c912b8f5
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-29 20:53:10 +0200

    New project:SlowWorker

---
+++ BackgroundProcessing/SlowWorker/SlowWorker/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  SlowWorker
+//
+//  Created by Domenico on 29/04/15.
+//  Copyright (c) 2015 Domenico. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var resultsTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
