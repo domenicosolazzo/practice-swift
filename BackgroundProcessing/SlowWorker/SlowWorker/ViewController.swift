c13d950613462caa19cda74a7838b376aceee37a
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-29 20:54:11 +0200

    Simulating a long consuming process

--- BackgroundProcessing/SlowWorker/SlowWorker/ViewController.swift
+++ BackgroundProcessing/SlowWorker/SlowWorker/ViewController.swift
@@ -23,6 +23,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func fetchSomethingFromServer() -> String {
+        NSThread.sleepForTimeInterval(1)
+        return "Hi there"
+    }
+    
+    func processData(data: String) -> String {
+        NSThread.sleepForTimeInterval(2)
+        return data.uppercaseString
+    }
+    
+    func calculateFirstResult(data: String) -> String {
+        NSThread.sleepForTimeInterval(3)
+        return "Number of chars: \(count(data))"
+    }
+    
+    func calculateSecondResult(data: String) -> String {
+        NSThread.sleepForTimeInterval(4)
+        return data.stringByReplacingOccurrencesOfString("E", withString: "e")
+    }
+    
+    @IBAction func doWork(sender: AnyObject) {
+        let startTime = NSDate()
+        self.resultsTextView.text = ""
+        let fetchedData = self.fetchSomethingFromServer()
+        let processedData = self.processData(fetchedData)
+        let firstResult = self.calculateFirstResult(processedData)
+        let secondResult = self.calculateSecondResult(processedData)
+        let resultsSummary =
+        "First: [\(firstResult)]\nSecond: [\(secondResult)]"
+        self.resultsTextView.text = resultsSummary
+        let endTime = NSDate()
+        println(
+            "Completed in \(endTime.timeIntervalSinceDate(startTime)) seconds")
+    }
 
 }
 
