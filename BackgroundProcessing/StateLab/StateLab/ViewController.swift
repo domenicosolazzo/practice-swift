92ccae7124cf289442ef1e731902b344aeda1c02
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-04-30 18:40:57 +0200

    Adding new label

--- BackgroundProcessing/StateLab/StateLab/ViewController.swift
+++ BackgroundProcessing/StateLab/StateLab/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var label:UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let bounds = view.bounds
+        let labelFrame:CGRect = CGRectMake(bounds.origin.x,
+            CGRectGetMidY(bounds) - 50, bounds.size.width, 100)
+        label = UILabel(frame:labelFrame)
+        label.font = UIFont(name:"Helvetica", size:70)
+        label.text = "Bazinga!"
+        label.textAlignment = NSTextAlignment.Center
+        label.backgroundColor = UIColor.clearColor()
+        view.addSubview(label)
     }
 
     override func didReceiveMemoryWarning() {
