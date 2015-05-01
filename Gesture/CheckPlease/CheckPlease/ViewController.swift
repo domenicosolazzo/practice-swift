33532d32534d3dc3c8c7f4a8c4df25df1fdd86e8
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-02 00:57:36 +0200

    Added image view

--- Gesture/CheckPlease/CheckPlease/ViewController.swift
+++ Gesture/CheckPlease/CheckPlease/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var imageView:UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
