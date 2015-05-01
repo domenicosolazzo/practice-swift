c9457c8f5764a4dc825a4dfcaae37219492d25dd
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-01 17:02:09 +0200

    Added StartScene

---
+++ SpriteKit/TextShooter/TextShooter/StartScene.swift
@@ -0,0 +1,44 @@
+//
+//  StartScene.swift
+//  TextShooter
+//
+//  Created by Domenico on 01/05/15.
+//  Copyright (c) 2015 Domenico. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class StartScene: SKScene {
+    override init(size: CGSize) {
+        super.init(size: size)
+        backgroundColor = SKColor.greenColor()
+        
+        let topLabel = SKLabelNode(fontNamed: "Courier")
+        topLabel.text = "TextShooter"
+        topLabel.fontColor = SKColor.blackColor()
+        topLabel.fontSize = 48
+        topLabel.position = CGPointMake(frame.size.width/2,
+            frame.size.height * 0.7)
+        addChild(topLabel)
+        
+        let bottomLabel = SKLabelNode(fontNamed: "Courier")
+        bottomLabel.text = "Touch anywhere to start"
+        bottomLabel.fontColor = SKColor.blackColor()
+        bottomLabel.fontSize = 20
+        bottomLabel.position = CGPointMake(frame.size.width/2,
+            frame.size.height * 0.3)
+        addChild(bottomLabel)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func touchesBegan(touches: NSSet,
+        withEvent event: UIEvent) {
+            let transition = SKTransition.doorwayWithDuration(1.0)
+            let game = GameScene(size:frame.size)
+            view!.presentScene(game, transition: transition)
+    }
+}
