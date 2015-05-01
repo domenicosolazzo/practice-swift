6f76fa8551156c27aa1602ebb85e010dc47446ef
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-01 22:21:16 +0200

    Added the gesture recognizers for the tap gesture

--- Gesture/TapTaps/TapTaps/ViewController.swift
+++ Gesture/TapTaps/TapTaps/ViewController.swift
@@ -17,11 +17,58 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let singleTap = UITapGestureRecognizer(target: self, action: "singleTap")
+        singleTap.numberOfTapsRequired = 1
+        singleTap.numberOfTouchesRequired = 1
+        view.addGestureRecognizer(singleTap)
+        
+        let doubleTap = UITapGestureRecognizer(target: self, action: "doubleTap")
+        doubleTap.numberOfTapsRequired = 2
+        doubleTap.numberOfTouchesRequired = 1
+        view.addGestureRecognizer(doubleTap)
+        // Important line for avoiding singleTap twice
+        singleTap.requireGestureRecognizerToFail(doubleTap)
+        
+        let tripleTap = UITapGestureRecognizer(target: self, action: "tripleTap")
+        tripleTap.numberOfTapsRequired = 3
+        tripleTap.numberOfTouchesRequired = 1
+        view.addGestureRecognizer(tripleTap)
+        doubleTap.requireGestureRecognizerToFail(tripleTap)
+        
+        let quadrupleTap = UITapGestureRecognizer(target: self, action: "quadrupleTap")
+        quadrupleTap.numberOfTapsRequired = 4
+        quadrupleTap.numberOfTouchesRequired = 1
+        view.addGestureRecognizer(quadrupleTap)
+        tripleTap.requireGestureRecognizerToFail(quadrupleTap)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func singleTap() {
+        singleLabel.text = "Single Tap Detected"
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(2 * NSEC_PER_SEC)),
+            dispatch_get_main_queue(),
+            { self.singleLabel.text = "" })
+    }
+    
+    func doubleTap() {
+        doubleLabel.text = "Double Tap Detected"
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(2 * NSEC_PER_SEC)),
+            dispatch_get_main_queue(),
+            { self.doubleLabel.text = "" })
+    }
+    
+    func tripleTap() {
+        tripleLabel.text = "Triple Tap Detected"
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(2 * NSEC_PER_SEC)),
+            dispatch_get_main_queue(),
+            { self.tripleLabel.text = "" })
+    }
+    
+    func quadrupleTap() {
+        quadrupleLabel.text = "Quadruple Tap Detected"
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(2 * NSEC_PER_SEC)),
+            dispatch_get_main_queue(),
+            { self.quadrupleLabel.text = "" })
     }
 
 
