99960b18e6531867d68963537a57ac70c6156b78
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-01 22:19:11 +0200

    Added labels and IBOutlets

--- Gesture/TapTaps/TapTaps/ViewController.swift
+++ Gesture/TapTaps/TapTaps/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var singleLabel:UILabel!
+    @IBOutlet var doubleLabel:UILabel!
+    @IBOutlet var tripleLabel:UILabel!
+    @IBOutlet var quadrupleLabel:UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
