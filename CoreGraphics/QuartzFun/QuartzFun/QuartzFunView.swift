fac26293f78023afefc0ff081d3cc6e0207cfca0
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-01 10:16:35 +0200

    Extension for UIColor for getting a random color

--- CoreGraphics/QuartzFun/QuartzFun/QuartzFunView.swift
+++ CoreGraphics/QuartzFun/QuartzFun/QuartzFunView.swift
@@ -6,7 +6,17 @@
 //  Copyright (c) 2015 Domenico. All rights reserved.
 //
 
-import Cocoa
+import UIKit
+
+// Random color extension of UIColor
+extension UIColor {
+    class func randomColor() -> UIColor {
+        let red = CGFloat(Double((arc4random() % 256))/255)
+        let green = CGFloat(Double(arc4random() % 256)/255)
+        let blue = CGFloat(Double(arc4random() % 256)/255)
+        return UIColor(red: red, green: green, blue: blue, alpha:1.0)
+    }
+}
 
 class QuartzFunView: UIView {
 
