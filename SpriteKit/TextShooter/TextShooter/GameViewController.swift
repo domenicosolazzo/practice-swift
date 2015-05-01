ccf8a3bbf5892bc0fe670deca1b2cfdff2fd34dc
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-01 13:28:18 +0200

    Initializing the first scene in the viewcontroller

--- SpriteKit/TextShooter/TextShooter/GameViewController.swift
+++ SpriteKit/TextShooter/TextShooter/GameViewController.swift
@@ -30,20 +30,19 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        if let scene = GameScene.unarchiveFromFile("GameScene") as? GameScene {
-            // Configure the view.
-            let skView = self.view as! SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
+        let scene = GameScene(size: view.frame.size, levelNumber: 1)
+        // Configure the view.
+        let skView = self.view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
             
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
-            skView.ignoresSiblingOrder = true
+        /* Sprite Kit applies additional optimizations to improve rendering performance */
+        skView.ignoresSiblingOrder = true
             
-            /* Set the scale mode to scale to fit the window */
-            scene.scaleMode = .AspectFill
+        /* Set the scale mode to scale to fit the window */
+        scene.scaleMode = .AspectFill
             
-            skView.presentScene(scene)
-        }
+        skView.presentScene(scene)
     }
 
     override func shouldAutorotate() -> Bool {
