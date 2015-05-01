bc04cd1062972bdf4675ee82aa2576e73abef15a
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-01 21:11:25 +0200

    Getting information from all the touch events

--- TouchExplorer/TouchExplorer/ViewController.swift
+++ TouchExplorer/TouchExplorer/ViewController.swift
@@ -18,9 +18,35 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func updateLabelsFromTouches(touches:Set<NSObject>!) {
+        let touch = touches.first as! UITouch
+        let numTaps = touch.tapCount
+        let tapsMessage = "\(numTaps) taps detected"
+        tapsLabel.text = tapsMessage
+        
+        let numTouches = touches.count
+        let touchMsg = "\(numTouches) touches detected"
+        touchesLabel.text = touchMsg
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        messageLabel.text = "Touches Began"
+        updateLabelsFromTouches(event.allTouches())
+    }
+    
+    override func touchesCancelled(touches: Set<NSObject>, withEvent event: UIEvent) {
+        messageLabel.text = "Touches Cancelled"
+        updateLabelsFromTouches(event.allTouches())
+    }
+    
+    override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
+        messageLabel.text = "Touches Ended"
+        updateLabelsFromTouches(event.allTouches())
+    }
+    
+    override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
+        messageLabel.text = "Drag Detected"
+        updateLabelsFromTouches(event.allTouches())
     }
 
 
