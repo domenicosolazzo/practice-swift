32d269a33cb19574a3a04d869125acc3d4312b0c
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-01 14:02:41 +0200

    Adding wobbling actions for the player node

--- SpriteKit/TextShooter/TextShooter/PlayerNode.swift
+++ SpriteKit/TextShooter/TextShooter/PlayerNode.swift
@@ -33,11 +33,25 @@ class PlayerNode: SKNode {
     
     func moveToward(location: CGPoint) {
         removeActionForKey("movement")
+        removeActionForKey("wobbling")
+        
         let distance = pointDistance(position, location)
         let screenWidth = UIScreen.mainScreen().bounds.size.width
         let duration = NSTimeInterval(2 * distance/screenWidth)
         
         runAction(SKAction.moveTo(location, duration: duration),
             withKey:"movement")
+        
+        // Wobble actions
+        let wobbleTime = 0.3
+        let halfWobbleTime = wobbleTime/2
+        let wobbling = SKAction.sequence([
+            SKAction.scaleXTo(0.2, duration: halfWobbleTime),
+            SKAction.scaleXTo(1.0, duration: halfWobbleTime)
+            ])
+        let wobbleCount = Int(duration/wobbleTime)
+        
+        runAction(SKAction.repeatAction(wobbling, count: wobbleCount),
+            withKey:"wobbling")
     }
 }
