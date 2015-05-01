bad09558a2690dfbc83088bfa207bc093d738b16
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-01 16:17:14 +0200

    Init physics on the EnemyNode

--- SpriteKit/TextShooter/TextShooter/EnemyNode.swift
+++ SpriteKit/TextShooter/TextShooter/EnemyNode.swift
@@ -14,6 +14,7 @@ class EnemyNode: SKNode {
         super.init()
         name = "Enemy \(self)"
         initNodeGraph()
+        initPhysicsBody()
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -40,5 +41,16 @@ class EnemyNode: SKNode {
         bottomRow.text = "x x"
         bottomRow.position = CGPointMake(0, -15)
         addChild(bottomRow)
+    }
+    
+    private func initPhysicsBody() {
+        let body = SKPhysicsBody(rectangleOfSize: CGSizeMake(40, 40))
+        body.affectedByGravity = false
+        body.categoryBitMask = EnemyCategory
+        body.contactTestBitMask = PlayerCategory | EnemyCategory
+        body.mass = 0.2
+        body.angularDamping = 0
+        body.linearDamping = 0
+        physicsBody = body
     }
 }
