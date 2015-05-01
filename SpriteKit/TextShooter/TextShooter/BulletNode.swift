486843cba7e2614019477ff23e8ae6f5b1fd7dad
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-01 14:30:35 +0200

    Adding BulletNode

---
+++ SpriteKit/TextShooter/TextShooter/BulletNode.swift
@@ -0,0 +1,14 @@
+//
+//  BulletNode.swift
+//  TextShooter
+//
+//  Created by Domenico on 01/05/15.
+//  Copyright (c) 2015 Domenico. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class BulletNode: SKNode {
+   
+}
