e1774ee0ceb30f8f1d6dd6619c60293bdd349f7a
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-01 21:39:23 +0200

    Added label

--- Gesture/Swipes/Swipes/ViewController.swift
+++ Gesture/Swipes/Swipes/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label:UILabel!
+    // First touch position
+    private var gestureStartPoint:CGPoint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
