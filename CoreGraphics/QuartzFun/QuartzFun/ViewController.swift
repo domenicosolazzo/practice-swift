54cb8f5a04d483716d9a0bf27842c19fb164523f
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-01 11:13:29 +0200

    Connecting the SegmentedControl

--- CoreGraphics/QuartzFun/QuartzFun/ViewController.swift
+++ CoreGraphics/QuartzFun/QuartzFun/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var colorControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func changeColor(sender: UISegmentedControl) {
+    }
 
 }
 
