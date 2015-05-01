282469782e04069a4cf17ecd704c2b46afa29957
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-01 13:26:08 +0200

    Adding initialization method in GameScene

--- SpriteKit/TextShooter/TextShooter/GameScene.swift
+++ SpriteKit/TextShooter/TextShooter/GameScene.swift
@@ -9,9 +9,63 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    private var levelNumber: UInt
+    private var playerLives: Int
+    private var finished = false
+    
+    class func scene(size:CGSize, levelNumber:UInt) -> GameScene {
+        return GameScene(size: size, levelNumber: levelNumber)
+    }
+    
+    override convenience init(size:CGSize) {
+        self.init(size: size, levelNumber: 1)
+    }
+    
+    init(size:CGSize, levelNumber:UInt) {
+        self.levelNumber = levelNumber
+        self.playerLives = 5
+        super.init(size: size)
+        
+        backgroundColor = SKColor.whiteColor()
+        
+        let lives = SKLabelNode(fontNamed: "Courier")
+        lives.fontSize = 16
+        lives.fontColor = SKColor.blackColor()
+        lives.name = "LivesLabel"
+        lives.text = "Lives: \(playerLives)"
+        lives.verticalAlignmentMode = .Top
+        lives.horizontalAlignmentMode = .Right
+        lives.position = CGPointMake(frame.size.width,
+            frame.size.height)
+        addChild(lives)
+        
+        let level = SKLabelNode(fontNamed: "Courier")
+        level.fontSize = 16
+        level.fontColor = SKColor.blackColor()
+        level.name = "LevelLabel"
+        level.text = "Level \(levelNumber)"
+        level.verticalAlignmentMode = .Top
+        level.horizontalAlignmentMode = .Left
+        level.position = CGPointMake(0, frame.height)
+        addChild(level)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        levelNumber = UInt(aDecoder.decodeIntegerForKey("level"))
+        playerLives = aDecoder.decodeIntegerForKey("playerLives")
+        super.init(coder: aDecoder)
+    }
+    
+    override func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeInteger(Int(levelNumber), forKey: "level")
+        aCoder.encodeInteger(playerLives, forKey: "playerLives")
+    }
+    
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
         /* Called when a touch begins */
-      
+        for touch: AnyObject in touches {
+            let location = touch.locationInNode(self)
+        }
     }
    
     override func update(currentTime: CFTimeInterval) {
