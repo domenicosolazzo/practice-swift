2f4c77f1d09ad50d9cf0326b77b41e74b6613100
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-01 21:06:17 +0200

    Added labels and their IBOutlets

--- TouchExplorer/TouchExplorer/ViewController.swift
+++ TouchExplorer/TouchExplorer/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var messageLabel:UILabel!
+    @IBOutlet var tapsLabel:UILabel!
+    @IBOutlet var touchesLabel:UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
