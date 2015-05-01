ad472ddb943b6e47420a332e15ecb67239fa0ad5
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-01 15:01:52 +0200

    Initialization method for the BulletNode

--- SpriteKit/TextShooter/TextShooter/BulletNode.swift
+++ SpriteKit/TextShooter/TextShooter/BulletNode.swift
@@ -10,5 +10,39 @@ import UIKit
 import SpriteKit
 
 class BulletNode: SKNode {
-   
+    var thrust: CGVector = CGVectorMake(0, 0)
+    
+    override init() {
+        super.init()
+        
+        let dot = SKLabelNode(fontNamed: "Courier")
+        dot.fontColor = SKColor.blackColor()
+        dot.fontSize = 40
+        dot.text = "."
+        addChild(dot)
+        
+        let body = SKPhysicsBody(circleOfRadius: 1)
+        body.dynamic = true
+        body.categoryBitMask = PlayerMissileCategory
+        body.contactTestBitMask = EnemyCategory
+        body.collisionBitMask = EnemyCategory
+        body.mass = 0.01
+        
+        physicsBody = body
+        name = "Bullet \(self)"
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        let dx = aDecoder.decodeFloatForKey("thrustX")
+        let dy = aDecoder.decodeFloatForKey("thrustY")
+        thrust = CGVectorMake(CGFloat(dx), CGFloat(dy))
+    }
+    
+    override func encodeWithCoder(aCoder: NSCoder) {
+        super.encodeWithCoder(aCoder)
+        aCoder.encodeFloat(Float(thrust.dx), forKey: "thrustX")
+        aCoder.encodeFloat(Float(thrust.dy), forKey: "thrustY")
+    }
+    
 }
