c109d27e6ce6329a86518602425631e76e020e27
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-01 10:14:19 +0200

    New project for CoreGraphics: QuartzFun

---
+++ CoreGraphics/QuartzFun/QuartzFun/QuartzFunView.swift
@@ -0,0 +1,13 @@
+//
+//  QuartzFunView.swift
+//  QuartzFun
+//
+//  Created by Domenico on 01/05/15.
+//  Copyright (c) 2015 Domenico. All rights reserved.
+//
+
+import Cocoa
+
+class QuartzFunView: UIView {
+
+}
