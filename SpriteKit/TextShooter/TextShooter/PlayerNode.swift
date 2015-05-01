7128208eb187bfa095d55853fefa0bef97cd43cf
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-01 13:45:10 +0200

    PlayerNode (SKNode)

---
+++ SpriteKit/TextShooter/TextShooter/PlayerNode.swift
@@ -0,0 +1,33 @@
+//
+//  PlayerNode.swift
+//  TextShooter
+//
+//  Created by Domenico on 01/05/15.
+//  Copyright (c) 2015 Domenico. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import Foundation
+
+class PlayerNode: SKNode {
+    override init() {
+        super.init()
+        name = "Player \(self)"
+        initNodeGraph()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    private func initNodeGraph() {
+        let label = SKLabelNode(fontNamed: "Courier")
+        label.fontColor = SKColor.darkGrayColor()
+        label.fontSize = 40
+        label.text = "v"
+        label.zRotation = CGFloat(M_PI)
+        label.name = "label"
+        self.addChild(label)
+    }
+}
