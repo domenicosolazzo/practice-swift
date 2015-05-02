4050b5df560ec369c7af0f83bd88ec0f2d7aef41
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-02 19:26:55 +0200

    Initialiazing CoreMotion and sending the results to a queue.

--- Gyroscope/MotionMonitor/MotionMonitor/ViewController.swift
+++ Gyroscope/MotionMonitor/MotionMonitor/ViewController.swift
@@ -7,21 +7,55 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
     @IBOutlet var gyroscopeLabel:UILabel!
     @IBOutlet var accelerometerLabel:UILabel!
     @IBOutlet var attitudeLabel:UILabel!
+    
+    private let motionManager = CMMotionManager()
+    private let queue = NSOperationQueue()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if motionManager.deviceMotionAvailable {
+            motionManager.deviceMotionUpdateInterval = 0.1
+            motionManager.startDeviceMotionUpdatesToQueue(queue,
+                withHandler: {
+                    (motion:CMDeviceMotion!, error:NSError!) -> Void in
+                    
+                    let rotationRate = motion.rotationRate
+                    let gravity = motion.gravity
+                    let userAcc = motion.userAcceleration
+                    let attitude = motion.attitude
+                    
+                    let gyroscopeText =
+                    String(format:"Rotation Rate:\n-----------------\n" +
+                        "x: %+.2f\ny: %+.2f\nz: %+.2f\n",
+                        rotationRate.x, rotationRate.y, rotationRate.z)
+                    let acceleratorText =
+                    String(format:"Acceleration:\n---------------\n" +
+                        "Gravity x: %+.2f\t\tUser x: %+.2f\n" +
+                        "Gravity y: %+.2f\t\tUser y: %+.2f\n" +
+                        "Gravity z: %+.2f\t\tUser z: %+.2f\n",
+                        gravity.x, userAcc.x, gravity.y,
+                        userAcc.y, gravity.z,userAcc.z)
+                    let attitudeText =
+                    String(format:"Attitude:\n----------\n" +
+                        "Roll: %+.2f\nPitch: %+.2f\nYaw: %+.2f\n",
+                        attitude.roll, attitude.pitch, attitude.yaw)
+                    
+                    // You need to dispatch the results to the main queue
+                    dispatch_async(dispatch_get_main_queue(), {
+                        self.gyroscopeLabel.text = gyroscopeText
+                        self.accelerometerLabel.text = acceleratorText
+                        self.attitudeLabel.text = attitudeText
+                    })
+            })
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
