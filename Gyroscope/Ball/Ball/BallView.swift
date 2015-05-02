e27f2cb0163218862f771b5c26daba6aa50fd4a8
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-02 18:11:23 +0200

    Added a new UIView

---
+++ Gyroscope/Ball/Ball/BallView.swift
@@ -0,0 +1,13 @@
+//
+//  BallView.swift
+//  Ball
+//
+//  Created by Domenico on 02/05/15.
+//  Copyright (c) 2015 Domenico. All rights reserved.
+//
+
+import UIKit
+
+class BallView: UIView {
+
+}
