01d9c3e3a9a62d5b4a8c502aca4379d651512c00
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-02 19:50:09 +0200

    Different implementation of the MotionMonitor

--- Gyroscope/MotionMonitor2/MotionMonitor/ViewController.swift
+++ Gyroscope/MotionMonitor2/MotionMonitor/ViewController.swift
@@ -15,44 +15,57 @@ class ViewController: UIViewController {
     @IBOutlet var attitudeLabel:UILabel!
     
     private let motionManager = CMMotionManager()
-    private let queue = NSOperationQueue()
+    private var updateTimer:NSTimer!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
         if motionManager.deviceMotionAvailable {
             motionManager.deviceMotionUpdateInterval = 0.1
-            motionManager.startDeviceMotionUpdatesToQueue(queue,
-                withHandler: {
-                    (motion:CMDeviceMotion!, error:NSError!) -> Void in
-                    
-                    let rotationRate = motion.rotationRate
-                    let gravity = motion.gravity
-                    let userAcc = motion.userAcceleration
-                    let attitude = motion.attitude
-                    
-                    let gyroscopeText =
-                    String(format:"Rotation Rate:\n-----------------\n" +
-                        "x: %+.2f\ny: %+.2f\nz: %+.2f\n",
-                        rotationRate.x, rotationRate.y, rotationRate.z)
-                    let acceleratorText =
-                    String(format:"Acceleration:\n---------------\n" +
-                        "Gravity x: %+.2f\t\tUser x: %+.2f\n" +
-                        "Gravity y: %+.2f\t\tUser y: %+.2f\n" +
-                        "Gravity z: %+.2f\t\tUser z: %+.2f\n",
-                        gravity.x, userAcc.x, gravity.y,
-                        userAcc.y, gravity.z,userAcc.z)
-                    let attitudeText =
-                    String(format:"Attitude:\n----------\n" +
-                        "Roll: %+.2f\nPitch: %+.2f\nYaw: %+.2f\n",
-                        attitude.roll, attitude.pitch, attitude.yaw)
-                    
-                    // You need to dispatch the results to the main queue
-                    dispatch_async(dispatch_get_main_queue(), {
-                        self.gyroscopeLabel.text = gyroscopeText
-                        self.accelerometerLabel.text = acceleratorText
-                        self.attitudeLabel.text = attitudeText
-                    })
+            motionManager.startDeviceMotionUpdates()
+            updateTimer =
+                NSTimer.scheduledTimerWithTimeInterval(0.1, target: self,
+                    selector: "updateDisplay", userInfo: nil, repeats: true)
+        }
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        if motionManager.deviceMotionAvailable {
+            motionManager.stopDeviceMotionUpdates()
+            updateTimer.invalidate()
+            updateTimer = nil
+        }
+    }
+    
+    func updateDisplay() {
+        let motion = motionManager.deviceMotion
+        if (motion != nil) {
+            let rotationRate = motion.rotationRate
+            let gravity = motion.gravity
+            let userAcc = motion.userAcceleration
+            let attitude = motion.attitude
+            
+            let gyroscopeText =
+            String(format:"Rotation Rate:\n-----------------\n" +
+                "x: %+.2f\ny: %+.2f\nz: %+.2f\n",
+                rotationRate.x, rotationRate.y, rotationRate.z)
+            let acceleratorText =
+            String(format:"Acceleration:\n---------------\n" +
+                "Gravity x: %+.2f\t\tUser x: %+.2f\n" +
+                "Gravity y: %+.2f\t\tUser y: %+.2f\n" +
+                "Gravity z: %+.2f\t\tUser z: %+.2f\n",
+                gravity.x, userAcc.x, gravity.y,
+                userAcc.y, gravity.z,userAcc.z)
+            println(acceleratorText)
+            let attitudeText =
+            String(format:"Attitude:\n----------\n" +
+                "Roll: %+.2f\nPitch: %+.2f\nYaw: %+.2f\n",
+                attitude.roll, attitude.pitch, attitude.yaw)
+            
+            dispatch_async(dispatch_get_main_queue(), {
+                self.gyroscopeLabel.text = gyroscopeText
+                self.accelerometerLabel.text = acceleratorText
+                self.attitudeLabel.text = attitudeText
             })
         }
     }
