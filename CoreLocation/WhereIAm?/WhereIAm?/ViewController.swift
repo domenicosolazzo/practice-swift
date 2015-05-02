494107625a2333d2312bba366c9719729a8b6645
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-02 15:18:00 +0200

    Added private variables

--- CoreLocation/WhereIAm?/WhereIAm?/ViewController.swift
+++ CoreLocation/WhereIAm?/WhereIAm?/ViewController.swift
@@ -11,6 +11,17 @@ import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    private let locationManager = CLLocationManager()
+    private var previousPoint:CLLocation?
+    private var totalMovementDistance:CLLocationDistance = 0
+    
+    @IBOutlet var latitudeLabel:UILabel!
+    @IBOutlet var longitudeLabel:UILabel!
+    @IBOutlet var horizontalAccuracyLabel:UILabel!
+    @IBOutlet var altitudeLabel:UILabel!
+    @IBOutlet var verticalAccuracyLabel:UILabel!
+    @IBOutlet var distanceTraveledLabel:UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
