c7b5a273e0db0dd0c995b2c34864ee60bd6ca77e
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-03 14:20:59 +0200

    Initializing the labels with localized content

--- Localization/Locale/Locale/ViewController.swift
+++ Localization/Locale/Locale/ViewController.swift
@@ -16,6 +16,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // User locale
+        let locale = NSLocale.currentLocale()
+        // User preferred language
+        let currentLangID = (NSLocale.preferredLanguages() as! [String])[0]
+        let displayLang = locale.displayNameForKey(NSLocaleLanguageCode, value: currentLangID)
+        let capitalized = displayLang?.capitalizedStringWithLocale(locale)
+        localeLabel.text = capitalized
+        
+        labels[0].text = NSLocalizedString("LABEL_ONE", comment: "The number 1")
+        labels[1].text = NSLocalizedString("LABEL_TWO", comment: "The number 2")
+        labels[2].text = NSLocalizedString("LABEL_THREE", comment: "The number 3")
+        labels[3].text = NSLocalizedString("LABEL_FOUR", comment: "The number 4")
+        labels[4].text = NSLocalizedString("LABEL_FIVE", comment: "The number 5")
+        
+        let flagFile = NSLocalizedString("FLAG_FILE", comment: "Name of the flag")
+        flagImageView.image = UIImage(named: flagFile)
     }
 
     override func didReceiveMemoryWarning() {
