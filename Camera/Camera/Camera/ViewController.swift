f30610754998406e9d62863ba0f7c0c2c504140f
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-03 11:59:06 +0200

    Conforming to both UIImagePickerControllerDelegate and UINavigationControllerDelegate; Adding private variables and IBOutlets

---
+++ Camera/Camera/Camera/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Camera
+//
+//  Created by Domenico on 03/05/15.
+//  Copyright (c) 2015 Domenico. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate,
+            UINavigationControllerDelegate {
+    @IBOutlet var imageView:UIImageView!
+    @IBOutlet var takePictureButton:UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
