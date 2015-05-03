dfbb090a90f3560825bd3dce39aace93f8a9f886
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-03 13:51:23 +0200

    Adding IBOutlets

--- Localization/Locale/Locale/ViewController.swift
+++ Localization/Locale/Locale/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var localeLabel : UILabel!
+    @IBOutlet var flagImageView : UIImageView!
+    @IBOutlet var labels : [UILabel]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
