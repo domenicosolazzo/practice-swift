1e1e04a9f257c3d078439fe3be2bb93638d8c8ae
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-03 12:48:34 +0200

    Initialization methods; Checking if an image or a video was taken; Constraints for the Video player

--- Camera/Camera/Camera/ViewController.swift
+++ Camera/Camera/Camera/ViewController.swift
@@ -25,17 +25,50 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if !UIImagePickerController.isSourceTypeAvailable(
+            UIImagePickerControllerSourceType.Camera) {
+                takePictureButton.hidden = true
+        }
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        updateDisplay()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
     
     @IBAction func shootPictureOrVideo(sender: UIButton) {
     }
     
     @IBAction func selectExistingPictureOrVideo(sender: UIButton) {
+    }
+    
+    func updateDisplay() {
+        if let mediaType = lastChosenMediaType {
+            if mediaType == kUTTypeImage as NSString {
+                imageView.image = image!
+                imageView.hidden = false
+                if moviePlayerController != nil {
+                    moviePlayerController!.view.hidden = true
+                }
+            } else if mediaType == kUTTypeMovie as NSString {
+                if moviePlayerController == nil {
+                    moviePlayerController =
+                        MPMoviePlayerController(contentURL: movieURL)
+                    let movieView = moviePlayerController!.view
+                    movieView.frame = imageView.frame
+                    movieView.clipsToBounds = true
+                    view.addSubview(movieView)
+                    setMoviePlayerLayoutConstraints()
+                } else {
+                    moviePlayerController!.contentURL = movieURL
+                }
+                imageView.hidden = true
+                moviePlayerController!.view.hidden = false
+                moviePlayerController!.play()
+            }
+        }
     }
 
 
