3ccb2bea84a58d27fede4c896081bbe5d319a409
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-05 00:21:27 +0200

    Intialization code; added images

--- Views/Sliders/SliderExample/SliderExample/ViewController.swift
+++ Views/Sliders/SliderExample/SliderExample/ViewController.swift
@@ -10,10 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Slider
+    var slider:UISlider?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        slider = UISlider(frame: CGRect(x: 0, y: 0, width: 200, height: 23))
+        slider!.center = self.view.center
+        // Max value
+        slider?.maximumValue = 100
+        // Min value
+        slider?.minimumValue = 0
         
+        // Current Value
+        slider?.value = slider!.maximumValue / 2.0
     }
 
 }
