96191c5a7ad91e8e36a37b3123014e6fe1f6842b
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-05 00:16:52 +0200

    New project: SliderExample

---
+++ Views/Sliders/SliderExample/SliderExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SliderExample
+//
+//  Created by Domenico Solazzo on 05/05/15.
+//  LICENSE MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+}
+
