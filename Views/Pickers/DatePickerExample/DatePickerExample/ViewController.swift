df8c1b367464d87dbd5e27c3f383ab13824d3c79
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-04 23:45:36 +0200

    Adding a date picker; initializing code

--- Views/Pickers/DatePickerExample/DatePickerExample/ViewController.swift
+++ Views/Pickers/DatePickerExample/DatePickerExample/ViewController.swift
@@ -9,9 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // Date Picker
+    var datePicker: UIDatePicker?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        datePicker = UIDatePicker()
+        datePicker.center = self.view.center
+        self.view.addSubview(datePicker)
     }
 }
 
