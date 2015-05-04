f4da9c56a398a9e1e3369e2a806c7675d41289ac
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-04 20:32:12 +0200

    Implemented mthe methods for conforming to the UIPickerDataSource

--- Views/Pickers/SimplePicker/SimplePicker/ViewController.swift
+++ Views/Pickers/SimplePicker/SimplePicker/ViewController.swift
@@ -17,10 +17,28 @@ class ViewController: UIViewController, UIPickerViewDataSource {
         super.viewDidLoad()
         
         uiPicker = UIPickerView()
+        // Setting the data source
+        uiPicker?.dataSource = self
         // Setting the picker in the center of the screen
         uiPicker?.center = view.center
         // Adding a subview
         self.view.addSubview(uiPicker!)
+    }
+    
+    // How many components you want the picker to render
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        if pickerView == uiPicker{
+            return 1
+        }
+        return 0
+    }
+    
+    // How many rows you want to render
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView == uiPicker{
+            return 10
+        }
+        return 0
     }
 }
 
