7fde97f22536cdb65789622160942735cc11707e
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-04 18:34:36 +0200

    Alert controller

--- Views/Alerts/AlertExample/AlertExample/ViewController.swift
+++ Views/Alerts/AlertExample/AlertExample/ViewController.swift
@@ -3,13 +3,16 @@
 //  AlertExample
 //
 //  Created by Domenico on 04/05/15.
-//  Copyright (c) 2015 Domenico. All rights reserved.
+//  License MIT
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    // Alert controller
+    var controller: UIAlertController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
