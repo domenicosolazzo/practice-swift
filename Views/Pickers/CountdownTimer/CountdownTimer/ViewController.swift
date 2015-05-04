9e7bae44932257ddef9cb5242e4b013ed84bbe9c
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-04 23:56:03 +0200

    New project: CountdownTimer

---
+++ Views/Pickers/CountdownTimer/CountdownTimer/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CountdownTimer
+//
+//  Created by Domenico Solazzo on 04/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
