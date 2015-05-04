1ed08963b5163012ed095c1d0bb23e17f80e8af3
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-04 20:13:48 +0200

    Initializationn code

--- Views/Pickers/SimplePicker/SimplePicker/ViewController.swift
+++ Views/Pickers/SimplePicker/SimplePicker/ViewController.swift
@@ -10,8 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // PickerView
+    var uiPicker: UIPickerView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        uiPicker = UIPickerView()
+        // Setting the picker in the center of the screen
+        uiPicker?.center = view.center
+        // Adding a subview
+        self.view.addSubview(uiPicker!)
     }
 }
 
