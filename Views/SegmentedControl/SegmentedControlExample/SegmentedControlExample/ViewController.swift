c487d73ad55f15be6b1770f81c61e0af5f771d4f
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-05 07:30:08 +0200

    Adding target to the SegmentedControl

--- Views/SegmentedControl/SegmentedControlExample/SegmentedControlExample/ViewController.swift
+++ Views/SegmentedControl/SegmentedControlExample/SegmentedControlExample/ViewController.swift
@@ -24,7 +24,22 @@ class ViewController: UIViewController {
         
         segmentedControl = UISegmentedControl(items: segments)
         segmentedControl.center = self.view.center
+        // Add a target
+        segmentedControl.addTarget(self, action: "segmentedControlValueChanged:", forControlEvents: UIControlEvents.ValueChanged)
+        
         self.view.addSubview(segmentedControl)
+        
+    }
+    
+    // Event
+    func segmentedControlValueChanged(segmentedCtrl:UISegmentedControl){
+        // Get the index
+        var index = segmentedCtrl.selectedSegmentIndex
+        
+        // Get the title by index
+        var title = segmentedCtrl.titleForSegmentAtIndex(index)
+        
+        println("Selected index \(index) with title \(title!)")
     }
 }
 
