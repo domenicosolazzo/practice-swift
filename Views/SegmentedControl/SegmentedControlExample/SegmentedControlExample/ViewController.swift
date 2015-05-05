7b6dd0f2caebc3389b0c83ec0c42d447dc4d0fc6
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-05 07:25:10 +0200

    Added SegmentedControl; Added initialization code

--- Views/SegmentedControl/SegmentedControlExample/SegmentedControlExample/ViewController.swift
+++ Views/SegmentedControl/SegmentedControlExample/SegmentedControlExample/ViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var segmentedControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let segments = [
+            "iPhone",
+            "iPad",
+            "iPod",
+            "iMac"
+        ]
+        
+        segmentedControl = UISegmentedControl(items: segments)
+        segmentedControl.center = self.view.center
+        self.view.addSubview(segmentedControl)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
