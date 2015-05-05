c7508298d8297a9cf4df6901a16df94b11a26548
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-05 19:37:23 +0200

    Refactoring

--- Views/NavigationBar/Image in NavigationBar/Image in NavigationBar/ViewController.swift
+++ Views/NavigationBar/Image in NavigationBar/Image in NavigationBar/ViewController.swift
@@ -12,6 +12,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        /* Create an Image View to replace the Title View */
+        let imageView = UIImageView(
+            frame: CGRect(x: 0, y: 0, width: 100, height: 40))
+        
+        imageView.contentMode = .ScaleAspectFit
+        
+        let image = UIImage(named:"Logo")
+        
+        imageView.image = image
+        
+        /* Set the Title View */
+        navigationItem.titleView = imageView
     }
 
 }
