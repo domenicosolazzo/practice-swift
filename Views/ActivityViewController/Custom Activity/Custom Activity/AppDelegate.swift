d3083748e6daed7802b4783bfe75bfc6e3c8c5de
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-05 18:37:04 +0200

    New project: Custom Activity

---
+++ Views/ActivityViewController/Custom Activity/Custom Activity/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Custom Activity
+//
+//  Created by Domenico Solazzo on 05/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
