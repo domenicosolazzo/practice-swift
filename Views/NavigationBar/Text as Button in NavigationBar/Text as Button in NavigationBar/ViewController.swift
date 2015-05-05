ec6c41963cc2903e61a78b2ef09452d4314d76b2
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-05 22:48:34 +0200

    Added an Add button

--- Views/NavigationBar/Text as Button in NavigationBar/Text as Button in NavigationBar/ViewController.swift
+++ Views/NavigationBar/Text as Button in NavigationBar/Text as Button in NavigationBar/ViewController.swift
@@ -10,8 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    func performAdd(sender: UIBarButtonItem){
+        println("Add method got called")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "Add",
+            style: .Plain,
+            target: self,
+            action: "performAdd:")
+        
     }
+
+    
 }
 
