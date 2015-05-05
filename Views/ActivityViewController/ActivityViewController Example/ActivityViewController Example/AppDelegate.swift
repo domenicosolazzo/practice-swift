3073e7a5eedc9242e069a928c534486fa2e3931d
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-05 17:49:08 +0200

    New project: ActActivityViewController

---
+++ Views/ActivityViewController/ActivityViewController Example/ActivityViewController Example/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  ActivityViewController Example
+//
+//  Created by Domenico Solazzo on 05/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
---
+++ Views/ActivityViewController/ActivityViewController Example/ActivityViewController Example/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ActivityViewController Example
+//
+//  Created by Domenico Solazzo on 05/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
