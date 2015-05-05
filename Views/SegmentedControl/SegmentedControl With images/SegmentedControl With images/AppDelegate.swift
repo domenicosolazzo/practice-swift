9041b68b2bbe0645da890580f59996272406da9e
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-05 07:36:35 +0200

    New project: SegmentedControl with Images

---
+++ Views/SegmentedControl/SegmentedControl With images/SegmentedControl With images/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  SegmentedControl With images
+//
+//  Created by Domenico Solazzo on 05/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
---
+++ Views/SegmentedControl/SegmentedControl With images/SegmentedControl With images/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SegmentedControl With images
+//
+//  Created by Domenico Solazzo on 05/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
+
