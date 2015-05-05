575ca218f62d0e411aa542d6b51a9a7006658e7c
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-05 07:41:37 +0200

    Added image; modAdded new NSArray

--- Views/SegmentedControl/SegmentedControl With images/SegmentedControl With images/ViewController.swift
+++ Views/SegmentedControl/SegmentedControl With images/SegmentedControl With images/ViewController.swift
@@ -9,10 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var segmentedControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let segments = NSArray(objects:
+            "Red",
+            UIImage(named: "blueDot")!,
+            "Green",
+            "Yellow"
+        )
+        
+        segmentedControl = UISegmentedControl(items: segments as [AnyObject])
+        segmentedControl.center = self.view.center
+        // Add a target
+        segmentedControl.addTarget(self, action: "segmentedControlValueChanged:", forControlEvents: UIControlEvents.ValueChanged)
+        
+        self.view.addSubview(segmentedControl)
+        
+    }
+    
+    // Event
+    func segmentedControlValueChanged(segmentedCtrl:UISegmentedControl){
+        // Get the index
+        var index = segmentedCtrl.selectedSegmentIndex
+        
+        // Get the title by index
+        var title = segmentedCtrl.titleForSegmentAtIndex(index)
+        if let title = segmentedControl.titleForSegmentAtIndex(index){
+            println("Selected index \(index) with title \(title)")
+        }
         
     }
 }
