a35b0ab823f3bafa00f7a113a244020c5f9e0d9e
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-05 19:24:13 +0200

    New project: Image in NavigationBar

---
+++ Views/NavigationBar/Image in NavigationBar/Image in NavigationBar/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Image in NavigationBar
+//
+//  Created by Domenico Solazzo on 05/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+}
+
---
+++ Views/NavigationBar/Image in NavigationBar/Image in NavigationBar/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Image in NavigationBar
+//
+//  Created by Domenico Solazzo on 05/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
