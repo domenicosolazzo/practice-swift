0386686bff87ccf241a71ea77a27d549d09117b8
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-05 18:09:51 +0200

    Added creation of the button; Added sharing handler

--- Views/ActivityViewController/ActivityViewController Example/ActivityViewController Example/ViewController.swift
+++ Views/ActivityViewController/ActivityViewController Example/ActivityViewController Example/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.createTextField()
+        self.createButton()
     }
     
     func createTextField(){
@@ -26,10 +28,51 @@ class ViewController: UIViewController, UITextFieldDelegate {
         self.view.addSubview(textField)
     }
     
+    func createButton(){
+        buttonShare = UIButton.buttonWithType(UIButtonType.System) as? UIButton
+        buttonShare.frame = CGRect(x: 20, y: 80, width: 280, height: 44)
+        buttonShare.setTitle("Share", forState: UIControlState.Normal)
+        buttonShare.addTarget(self,
+            action: "handleShare:",
+            forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(buttonShare)
+    }
+    
     //- MARK: UITextFieldDelegate
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
+    }
+    
+    //- MARK: UIButton
+    func handleShare(sender: UIButton){
+        if (textField.text.isEmpty){
+            var message = "Please enter a text and then click Share"
+            var alert = UIAlertController(title: nil,
+                message: message,
+                preferredStyle: UIAlertControllerStyle.Alert)
+            
+            alert.addAction(
+                UIAlertAction(title: "OK",
+                    style: UIAlertActionStyle.Default,
+                    handler: nil)
+            )
+            
+            presentViewController(alert, animated: true, completion: nil)
+            return
+        }
+        
+        /* it is VERY important to cast your strings to NSString
+        otherwise the controller cannot display 
+        the appropriate sharing options */
+        var activityController = UIActivityViewController(
+            activityItems: [textField.text as NSString],
+            applicationActivities: nil
+        )
+        presentViewController(activityController,
+            animated: true,
+            completion: nil
+        )
     }
 }
 
