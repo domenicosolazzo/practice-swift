abbd6f00ccb0ad22510eb1dc5b15e86bf325f9fa
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-05 19:59:46 +0200

    New project: Text as Button in the NavigationBar

---
+++ Views/NavigationBar/Text as Button in NavigationBar/Text as Button in NavigationBar/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Text as Button in NavigationBar
+//
+//  Created by Domenico Solazzo on 05/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
---
+++ Views/NavigationBar/Text as Button in NavigationBar/Text as Button in NavigationBar/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Text as Button in NavigationBar
+//
+//  Created by Domenico Solazzo on 05/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
