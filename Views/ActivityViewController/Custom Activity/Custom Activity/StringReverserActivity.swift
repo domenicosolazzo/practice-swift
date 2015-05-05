f30a198a11ffbfbfd64387f5a62c9b6966176c18
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-05 18:43:43 +0200

    Subclassing UIActivity

---
+++ Views/ActivityViewController/Custom Activity/Custom Activity/StringReverserActivity.swift
@@ -0,0 +1,14 @@
+//
+//  StringReverserActivity.swift
+//  Custom Activity
+//
+//  Created by Domenico Solazzo on 05/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class StringReverserActivity: UIActivity {
+
+    
+}
