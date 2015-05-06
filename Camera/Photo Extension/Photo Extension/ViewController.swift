fc7487d29e48c4e18e66318d386a956a4ccba9fd
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-06 22:37:10 +0200

    New project: Photo Extension

---
+++ Camera/Photo Extension/Photo Extension/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Photo Extension
+//
+//  Created by Domenico Solazzo on 06/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
