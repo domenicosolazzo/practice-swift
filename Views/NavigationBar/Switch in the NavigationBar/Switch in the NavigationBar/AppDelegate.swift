8bf7449d7e761174b5d99a67d27034ca92305857
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-06 07:48:49 +0200

    New project: Switch in the NavigationBar

---
+++ Views/NavigationBar/Switch in the NavigationBar/Switch in the NavigationBar/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Switch in the NavigationBar
+//
+//  Created by Domenico Solazzo on 06/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
