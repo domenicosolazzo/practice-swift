2cd7b74e61fbf7164873f9595f8cdb3b259003bb
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-06 07:57:42 +0200

    New project: SegmentedControl in the NavigationBar

---
+++ Views/NavigationBar/SegmentedControl as button in the NavigationBar/SegmentedControl as button in the NavigationBar/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SegmentedControl as button in the NavigationBar
+//
+//  Created by Domenico Solazzo on 06/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
