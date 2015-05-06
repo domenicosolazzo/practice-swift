3a34a589f85d112f9e56b88cbde1ad13e4714307
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-06 07:31:45 +0200

    New Project: Styled button in the NavigationBar

---
+++ Views/NavigationBar/Styled Button in the NavigationBar/Styled Button in the NavigationBarTests/Styled_Button_in_the_NavigationBarTests.swift
@@ -0,0 +1,36 @@
+//
+//  Styled_Button_in_the_NavigationBarTests.swift
+//  Styled Button in the NavigationBarTests
+//
+//  Created by Domenico on 06/05/15.
+//  Copyright (c) 2015 Domenico. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Styled_Button_in_the_NavigationBarTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
