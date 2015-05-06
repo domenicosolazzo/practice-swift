b39ca7565e42457344bae5ee9e75ad8954f648ef
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-06 07:35:45 +0200

    Adding right styled button in the navigation bar

--- Views/NavigationBar/Styled Button in the NavigationBar/Styled Button in the NavigationBar/ViewController.swift
+++ Views/NavigationBar/Styled Button in the NavigationBar/Styled Button in the NavigationBar/ViewController.swift
@@ -10,10 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    func performAdd(){
+        println("AAAS => Add As A Service :)")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: UIBarButtonSystemItem.Add,
+            target: self,
+            action: "performAdd:")
     }
 }
 
