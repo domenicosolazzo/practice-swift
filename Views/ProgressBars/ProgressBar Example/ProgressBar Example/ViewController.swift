151dced20077bb7420ede5e1615b3976dc1fe143
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-06 21:04:37 +0200

    Initialization code

--- Views/ProgressBars/ProgressBar Example/ProgressBar Example/ViewController.swift
+++ Views/ProgressBars/ProgressBar Example/ProgressBar Example/ViewController.swift
@@ -2,8 +2,8 @@
 //  ViewController.swift
 //  ProgressBar Example
 //
-//  Created by Domenico on 06/05/15.
-//  Copyright (c) 2015 Domenico. All rights reserved.
+//  Created by Domenico Solazzo on 06/05/15.
+//  License MIT
 //
 
 import UIKit
@@ -12,6 +12,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var progressBar = UIProgressView(progressViewStyle: UIProgressViewStyle.Bar)
+        progressBar.center = view.center
+        progressBar.progress = 1 / 2
+        progressBar.trackTintColor = UIColor.blueColor()
+        progressBar.tintColor = UIColor.redColor()
+        
+        self.view.addSubview(progressBar)
     }
 }
 
