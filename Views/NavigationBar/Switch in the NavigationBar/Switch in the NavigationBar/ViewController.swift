8ec43b86ef6bcf6b3b95135004964ad0b393d192
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-06 07:53:25 +0200

    Adding a switch as right button in the navigation bar

--- Views/NavigationBar/Switch in the NavigationBar/Switch in the NavigationBar/ViewController.swift
+++ Views/NavigationBar/Switch in the NavigationBar/Switch in the NavigationBar/ViewController.swift
@@ -10,8 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    func switchChangedValue(sender: UISwitch){
+        if sender.on{
+            println("Switching off")
+        }else{
+            println("Switching on")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var simpleSwitch = UISwitch()
+        simpleSwitch.on = true
+        
+        simpleSwitch.addTarget(self,
+            action: "switchChangedValue:",
+            forControlEvents: UIControlEvents.ValueChanged)
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(
+            customView: simpleSwitch)
+        
     }
 }
 
