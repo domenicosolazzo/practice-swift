62280b606ea6b183085470e30f8f0578cc020211
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-06 08:01:20 +0200

    Adding a segmented control in the navigation bar

--- Views/NavigationBar/SegmentedControl as button in the NavigationBar/SegmentedControl as button in the NavigationBar/ViewController.swift
+++ Views/NavigationBar/SegmentedControl as button in the NavigationBar/SegmentedControl as button in the NavigationBar/ViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let items = ["Up", "Down"]
+    
+    func segmentedControlValueChanged(sender: UISegmentedControl){
+        if (sender.selectedSegmentIndex < items.count){
+            println(items[sender.selectedSegmentIndex])
+        }else{
+            println("Unknown button")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        var segmentedControl = UISegmentedControl(items: items)
+        segmentedControl.momentary = true
+        
+        segmentedControl.addTarget(self, action: "segmentedControlValueChanged:", forControlEvents: UIControlEvents.ValueChanged)
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: segmentedControl)
     }
 }
 
