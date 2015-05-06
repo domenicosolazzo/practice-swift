03fb4da85d58e61fa2fc37d267ba983b19626cda
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-06 19:24:15 +0200

    Adding the initialization code

--- Views/ScrollViews/ScrollView Example/ScrollView Example/ViewController.swift
+++ Views/ScrollViews/ScrollView Example/ScrollView Example/ViewController.swift
@@ -8,13 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
     var imageView: UIImageView!
     var scrollView: UIScrollView!
     var image = UIImage(named: "earth")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imageView = UIImageView(image: image)
+        scrollView = UIScrollView(frame: view.bounds)
+        
+        if let theScrollView = scrollView{
+            scrollView.addSubview(imageView)
+            // Set the content size
+            scrollView.contentSize = imageView.bounds.size
+            scrollView.delegate = self
+            
+            view.addSubview(scrollView)
+            
+        }
     }
 }
 
