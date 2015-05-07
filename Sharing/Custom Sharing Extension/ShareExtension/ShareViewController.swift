25eaba03d9d88e685297e0bf92a545e465cd0b41
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-07 19:33:32 +0200

    Adding the Sha the Sharing Extension

---
+++ Sharing/Custom Sharing Extension/ShareExtension/ShareViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ShareViewController.swift
+//  ShareExtension
+///
+//  Created by Domenico Solazzo on 07/05/15.
+//  License MIT
+//
+
+import UIKit
+import Social
+
+class ShareViewController: SLComposeServiceViewController {
+
+
+}
