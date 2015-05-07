32cf03f89a42e7dbbfee13394b8182f5cb9b23f2
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-07 19:43:05 +0200

    Checking if the view controller should enable the post button

--- Sharing/Custom Sharing Extension/ShareExtension/ShareViewController.swift
+++ Sharing/Custom Sharing Extension/ShareExtension/ShareViewController.swift
@@ -15,4 +15,18 @@ AudienceSelectionViewControllerDelegate, NSURLSessionDelegate  {
 
     
     var imageData: NSData?
+    
+    // Used from to iOS to ask to your view controller if it must enable the 
+    // Post button
+    override func isContentValid() -> Bool {
+        /* The post button should be enabled only if we have the image data
+        and the user has entered at least one character of text */
+        if let data = imageData{
+            if count(contentText) > 0{
+                return true
+            }
+        }
+        
+        return false
+    }
 }
