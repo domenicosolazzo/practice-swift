45425540ba1899b8dfce95b0d88c6e8ecac0cb60
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-07 17:09:53 +0200

    New Project: Editing photos

---
+++ Photos/Editing Photos/Editing Photos/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Editing Photos
+//
+//  Created by Domenico Solazzo on 07/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
---
+++ Photos/Editing Photos/Editing Photos/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Editing Photos
+//
+//  Created by Domenico Solazzo on 07/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
