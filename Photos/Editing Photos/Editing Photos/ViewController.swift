13eee20b74cd87e3ec68032a9621ef6794bb9035
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-07 17:13:57 +0200

    Some private variables used during editing

--- Photos/Editing Photos/Editing Photos/ViewController.swift
+++ Photos/Editing Photos/Editing Photos/ViewController.swift
@@ -12,6 +12,14 @@ import OpenGLES
 
 class ViewController: UIViewController {
 
+    /* These two values are our way of telling the Photos framework about
+    the identifier of the changes that we are going to make to the photo */
+    let editFormatIdentifier = NSBundle.mainBundle().bundleIdentifier;
+    /* Just an application-specific editing version */
+    let editFormatVersion = "0.1"
+    /* This is our filter name. We will use this for our Core Image filter */
+    let filterName = "CIColorPosterize"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
