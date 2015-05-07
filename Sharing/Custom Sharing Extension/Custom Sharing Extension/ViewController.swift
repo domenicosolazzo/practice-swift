48ac8f26943bed273dcecfcf5fdbb39b4cf0c05a
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-07 19:25:14 +0200

    New project: Custom Sharing extension

---
+++ Sharing/Custom Sharing Extension/Custom Sharing Extension/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Custom Sharing Extension
+//
+//  Created by Domenico Solazzo on 07/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
