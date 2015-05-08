06437b45e92528c66a4e8a19449168fc8e4cf94a
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-08 19:23:50 +0200

    New project: Read and write Health data

---
+++ HealthKit/Read and Write Health Data/Read and Write Health Data/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Read and Write Health Data
+//
+//  Created by Domenico Solazzo on 08/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
---
+++ HealthKit/Read and Write Health Data/Read and Write Health Data/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Read and Write Health Data
+//
+//  Created by Domenico Solazzo on 08/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
