3898e72e4ba02fe492e9839de4acdd763ff5d041
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-08 20:25:57 +0200

    Clean up

--- HealthKit/Retrieving User Characteristics/Retrieving User Characteristics/ViewController.swift
+++ HealthKit/Retrieving User Characteristics/Retrieving User Characteristics/ViewController.swift
@@ -2,24 +2,13 @@
 //  ViewController.swift
 //  Retrieving User Characteristics
 //
-//  Created by Domenico on 08/05/15.
-//  Copyright (c) 2015 Domenico. All rights reserved.
+//  Created by Domenico Solazzo on 08/05/15.
+//  License MIT
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
