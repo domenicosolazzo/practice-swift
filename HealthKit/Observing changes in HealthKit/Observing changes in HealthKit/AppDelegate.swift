c94d63daab0b772af38453a08b664760b3375490
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-08 23:19:34 +0200

    New prooject: Oect: Observing changes in Health Kit

---
+++ HealthKit/Observing changes in HealthKit/Observing changes in HealthKit/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Observing changes in HealthKit
+//
+//  Created by Domenico Solazzo on 08/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
---
+++ HealthKit/Observing changes in HealthKit/Observing changes in HealthKit/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Observing changes in HealthKit
+//
+//  Created by Domenico Solazzo on 08/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
