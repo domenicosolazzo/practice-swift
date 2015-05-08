e7e14353fc0eee0cd342186aa9a9b3779121acec
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-08 19:25:18 +0200

    Initialization code for authorize the user

--- HealthKit/Read and Write Health Data/Read and Write Health Data/ViewController.swift
+++ HealthKit/Read and Write Health Data/Read and Write Health Data/ViewController.swift
@@ -7,11 +7,56 @@
 //
 
 import UIKit
+import HealthKit
 
 class ViewController: UIViewController {
 
+    // Height
+    let heightQuantity = HKQuantityType.quantityTypeForIdentifier(HKQuantityTypeIdentifierHeight)
+    // Weight
+    let weightQuantity = HKQuantityType.quantityTypeForIdentifier(HKQuantityTypeIdentifierBodyMass)
+    // Hearth rate
+    let hearthRateQuantity = HKQuantityType.quantityTypeForIdentifier(HKQuantityTypeIdentifierHeartRate)
+    
+    // Health kit store
+    lazy var healthStore = HKHealthStore()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    
+    // Information that we would write into the HealthKit
+    lazy var typesToShare: Set<NSObject> = {
+        return Set<NSObject>(arrayLiteral: self.heightQuantity, self.weightQuantity)
+        }()
+    
+    // We want to read these types of data */
+    lazy var typesToRead: Set<NSObject> = {
+        return Set<NSObject>(arrayLiteral: self.heightQuantity,
+            self.weightQuantity,
+            self.hearthRateQuantity
+        )
+        }()
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // Check if the HealthKit is available
+        if HKHealthStore.isHealthDataAvailable(){
+            // Request authorization
+            healthStore.requestAuthorizationToShareTypes(typesToShare, readTypes: typesToRead){ (succeeded: Bool, error: NSError!) in
+                if succeeded && error == nil{
+                    println("Authorization succeeded")
+                }else{
+                    if let theError = error{
+                        println("Error \(theError)")
+                    }
+                }
+            }
+        }else{
+            println("HealthData is not available")
+        }
     }
 }
 
