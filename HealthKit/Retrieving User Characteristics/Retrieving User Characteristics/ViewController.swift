a5693faf00af412d1e0096558049e71fb444fb79
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-08 20:35:56 +0200

    Authorization irequest for health kit

--- HealthKit/Retrieving User Characteristics/Retrieving User Characteristics/ViewController.swift
+++ HealthKit/Retrieving User Characteristics/Retrieving User Characteristics/ViewController.swift
@@ -22,5 +22,23 @@ class ViewController: UIViewController {
     // HealthKit store
     lazy var healthStore = HKHealthStore()
 
+    /* Ask for permission to access the health store */
+    override func viewDidAppear(animated: Bool) {
+        if HKHealthStore.isHealthDataAvailable(){
+            healthStore.requestAuthorizationToShareTypes(nil, readTypes: types, completion: {[weak self]
+                (succeeded:Bool, error:NSError!) -> Void in
+                let strongSelf = self!
+                if succeeded && error == nil{
+                    dispatch_async(dispatch_get_main_queue(), strongSelf.readDateOfBirthInformation)
+                }else{
+                    if let theError = error{
+                        println("Error occurred \(theError)")
+                    }
+                }
+            })
+        }else{
+            println("Health data is not available")
+        }
+    }
 }
 
