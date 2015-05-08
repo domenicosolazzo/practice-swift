a31c73e381ca71ad24fe5847c2e9f9ed634172ea
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-08 18:23:33 +0200

    New project: HealthKit Authorization

---
+++ HealthKit/HealthKit Authorization/HealthKit Authorization/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  HealthKit Authorization
+//
+//  Created by Domenico Solazzo on 08/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
---
+++ HealthKit/HealthKit Authorization/HealthKit Authorization/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  HealthKit Authorization
+//
+//  Created by Domenico Solazzo on 08/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
