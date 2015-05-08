95c79a8af0a8f68017f4493d3010d82edd2334f1
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-08 19:42:50 +0200

    Reading the data

--- HealthKit/Read and Write Health Data/Read and Write Health Data/ViewController.swift
+++ HealthKit/Read and Write Health Data/Read and Write Health Data/ViewController.swift
@@ -56,9 +56,12 @@ class ViewController: UIViewController, UITextFieldDelegate {
         // Check if the HealthKit is available
         if HKHealthStore.isHealthDataAvailable(){
             // Request authorization
-            healthStore.requestAuthorizationToShareTypes(typesToShare, readTypes: typesToRead){ (succeeded: Bool, error: NSError!) in
+            healthStore.requestAuthorizationToShareTypes(typesToShare,
+                readTypes: typesToRead){[weak self](succeeded: Bool, error: NSError!) in
+                
+                let strongSelf = self!
                 if succeeded && error == nil{
-                    println("Authorization succeeded")
+                    strongSelf.readWeightInformation()
                 }else{
                     if let theError = error{
                         println("Error \(theError)")
@@ -68,6 +71,64 @@ class ViewController: UIViewController, UITextFieldDelegate {
         }else{
             println("HealthData is not available")
         }
+    }
+    
+    func readWeightInformation(){
+        // Sorting. Descending.
+        let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: false)
+        
+        // Set the query
+        let query = HKSampleQuery(sampleType: weightQuantity,
+            predicate: nil,
+            limit: 1,
+            sortDescriptors: [sortDescriptor],
+            resultsHandler: {[weak self] (query: HKSampleQuery!,
+                results: [AnyObject]!,
+                error: NSError!) in
+                
+                if results.count > 0{
+                    
+                    /* We really have only one sample */
+                    let sample = results[0] as! HKQuantitySample
+                    /* Get the weight in kilograms from the quantity */
+                    let weightInKilograms = sample.quantity.doubleValueForUnit(
+                        HKUnit.gramUnitWithMetricPrefix(.Kilo))
+                    
+                    /* This is the value of KG, localized in user's language */
+                    let formatter = NSMassFormatter()
+                    let kilogramSuffix = formatter.unitStringFromValue(weightInKilograms,
+                        unit: .Kilogram)
+                    
+                    dispatch_async(dispatch_get_main_queue(), {
+                        
+                        let strongSelf = self!
+                        
+                        
+                        /* Set the value of KG on the righthand side of the
+                        text field */
+                        strongSelf.textFieldRightLabel.text = kilogramSuffix
+                        strongSelf.textFieldRightLabel.sizeToFit()
+                        
+                        /* And finally set the text field's value to the user's
+                        weight */
+                        let weightFormattedAsString =
+                        NSNumberFormatter.localizedStringFromNumber(
+                            NSNumber(double: weightInKilograms),
+                            numberStyle: .NoStyle)
+                        
+                        strongSelf.textField.text = weightFormattedAsString
+                        
+                    })
+                    
+                } else {
+                    print("Could not read the user's weight ")
+                    println("or no weight data was available")
+                }
+                
+                
+            })
+        // Execute the query
+        healthStore.executeQuery(query)
     }
 }
 
