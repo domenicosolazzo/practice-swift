fb12ed966bd17c0c22382e2dc6ceb5c1586eb94c
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-08 20:30:12 +0200

    Private variables for retrieving characteristics data

--- HealthKit/Retrieving User Characteristics/Retrieving User Characteristics/ViewController.swift
+++ HealthKit/Retrieving User Characteristics/Retrieving User Characteristics/ViewController.swift
@@ -11,5 +11,16 @@ import HealthKit
 
 class ViewController: UIViewController {
 
+    // Date of birth characteristic type
+    let dateOfBirthType = HKCharacteristicType.quantityTypeForIdentifier(HKCharacteristicTypeIdentifierDateOfBirth)
+    
+    // Types that you want to read
+    lazy var types: Set<NSObject> = {
+        return Set<NSObject>(arrayLiteral: self.dateOfBirthType)
+    }()
+    
+    // HealthKit store
+    lazy var healthStore = HKHealthStore()
+
 }
 
