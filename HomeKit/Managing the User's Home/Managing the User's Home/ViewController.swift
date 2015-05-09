cbaa92bebead062878909910b527f32d49ad4adf
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-09 18:13:53 +0200

    Clean up

--- HomeKit/Managing the User's Home/Managing the User's Home/ViewController.swift
+++ HomeKit/Managing the User's Home/Managing the User's Home/ViewController.swift
@@ -2,24 +2,13 @@
 //  ViewController.swift
 //  Managing the User's Home
 //
-//  Created by Domenico on 09/05/15.
-//  Copyright (c) 2015 Domenico. All rights reserved.
+//  Created by Domenico Solazzo on 09/05/15.
+//  License MIT
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
