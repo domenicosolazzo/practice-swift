a39d3b72012342426cbb07353e5ee138cdd20151
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-09 11:53:01 +0200

    Converting grams to kilograms

--- playgrounds/HealthKit Measures.playground/Contents.swift
+++ playgrounds/HealthKit Measures.playground/Contents.swift
@@ -1,5 +1,19 @@
 import UIKit
 import HealthKit
 
+/*
+ Convert grams to Kilograms
+*/
+let gramUnit = HKUnit(fromMassFormatterUnit: NSMassFormatterUnit.Gram)
+let kilogramUnit = HKUnit(fromMassFormatterUnit: NSMassFormatterUnit.Kilogram)
+
+let weightInGrams:Double = 74_250
+
+// Create a quantity object
+let weightQuantity = HKQuantity(unit: gramUnit, doubleValue: weightInGrams)
+
+// Convert the quantity to Kilograms
+let weightInKilograms = weightQuantity.doubleValueForUnit(kilogramUnit)
+
 
 
