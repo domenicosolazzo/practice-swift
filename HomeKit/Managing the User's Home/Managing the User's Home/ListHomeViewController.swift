93b647cfd28801c41e2b4ef7871a740f1a1baeea
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-09 18:26:06 +0200

    Implementing some tableView methods

--- HomeKit/Managing the User's Home/Managing the User's Home/ListHomeViewController.swift
+++ HomeKit/Managing the User's Home/Managing the User's Home/ListHomeViewController.swift
@@ -23,4 +23,19 @@ class ListHomeViewController: UITableViewController, HMHomeManagerDelegate {
         manager.delegate = self
         return manager
     }()
+    
+    //- MARK: TableView
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return homeManager.homes.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(TableViewValues.identifier, forIndexPath: indexPath) as UITableViewCell
+        
+        // Home
+        let home = homeManager.homes[indexPath.row] as HMHome
+        
+        cell.textLabel.text = home.name
+        return cell
+    }
 }
