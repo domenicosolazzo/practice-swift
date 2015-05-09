279d40f3ff8ddb44b94e8e39608e047af5d5b179
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-09 18:12:42 +0200

    New Project: Managing the user home

---
+++ HomeKit/Managing the User's Home/Managing the User's Home/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Managing the User's Home
+//
+//  Created by Domenico on 09/05/15.
+//  Copyright (c) 2015 Domenico. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
