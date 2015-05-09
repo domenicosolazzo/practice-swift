6a9d19bff97a3aac9a104a8c674e14eb9b1398b0
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-09 18:19:09 +0200

    Added AddHome view controller

---
+++ HomeKit/Managing the User's Home/Managing the User's Home/AddHomeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AddHomeViewController.swift
+//  Managing the User's Home
+//
+//  Created by Domenico on 09/05/15.
+//  Copyright (c) 2015 Domenico. All rights reserved.
+//
+
+import Cocoa
+
+class AddHomeViewController: UIViewController {
+
+}
