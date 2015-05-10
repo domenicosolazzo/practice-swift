b1ff2225139e6e79eff7b6864871f59590dcfb60
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-10 21:46:24 +0200

    List of images

--- Views/TableViews/CollectionView using custom xib files/CollectionView using custom xib files/MyCollectionViewCell.swift
+++ Views/TableViews/CollectionView using custom xib files/CollectionView using custom xib files/MyCollectionViewCell.swift
@@ -11,4 +11,9 @@ import UIKit
 class MyCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var imageViewBackgroundImage: UIImageView!
+    let allImages = [
+        UIImage(named: "1"),
+        UIImage(named: "2"),
+        UIImage(named: "3")
+    ]
 }
