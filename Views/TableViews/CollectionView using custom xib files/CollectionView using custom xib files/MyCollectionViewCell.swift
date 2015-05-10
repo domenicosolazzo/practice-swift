51b400fe1b2fe906f95e6f3638b9a483c87012af
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-10 21:41:25 +0200

    Added custom .xib file; Added UICollectionViewCell class

---
+++ Views/TableViews/CollectionView using custom xib files/CollectionView using custom xib files/MyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyCollectionViewCell.swift
+//  CollectionView using custom xib files
+//
+//  Created by Domenico Solazzo on 10/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+}
