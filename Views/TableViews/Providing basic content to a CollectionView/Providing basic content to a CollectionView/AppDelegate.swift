2742888965749d4881cca9cbed6fd924093a8e8f
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-10 17:51:21 +0200

    New project: Prooviding basic content to a colection view

---
+++ Views/TableViews/Providing basic content to a CollectionView/Providing basic content to a CollectionView/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Providing basic content to a CollectionView
+//
+//  Created by Domenico Solazzo on 10/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
---
+++ Views/TableViews/Providing basic content to a CollectionView/Providing basic content to a CollectionView/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Providing basic content to a CollectionView
+//
+//  Created by Domenico Solazzo on 10/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
