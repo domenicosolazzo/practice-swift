56f98677b6ffc3901fba8db7afdd978cf117e267
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-10 16:10:37 +0200

    Initialization code for the table view

--- Views/TableViews/Headers and Footers in TableViews/Headers and Footers in TableViews/ViewController.swift
+++ Views/TableViews/Headers and Footers in TableViews/Headers and Footers in TableViews/ViewController.swift
@@ -8,7 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource,
+                        UITableViewDelegate {
 
+    var tableView:UITableView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView = UITableView(frame: view.bounds, style: UITableViewStyle.Grouped)
+        
+        if let theTableView = tableView{
+            theTableView.registerClass(
+                UITableViewCell.classForCoder(),
+                forCellReuseIdentifier: "identifier")
+            
+            theTableView.dataSource = self
+            theTableView.delegate = self
+            theTableView.autoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight
+            
+            // Add the table view as subview
+            self.view.addSubview(theTableView)
+        }
+    }
 }
 
