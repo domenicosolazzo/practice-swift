fb2b07d2aa655d4bee8034bc1042310d6e23e428
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-10 21:36:05 +0200

    New project: CCollectionViewCell using custom xib file

---
+++ Views/TableViews/CollectionView using custom xib files/CollectionView using custom xib files/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  CollectionView using custom xib files
+//
+//  Created by Domenico Solazzo on 10/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
