3bf12f46c67cd883f42315724efdbf88c6ff03a0
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-10 16:43:33 +0200

    Initializing both table view and refresh control

--- Views/TableViews/RefreshControl for TableViews/RefreshControl for TableViews/ViewController.swift
+++ Views/TableViews/RefreshControl for TableViews/RefreshControl for TableViews/ViewController.swift
@@ -9,6 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var tableView: UITableView?
+    var refreshControl: UIRefreshControl?
+    // Data
+    var allTimes = [NSDate]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView = UITableView(frame: view.bounds, style: UITableViewStyle.Plain)
+        if let theTableView = tableView {
+            theTableView.registerClass(UITableViewCell.classForCoder(), forCellReuseIdentifier: "identifier")
+            
+            theTableView.dataSource = self
+            theTableView.autoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight
+            
+            /** Refresh control */
+            refreshControl = UIRefreshControl()
+            // Add target to the refresh control
+            refreshControl?.addTarget(self, action: "handleRefresh:", forControlEvents: UIControlEvents.ValueChanged)
+            
+            // Adding the refresh control as subview of the table view
+            theTableView.addSubview(refreshControl!)
+            
+            self.view.addSubview(theTableView)
+        }
+    }
+    
 }
 
