04c665a462bb66f76ed24e3dd7263996d0c17939
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-10 21:56:36 +0200

    Adding initialization methods

--- Views/TableViews/CollectionView using custom xib files/CollectionView using custom xib files/MyCollectionViewCell.swift
+++ Views/TableViews/CollectionView using custom xib files/CollectionView using custom xib files/MyCollectionViewCell.swift
@@ -11,15 +11,5 @@ import UIKit
 class MyCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var imageViewBackgroundImage: UIImageView!
-    let allImages = [
-        UIImage(named: "1"),
-        UIImage(named: "2"),
-        UIImage(named: "3")
-    ]
     
-    func randomImage() -> UIImage{
-        let randomNumber = arc4random_uniform(UInt32(allImages.count))
-        let randomImage = allImages[Int(randomNumber)]
-        return randomImage
-    }
 }
--- Views/TableViews/CollectionView using custom xib files/CollectionView using custom xib files/ViewController.swift
+++ Views/TableViews/CollectionView using custom xib files/CollectionView using custom xib files/ViewController.swift
@@ -8,7 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UICollectionViewController {
+    let allImages = [
+        UIImage(named: "1"),
+        UIImage(named: "2"),
+        UIImage(named: "3")
+    ]
+    
+    func randomImage() -> UIImage{
+        let randomNumber = arc4random_uniform(UInt32(allImages.count))
+        let randomImage = allImages[Int(randomNumber)]
+        return randomImage!
+    }
+    
+    override init(collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(collectionViewLayout: layout)
+        
+        let nib = UINib(nibName: "MyCollectionViewCell", bundle: nil)
+        
+        collectionView!.registerNib(nib, forCellWithReuseIdentifier: "cell")
+        collectionView!.backgroundColor = UIColor.whiteColor()
+    }
+    
+    convenience required init(coder aDecoder: NSCoder) {
+        let flowLayout = UICollectionViewFlowLayout()
+        
+        flowLayout.minimumLineSpacing = 20
+        flowLayout.minimumInteritemSpacing = 10
+        flowLayout.itemSize = CGSize(width: 80, height: 120);
+        flowLayout.scrollDirection = .Vertical
+        
+        flowLayout.sectionInset =
+            UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 20)
+        
+        self.init(collectionViewLayout: flowLayout)
+    }
 }
 
