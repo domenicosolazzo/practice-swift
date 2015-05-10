20de465b7735139e8a408f2c1367b284ccd68fa9
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-10 12:31:28 +0200

    Initializing the table view

--- Views/TableViews/Populate TableViews/Populate TableViews/ViewController.swift
+++ Views/TableViews/Populate TableViews/Populate TableViews/ViewController.swift
@@ -8,7 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    var tableView: UITableView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView = UITableView(frame: view.bounds, style: UITableViewStyle.Plain)
+        
+        if let theTableView = tableView{
+            // It takes a class name that denotes the type of object that you want your table view to load when it renders each cell (subclass of UITableViewCell) and a cell identifier.
+            theTableView.registerClass(UITableViewCell.classForCoder(),
+                forCellReuseIdentifier: "identifier")
+            
+            // It takes an object that conform to UITableViewDataSource
+            theTableView.dataSource = self
+            theTableView.autoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight
+            
+            // Add the table view as subview
+            view.addSubview(theTableView)
+        }
+    }
 }
 
