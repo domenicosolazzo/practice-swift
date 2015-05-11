ab77d4a2dcc2281ff28b9a59c0ccb58400e140f8
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-11 16:50:00 +0200

    New Project: Handling events in collection views

---
+++ Views/TableViews/Handling events in CollectionViews/Handling events in CollectionViews/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Handling events in CollectionViews
+//
+//  Created by Domenico Solazzo on 11/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
