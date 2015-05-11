c5161b00f6325325b32e82e9c725a49e95d4b33e
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-11 18:32:57 +0200

    New project: Adding interaction with CollectionViews

---
+++ Views/TableViews/Interaction with CollectionViews/Interaction with CollectionViews/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Interaction with CollectionViews
+//
+//  Created by Domenico Solazzo on 11/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
---
+++ Views/TableViews/Interaction with CollectionViews/Interaction with CollectionViews/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  Interaction with CollectionViews
+//
+//  Created by Domenico Solazzo on 11/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+}
+
