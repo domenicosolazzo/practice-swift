785e5381fd16ab040d722461d17180ad1347df26
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-11 18:39:42 +0200

    Adding custom cell

---
+++ Views/TableViews/Interaction with CollectionViews/Interaction with CollectionViews/MyCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MyCollectionViewCell.swift
+//  Interaction with CollectionViews
+//
+//  Created by Domenico on 11/05/15.
+//  Copyright (c) 2015 Domenico. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageViewBackgroundImage: UIImageView!
+    
+}
