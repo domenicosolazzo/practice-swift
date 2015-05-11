c6e1590f3d7432bbc97376393b845280d412a973
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-11 17:50:22 +0200

    Added Header and Footer class (UICollectionReusableView

---
+++ Views/TableViews/Providing Header and Footer in a CollectionView/Providing Header and Footer in a CollectionView/Footer.swift
@@ -0,0 +1,13 @@
+//
+//  Footer.swift
+//  Providing Header and Footer in a CollectionView
+//
+//  Created by Domenico Solazzo on 11/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class Footer: UICollectionReusableView {
+        
+}
---
+++ Views/TableViews/Providing Header and Footer in a CollectionView/Providing Header and Footer in a CollectionView/Header.swift
@@ -0,0 +1,13 @@
+//
+//  Header.swift
+//  Providing Header and Footer in a CollectionView
+//
+//  Created by Domenico Solazzo on 11/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class Header: UICollectionReusableView {
+        
+}
