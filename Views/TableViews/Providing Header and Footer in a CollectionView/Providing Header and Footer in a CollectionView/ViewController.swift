f801f83b93da22dceed79253d1b1a552fe393ec8
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-11 18:12:36 +0200

    Initialization of the CollectionView

--- Views/TableViews/Providing Header and Footer in a CollectionView/Providing Header and Footer in a CollectionView/ViewController.swift
+++ Views/TableViews/Providing Header and Footer in a CollectionView/Providing Header and Footer in a CollectionView/ViewController.swift
@@ -8,7 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UICollectionViewController {
+    
+    
+    override init(collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(collectionViewLayout: layout)
+        
+        let nib = UINib(nibName: "MyCollectionViewCell", bundle: nil)
+        
+        collectionView!.registerNib(nib, forCellWithReuseIdentifier: "cell")
+        
+        /* Register the header's nib */
+        let headerNib = UINib(nibName: "Header", bundle: nil)
+        collectionView!.registerNib(headerNib,
+            forSupplementaryViewOfKind: UICollectionElementKindSectionHeader,
+            withReuseIdentifier: "header")
+        
+        /* Register the footer's nib */
+        let footerNib = UINib(nibName: "Footer", bundle: nil)
+        collectionView!.registerNib(footerNib,
+            forSupplementaryViewOfKind: UICollectionElementKindSectionFooter,
+            withReuseIdentifier: "footer")
+        
+        collectionView!.backgroundColor = UIColor.whiteColor()
+    }
+    
+    convenience required init(coder aDecoder: NSCoder) {
+        let flowLayout = UICollectionViewFlowLayout()
+        
+        flowLayout.minimumLineSpacing = 20
+        flowLayout.minimumInteritemSpacing = 10
+        flowLayout.itemSize = CGSize(width: 80, height: 120);
+        flowLayout.scrollDirection = .Vertical
+        
+        flowLayout.sectionInset =
+            UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 20)
+        
+        self.init(collectionViewLayout: flowLayout)
+    }
 }
 
