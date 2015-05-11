c6330904c37f0d209386d139ae93c1e39056ad07
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-11 16:52:59 +0200

    Added MyCollectionViewCell

---
+++ Views/TableViews/Handling events in CollectionViews/Handling events in CollectionViews/MyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyCollectionViewCell.swift
+//  Handling events in CollectionViews
+//
+//  Created by Domenico Solazzo on 11/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageViewBackgroundImage: UIImageView!
+}
