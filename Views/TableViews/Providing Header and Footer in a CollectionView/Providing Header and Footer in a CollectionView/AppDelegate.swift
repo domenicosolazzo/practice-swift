03feec91ff5adad29d60b92b08efef4bb0a4be19
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-11 17:40:09 +0200

    Header and Footer's xib files

---
+++ Views/TableViews/Providing Header and Footer in a CollectionView/Providing Header and Footer in a CollectionView/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Providing Header and Footer in a CollectionView
+//
+//  Created by Domenico Solazzo on 11/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
---
+++ Views/TableViews/Providing Header and Footer in a CollectionView/Providing Header and Footer in a CollectionView/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Providing Header and Footer in a CollectionView
+//
+//  Created by Domenico Solazzo on 11/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
