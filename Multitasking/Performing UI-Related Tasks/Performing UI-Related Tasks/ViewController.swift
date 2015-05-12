62769d1ad8b40c47b4c0d450204a66f8f1677979
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-12 18:04:51 +0200

    Creating an AlertController in the main queue

--- Multitasking/Performing UI-Related Tasks/Performing UI-Related Tasks/ViewController.swift
+++ Multitasking/Performing UI-Related Tasks/Performing UI-Related Tasks/ViewController.swift
@@ -9,5 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        dispatch_async(dispatch_get_main_queue(), {[weak self] in
+            let alertController = UIAlertController(
+                title: "GCD",
+                message: "GCD is amazing",
+                preferredStyle: UIAlertControllerStyle.Alert)
+            
+            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+            
+            self!.presentViewController(alertController, animated: true, completion: nil)
+        })
+    }
 }
 
