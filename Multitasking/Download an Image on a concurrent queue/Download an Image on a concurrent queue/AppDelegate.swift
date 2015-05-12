ebe2e5ab710c13a592b3ddebc2e1316750244bef
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-12 18:52:09 +0200

    New project: Downloading an image in a concurrent queue

---
+++ Multitasking/Download an Image on a concurrent queue/Download an Image on a concurrent queue/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Download an Image on a concurrent queue
+//
+//  Created by Domenico Solazzo on 12/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
---
+++ Multitasking/Download an Image on a concurrent queue/Download an Image on a concurrent queue/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Download an Image on a concurrent queue
+//
+//  Created by Domenico Solazzo on 12/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
