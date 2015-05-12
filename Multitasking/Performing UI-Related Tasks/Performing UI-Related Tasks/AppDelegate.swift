a39bdd226333b601ea98378e6d3467844b2a342e
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-12 17:51:54 +0200

    New project: Performing UI-Related Tasks

---
+++ Multitasking/Performing UI-Related Tasks/Performing UI-Related Tasks/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Performing UI-Related Tasks
+//
+//  Created by Domenico Solazzo on 12/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
---
+++ Multitasking/Performing UI-Related Tasks/Performing UI-Related Tasks/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  Performing UI-Related Tasks
+//
+//  Created by Domenico Solazzo on 12/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+}
+
