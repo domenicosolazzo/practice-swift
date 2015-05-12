5fbfc47a28510939414f52074eaa868e30fd91df
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-12 20:44:48 +0200

    New project: Performing a task only once

---
+++ Multitasking/Performing a task only once/Performing a task only once/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  Performing a task only once
+//
+//  Created by Domenico Solazzo on 12/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+}
+
