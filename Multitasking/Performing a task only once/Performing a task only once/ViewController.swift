f903458210692c3c22a96b077c8f062165bd4d31
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-12 20:48:28 +0200

    Showing how a function will be executed only once

--- Multitasking/Performing a task only once/Performing a task only once/ViewController.swift
+++ Multitasking/Performing a task only once/Performing a task only once/ViewController.swift
@@ -9,5 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // Token used from the dispatch_one method to detect if a block of code
+    // as been already executed
+    var token: dispatch_once_t = 0
+    
+    var numberOfEntries = 0
+    
+    func executedOnlyOnce(){
+        numberOfEntries++
+        println("This code is executed only once. Entries: \(numberOfEntries)")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        dispatch_once(&token, executedOnlyOnce)
+        dispatch_once(&token, executedOnlyOnce) // It will not be executed
+    }
 }
 
