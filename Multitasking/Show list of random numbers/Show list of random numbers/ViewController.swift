ef6730026e74f37ad72f836c977f6a70d926102e
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-12 19:43:05 +0200

    Code structure for creating, sorting and showing a list of random numbers in an async way

--- Multitasking/Show list of random numbers/Show list of random numbers/ViewController.swift
+++ Multitasking/Show list of random numbers/Show list of random numbers/ViewController.swift
@@ -9,6 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // Fetch a concurrent queue
+        let concurrentQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+        
+        /* If we have not already saved an array of 10,000
+        random numbers to the disk before, generate these numbers now
+        and then save them to the disk in an array */
+        dispatch_async(concurrentQueue, {
+        
+        })
+        
+        /* Read the numbers from disk and sort them in an
+        ascending fashion */
+        dispatch_sync(concurrentQueue, {
+        
+        })
+        
+        /* Show the numbers in the main queue */
+        dispatch_async(dispatch_get_main_queue(), {
+        
+        })
+    }
+    
+    //- MARK: Helpers
     // Find the location of the file
     func findLocation() -> String?{
         /* Get the document folder(s) */
