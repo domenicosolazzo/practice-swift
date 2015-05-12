5eddd9f567d466b2770e06bd179bddd45454a9d8
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-12 18:58:13 +0200

    Code structure for asynchrouns download of an image

--- Multitasking/Download an Image on a concurrent queue/Download an Image on a concurrent queue/ViewController.swift
+++ Multitasking/Download an Image on a concurrent queue/Download an Image on a concurrent queue/ViewController.swift
@@ -9,6 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    override func viewDidAppear(animated: Bool) {
+        
+        // Get the concurrent queue
+        println("Fetching the queue: DISPATCH_QUEUE_PRIORITY_DEFAULT")
+        let queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+        // Sending a block object to the queue
+        dispatch_async(queue, {
+            
+            dispatch_sync(queue, {
+                // Download the picture here
+            })
+            
+            dispatch_sync(dispatch_get_main_queue(), {
+                // Show the image in the UI
+            })
+        
+        })
+    }
 }
 
