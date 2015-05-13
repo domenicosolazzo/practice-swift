5dd470a3dbbe92dfaec7b03db8ce927700ee4e12
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-13 18:55:14 +0200

    Grouping background tasks using dispatch_group_

--- Multitasking/Grouping Tasks together/Grouping Tasks together/ViewController.swift
+++ Multitasking/Grouping Tasks together/Grouping Tasks together/ViewController.swift
@@ -24,5 +24,40 @@ class ViewController: UIViewController {
         /* Reload the image view here */
         println(__FUNCTION__)
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let taskGroup = dispatch_group_create()
+        let mainQueue = dispatch_get_main_queue()
+        
+        // Reload the table view in the main queue
+        dispatch_group_async(taskGroup, mainQueue, {[weak self] in
+            self!.reloadTableView()
+        })
+        
+        // Reload the scrollView in the main queue
+        dispatch_group_async(taskGroup, mainQueue, {[weak self] in
+            self!.reloadScrollView()
+        })
+
+        // Reload the ImageView in the main queue
+        dispatch_group_async(taskGroup, mainQueue, {[weak self] in
+            self!.reloadImageView()
+        })
+        
+        /* When we are done, dispatch the following block */
+        dispatch_group_notify(taskGroup, mainQueue, {[weak self] in
+            // Do some processing here
+            
+            let alertController = UIAlertController(title: "Task group", message: "We have finished all the operations in background", preferredStyle: UIAlertControllerStyle.Alert)
+            
+            alertController.addAction(
+                UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil)
+            )
+            
+            self!.presentViewController(alertController, animated: true, completion: nil)
+        })
+    }
 }
 
