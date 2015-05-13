a0cc99bb34d7ab593eef12232c6a290beb084422
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-13 18:30:45 +0200

    New project: Group tasks together

---
+++ Multitasking/Grouping Tasks together/Grouping Tasks together/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Grouping Tasks together
+//
+//  Created by Domenico Solazzo on 13/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
---
+++ Multitasking/Grouping Tasks together/Grouping Tasks together/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Grouping Tasks together
+//
+//  Created by Domenico Solazzo on 13/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+}
+
