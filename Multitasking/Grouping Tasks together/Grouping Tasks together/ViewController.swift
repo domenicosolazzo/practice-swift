95e1e5b5ccc5bb5909af412fa5f9c4fbb1495f5a
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-13 18:30:57 +0200

    Mock functions

--- Multitasking/Grouping Tasks together/Grouping Tasks together/ViewController.swift
+++ Multitasking/Grouping Tasks together/Grouping Tasks together/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    func reloadTableView(){
+        /* Reload the table view here */
+        println(__FUNCTION__)
+    }
     
+    func reloadScrollView(){
+        /* Do the work here */
+        println(__FUNCTION__)
+    }
+    
+    func reloadImageView(){
+        /* Reload the image view here */
+        println(__FUNCTION__)
+    }
 }
 
