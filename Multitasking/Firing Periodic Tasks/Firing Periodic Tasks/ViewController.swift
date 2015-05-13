d2a83c31f67858022388db89144474212d26fb34
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-13 20:44:17 +0200

    New project: Fi Firing periodic tasks

---
+++ Multitasking/Firing Periodic Tasks/Firing Periodic Tasks/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  Firing Periodic Tasks
+//
+//  Created by Domenico Solazzo on 13/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+}
+
