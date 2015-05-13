4faea01703a4add1bd28bc6fbb9ae2c566d5ead8
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-13 19:02:37 +0200

    New project: Creating simple concurrency using Operations

---
+++ Multitasking/Creating simple concurrency using Operations/Creating simple concurrency using Operations/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Creating simple concurrency using Operations
+//
+//  Created by Domenico Solazzo on 13/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
