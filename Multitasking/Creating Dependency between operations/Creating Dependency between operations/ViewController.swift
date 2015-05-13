089dd2f17d02c7a6cc2a5ac6c1db73b86f9e70d7
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-13 20:22:33 +0200

    Creating helper functions

--- Multitasking/Creating Dependency between operations/Creating Dependency between operations/ViewController.swift
+++ Multitasking/Creating Dependency between operations/Creating Dependency between operations/ViewController.swift
@@ -9,6 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var firstNumber = 111
+        var secondNumber = 222
+        
+        
+    }
+    
+    //- MARK: Helpers
+    // Perform the actual work
+    func performWorkForParameter(param: AnyObject?, operationName: String){
+        if let theParam: AnyObject = param{
+            println("First operation - Object = \(theParam)")
+        }
+        
+        println("\(operationName) Operation - " +
+            "Main Thread = \(NSThread.mainThread())")
+        
+        println("\(operationName) Operation - " +
+            "Current Thread = \(NSThread.currentThread())")
+    }
+    
+    func firstOperationEntry(param: AnyObject?){
+        performWorkForParameter(param, operationName: "First")
+    }
+    
+    func secondOperationEntry(param: AnyObject?){
+        
+        performWorkForParameter(param, operationName: "Second")
+        
+    }
 
 }
 
