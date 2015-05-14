41cc330cc58f46f13735efab2ca84e9429515f27
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-14 21:42:10 +0200

    Location Manager delegate

--- Multitasking/Handling Location Changes in Background/Handling Location Changes in Background/AppDelegate.swift
+++ Multitasking/Handling Location Changes in Background/Handling Location Changes in Background/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 import CoreLocation
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate {
 
     var window: UIWindow?
 
