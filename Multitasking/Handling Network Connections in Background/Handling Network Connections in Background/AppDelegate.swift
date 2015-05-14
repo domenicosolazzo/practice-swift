46a196771154a6f881414a55623fb6575748f00b
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-14 22:01:34 +0200

    Handling a network connection in background

--- Multitasking/Handling Network Connections in Background/Handling Network Connections in Background/AppDelegate.swift
+++ Multitasking/Handling Network Connections in Background/Handling Network Connections in Background/AppDelegate.swift
@@ -13,5 +13,37 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    func application(application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+            
+            let dispatchQueue =
+            dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+            
+            dispatch_async(dispatchQueue, {
+                /* Replace this URL with the URL of a file that is
+                rather big in size */
+                let urlAsString = "http://www.apple.com"
+                let url = NSURL(string: urlAsString)
+                let urlRequest = NSURLRequest(URL: url!)
+                let queue = NSOperationQueue()
+                var error: NSError?
+                
+                let data = NSURLConnection.sendSynchronousRequest(urlRequest,
+                    returningResponse: nil,
+                    error: &error)
+                
+                if data != nil && error == nil{
+                    /* Date did come back */
+                }
+                else if data!.length == 0 && error == nil{
+                    /* No data came back */
+                }
+                else if error != nil{
+                    /* Error happened. Make sure you handle this properly */
+                }
+            })
+            
+            return true
+    }
 }
 
