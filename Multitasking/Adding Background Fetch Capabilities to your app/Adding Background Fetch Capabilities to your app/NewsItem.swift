dd798a705cf6e8ca24bbbac5dcf2398a99c8ad02
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-14 19:04:46 +0200

    Added NewsItem struct

---
+++ Multitasking/Adding Background Fetch Capabilities to your app/Adding Background Fetch Capabilities to your app/NewsItem.swift
@@ -0,0 +1,14 @@
+//
+//  NewsItem.swift
+//  Adding Background Fetch Capabilities to your app
+//
+//  Created by Domenico Solazzo on 14/05/15.
+//  License MIT
+//
+
+import Foundation
+
+struct NewsItem{
+    var date: NSDate,
+    var text: String
+}
