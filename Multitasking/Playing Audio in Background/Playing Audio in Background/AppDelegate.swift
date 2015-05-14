edef16c08c6bd2e8683818250e2f3509de8c9552
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-14 21:13:20 +0200

    Initialization of the audio session and the audio player

--- Multitasking/Playing Audio in Background/Playing Audio in Background/AppDelegate.swift
+++ Multitasking/Playing Audio in Background/Playing Audio in Background/AppDelegate.swift
@@ -10,10 +10,63 @@ import UIKit
 import AVFoundation
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate,AVAudioPlayerDelegate {
 
     var window: UIWindow?
-    var audioPlayer: AVPlayer?
+    var audioPlayer: AVAudioPlayer?
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+        
+        let queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+        dispatch_async(queue, {[weak self] in
+            var strongSelf = self!
+            var audioSessionError: NSError?
+            // Shared instance of AVAudioSession
+            let audioSession = AVAudioSession.sharedInstance()
+            
+            // Adding notification when the audio session is interrupted
+            NSNotificationCenter.defaultCenter().addObserver(
+                strongSelf,
+                selector: "handleInterruption:",
+                name: AVAudioSessionInterruptionNotification,
+                object: nil)
+            
+            audioSession.setActive(true, error: nil)
+            
+            if audioSession.setCategory(AVAudioSessionCategoryPlayback,
+                error: &audioSessionError){
+                    println("Successfully set the audio session")
+            } else {
+                println("Could not set the audio session")
+            }
+            
+            // File path for the audio file
+            let filePath = NSBundle.mainBundle().pathForResource("MySong", ofType: "mp3")
+            // File data
+            let fileData = NSData(contentsOfFile: filePath!, options: NSDataReadingOptions.DataReadingMappedIfSafe, error: nil)
+            
+            var error:NSError?
+            
+            /* Start the audio player */
+            self!.audioPlayer = AVAudioPlayer(data: fileData, error: &error)
+            
+            /* Did we get an instance of AVAudioPlayer? */
+            if let theAudioPlayer = self!.audioPlayer{
+                theAudioPlayer.delegate = self;
+                if theAudioPlayer.prepareToPlay() &&
+                    theAudioPlayer.play(){
+                        println("Successfully started playing")
+                } else {
+                    println("Failed to play")
+                }
+            } else {
+                println("Error instantiating a new audio player....Help!")
+            }
+            
+        })
+        return true
+    }
+    
 
 }
 
