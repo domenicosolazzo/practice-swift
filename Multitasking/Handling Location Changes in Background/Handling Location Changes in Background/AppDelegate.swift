3b2982025f40c961cd8dd44481ab28e304be5064
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-14 21:26:17 +0200

    New project: Handling Location Changes in Background

---
+++ Multitasking/Handling Location Changes in Background/Handling Location Changes in Background/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Handling Location Changes in Background
+//
+//  Created by Domenico Solazzo on 14/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
---
+++ Multitasking/Handling Location Changes in Background/Handling Location Changes in Background/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Handling Location Changes in Background
+//
+//  Created by Domenico Solazzo on 14/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
