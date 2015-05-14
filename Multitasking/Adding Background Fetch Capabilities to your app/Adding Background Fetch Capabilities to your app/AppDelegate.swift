eb041ae8df25b639869827f36c284c9772042154
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-14 10:57:49 +0200

    New project: Adding background task capabilities to your app

---
+++ Multitasking/Adding Background Fetch Capabilities to your app/Adding Background Fetch Capabilities to your app/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Adding Background Fetch Capabilities to your app
+//
+//  Created by Domenico Solazzo on 14/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
---
+++ Multitasking/Adding Background Fetch Capabilities to your app/Adding Background Fetch Capabilities to your app/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Adding Background Fetch Capabilities to your app
+//
+//  Created by Domenico Solazzo on 14/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
