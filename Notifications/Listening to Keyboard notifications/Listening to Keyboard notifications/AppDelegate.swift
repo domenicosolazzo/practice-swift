9b399cd9d2172d506ca1fd7a79177f411654b50a
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-15 11:20:46 +0200

    Listening to Keyboard notifications

---
+++ Notifications/Listening to Keyboard notifications/Listening to Keyboard notifications/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Listening to Keyboard notifications
+//
+//  Created by Domenico Solazzo on 15/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
---
+++ Notifications/Listening to Keyboard notifications/Listening to Keyboard notifications/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Listening to Keyboard notifications
+//
+//  Created by Domenico Solazzo on 15/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+}
+
