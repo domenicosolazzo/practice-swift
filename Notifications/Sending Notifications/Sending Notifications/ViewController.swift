148158aa300cb2df2e510ebd4e78268f0f69c891
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-15 09:45:46 +0200

    Creating a notification; Sending the notification with postNotification

--- Notifications/Sending Notifications/Sending Notifications/ViewController.swift
+++ Notifications/Sending Notifications/Sending Notifications/ViewController.swift
@@ -9,6 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let notificationName = "MyNotificationName"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
+        // Creating the notification
+        let notification = NSNotification(
+            name: notificationName,
+            object: self,
+            userInfo: [
+                "Key1": "Value1",
+                "Key2": "Value2"
+            ])
+        // Sending the notification
+        NSNotificationCenter.defaultCenter().postNotification(notification)
+    }
 }
 
