9010eaeda44d1ec5756ebbe2cae60bcc26b99b37
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-15 10:33:20 +0200

    New project: Listening notifications

---
+++ Notifications/Listening Notifications/Listening Notifications/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Listening Notifications
+//
+//  Created by Domenico Solazzo on 15/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
