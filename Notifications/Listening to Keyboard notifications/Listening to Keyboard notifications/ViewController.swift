2e350f8193d20597e01ef8f21be667c48d8804f1
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-15 17:05:53 +0200

    IBoutlets

--- Notifications/Listening to Keyboard notifications/Listening to Keyboard notifications/ViewController.swift
+++ Notifications/Listening to Keyboard notifications/Listening to Keyboard notifications/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var scrollView: UIScrollView!
 
 }
 
