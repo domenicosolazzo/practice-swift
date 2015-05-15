0effe78b36002fc1a068f868fcf9fd4f5212461c
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-15 17:14:04 +0200

    Implemented handleKeyboardWillShow

--- Notifications/Listening to Keyboard notifications/Listening to Keyboard notifications/ViewController.swift
+++ Notifications/Listening to Keyboard notifications/Listening to Keyboard notifications/ViewController.swift
@@ -38,5 +38,50 @@ class ViewController: UIViewController {
         
         NSNotificationCenter.defaultCenter().removeObserver(self)
     }
+    
+    func handleKeyboardWillShow(notification:NSNotification){
+        var userInfo = notification.userInfo
+        
+        if let info = userInfo{
+            /* Get the duration of the animation of the keyboard for when it
+            gets displayed on the screen. We will animate our contents using
+            the same animation duration */
+            let animationDurationObject =
+            info[UIKeyboardAnimationDurationUserInfoKey] as! NSValue
+            
+            let keyboardEndRectObject =
+            info[UIKeyboardFrameEndUserInfoKey] as! NSValue
+            
+            var animationDuration = 0.0
+            var keyboardEndRect = CGRectZero
+            
+            animationDurationObject.getValue(&animationDuration)
+            keyboardEndRectObject.getValue(&keyboardEndRect)
+            
+            let window = UIApplication.sharedApplication().keyWindow
+            
+            /* Convert the frame from the window's coordinate system to
+            our view's coordinate system */
+            keyboardEndRect = view.convertRect(keyboardEndRect, fromView: window)
+            
+            /* Find out how much of our view is being covered by the keyboard */
+            let intersectionOfKeyboardRectAndWindowRect =
+            CGRectIntersection(view.frame, keyboardEndRect);
+            
+            
+            /* Scroll the scroll view up to show the full contents of our view */
+            UIView.animateWithDuration(animationDuration, animations: {[weak self] in
+                
+                self!.scrollView.contentInset = UIEdgeInsets(top: 0,
+                    left: 0,
+                    bottom: intersectionOfKeyboardRectAndWindowRect.size.height,
+                    right: 0)
+                
+                self!.scrollView.scrollRectToVisible(self!.textField.frame,
+                    animated: false)
+                
+            })
+        }
+    }
 }
 
