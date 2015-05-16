f7a45c7f83d0f90e9d1f7f5f971bb102067a39b4
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-16 20:14:17 +0200

    New project: Downloading synchronously with NSURLConnection

---
+++ Networking/Downloading synchronously with NSURLConnection/Downloading synchronously with NSURLConnection/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Downloading synchronously with NSURLConnection
+//
+//  Created by Domenico Solazzo on 16/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
