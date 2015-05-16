25762c34213518b4ddba0948a43476d2a7a551f1
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-16 16:46:09 +0200

    New project: Downloading data using NSURLSession - DataTask

---
+++ Networking/Downloading data using NSURLSession - DataTask/Downloading data using NSURLSession - DataTask/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Downloading data using NSURLSession - DataTask
+//
+//  Created by Domenico Solazzo on 16/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
---
+++ Networking/Downloading data using NSURLSession - DataTask/Downloading data using NSURLSession - DataTask/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Downloading data using NSURLSession - DataTask
+//
+//  Created by Domenico Solazzo on 16/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+}
+
