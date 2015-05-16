864b1c65472981d9da2efc03cf1d524a2baa4bbd
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-16 18:10:28 +0200

    New project: Downloading data in background

---
+++ Networking/Downloading data in background/Downloading data in background/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Downloading data in background
+//
+//  Created by Domenico Solazzo on 16/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
---
+++ Networking/Downloading data in background/Downloading data in background/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Downloading data in background
+//
+//  Created by Domenico Solazzo on 16/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
