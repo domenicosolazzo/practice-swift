8b015843ef288c495840742b5534d7bd38c188e7
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-16 17:01:13 +0200

    New project: Downloading an URL using NSURLSession - DownloadTask

---
+++ Networking/Downloading an url using NSURLSession - DownloadTask/Downloading an url using NSURLSession - DownloadTask/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Downloading an url using NSURLSession - DownloadTask
+//
+//  Created by Domenico Solazzo on 16/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
---
+++ Networking/Downloading an url using NSURLSession - DownloadTask/Downloading an url using NSURLSession - DownloadTask/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Downloading an url using NSURLSession - DownloadTask
+//
+//  Created by Domenico Solazzo  on 16/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+}
+
