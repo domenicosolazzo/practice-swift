954ce3ecd92f18b87f1c3f0410bdf83e9cbb0282
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-16 19:34:00 +0200

    Using NSURLConnection for downloading a file

--- Networking/Downloading asynchronously with NSURLConnection/Downloading asynchronously with NSURLConnection/ViewController.swift
+++ Networking/Downloading asynchronously with NSURLConnection/Downloading asynchronously with NSURLConnection/ViewController.swift
@@ -22,6 +22,36 @@ extension NSURL{
 }
 
 class ViewController: UIViewController {
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = NSURL(string: "http://www.domenicosolazzo/swiftcode")
+        let request = NSURLRequest(URL: url!)
+        
+        let operationQueue = NSOperationQueue()
+        
+        NSURLConnection.sendAsynchronousRequest(request, queue: operationQueue) {[weak self]
+            (response:NSURLResponse!, data:NSData!, error:NSError!) in
+            /* Now we may have access to the data, but check if an error came back
+            first or not */
+            if data.length > 0 && error == nil{
+                
+                /* Append the filename to the documents directory */
+                let filePath =
+                NSURL.documentsFolder().URLByAppendingPathComponent("apple.html")
+                
+                if data.writeToURL(filePath, atomically: true){
+                    println("Successfully saved the file to \(filePath)")
+                } else {
+                    println("Failed to save the file to \(filePath)")
+                }
+                
+            } else if data.length == 0 && error == nil{
+                println("Nothing was downloaded")
+            } else if error != nil{
+                println("Error happened = \(error)")
+            }
+        }
+    }
 }
 
