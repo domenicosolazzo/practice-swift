0993e34d48669ee716e4353bd94538418f024a59
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-16 17:37:58 +0200

    New project: Get informed of the download's progress

---
+++ Networking/Get informed of the download's progress/Get informed of the download's progress/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Get informed of the download's progress
+//
+//  Created by Domenico Solazzo on 16/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+}
+
---
+++ Networking/Get informed of the download's progress/Get informed of the download's progress/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Get informed of the download's progress
+//
+//  Created by Domenico Solazzo on 16/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
