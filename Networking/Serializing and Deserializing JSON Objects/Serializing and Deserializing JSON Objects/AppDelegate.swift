7f79357f0de6e2fb5bdb84197c0e4869b4f9c14a
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-16 20:51:15 +0200

    New project: Serializing and Deserializing JSON Objects

---
+++ Networking/Serializing and Deserializing JSON Objects/Serializing and Deserializing JSON Objects/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Serializing and Deserializing JSON Objects
+//
+//  Created by Domenico Solazzo on 16/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+}
+
---
+++ Networking/Serializing and Deserializing JSON Objects/Serializing and Deserializing JSON Objects/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Serializing and Deserializing JSON Objects
+//
+//  Created by Domenico Solazzo on 16/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
