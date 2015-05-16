95106e89ea729d4711172e61ea754320c6464cc8
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-16 17:39:54 +0200

    Session object

--- Networking/Get informed of the download's progress/Get informed of the download's progress/ViewController.swift
+++ Networking/Get informed of the download's progress/Get informed of the download's progress/ViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class ViewController: UIViewController, NSURLSessionDelegate,
             NSURLSessionDataDelegate {
     
+    // Session object
+    var session: NSURLSession!
+    
     
 }
 
