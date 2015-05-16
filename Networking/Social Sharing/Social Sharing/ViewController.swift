4dcdb025e25ecb0f3ddc7fa1b5c07a48239f78db
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-16 21:03:42 +0200

    New project: Social Sharing

---
+++ Networking/Social Sharing/Social Sharing/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  Social Sharing
+//
+//  Created by Domenico Solazzo on 16/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+}
+
