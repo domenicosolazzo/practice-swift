b33225dbc6a4959d05c4d4746b7cbd7d8214a856
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-16 19:01:03 +0200

    New project: Uploading data using NSURLSession

---
+++ Networking/Uploading data using NSURLSession/Uploading data using NSURLSession/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Uploading data using NSURLSession
+//
+//  Created by Domenico Solazzo on 16/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
---
+++ Networking/Uploading data using NSURLSession/Uploading data using NSURLSession/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Uploading data using NSURLSession
+//
+//  Created by Domenico Solazzo on 16/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+}
+
