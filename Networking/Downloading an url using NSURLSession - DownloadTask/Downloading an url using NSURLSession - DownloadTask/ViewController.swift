07da5eff6004432a1ba488511c7935f6c79a4e3c
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-16 17:18:58 +0200

    Retrieving an URL using a downloadTask

--- Networking/Downloading an url using NSURLSession - DownloadTask/Downloading an url using NSURLSession - DownloadTask/ViewController.swift
+++ Networking/Downloading an url using NSURLSession - DownloadTask/Downloading an url using NSURLSession - DownloadTask/ViewController.swift
@@ -23,9 +23,52 @@ class ViewController: UIViewController, NSURLSessionDelegate {
         session = NSURLSession(configuration: configuration, delegate: self, delegateQueue: nil)
     }
 
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        var urlString = "http://www.domenicosolazzo.com/swiftcode"
+        var url = NSURL(string: urlString)
+        
+        session.downloadTaskWithURL(url!, completionHandler: {[weak self]
+            (url:NSURL!, response:NSURLResponse!, error:NSError!) in
+            if error == nil{
+                
+                // NSFileManager
+                let manager = NSFileManager()
+                
+                /* Get the path to the caches folder */
+                var error: NSError?
+                var destinationPath = manager.URLForDirectory(
+                    NSSearchPathDirectory.CachesDirectory,
+                    inDomain: NSSearchPathDomainMask.UserDomainMask,
+                    appropriateForURL: url,
+                    create: true,
+                    error: &error
+                )!
+                
+                // Extract the name of the downloaded file
+                let componentsOfUrl = url.absoluteString!.componentsSeparatedByString("/")
+                let fileNameOfUrl = componentsOfUrl[componentsOfUrl.count - 1]
+                
+                // Append the name of the file to the destination folder
+                destinationPath = destinationPath.URLByAppendingPathComponent(fileNameOfUrl)
+                
+                // Move the file over
+                manager.moveItemAtURL(url, toURL: destinationPath, error: nil)
+                
+                // Message
+                let message = "Saved the downloaded data to \(destinationPath)"
+                self!.showAlertWithTitle("Saved", message: message)
+                
+            }else{
+                self!.showAlertWithTitle("Error", message: "Could not download the data!")
+            }
+        })
+    }
+    
     //- MARK: Helper methods
-    func showAlertWithTitle(title: String){
-        let controller = UIAlertController(title: title, message: nil, preferredStyle: UIAlertControllerStyle.Alert)
+    func showAlertWithTitle(title: String, message:String){
+        let controller = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
         
         controller.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
         presentViewController(controller, animated: true, completion: nil)
