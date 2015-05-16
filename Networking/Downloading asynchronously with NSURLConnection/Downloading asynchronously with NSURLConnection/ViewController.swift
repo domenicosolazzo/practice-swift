073425ebfbaaa23c528ffde769b54e35b8c567c2
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-16 19:31:01 +0200

    NSURL extension

--- Networking/Downloading asynchronously with NSURLConnection/Downloading asynchronously with NSURLConnection/ViewController.swift
+++ Networking/Downloading asynchronously with NSURLConnection/Downloading asynchronously with NSURLConnection/ViewController.swift
@@ -8,6 +8,19 @@
 
 import UIKit
 
+extension NSURL{
+    /* An extension on the NSURL class that allows us to retrieve the current
+    documents folder path */
+    class func documentsFolder() -> NSURL{
+        let fileManager = NSFileManager()
+        return fileManager.URLForDirectory(.DocumentDirectory,
+            inDomain: .UserDomainMask,
+            appropriateForURL: nil,
+            create: false,
+            error: nil)!
+    }
+}
+
 class ViewController: UIViewController {
 
 }
