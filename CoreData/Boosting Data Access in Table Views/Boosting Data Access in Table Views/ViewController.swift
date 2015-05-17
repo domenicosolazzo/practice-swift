db541ab9e447435e2ac1d1020f27738e090325be
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-17 15:57:59 +0200

    New project: Boosting data access in TableViews

---
+++ CoreData/Boosting Data Access in Table Views/Boosting Data Access in Table Views/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Boosting Data Access in Table Views
+//
+//  Created by Domenico Solazzo on 17/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
