ad29d44cc2a80ad89872d605dd1bc51f7df60a67
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-17 11:50:04 +0200

    New project: Performing batch updates in CoreData

---
+++ CoreData/Performing Batch Updates/Performing Batch Updates/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Performing Batch Updates
+//
+//  Created by Domenico on 17/05/15.
+//  Copyright (c) 2015 Domenico. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
