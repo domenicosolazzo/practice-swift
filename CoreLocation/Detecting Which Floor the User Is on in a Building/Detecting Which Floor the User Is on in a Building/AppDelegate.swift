c69a20e9930121de05ef4af2d17aafb9a4a0f7d0
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-17 20:26:06 +0200

    New project: Detecting the floor

---
+++ CoreLocation/Detecting Which Floor the User Is on in a Building/Detecting Which Floor the User Is on in a Building/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Detecting Which Floor the User Is on in a Building
+//
+//  Created by Domenico Solazzo on 17/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+}
+
