e5dd64a15f75af093cb04c8bb1ce20066088c668
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-17 12:45:43 +0200

    New project: Reading data from CoreData

---
+++ CoreData/Reading data from CoreData/Reading data from CoreDataTests/Reading_data_from_CoreDataTests.swift
@@ -0,0 +1,36 @@
+//
+//  Reading_data_from_CoreDataTests.swift
+//  Reading data from CoreDataTests
+//
+//  Created by Domenico on 17/05/15.
+//  Copyright (c) 2015 Domenico. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Reading_data_from_CoreDataTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
