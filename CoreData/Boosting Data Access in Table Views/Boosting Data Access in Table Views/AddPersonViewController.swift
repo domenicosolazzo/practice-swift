433995e5cfa0657df78a802681c1fbbda9da0a4b
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-17 16:43:24 +0200

    Added AddPersonViewController

---
+++ CoreData/Boosting Data Access in Table Views/Boosting Data Access in Table Views/AddPersonViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AddPersonViewController.swift
+//  Boosting Data Access in Table Views
+//
+//  Created by Domenico Solazzo on 17/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class AddPersonViewController: UIViewController {
+
+}
