f451b33d4635f864a7c01be01226090ad0927dae
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-17 11:55:08 +0200

    Entity name

--- CoreData/Performing Batch Updates/Performing Batch Updates/AppDelegate.swift
+++ CoreData/Performing Batch Updates/Performing Batch Updates/AppDelegate.swift
@@ -13,6 +13,7 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    let entityName = NSStringFromClass(Person.classForCoder())
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
