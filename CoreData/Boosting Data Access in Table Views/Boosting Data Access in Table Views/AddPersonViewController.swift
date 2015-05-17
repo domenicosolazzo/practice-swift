ed193b7ed6899c86981afc2c1044b318c9198b95
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-17 16:44:53 +0200

    Implementing the UI in viewDidLoad

--- CoreData/Boosting Data Access in Table Views/Boosting Data Access in Table Views/AddPersonViewController.swift
+++ CoreData/Boosting Data Access in Table Views/Boosting Data Access in Table Views/AddPersonViewController.swift
@@ -15,4 +15,47 @@ class AddPersonViewController: UIViewController {
     var textFieldAge: UITextField!
     var barButtonAdd: UIBarButtonItem!
 
+    //- ViewController
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "New Person"
+        
+        var textFieldRect = CGRect(x: 20,
+            y: 80,
+            width: view.bounds.size.width - 40,
+            height: 31)
+        
+        textFieldFirstName = UITextField(frame: textFieldRect)
+        textFieldFirstName.placeholder = "First Name"
+        textFieldFirstName.borderStyle = .RoundedRect
+        textFieldFirstName.autoresizingMask = .FlexibleWidth
+        textFieldFirstName.contentVerticalAlignment = .Center
+        view.addSubview(textFieldFirstName)
+        
+        textFieldRect.origin.y += 37
+        textFieldLastName = UITextField(frame: textFieldRect)
+        textFieldLastName.placeholder = "Last Name"
+        textFieldLastName.borderStyle = .RoundedRect
+        textFieldLastName.autoresizingMask = .FlexibleWidth
+        textFieldLastName.contentVerticalAlignment = .Center
+        view.addSubview(textFieldLastName)
+        
+        textFieldRect.origin.y += 37
+        textFieldAge = UITextField(frame: textFieldRect)
+        textFieldAge.placeholder = "Age"
+        textFieldAge.borderStyle = .RoundedRect
+        textFieldAge.autoresizingMask = .FlexibleWidth
+        textFieldAge.keyboardType = .NumberPad
+        textFieldAge.contentVerticalAlignment = .Center
+        view.addSubview(textFieldAge)
+        
+        barButtonAdd = UIBarButtonItem(title: "Add",
+            style: .Plain,
+            target: self,
+            action: "createNewPerson:")
+        
+        navigationItem.rightBarButtonItem = barButtonAdd
+        
+    }
 }
