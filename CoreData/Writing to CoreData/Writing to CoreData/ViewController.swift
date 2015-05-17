b0335a457a3c0769fb70f145978c4118d65d91f2
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-17 12:17:31 +0200

    Added Person entity

---
+++ CoreData/Writing to CoreData/Writing to CoreData/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Writing to CoreData
+//
+//  Created by Domenico Solazzo on 17/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
