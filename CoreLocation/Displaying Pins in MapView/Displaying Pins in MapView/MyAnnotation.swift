a0832082aefc5a8afa528fff5523642565288abb
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-18 21:05:48 +0200

    Added constructor for the class

--- CoreLocation/Displaying Pins in MapView/Displaying Pins in MapView/MyAnnotation.swift
+++ CoreLocation/Displaying Pins in MapView/Displaying Pins in MapView/MyAnnotation.swift
@@ -14,4 +14,11 @@ class MyAnnotation: NSObject {
     let coordinate: CLLocationCoordinate2D
     let title:String!
     let subtitle:String!
+    
+    init(coordinate: CLLocationCoordinate2D, title:String, subtitle:String){
+        self.coordinate = coordinate
+        self.title = title
+        self.subtitle = subtitle
+        super.init()
+    }
 }
