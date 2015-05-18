596a2ad0e479a759448a2a0ad77e89106dcda4a4
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-18 21:06:14 +0200

    Conforming to the class MKAnnotation

--- CoreLocation/Displaying Pins in MapView/Displaying Pins in MapView/MyAnnotation.swift
+++ CoreLocation/Displaying Pins in MapView/Displaying Pins in MapView/MyAnnotation.swift
@@ -10,7 +10,7 @@
 import UIKit
 import MapKit
 
-class MyAnnotation: NSObject {
+class MyAnnotation: NSObject, MKAnnotation {
     let coordinate: CLLocationCoordinate2D
     let title:String!
     let subtitle:String!
