abb7ee4a528aa7517dd90bc7cbfa41fd3abc2eb9
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-18 20:14:27 +0200

    locationManager:didChangeAuthorizationStatus: It is called when the authorisation status changes

--- CoreLocation/Pinpointing the Location of a Device/Pinpointing the Location of a Device/ViewController.swift
+++ CoreLocation/Pinpointing the Location of a Device/Pinpointing the Location of a Device/ViewController.swift
@@ -11,5 +11,26 @@ import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    // It is called when the authorization status of your location manager is changed by the user
+    func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        print("The authorization status of location services is changed to: ")
+        
+        switch CLLocationManager.authorizationStatus(){
+        case .AuthorizedAlways:
+            println("Authorized always")
+        case .AuthorizedWhenInUse:
+            println("Authorized when in use")
+        case .Denied:
+            println("Denied")
+        case .NotDetermined:
+            println("Not determined")
+        case .Restricted:
+            println("Restricted")
+        default:
+            println("Unhandled")
+        }
+    }
+    
+    
 }
 
