54bcf0d904ee9ebba90e44a473e1136ddbdeca3f
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-18 20:59:25 +0200

    Added a new annotation class

---
+++ CoreLocation/Displaying Pins in MapView/Displaying Pins in MapView/MyAnnotation.swift
@@ -0,0 +1,15 @@
+
+//
+//  MyAnnotation.swift
+//  Displaying Pins in MapView
+//
+//  Created by Domenico Solazzo on 18/05/15.
+//  License MIT
+//
+
+import UIKit
+import MapKit
+
+class MyAnnotation: NSObject {
+
+}
