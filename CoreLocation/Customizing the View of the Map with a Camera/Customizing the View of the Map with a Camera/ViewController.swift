9b39759cd635090a330f19bee6c118a54a2b14d8
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-19 20:32:34 +0200

    Implementing viewDidAppear

--- CoreLocation/Customizing the View of the Map with a Camera/Customizing the View of the Map with a Camera/ViewController.swift
+++ CoreLocation/Customizing the View of the Map with a Camera/Customizing the View of the Map with a Camera/ViewController.swift
@@ -31,6 +31,43 @@ class ViewController: UIViewController,
     }
     
     
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        /* Are location services available on this device? */
+        if CLLocationManager.locationServicesEnabled(){
+            
+            /* Do we have authorization to access location services? */
+            switch CLLocationManager.authorizationStatus(){
+            case .Denied:
+                /* No */
+                displayAlertWithTitle("Not Determined",
+                    message: "Location services are not allowed for this app")
+            case .NotDetermined:
+                /* We don't know yet, we have to ask */
+                locationManager = CLLocationManager()
+                if let manager = locationManager{
+                    manager.delegate = self
+                    manager.requestWhenInUseAuthorization()
+                }
+            case .Restricted:
+                /* Restrictions have been applied, we have no access
+                to location services */
+                displayAlertWithTitle("Restricted",
+                    message: "Location services are not allowed for this app")
+            default:
+                showUserLocationOnMapView()
+            }
+            
+            
+        } else {
+            /* Location services are not enabled.
+            Take appropriate action: for instance, prompt the
+            user to enable the location services */
+            println("Location services are not enabled")
+        }
+    }
+    
     //- MARK: Helper methods
     /* Just a little method to help us display alert dialogs to the user */
     func displayAlertWithTitle(title: String, message: String){
