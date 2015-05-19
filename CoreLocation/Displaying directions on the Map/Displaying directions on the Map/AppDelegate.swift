2ea243239cdf7ded3dfb2a2aa3c108b07483c62e
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-19 20:00:35 +0200

    New project: Displaying directions on the map

---
+++ CoreLocation/Displaying directions on the Map/Displaying directions on the Map/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Displaying directions on the Map
+//
+//  Created by Domenico Solazzo on 19/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
---
+++ CoreLocation/Displaying directions on the Map/Displaying directions on the Map/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Displaying directions on the Map
+//
+//  Created by Domenico Solazzo on 19/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
