6a2911dce657189919201bade0c7ef457fb02f69
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-19 18:09:40 +0200

    New project: Displaying Custom Pins in a MapView

---
+++ CoreLocation/Displaying Custom Pins in a MapView/Displaying Custom Pins in a MapView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Displaying Custom Pins in a MapView
+//
+//  Created by Domenico Solazzo on 19/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
