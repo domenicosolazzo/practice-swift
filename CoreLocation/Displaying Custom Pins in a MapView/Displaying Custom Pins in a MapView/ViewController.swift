464a8f5c181c1929f655bc0c6cfe8649d72bae8f
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-19 18:14:20 +0200

    setCenterOfMapToLocation: set the region of the MapView

--- CoreLocation/Displaying Custom Pins in a MapView/Displaying Custom Pins in a MapView/ViewController.swift
+++ CoreLocation/Displaying Custom Pins in a MapView/Displaying Custom Pins in a MapView/ViewController.swift
@@ -17,6 +17,14 @@ class ViewController: UIViewController, MKMapViewDelegate {
         super.init(coder: aDecoder)
         mapView = MKMapView()
     }
+    
+    /* We have a pin on the map, now zoom into it and make that pin
+    the center of the map */
+    func setCenterOfMapToLocation(location: CLLocationCoordinate2D){
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+    }
 
 }
 
