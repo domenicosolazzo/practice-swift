c5695262c75b840944d7741318d250963d3d9c40
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-19 19:05:46 +0200

    New Project: Searching in a MapView

---
+++ CoreLocation/Searching on a MapView/Searching on a MapView/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Searching on a MapView
+//
+//  Created by Domenico Solazzo on 19/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
---
+++ CoreLocation/Searching on a MapView/Searching on a MapView/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Searching on a MapView
+//
+//  Created by Domenico Solazzo on 19/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
