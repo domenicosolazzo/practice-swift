e24fb1dfbd5aef0e675eff67f810283986a13955
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-19 18:13:14 +0200

    Init code; mapView;

--- CoreLocation/Displaying Custom Pins in a MapView/Displaying Custom Pins in a MapView/ViewController.swift
+++ CoreLocation/Displaying Custom Pins in a MapView/Displaying Custom Pins in a MapView/ViewController.swift
@@ -11,16 +11,12 @@ import MapKit
 
 class ViewController: UIViewController, MKMapViewDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var mapView: MKMapView!
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        mapView = MKMapView()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
