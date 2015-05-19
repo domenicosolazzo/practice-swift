cdf007473c449173d57969bf18b27d0b8f0b6152
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-19 19:13:46 +0200

    Implementing delegate methods from CLLocationManagerDelegate

--- CoreLocation/Searching on a MapView/Searching on a MapView/ViewController.swift
+++ CoreLocation/Searching on a MapView/Searching on a MapView/ViewController.swift
@@ -28,5 +28,41 @@ class ViewController: UIViewController,
         mapView.delegate = self
         view.addSubview(mapView)
     }
+    
+    //- MARK: Location Manager
+    func locationManager(manager: CLLocationManager!,
+        didUpdateToLocation newLocation: CLLocation!,
+        fromLocation oldLocation: CLLocation!){
+            
+            println("Latitude = \(newLocation.coordinate.latitude)")
+            println("Longitude = \(newLocation.coordinate.longitude)")
+            
+    }
+    
+    func locationManager(manager: CLLocationManager!,
+        didFailWithError error: NSError!){
+            println("Location manager failed with error = \(error)")
+    }
+    
+    /* The authorization status of the user has changed, we need to react
+    to that so that if she has authorized our app to to view her location,
+    we will accordingly attempt to do so */
+    func locationManager(manager: CLLocationManager!,
+        didChangeAuthorizationStatus status: CLAuthorizationStatus){
+            
+            print("The authorization status of location services is changed to: ")
+            
+            switch CLLocationManager.authorizationStatus(){
+            case .Denied:
+                println("Denied")
+            case .NotDetermined:
+                println("Not determined")
+            case .Restricted:
+                println("Restricted")
+            default:
+                showUserLocationOnMapView()
+            }
+            
+    }
 }
 
