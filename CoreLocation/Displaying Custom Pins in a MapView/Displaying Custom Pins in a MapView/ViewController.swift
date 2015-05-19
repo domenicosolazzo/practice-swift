356c1611b51a0b2daba23ab533910bfeefb72077
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-19 18:15:42 +0200

    addPinToMapView: Adding some pins in the MapView

--- CoreLocation/Displaying Custom Pins in a MapView/Displaying Custom Pins in a MapView/ViewController.swift
+++ CoreLocation/Displaying Custom Pins in a MapView/Displaying Custom Pins in a MapView/ViewController.swift
@@ -25,6 +25,54 @@ class ViewController: UIViewController, MKMapViewDelegate {
         let region = MKCoordinateRegion(center: location, span: span)
         mapView.setRegion(region, animated: true)
     }
+    
+    func addPinToMapView(){
+        
+        /* These are just sample locations */
+        let purpleLocation = CLLocationCoordinate2D(latitude: 58.592737,
+            longitude: 16.185898)
+        
+        let blueLocation = CLLocationCoordinate2D(latitude: 58.593038,
+            longitude: 16.188129)
+        
+        let redLocation = CLLocationCoordinate2D(latitude: 58.591831,
+            longitude: 16.189073)
+        
+        let greenLocation = CLLocationCoordinate2D(latitude: 58.590522,
+            longitude: 16.185726)
+        
+        /* Create the annotations using the location */
+        let purpleAnnotation = MyAnnotation(coordinate: purpleLocation,
+            title: "Purple",
+            subtitle: "Pin",
+            pinColor: .Purple)
+        
+        /* This calls the convenience constructor which will by default
+        create a blue pin for us */
+        let blueAnnotation = MyAnnotation(coordinate: blueLocation,
+            title: "Blue",
+            subtitle: "Pin")
+        
+        let redAnnotation = MyAnnotation(coordinate: redLocation,
+            title: "Red",
+            subtitle: "Pin",
+            pinColor: .Red)
+        
+        let greenAnnotation = MyAnnotation(coordinate: greenLocation,
+            title: "Green",
+            subtitle: "Pin",
+            pinColor: .Green)
+        
+        /* And eventually add them to the map */
+        mapView.addAnnotations([purpleAnnotation,
+            blueAnnotation,
+            redAnnotation,
+            greenAnnotation])
+        
+        /* And now center the map around the point */
+        setCenterOfMapToLocation(purpleLocation)
+        
+    }
 
 }
 
