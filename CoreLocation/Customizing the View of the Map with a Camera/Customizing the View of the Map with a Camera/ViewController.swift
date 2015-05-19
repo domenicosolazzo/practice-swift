da4e78c941b13a6d0a7a40365b05f1a5f1677ac6
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-19 20:31:15 +0200

    Set of helper methods

--- CoreLocation/Customizing the View of the Map with a Camera/Customizing the View of the Map with a Camera/ViewController.swift
+++ CoreLocation/Customizing the View of the Map with a Camera/Customizing the View of the Map with a Camera/ViewController.swift
@@ -14,5 +14,27 @@ class ViewController: UIViewController,
 
     var mapView: MKMapView!
     var locationManager: CLLocationManager?
+    
+    //- MARK: Helper methods
+    /* Just a little method to help us display alert dialogs to the user */
+    func displayAlertWithTitle(title: String, message: String){
+        let controller = UIAlertController(title: title,
+            message: message,
+            preferredStyle: .Alert)
+        
+        controller.addAction(UIAlertAction(title: "OK",
+            style: .Default,
+            handler: nil))
+        
+        presentViewController(controller, animated: true, completion: nil)
+        
+    }
+    
+    /* We will call this method when we are sure that the user has given
+    us access to her location */
+    func showUserLocationOnMapView(){
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .Follow
+    }
 }
 
