272f39f32d6204c95563c6fc3f6920e2396905db
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-20 20:41:15 +0200

    New project: Retrieving Altitude data

---
+++ Gyroscope/Retrieving Altitude Data/Retrieving Altitude Data/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Retrieving Altitude Data
+//
+//  Created by Domenico Solazzo on 20/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
---
+++ Gyroscope/Retrieving Altitude Data/Retrieving Altitude Data/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Retrieving Altitude Data
+//
+//  Created by Domenico Solazzo on 20/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
