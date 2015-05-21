37e35069f7e609050f0ad5048ebfda277febc573
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-21 19:04:29 +0200

    New project: Retrieving Pedometer data

---
+++ CoreMotion/Retrieving Pedometer Data/Retrieving Pedometer Data/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Retrieving Pedometer Data
+//
+//  Created by Domenico on 21/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
