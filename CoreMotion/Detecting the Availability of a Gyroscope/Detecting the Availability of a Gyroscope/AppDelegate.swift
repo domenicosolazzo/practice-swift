5140e26ffe32b3851c0685ea96c357c3e991c0d4
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-21 20:12:41 +0200

    New project: Detecting the Availability of a Gyroscope

---
+++ CoreMotion/Detecting the Availability of a Gyroscope/Detecting the Availability of a Gyroscope/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Detecting the Availability of a Gyroscope
+//
+//  Created by Domenico on 21/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+}
+
---
+++ CoreMotion/Detecting the Availability of a Gyroscope/Detecting the Availability of a Gyroscope/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Detecting the Availability of a Gyroscope
+//
+//  Created by Domenico on 21/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
