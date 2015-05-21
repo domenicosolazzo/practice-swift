db09592c4711cc37c8ac05d55d7e3d74c14b0e31
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-21 20:15:12 +0200

    Import CoreMotion

--- CoreMotion/Detecting the Availability of a Gyroscope/Detecting the Availability of a Gyroscope/AppDelegate.swift
+++ CoreMotion/Detecting the Availability of a Gyroscope/Detecting the Availability of a Gyroscope/AppDelegate.swift
@@ -7,15 +7,15 @@
 //
 
 import UIKit
+import CoreMotion
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+
         return true
     }
 
