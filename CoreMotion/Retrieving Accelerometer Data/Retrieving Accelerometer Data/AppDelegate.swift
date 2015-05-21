f18db8b903aa3d262cd82347a82c4d0932ccb10e
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-21 20:54:08 +0200

    New project: Retrieving Accelerometer Data

---
+++ CoreMotion/Retrieving Accelerometer Data/Retrieving Accelerometer Data/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Retrieving Accelerometer Data
+//
+//  Created by Domenico on 21/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
---
+++ CoreMotion/Retrieving Accelerometer Data/Retrieving Accelerometer Data/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Retrieving Accelerometer Data
+//
+//  Created by Domenico on 21/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
