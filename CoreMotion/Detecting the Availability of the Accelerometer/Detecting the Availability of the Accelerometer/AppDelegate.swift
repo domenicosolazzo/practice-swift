bd8aa395db3f47a60cee230b41c5e2ae1ebaa744
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-21 19:57:22 +0200

    New project: Detecting the Availability of the Accelerometer

---
+++ CoreMotion/Detecting the Availability of the Accelerometer/Detecting the Availability of the Accelerometer/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Detecting the Availability of the Accelerometer
+//
+//  Created by Domenico on 21/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
---
+++ CoreMotion/Detecting the Availability of the Accelerometer/Detecting the Availability of the Accelerometer/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Detecting the Availability of the Accelerometer
+//
+//  Created by Domenico on 21/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
