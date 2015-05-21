6a1cd3e75717676b7f866fcc808bcbae9e5b191d
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-21 21:00:49 +0200

    Getting the accelerometer data

--- CoreMotion/Retrieving Accelerometer Data/Retrieving Accelerometer Data/ViewController.swift
+++ CoreMotion/Retrieving Accelerometer Data/Retrieving Accelerometer Data/ViewController.swift
@@ -15,6 +15,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        if motionManager.accelerometerAvailable{
+            let queue = NSOperationQueue()
+            motionManager.startAccelerometerUpdatesToQueue(queue, withHandler:
+                {(data: CMAccelerometerData!, error: NSError!) in
+                    
+                    println("X = \(data.acceleration.x)")
+                    println("Y = \(data.acceleration.y)")
+                    println("Z = \(data.acceleration.z)")
+                    
+                }
+            )
+        } else {
+            println("Accelerometer is not available")
+        }
+
     }
 }
 
