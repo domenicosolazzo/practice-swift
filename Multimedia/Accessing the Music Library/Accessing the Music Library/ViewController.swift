831032a439c1fc511cac753a47b84e10f262c7dc
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-23 12:59:36 +0200

    New project: Accessing the Music Library

---
+++ Multimedia/Accessing the Music Library/Accessing the Music Library/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Accessing the Music Library
+//
+//  Created by Domenico on 23/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
