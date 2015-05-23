380021f60a1c579086253fb7805b041a3f3570f2
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-23 10:05:59 +0200

    New project: Playing Audio Files

---
+++ Multimedia/Playing Audio Files/Playing Audio Files/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Playing Audio Files
+//
+//  Created by Domenico on 23/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
