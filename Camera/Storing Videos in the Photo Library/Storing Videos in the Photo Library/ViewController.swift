3153fe1d33171226604051e8d8de3f756356e8b8
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-23 20:44:39 +0200

    Saving the file in the library

--- Camera/Storing Videos in the Photo Library/Storing Videos in the Photo Library/ViewController.swift
+++ Camera/Storing Videos in the Photo Library/Storing Videos in the Photo Library/ViewController.swift
@@ -11,5 +11,38 @@ import AssetsLibrary
 
 class ViewController: UIViewController {
 
+    var assetsLibrary: ALAssetsLibrary?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        assetsLibrary = ALAssetsLibrary()
+        
+        let videoURL = NSBundle.mainBundle().URLForResource("sample_iTunes",
+            withExtension: "mov") as NSURL?
+        
+        if let library = assetsLibrary{
+            
+            if let url = videoURL{
+                
+                library.writeVideoAtPathToSavedPhotosAlbum(url,
+                    completionBlock: {(url: NSURL!, error: NSError!) in
+                        
+                        println(url)
+                        
+                        if let theError = error{
+                            println("Error happened while saving the video")
+                            println("The error is = \(theError)")
+                        } else {
+                            println("no errors happened")
+                        }
+                        
+                })
+            } else {
+                println("Could not find the video in the app bundle")
+            }
+            
+        }
+    }
 }
 
