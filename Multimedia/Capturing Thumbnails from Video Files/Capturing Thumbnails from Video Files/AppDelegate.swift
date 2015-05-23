be62c39946d13321a7c8847ff1df3f455a40e3f3
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-23 12:50:03 +0200

    New project: Capturing Thumbnails from Video Files

---
+++ Multimedia/Capturing Thumbnails from Video Files/Capturing Thumbnails from Video Files/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Capturing Thumbnails from Video Files
+//
+//  Created by Domenico on 23/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
---
+++ Multimedia/Capturing Thumbnails from Video Files/Capturing Thumbnails from Video Files/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  Capturing Thumbnails from Video Files
+//
+//  Created by Domenico on 23/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+}
+
