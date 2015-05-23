5d4d1f79b26bb6d49b9301a8e54aeff7f88df8a6
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-23 10:55:46 +0200

    Function that returns the audio settings for recording

--- Multimedia/Recording Audio Files/Recording Audio Files/ViewController.swift
+++ Multimedia/Recording Audio Files/Recording Audio Files/ViewController.swift
@@ -22,6 +22,22 @@ class ViewController: UIViewController, AVAudioPlayerDelegate,
         let documentFoldersUrl = fileManager.URLForDirectory(NSSearchPathDirectory.DocumentDirectory, inDomain: NSSearchPathDomainMask.UserDomainMask, appropriateForURL: nil, create: false, error: nil)
         return documentFoldersUrl!.URLByAppendingPathComponent("Recording.m4a")
     }
+    
+    // Audio Recorder settings
+    func audioRecordingSettings() -> NSDictionary{
+        
+        /* Let's prepare the audio recorder options in the dictionary.
+        Later we will use this dictionary to instantiate an audio
+        recorder of type AVAudioRecorder */
+        
+        return [
+            AVFormatIDKey : kAudioFormatMPEG4AAC as NSNumber,
+            AVSampleRateKey : 16000.0 as NSNumber,
+            AVNumberOfChannelsKey : 1 as NSNumber,
+            AVEncoderAudioQualityKey : AVAudioQuality.Low.rawValue as NSNumber
+        ]
+        
+    }
 
 }
 
