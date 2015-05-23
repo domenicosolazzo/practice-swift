45c68f1dea2966613922af024837121743e9edb5
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-23 13:03:10 +0200

    Displaying the media picker

--- Multimedia/Accessing the Music Library/Accessing the Music Library/ViewController.swift
+++ Multimedia/Accessing the Music Library/Accessing the Music Library/ViewController.swift
@@ -12,5 +12,23 @@ import MediaPlayer
 class ViewController: UIViewController, MPMediaPickerControllerDelegate {
 
     var mediaPicker: MPMediaPickerController?
+    
+    func displayMediaPicker(){
+        
+        mediaPicker = MPMediaPickerController(mediaTypes: .Any)
+        
+        if let picker = mediaPicker{
+            
+            println("Successfully instantiated a media picker")
+            picker.delegate = self
+            picker.allowsPickingMultipleItems = false
+            
+            presentViewController(picker, animated: true, completion: nil)
+            
+        } else {
+            println("Could not instantiate a media picker")
+        }
+        
+    }
 }
 
