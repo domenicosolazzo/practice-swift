bf20a9b48f1b5c0aceac4822ea565b531fa5b0ed
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-23 11:54:32 +0200

    New project: Playing Video Files

---
+++ Multimedia/Playing Video Files/Playing Video Files/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Playing Video Files
+//
+//  Created by Domenico on 23/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
