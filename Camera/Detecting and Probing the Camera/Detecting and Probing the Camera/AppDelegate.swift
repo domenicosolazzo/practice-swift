ffc09184c1b2d7fdf5639290f901b5c76fd37b7c
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-23 13:26:58 +0200

    New project: Detecting and Probing the Camera

---
+++ Camera/Detecting and Probing the Camera/Detecting and Probing the Camera/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Detecting and Probing the Camera
+//
+//  Created by Domenico on 23/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
---
+++ Camera/Detecting and Probing the Camera/Detecting and Probing the Camera/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Detecting and Probing the Camera
+//
+//  Created by Domenico on 23/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
