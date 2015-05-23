585d4511e974b4c80e0f475d705416c013abd64a
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-23 19:35:46 +0200

    Private variables

--- Camera/Taking photos with the camera/Taking photos with the camera/ViewController.swift
+++ Camera/Taking photos with the camera/Taking photos with the camera/ViewController.swift
@@ -11,5 +11,10 @@ import UIKit
 class ViewController: UIViewController,
        UINavigationControllerDelegate, UIImagePickerControllerDelegate{
 
+    /* We use this variable to determine if the viewDidAppear:
+    method of our view controller has been called or not. If not, we
+    display the camera view */
+    var beenHereBefore = false
+    var controller: UIImagePickerController?
 }
 
