8f0fe8e676125f7969c1b8e390d961a9b6c9a351
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-23 20:16:29 +0200

    New project: Storing Photos in the Photo Library

---
+++ Camera/Storing Photos in the Photo Library/Storing Photos in the Photo Library/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Storing Photos in the Photo Library
+//
+//  Created by Domenico on 23/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
