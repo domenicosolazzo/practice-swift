50f1dbbbc7388654641e576f464173b4a8cb1b68
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-23 19:34:58 +0200

    New project: Taking pictures with the Camera

---
+++ Camera/Taking photos with the camera/Taking photos with the camera/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Taking photos with the camera
+//
+//  Created by Domenico on 23/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
---
+++ Camera/Taking photos with the camera/Taking photos with the camera/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Taking photos with the camera
+//
+//  Created by Domenico on 23/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
