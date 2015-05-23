aaebb2f43c213ac8892116d64d1a236fd1999e77
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-23 20:41:42 +0200

    New project: Storing Videos in the Photo Library

---
+++ Camera/Storing Videos in the Photo Library/Storing Videos in the Photo Library/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Storing Videos in the Photo Library
+//
+//  Created by Domenico on 23/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
---
+++ Camera/Storing Videos in the Photo Library/Storing Videos in the Photo LibraryTests/Storing_Videos_in_the_Photo_LibraryTests.swift
@@ -0,0 +1,36 @@
+//
+//  Storing_Videos_in_the_Photo_LibraryTests.swift
+//  Storing Videos in the Photo LibraryTests
+//
+//  Created by Domenico on 23/05/15.
+//  Copyright (c) 2015 Domenico. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Storing_Videos_in_the_Photo_LibraryTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
