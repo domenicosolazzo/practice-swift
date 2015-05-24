274806a64e5925c8275f898d3bb29467b4d7e4bc
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-24 22:12:38 +0200

    New project: Retrieving Calendar Groups

---
+++ Calendar/Retrieving Calendar Groups/Retrieving Calendar Groups/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Retrieving Calendar Groups
+//
+//  Created by Domenico on 24/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
