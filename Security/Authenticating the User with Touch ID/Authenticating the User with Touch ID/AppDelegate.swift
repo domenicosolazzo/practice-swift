ffb7ed467a9f8fa0db42fe75784e243ec99c9e0e
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-24 10:58:49 +0200

    New project: Authenticating the User with Touch ID

---
+++ Security/Authenticating the User with Touch ID/Authenticating the User with Touch ID/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Authenticating the User with Touch ID
+//
+//  Created by Domenico on 24/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
---
+++ Security/Authenticating the User with Touch ID/Authenticating the User with Touch ID/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Authenticating the User with Touch ID
+//
+//  Created by Domenico on 24/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
