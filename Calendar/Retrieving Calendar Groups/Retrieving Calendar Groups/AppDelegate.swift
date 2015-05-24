f8438c14bbae52fe4a64d5a55d58e0a5b2a40e1d
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-24 22:14:43 +0200

    Requesting Calendar authorization

--- Calendar/Retrieving Calendar Groups/Retrieving Calendar Groups/AppDelegate.swift
+++ Calendar/Retrieving Calendar Groups/Retrieving Calendar Groups/AppDelegate.swift
@@ -17,9 +17,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        requestCalendarAuthorization()
         return true
     }
     
+    func requestCalendarAuthorization(){
+        
+        let eventStore = EKEventStore()
+        
+        switch EKEventStore.authorizationStatusForEntityType(EKEntityTypeEvent){
+            
+        case .Authorized:
+            findIcloudEventSource()
+        case .Denied:
+            displayAccessDenied()
+        case .NotDetermined:
+            eventStore.requestAccessToEntityType(EKEntityTypeEvent, completion:
+                {[weak self] (granted: Bool, error: NSError!) -> Void in
+                    if granted{
+                        self!.findIcloudEventSource()
+                    } else {
+                        self!.displayAccessDenied()
+                    }
+                })
+        case .Restricted:
+            displayAccessRestricted()
+        }
+        
+        
+    }
     //- MARK: Helper methods
     func displayAccessDenied(){
         println("Access to the event store is denied.")
