5c4157b0d59bf82cbb338f678b596139a9f4a879
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-24 12:04:37 +0200

    Creating a date in BC era

--- playgrounds/Date.playground/Contents.swift
+++ playgrounds/Date.playground/Contents.swift
@@ -8,4 +8,14 @@ enum GregorianEra: Int{
     case AD
 }
 
+// Create a date in BC era
+let date = NSCalendar.currentCalendar().dateWithEra(GregorianEra.BC.rawValue, year: 2015, month: 08, day: 25, hour: 20, minute: 22, second: 1, nanosecond: 22)
+
+if date != nil{
+    println("The date is \(date)")
+}else{
+    println("Error")
+}
+
+
 
