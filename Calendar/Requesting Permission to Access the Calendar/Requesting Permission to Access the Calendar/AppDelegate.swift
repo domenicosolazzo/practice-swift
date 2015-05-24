41162446f5154569ad4a3cd198893afc73a04526
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-24 18:15:57 +0200

    Extracting the vents

--- Calendar/Requesting Permission to Access the Calendar/Requesting Permission to Access the Calendar/AppDelegate.swift
+++ Calendar/Requesting Permission to Access the Calendar/Requesting Permission to Access the Calendar/AppDelegate.swift
@@ -14,6 +14,37 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
-    
+    // Extract the events from an event store
+    func extractEventEntityCalendarsOutOfStore(eventStore: EKEventStore){
+        // Calendar types
+        let calendarTypes = [
+            "Local",
+            "CalDAV",
+            "Exchange",
+            "Subscription",
+            "Birthday",
+        ]
+        
+        // Extract the calendars by EntityType: EKEntityTypeEvent or EKEntityTypeReminder
+        let calendars = eventStore.calendarsForEntityType(EKEntityTypeEvent)
+            as! [EKCalendar]
+        for calendar in calendars{
+            
+            println("Calendar title = \(calendar.title)")
+            println("Calendar type = \(calendarTypes[Int(calendar.type.value)])")
+            
+            let color = UIColor(CGColor: calendar.CGColor)
+            println("Calendar color = \(color)")
+            
+            if calendar.allowsContentModifications{
+                println("This calendar allows modifications")
+            } else {
+                println("This calendar does not allow modifications")
+            }
+            
+            println("--------------------------")
+            
+        }
+    }
 }
 
