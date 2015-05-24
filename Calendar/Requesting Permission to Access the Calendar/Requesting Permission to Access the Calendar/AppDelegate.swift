0cd980c60323cba5dc74e906d17138afec748310
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-24 12:51:07 +0200

    New project: Requesting permissions to access the calendar

---
+++ Calendar/Requesting Permission to Access the Calendar/Requesting Permission to Access the Calendar/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Requesting Permission to Access the Calendar
+//
+//  Created by Domenico on 24/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
---
+++ Calendar/Requesting Permission to Access the Calendar/Requesting Permission to Access the Calendar/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Requesting Permission to Access the Calendar
+//
+//  Created by Domenico on 24/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
