2fd9ac2783ae3571833e780c604266fa8b04b4e9
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-24 11:14:05 +0200

    Adding the LAContext that will allow us to use the TouchID API; checking the TouchID is available

--- Security/Authenticating the User with Touch ID/Authenticating the User with Touch ID/ViewController.swift
+++ Security/Authenticating the User with Touch ID/Authenticating the User with Touch ID/ViewController.swift
@@ -14,6 +14,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var useButton: UIButton!
 
     @IBAction func checkAvailability(sender: UIButton) {
+        // Handle for using the TouchID API
+        let context = LAContext()
+        var error: NSError?
+        
+        let isTouchIDAvailable = context.canEvaluatePolicy(
+            LAPolicy.DeviceOwnerAuthenticationWithBiometrics,
+            error: &error)
+        
+        useButton.enabled = isTouchIDAvailable
     }
     @IBAction func useTouchID(sender: UIButton) {
     }
