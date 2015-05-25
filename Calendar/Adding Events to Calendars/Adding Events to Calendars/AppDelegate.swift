8cd033eb280f2e899686e664964c85d77fb66c2e
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-25 15:12:06 +0200

    New project: Adding events to calendars

---
+++ Calendar/Adding Events to Calendars/Adding Events to Calendars/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Adding Events to Calendars
+//
+//  Created by Domenico on 25/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
---
+++ Calendar/Adding Events to Calendars/Adding Events to Calendars/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Adding Events to Calendars
+//
+//  Created by Domenico on 25/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
