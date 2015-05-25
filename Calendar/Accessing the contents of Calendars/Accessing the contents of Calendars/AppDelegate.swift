b89cc2c780d40d0029de0daac08d9e535def5bec
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-25 18:53:39 +0200

    Finding a calendar by Title

--- Calendar/Accessing the contents of Calendars/Accessing the contents of Calendars/AppDelegate.swift
+++ Calendar/Accessing the contents of Calendars/Accessing the contents of Calendars/AppDelegate.swift
@@ -35,6 +35,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             
             return nil
     }
+    
+    // Finding a calendar by Title
+    func calendarWithTitle(
+        title: String,
+        type: EKCalendarType,
+        source: EKSource,
+        eventType: EKEntityType) -> EKCalendar?{
+            
+            for calendar in source.calendarsForEntityType(eventType)
+                as! Set<EKCalendar>{
+                    if calendar.title.caseInsensitiveCompare(title) ==
+                        NSComparisonResult.OrderedSame &&
+                        calendar.type.value == type.value{
+                            return calendar
+                    }
+            }
+            
+            return nil
+    }
 
 }
 
