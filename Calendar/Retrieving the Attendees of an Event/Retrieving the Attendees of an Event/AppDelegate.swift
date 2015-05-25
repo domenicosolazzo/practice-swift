4e7ef3819fc241868208b5f92e6ee3fa45c49368
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-25 21:13:57 +0200

    New project:Retrieving the Attendees of an Event

---
+++ Calendar/Retrieving the Attendees of an Event/Retrieving the Attendees of an Event/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Retrieving the Attendees of an Event
+//
+//  Created by Domenico on 25/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        return true
+    }
+
+
+}
+
---
+++ Calendar/Retrieving the Attendees of an Event/Retrieving the Attendees of an Event/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Retrieving the Attendees of an Event
+//
+//  Created by Domenico on 25/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
