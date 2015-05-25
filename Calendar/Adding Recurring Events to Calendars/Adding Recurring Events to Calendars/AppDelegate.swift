eb1945a0cb722172dedb4ad8c6f2764ee72ee087
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-25 21:07:32 +0200

    New project: Adding Recurring Events to Calendars

---
+++ Calendar/Adding Recurring Events to Calendars/Adding Recurring Events to Calendars/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Adding Recurring Events to Calendars
+//
+//  Created by Domenico on 25/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+}
+
---
+++ Calendar/Adding Recurring Events to Calendars/Adding Recurring Events to Calendars/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Adding Recurring Events to Calendars
+//
+//  Created by Domenico on 25/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
