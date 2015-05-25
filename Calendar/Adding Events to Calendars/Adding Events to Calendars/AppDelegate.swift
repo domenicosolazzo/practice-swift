51142e9b43bb3f92eb3d19cd09f0731e244198be
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-25 15:29:52 +0200

    Inserting an event in the eventStore

--- Calendar/Adding Events to Calendars/Adding Events to Calendars/AppDelegate.swift
+++ Calendar/Adding Events to Calendars/Adding Events to Calendars/AppDelegate.swift
@@ -48,5 +48,49 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             
             return nil
     }
+    
+    // Insert event in the event store
+    func insertEventIntoStore(store: EKEventStore){
+        
+        let icloudSource = sourceInEventStore(store,
+            type: EKSourceTypeCalDAV,
+            title: "iCloud")
+        
+        if icloudSource == nil{
+            println("You have not configured iCloud for your device.")
+            return
+        }
+        
+        let calendar = calendarWithTitle("Calendar",
+            type: EKCalendarTypeCalDAV,
+            source: icloudSource!,
+            eventType: EKEntityTypeEvent)
+        
+        if calendar == nil{
+            println("Could not find the calendar we were looking for.")
+            return
+        }
+        
+        /* The event starts from today, right now */
+        let startDate = NSDate()
+        
+        /* And the event ends this time tomorrow.
+        24 hours, 60 minutes per hour and 60 seconds per minute
+        hence 24 * 60 * 60 */
+        let endDate = startDate.dateByAddingTimeInterval(24 * 60 * 60)
+        
+        if createEventWithTitle("My Concert",
+            startDate: startDate,
+            endDate: endDate,
+            inCalendar: calendar!,
+            inEventStore: store,
+            notes: ""){
+                println("Successfully created the event.")
+        } else {
+            println("Failed to create the event.")
+        }
+        
+    }
+    
 }
 
