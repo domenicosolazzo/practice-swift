1b949af59985459bb37401247f20c09bc2a49c1f
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-25 21:09:01 +0200

    Find a calendar by Title

--- Calendar/Adding Recurring Events to Calendars/Adding Recurring Events to Calendars/AppDelegate.swift
+++ Calendar/Adding Recurring Events to Calendars/Adding Recurring Events to Calendars/AppDelegate.swift
@@ -36,5 +36,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             return nil
     }
     
+    
+    // Find a calendar by Title
+    func calendarWithTitle(
+        title: String,
+        type: EKCalendarType,
+        source: EKSource,
+        eventType: EKEntityType) -> EKCalendar?{
+            
+            for calendar in source.calendarsForEntityType(eventType) as! Set<EKCalendar>{
+                if calendar.title.caseInsensitiveCompare(title) ==
+                    NSComparisonResult.OrderedSame &&
+                    calendar.type.value == type.value{
+                        return calendar
+                }
+            }
+            
+            return nil
+    }
 }
 
