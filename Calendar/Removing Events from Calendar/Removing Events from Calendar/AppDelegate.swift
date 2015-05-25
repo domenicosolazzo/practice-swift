8ca58ee79c1169175818f63614e7339fc773b33b
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-25 20:53:33 +0200

    Find a Calendar by Title

--- Calendar/Removing Events from Calendar/Removing Events from Calendar/AppDelegate.swift
+++ Calendar/Removing Events from Calendar/Removing Events from Calendar/AppDelegate.swift
@@ -36,6 +36,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             return nil
     }
     
+    // Find a calendar by Title
+    func calendarWithTitle(
+        title: String,
+        type: EKCalendarType,
+        source: EKSource,
+        eventType: EKEntityType) -> EKCalendar?{
+            
+            for calendar in source.calendarsForEntityType(eventType) as! Set<EKCalendar>{
+                if calendar.title.caseInsensitiveCompare(title) ==
+                    NSComparisonResult.OrderedSame &&
+                    calendar.type.value == type.value{
+                        return calendar
+                }
+            }
+            
+            return nil
+    }
+    
     
     
     
