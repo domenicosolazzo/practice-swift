9f6ac4c4d9ca79bf6fa4925dc59806bc38b41ae1
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-25 21:09:55 +0200

    Create a new event

--- Calendar/Adding Recurring Events to Calendars/Adding Recurring Events to Calendars/AppDelegate.swift
+++ Calendar/Adding Recurring Events to Calendars/Adding Recurring Events to Calendars/AppDelegate.swift
@@ -55,6 +55,48 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             return nil
     }
     
+    // Create a new event
+    func createEventWithTitle(
+        title: String,
+        startDate: NSDate,
+        endDate: NSDate,
+        inCalendar: EKCalendar,
+        inEventStore: EKEventStore,
+        notes: String) -> Bool{
+            
+            /* If a calendar does not allow modification of its contents
+            then we cannot insert an event into it */
+            if inCalendar.allowsContentModifications == false{
+                println("The selected calendar does not allow modifications.")
+                return false
+            }
+            
+            /* Create an event */
+            var event = EKEvent(eventStore: inEventStore)
+            event.calendar = inCalendar
+            
+            /* Set the properties of the event such as its title,
+            start date/time, end date/time, etc. */
+            event.title = title
+            event.notes = notes
+            event.startDate = startDate
+            event.endDate = endDate
+            
+            /* Finally, save the event into the calendar */
+            var error:NSError?
+            
+            let result = inEventStore.saveEvent(event,
+                span: EKSpanThisEvent,
+                error: &error)
+            
+            if result == false{
+                if let theError = error{
+                    println("An error occurred \(theError)")
+                }
+            }
+            
+            return result
+    }
     
     //- MARK: Helper Methods
     func displayAccessDenied(){
