5751c95dfb55f8e93945b450b25e1eab4c9b6cb7
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-25 15:40:57 +0200

    New project: Accessing contents in Calendars

---
+++ Calendar/Accessing the contents of Calendars/Accessing the contents of Calendars/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Accessing the contents of Calendars
+//
+//  Created by Domenico on 25/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+
+}
+
---
+++ Calendar/Accessing the contents of Calendars/Accessing the contents of Calendars/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Accessing the contents of Calendars
+//
+//  Created by Domenico on 25/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
