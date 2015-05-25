177f0c8876f9891df9ed7e920b084e9e1de0ad9a
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-25 20:53:08 +0200

    Find the source in the EventStore

--- Calendar/Removing Events from Calendar/Removing Events from Calendar/AppDelegate.swift
+++ Calendar/Removing Events from Calendar/Removing Events from Calendar/AppDelegate.swift
@@ -19,6 +19,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
     
+    // Find the source in the event store
+    func sourceInEventStore(
+        eventStore: EKEventStore,
+        type: EKSourceType,
+        title: String) -> EKSource?{
+            
+            for source in eventStore.sources() as! [EKSource]{
+                if source.sourceType.value == type.value &&
+                    source.title.caseInsensitiveCompare(title) ==
+                    NSComparisonResult.OrderedSame{
+                        return source
+                }
+            }
+            
+            return nil
+    }
+    
+    
+    
+    
+
     
 }
 
