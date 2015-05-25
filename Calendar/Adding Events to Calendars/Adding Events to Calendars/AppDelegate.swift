64d58908955c7784c6893c32d9e2a4e33b141c99
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-25 15:27:59 +0200

    Find the source in the event store

--- Calendar/Adding Events to Calendars/Adding Events to Calendars/AppDelegate.swift
+++ Calendar/Adding Events to Calendars/Adding Events to Calendars/AppDelegate.swift
@@ -14,5 +14,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    // Find the source in the eventStore
+    func sourceInEventStore(
+        eventStore: EKEventStore,
+        type: EKSourceType,
+        title: String) -> EKSource?{
+            
+            for source in eventStore.sources() as! [EKSource]{
+                if source.sourceType.value == type.value &&
+                    source.title.caseInsensitiveCompare(title) ==
+                    NSComparisonResult.OrderedSame{
+                        return source
+                }
+            }
+            
+            return nil
+    }
 }
 
