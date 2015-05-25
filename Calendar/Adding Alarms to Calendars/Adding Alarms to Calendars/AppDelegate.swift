7b821d479b9c3c61dd71b3ba094b55e76a6aef21
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-25 21:25:18 +0200

    Import EventKit

--- Calendar/Adding Alarms to Calendars/Adding Alarms to Calendars/AppDelegate.swift
+++ Calendar/Adding Alarms to Calendars/Adding Alarms to Calendars/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import EventKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,5 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         return true
     }
+    
+    
 }
 
