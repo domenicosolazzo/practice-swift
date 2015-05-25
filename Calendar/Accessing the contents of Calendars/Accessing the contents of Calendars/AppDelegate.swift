79e231e9373a52909af81aaaf5023ea9ce818a9b
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-25 18:53:17 +0200

    Finding the Source in the EventStore

--- Calendar/Accessing the contents of Calendars/Accessing the contents of Calendars/AppDelegate.swift
+++ Calendar/Accessing the contents of Calendars/Accessing the contents of Calendars/AppDelegate.swift
@@ -18,6 +18,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         return true
     }
+    
+    // Find the source in the event store
+    func sourceInEventStore(
+        eventStore: EKEventStore,
+        type: EKSourceType,
+        title: String) -> EKSource?{
+            
+            for source in eventStore.sources() as! [EKSource]{
+                if source.sourceType.value == type.value &&
+                    source.title.caseInsensitiveCompare(title) ==
+                    NSComparisonResult.OrderedSame{
+                        return source
+                }
+            }
+            
+            return nil
+    }
 
 }
 
