42ba09c6e4b318a85d55e48aa4f1dec7fd2d7fe7
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-26 20:39:14 +0200

    New Project: Inserting a group entry into the AddressBook

---
+++ Address Book/Inserting a group entry into the AddressBook/Inserting a group entry into the AddressBook/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Inserting a group entry into the AddressBook
+//
+//  Created by Domenico on 26/05/15.
+//  License MIT
+//
+
+import UIKit
+import AddressBook
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    var addressBook: ABAddressBookRef?
+    
+    
+
+}
+
---
+++ Address Book/Inserting a group entry into the AddressBook/Inserting a group entry into the AddressBook/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Inserting a group entry into the AddressBook
+//
+//  Created by Domenico on 26/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
