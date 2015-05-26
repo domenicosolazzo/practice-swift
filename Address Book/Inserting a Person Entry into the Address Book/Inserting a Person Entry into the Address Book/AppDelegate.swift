20180164cf4e6ea17ecfc5926c96879bcf8be608
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-26 20:25:24 +0200

    New Project: Inserting a person entry into the Address Book

---
+++ Address Book/Inserting a Person Entry into the Address Book/Inserting a Person Entry into the Address Book/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Inserting a Person Entry into the Address Book
+//
+//  Created by Domenico on 26/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
---
+++ Address Book/Inserting a Person Entry into the Address Book/Inserting a Person Entry into the Address Book/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Inserting a Person Entry into the Address Book
+//
+//  Created by Domenico on 26/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
