f41a9beb82be6b261530f1f9d4e432c8915a3673
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-26 21:49:07 +0200

    New project: Setting and Retrieving image data

---
+++ Address Book/Retrieving and Setting a Person's Address Book Retrieving and Setting a Person's Address Book/Retrieving and Setting a Person's Address Book Retrieving and Setting a Person's Address Book/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Retrieving and Setting a Person's Address Book Retrieving and Setting a Person's Address Book
+//
+//  Created by Domenico on 26/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+}
+
---
+++ Address Book/Retrieving and Setting a Person's Address Book Retrieving and Setting a Person's Address Book/Retrieving and Setting a Person's Address Book Retrieving and Setting a Person's Address Book/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Retrieving and Setting a Person's Address Book Retrieving and Setting a Person's Address Book
+//
+//  Created by Domenico on 26/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
