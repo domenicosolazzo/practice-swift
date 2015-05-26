d2ba057263beabeb1fe1212a44e102e2ca5ff36c
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-26 20:51:54 +0200

    Adding a new person

--- Address Book/Adding a person to a group/Adding a person to a group/AppDelegate.swift
+++ Address Book/Adding a person to a group/Adding a person to a group/AppDelegate.swift
@@ -130,6 +130,59 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         
     }
+    
+    //- MARK: Person
+    func newPersonWithFirstName(firstName: String,
+        lastName: String,
+        inAddressBook: ABAddressBookRef) -> ABRecordRef?{
+            
+            let person: ABRecordRef = ABPersonCreate().takeRetainedValue()
+            
+            let couldSetFirstName = ABRecordSetValue(person,
+                kABPersonFirstNameProperty,
+                firstName as CFTypeRef,
+                nil)
+            
+            let couldSetLastName = ABRecordSetValue(person,
+                kABPersonLastNameProperty,
+                lastName as CFTypeRef,
+                nil)
+            
+            var error: Unmanaged<CFErrorRef>? = nil
+            
+            let couldAddPerson = ABAddressBookAddRecord(inAddressBook, person, &error)
+            
+            if couldAddPerson{
+                println("Successfully added the person.")
+            } else {
+                println("Failed to add the person.")
+                return nil
+            }
+            
+            if ABAddressBookHasUnsavedChanges(inAddressBook){
+                
+                var error: Unmanaged<CFErrorRef>? = nil
+                let couldSaveAddressBook = ABAddressBookSave(inAddressBook, &error)
+                
+                if couldSaveAddressBook{
+                    println("Successfully saved the address book.")
+                } else {
+                    println("Failed to save the address book.")
+                }
+            }
+            
+            if couldSetFirstName && couldSetLastName{
+                println("Successfully set the first name " +
+                    "and the last name of the person")
+            } else {
+                println("Failed to set the first name and/or " +
+                    "the last name of the person")
+            }
+            
+            return person
+            
+    }
+
 
 
     
