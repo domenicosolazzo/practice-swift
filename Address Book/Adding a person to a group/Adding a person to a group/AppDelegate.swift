9dea19db7b5825f34a6a91f410ea850c17032cf5
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-26 20:47:08 +0200

    New project: Adding a person to a group

---
+++ Address Book/Adding a person to a group/Adding a person to a group/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Adding a person to a group
+//
+//  Created by Domenico on 26/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        return true
+    }
+}
+
---
+++ Address Book/Adding a person to a group/Adding a person to a group/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Adding a person to a group
+//
+//  Created by Domenico on 26/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
