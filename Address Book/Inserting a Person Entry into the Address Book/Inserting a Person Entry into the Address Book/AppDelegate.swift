8682b231bdbd31e6c951edb27b43800fa43a9ef3
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-26 20:28:40 +0200

    Authorising and adding a new contact person

--- Address Book/Inserting a Person Entry into the Address Book/Inserting a Person Entry into the Address Book/AppDelegate.swift
+++ Address Book/Inserting a Person Entry into the Address Book/Inserting a Person Entry into the Address Book/AppDelegate.swift
@@ -13,10 +13,62 @@ import AddressBook
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var addressBook: ABAddressBookRef?
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+        switch ABAddressBookGetAuthorizationStatus(){
+        case .Authorized:
+            println("Already authorized")
+            createAddressBook()
+            /* Now you can use the address book */
+            self.newPersonWithFirstName("Domenico", lastName: "Solazzo", inAddressBook: addressBook!)
+        case .Denied:
+            println("You are denied access to address book")
+            
+        case .NotDetermined:
+            createAddressBook()
+            if let theBook: ABAddressBookRef = addressBook{
+                ABAddressBookRequestAccessWithCompletion(theBook,
+                    {(granted: Bool, error: CFError!) in
+                        
+                        if granted{
+                            println("Access is granted")
+                        } else {
+                            println("Access is not granted")
+                        }
+                        
+                })
+            }
+            
+        case .Restricted:
+            println("Access is restricted")
+            
+        default:
+            println("Unhandled")
+        }
+
+        
         return true
         
+    }
+    
+    func createAddressBook(){
+        var error: Unmanaged<CFError>?
+        
+        addressBook = ABAddressBookCreateWithOptions(nil,
+            &error).takeRetainedValue()
+        
+        /* You can use the address book here */
+        func createAddressBook(){
+            var error: Unmanaged<CFError>?
+            
+            addressBook = ABAddressBookCreateWithOptions(nil,
+                &error).takeRetainedValue()
+            
+            /* You can use the address book here */
+            self.newPersonWithFirstName("Domenico", lastName: "Solazzo", inAddressBook: addressBook!)
+            
+        }
     }
     
     func newPersonWithFirstName(firstName: String,
