be8176d787b455ad05612906e3adcad76910335b
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-26 19:44:54 +0200

    New Project: Requesting all people from the address book

---
+++ Address Book/Requesting all the people in the AddressBook/Requesting all the people in the AddressBook/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Requesting all the people in the AddressBook
+//
+//  Created by Domenico on 26/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+
+}
+
---
+++ Address Book/Requesting all the people in the AddressBook/Requesting all the people in the AddressBook/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Requesting all the people in the AddressBook
+//
+//  Created by Domenico on 26/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+}
+
