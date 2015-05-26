2039fd42dc26a6d757adb58f7282583ceefa9225
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-26 21:05:45 +0200

    Method for checking if a person is the address book

--- Address Book/Searching the address book/Searching the address book/AppDelegate.swift
+++ Address Book/Searching the address book/Searching the address book/AppDelegate.swift
@@ -19,6 +19,64 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             &error).takeRetainedValue() as ABAddressBookRef
         }()
 
+    func doesPersonExistWithFirstName(firstName paramFirstName: String,
+        lastName paramLastName: String,
+        inAddressBook addressBook: ABAddressBookRef) -> Bool{
+            
+            var exists = false
+            let people = ABAddressBookCopyArrayOfAllPeople(
+                addressBook).takeRetainedValue() as NSArray as [ABRecordRef]
+            
+            for person: ABRecordRef in people{
+                
+                let firstName = ABRecordCopyValue(person,
+                    kABPersonFirstNameProperty).takeRetainedValue() as! String
+                
+                let lastName = ABRecordCopyValue(person,
+                    kABPersonLastNameProperty).takeRetainedValue() as! String
+                
+                if firstName == paramFirstName &&
+                    lastName == paramLastName{
+                        return true
+                }
+                
+            }
+            return false
+    }
+    
+    func doesGroupExistWithGroupName(name: String,
+        inAddressBook addressBook: ABAddressBookRef) -> Bool{
+            
+            let groups = ABAddressBookCopyArrayOfAllGroups(
+                addressBook).takeRetainedValue() as NSArray as [ABRecordRef]
+            
+            for group: ABRecordRef in groups{
+                
+                let groupName = ABRecordCopyValue(group,
+                    kABGroupNameProperty).takeRetainedValue() as! String
+                
+                if groupName == name{
+                    return true
+                }
+                
+            }
+            return false
+    }
+    
+    func doesPersonExistWithFullName(fullName: String,
+        inAddressBook addressBook: ABAddressBookRef) -> Bool{
+            
+            let people = ABAddressBookCopyPeopleWithName(addressBook,
+                fullName as NSString as CFStringRef).takeRetainedValue() as NSArray
+            
+            if people.count > 0{
+                return true
+            }
+            
+            return false
+            
+    }
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         return true
     }
