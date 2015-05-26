8c15e91cab18f86e9c1e070715f03747e4107f5d
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-26 21:49:45 +0200

    address book variable

--- Address Book/Retrieving and Setting a Person's Address Book Retrieving and Setting a Person's Address Book/Retrieving and Setting a Person's Address Book Retrieving and Setting a Person's Address Book/AppDelegate.swift
+++ Address Book/Retrieving and Setting a Person's Address Book Retrieving and Setting a Person's Address Book/Retrieving and Setting a Person's Address Book Retrieving and Setting a Person's Address Book/AppDelegate.swift
@@ -13,7 +13,11 @@ import AddressBook
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    lazy var addressBook: ABAddressBookRef = {
+        var error: Unmanaged<CFError>?
+        return ABAddressBookCreateWithOptions(nil,
+            &error).takeRetainedValue() as ABAddressBookRef
+        }()
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         return true
