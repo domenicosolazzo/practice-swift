27874fa54a1269a61f72df49cf6354b82ec927a1
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-26 19:36:50 +0200

    Adding authorisation for accessing the address book

--- Address Book/Requesting accesso to the Address Book/Requesting accesso to the Address Book/AppDelegate.swift
+++ Address Book/Requesting accesso to the Address Book/Requesting accesso to the Address Book/AppDelegate.swift
@@ -15,6 +15,40 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     var addressBook: ABAddressBookRef?
     
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+        switch ABAddressBookGetAuthorizationStatus(){
+        case .Authorized:
+            println("Already authorized")
+            createAddressBook()
+            /* Now you can use the address book */
+        case .Denied:
+            println("You are denied access to address book")
+            
+        case .NotDetermined:
+            createAddressBook()
+            if let theBook: ABAddressBookRef = addressBook{
+                ABAddressBookRequestAccessWithCompletion(theBook,
+                    {(granted: Bool, error: CFError!) in
+                        
+                        if granted{
+                            println("Access is granted")
+                        } else {
+                            println("Access is not granted")
+                        }
+                        
+                })
+            }
+            
+        case .Restricted:
+            println("Access is restricted")
+            
+        default:
+            println("Unhandled")
+        }
+        
+        return true
+    }
+    
     func createAddressBook(){
         var error: Unmanaged<CFError>?
         
