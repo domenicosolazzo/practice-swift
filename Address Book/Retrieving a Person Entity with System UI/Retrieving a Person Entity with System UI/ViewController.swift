ace35c615d2f990a2916b88665992383d6114ce3
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-26 18:39:04 +0200

    Implementing the delegate methods

--- Address Book/Retrieving a Person Entity with System UI/Retrieving a Person Entity with System UI/ViewController.swift
+++ Address Book/Retrieving a Person Entity with System UI/Retrieving a Person Entity with System UI/ViewController.swift
@@ -15,10 +15,22 @@ class ViewController: UIViewController,
     //- MARK: Private variables
     let personPicker: ABPeoplePickerNavigationController
     
+    //- MARK: Constructors
     required init(coder aDecoder: NSCoder) {
         personPicker = ABPeoplePickerNavigationController()
         super.init(coder: aDecoder)
         personPicker.peoplePickerDelegate = self
+    }
+    
+    //-  MARK: ABPeoplePickerNavigationControllerDelegate
+    // When the user cancel the action
+    func peoplePickerNavigationControllerDidCancel(peoplePicker: ABPeoplePickerNavigationController!) {
+        println("User cancelled the action...")
+    }
+    
+    // When a contact has been selected
+    func peoplePickerNavigationController(peoplePicker: ABPeoplePickerNavigationController!, didSelectPerson person: ABRecord!, property: ABPropertyID, identifier: ABMultiValueIdentifier) {
+        println("User selected a contact")
     }
 }
 
