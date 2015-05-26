57a47c1621102bbbc78d818491cb940dcc2f8614
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-26 18:46:08 +0200

    Implemented method when the user has been selected

--- Address Book/Retrieving a Person Entity with System UI/Retrieving a Person Entity with System UI/ViewController.swift
+++ Address Book/Retrieving a Person Entity with System UI/Retrieving a Person Entity with System UI/ViewController.swift
@@ -31,6 +31,23 @@ class ViewController: UIViewController,
     // When a contact has been selected
     func peoplePickerNavigationController(peoplePicker: ABPeoplePickerNavigationController!, didSelectPerson person: ABRecord!, property: ABPropertyID, identifier: ABMultiValueIdentifier) {
         println("User selected a contact")
+        
+        /* Do we know which picker this is? */
+        if peoplePicker != personPicker{
+            return
+        }
+        
+        let phones: ABMultiValueRef = ABRecordCopyValue(person,
+            kABPersonPhoneProperty).takeRetainedValue() // If you want emails, use kABPersonEmailProperty
+        let countOfPhones =  ABMultiValueGetCount(phones)
+        
+        for index in 0..<countOfPhones{
+            let phone = ABMultiValueCopyValueAtIndex(phones,
+                index).takeRetainedValue() as! String
+            
+            println(phone)
+            
+        }
     }
     
     @IBAction func performPickPerson(sender: UIButton) {
