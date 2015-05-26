e0ae2dbf9ce1b2a38970baedf411c569f2037ea9
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-26 21:50:23 +0200

    Creating new person

--- Address Book/Retrieving and Setting a Person's Address Book Retrieving and Setting a Person's Address Book/Retrieving and Setting a Person's Address Book Retrieving and Setting a Person's Address Book/AppDelegate.swift
+++ Address Book/Retrieving and Setting a Person's Address Book Retrieving and Setting a Person's Address Book/Retrieving and Setting a Person's Address Book Retrieving and Setting a Person's Address Book/AppDelegate.swift
@@ -30,5 +30,56 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let image = UIImage(data: data)
         return image
     }
+    
+    func newPersonWithFirstName(firstName: String,
+        lastName: String,
+        inAddressBook: ABAddressBookRef) -> ABRecordRef?{
+            
+            let person: ABRecordRef = ABPersonCreate().takeRetainedValue()
+            
+            let couldSetFirstName = ABRecordSetValue(person,
+                kABPersonFirstNameProperty,
+                firstName as CFTypeRef,
+                nil)
+            
+            let couldSetLastName = ABRecordSetValue(person,
+                kABPersonLastNameProperty,
+                lastName as CFTypeRef,
+                nil)
+            
+            var error: Unmanaged<CFErrorRef>? = nil
+            
+            let couldAddPerson = ABAddressBookAddRecord(inAddressBook, person, &error)
+            
+            if couldAddPerson{
+                println("Successfully added the person")
+            } else {
+                println("Failed to add the person.")
+                return nil
+            }
+            
+            if ABAddressBookHasUnsavedChanges(inAddressBook){
+                
+                var error: Unmanaged<CFErrorRef>? = nil
+                let couldSaveAddressBook = ABAddressBookSave(inAddressBook, &error)
+                
+                if couldSaveAddressBook{
+                    println("Successfully saved the address book")
+                } else {
+                    println("Failed to save the address book.")
+                }
+            }
+            
+            if couldSetFirstName && couldSetLastName{
+                println("Successfully set the first name " +
+                    "and the last name of the person")
+            } else {
+                println("Failed to set the first name and/or " +
+                    "the last name of the person")
+            }
+            
+            return person
+            
+    }
 }
 
