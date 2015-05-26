a23ba60b62998f209103bcf61c77cd8d71eef78d
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-26 21:01:47 +0200

    New project: Searching the AddressBook

---
+++ Address Book/Searching the address book/Searching the address book/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Searching the address book
+//
+//  Created by Domenico on 26/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+}
+
---
+++ Address Book/Searching the address book/Searching the address book/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Searching the address book
+//
+//  Created by Domenico on 26/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
