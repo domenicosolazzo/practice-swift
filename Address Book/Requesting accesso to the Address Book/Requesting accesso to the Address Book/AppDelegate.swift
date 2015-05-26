135695cf63a73143d7c2fe17471b0c74ff3f583e
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-26 19:16:26 +0200

    New Project: Requesting accesso to the Address Book

---
+++ Address Book/Requesting accesso to the Address Book/Requesting accesso to the Address Book/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Requesting accesso to the Address Book
+//
+//  Created by Domenico on 26/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
---
+++ Address Book/Requesting accesso to the Address Book/Requesting accesso to the Address Book/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Requesting accesso to the Address Book
+//
+//  Created by Domenico on 26/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
