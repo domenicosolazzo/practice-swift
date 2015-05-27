8fea90a6a5b5d788b647353f523a090a54e9567e
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-27 18:28:33 +0200

    Reading and saving string

--- File Management/Writing to and Reading from Files/Writing to and Reading from Files/AppDelegate.swift
+++ File Management/Writing to and Reading from Files/Writing to and Reading from Files/AppDelegate.swift
@@ -13,6 +13,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    func saveAndReadString(){
+        let someText = NSString(string: "Put some string here")
+        let destinationPath = NSTemporaryDirectory() + "MyFile.txt"
+        var error:NSError?
+        
+        let written = someText.writeToFile(destinationPath,
+            atomically: true,
+            encoding: NSUTF8StringEncoding,
+            error: &error)
+        
+        if written{
+            println("Successfully stored the file at path \(destinationPath)")
+        } else {
+            if let errorValue = error{
+                println("An error occurred: \(errorValue)")
+            }
+        }
+    }
 
 }
 
