70727efca16f3e34fb5437d43fdd0aea2340fe0d
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-27 19:17:00 +0200

    Saving the files

--- File Management/Saving Objects to File/Saving Objects to File/AppDelegate.swift
+++ File Management/Saving Objects to File/Saving Objects to File/AppDelegate.swift
@@ -12,6 +12,25 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+        let path = NSTemporaryDirectory() + "person"
+        var firstPerson = Person()
+        NSKeyedArchiver.archiveRootObject(firstPerson, toFile: path)
+        
+        var secondPerson = NSKeyedUnarchiver.unarchiveObjectWithFile(path)
+            as! Person!
+        
+        if firstPerson == secondPerson{
+            println("Both persons are the same")
+        } else {
+            println("Could not read the archive")
+        }
+        
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        // Override point for customization after application launch.
+        self.window!.backgroundColor = UIColor.whiteColor()
+        self.window!.makeKeyAndVisible()
+        return true
+    }
 }
 
