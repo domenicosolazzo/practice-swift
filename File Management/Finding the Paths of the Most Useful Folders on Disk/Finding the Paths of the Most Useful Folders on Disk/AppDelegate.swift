27fed1bbac8556862d7c97c2758aec4856b0dde0
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-27 18:12:21 +0200

    Get the document folder

--- File Management/Finding the Paths of the Most Useful Folders on Disk/Finding the Paths of the Most Useful Folders on Disk/AppDelegate.swift
+++ File Management/Finding the Paths of the Most Useful Folders on Disk/Finding the Paths of the Most Useful Folders on Disk/AppDelegate.swift
@@ -12,5 +12,23 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+        return true
+    }
+    
+    func getDocumentFolder() -> NSURL?{
+        let fileManager = NSFileManager()
+        let urls = fileManager.URLsForDirectory(
+            NSSearchPathDirectory.DocumentDirectory,
+            inDomains: NSSearchPathDomainMask.UserDomainMask)
+        if urls.count > 0{
+            let url = urls[0] as! NSURL
+            println(url)
+            return url
+        }
+        
+        return null
+    }
 }
 
