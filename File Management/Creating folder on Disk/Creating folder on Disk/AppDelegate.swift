fa49c32256ae7805fd9bf3399eabd3b0dd88821e
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-27 18:32:46 +0200

    New project: Creating folder on disk

---
+++ File Management/Creating folder on Disk/Creating folder on Disk/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Creating folder on Disk
+//
+//  Created by Domenico on 27/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+
+}
+
---
+++ File Management/Creating folder on Disk/Creating folder on Disk/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Creating folder on Disk
+//
+//  Created by Domenico on 27/05/15.
+//  Copyright (c) 2015 Domenico. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
