70e4f60b4a2a4427b57403fde6c05e816c0282a8
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-27 18:43:22 +0200

    New project: Enumerating Files and Folders

---
+++ File Management/Enumerating Files and Folders/Enumerating Files and Folders/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Enumerating Files and Folders
+//
+//  Created by Domenico on 27/05/15.
+//  License MIT.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
---
+++ File Management/Enumerating Files and Folders/Enumerating Files and Folders/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Enumerating Files and Folders
+//
+//  Created by Domenico on 27/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
