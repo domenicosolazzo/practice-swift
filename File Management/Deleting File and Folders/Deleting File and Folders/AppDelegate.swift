8b71390b9f35c3fbfd74a6952c2917ba5db4fbfa
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-27 18:48:02 +0200

    New project: Deleting File and Folders

---
+++ File Management/Deleting File and Folders/Deleting File and Folders/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Deleting File and Folders
+//
+//  Created by Domenico on 27/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
