632c0318633b808e89a206580106b486a1c129f1
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-29 19:15:23 +0200

    New project: Setting up an app with CloudKit

---
+++ iCloud/Setting Up an App with CloudKit/Setting Up an App with CloudKit/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Setting Up an App with CloudKit
+//
+//  Created by Domenico on 29/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
---
+++ iCloud/Setting Up an App with CloudKit/Setting Up an App with CloudKit/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Setting Up an App with CloudKit
+//
+//  Created by Domenico on 29/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
