6e925c3113f9f94d013a56dbb2b0020e34f847f1
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-29 20:03:06 +0200

    New project: Querying the Cloud with CloudKit

---
+++ iCloud/Querying the Cloud with CloudKit/Querying the Cloud with CloudKit/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Querying the Cloud with CloudKit
+//
+//  Created by Domenico on 29/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
---
+++ iCloud/Querying the Cloud with CloudKit/Querying the Cloud with CloudKit/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Querying the Cloud with CloudKit
+//
+//  Created by Domenico on 29/05/15.
+//  License MIT
+//
+
+import UIKit
+import CloudKit
+
+class ViewController: UIViewController {
+
+}
+
