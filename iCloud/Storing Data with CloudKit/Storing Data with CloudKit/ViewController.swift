b83425705d5a60f6393ab5afbcb50489301a5079
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-29 19:34:37 +0200

    Car type

--- iCloud/Storing Data with CloudKit/Storing Data with CloudKit/ViewController.swift
+++ iCloud/Storing Data with CloudKit/Storing Data with CloudKit/ViewController.swift
@@ -13,5 +13,21 @@ class ViewController: UIViewController {
 
    let database = CKContainer.defaultContainer().privateCloudDatabase
     
+    enum CarType: String{
+        case Hatchback = "Hatchback"
+        case Estate = "Estate"
+        
+        func zoneId() -> CKRecordZoneID{
+            let zoneId = CKRecordZoneID(zoneName: self.rawValue,
+                ownerName: CKOwnerDefaultName)
+            return zoneId
+        }
+        
+        func zone() -> CKRecordZone{
+            return CKRecordZone(zoneID: self.zoneId())
+        }
+        
+    }
+    
 }
 
