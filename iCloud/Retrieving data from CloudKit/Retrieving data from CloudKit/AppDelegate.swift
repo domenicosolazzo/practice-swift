69c0b4ed885518ea64e80cfc112d73a965ab85ce
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-29 19:51:08 +0200

    New project: Retrieving data from CloudKit

---
+++ iCloud/Retrieving data from CloudKit/Retrieving data from CloudKit/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Retrieving data from CloudKit
+//
+//  Created by Domenico on 29/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+}
+
---
+++ iCloud/Retrieving data from CloudKit/Retrieving data from CloudKit/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Retrieving data from CloudKit
+//
+//  Created by Domenico on 29/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
