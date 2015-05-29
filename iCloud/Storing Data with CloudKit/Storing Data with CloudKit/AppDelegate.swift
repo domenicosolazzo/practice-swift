257dd44c0d642dc053f7a62bac0013cab3cd4958
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-29 19:24:33 +0200

    New project: Storing the data with cloudkit

---
+++ iCloud/Storing Data with CloudKit/Storing Data with CloudKit/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Storing Data with CloudKit
+//
+//  Created by Domenico on 29/05/15.
+//  License MIT
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
---
+++ iCloud/Storing Data with CloudKit/Storing Data with CloudKit/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Storing Data with CloudKit
+//
+//  Created by Domenico on 29/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   
+}
+
