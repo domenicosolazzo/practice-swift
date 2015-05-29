7b84597f1d33dfaa170ce797c7d1d737f9296711
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-29 20:20:44 +0200

    Fetch the records

--- iCloud/Querying the Cloud with CloudKit/Querying the Cloud with CloudKit/ViewController.swift
+++ iCloud/Querying the Cloud with CloudKit/Querying the Cloud with CloudKit/ViewController.swift
@@ -35,6 +35,50 @@ class ViewController: UIViewController {
         }
     }
     
+    func recordFetchBlock(record: CKRecord!){
+        
+        println("Fetched a record = \(record)")
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if isIcloudAvailable(){
+            displayAlertWithTitle("iCloud", message: "iCloud is not available." +
+                " Please sign into your iCloud account and restart this app")
+            return
+        }
+        
+        let makerToLookFor = "Volvo"
+        let smallestYearToLookFor = 2013
+        
+        let predicate = NSPredicate(format: "maker = %@ AND year >= %@",
+            makerToLookFor, NSNumber(integer: smallestYearToLookFor))
+        
+        let query = CKQuery(recordType: "MyCar", predicate: predicate)
+        
+        let operation = CKQueryOperation(query: query)
+        operation.recordFetchedBlock = recordFetchBlock
+        operation.queryCompletionBlock = {[weak self]
+            (cursor: CKQueryCursor!, error: NSError!) in
+            if cursor != nil{
+                /* There is so much data that a cursor came back to us and we will
+                need to fetch the rest of the results in a separate operation */
+                println("A cursor was sent to us. Fetching the rest of the records...")
+                let newOperation = CKQueryOperation(cursor: cursor)
+                newOperation.recordFetchedBlock = self!.recordFetchBlock
+                newOperation.queryCompletionBlock = operation.queryCompletionBlock
+                self!.operationQueue.addOperation(newOperation)
+            } else {
+                println("No cursor came back. We've fetched all the data")
+            }
+        }
+        
+        operationQueue.addOperation(operation)
+        
+    }
+    
     /* Just a little method to help us display alert dialogs to the user */
     func displayAlertWithTitle(title: String, message: String){
         let controller = UIAlertController(title: title,
