7a5666ff1d95f53dcb59e99415fa883eb978d109
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-29 20:19:29 +0200

    Car types

--- iCloud/Querying the Cloud with CloudKit/Querying the Cloud with CloudKit/ViewController.swift
+++ iCloud/Querying the Cloud with CloudKit/Querying the Cloud with CloudKit/ViewController.swift
@@ -13,5 +13,17 @@ class ViewController: UIViewController {
 
     let database = CKContainer.defaultContainer().privateCloudDatabase
     lazy var operationQueue = NSOperationQueue()
+    
+    /* Defines our car types */
+    enum CarType: String{
+        case Estate = "Estate"
+        
+        func zoneId() -> CKRecordZoneID{
+            let zoneId = CKRecordZoneID(zoneName: self.rawValue,
+                ownerName: CKOwnerDefaultName)
+            return zoneId
+        }
+        
+    }
 }
 
