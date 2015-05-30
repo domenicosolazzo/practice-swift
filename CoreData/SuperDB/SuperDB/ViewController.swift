9bb5248b6c23cd671cba13f8c3d7f7628346aace
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-30 13:27:16 +0200

    New project: SuperDB

---
+++ CoreData/SuperDB/SuperDB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SuperDB
+//
+//  Created by Domenico on 30/05/15.
+//  License MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
