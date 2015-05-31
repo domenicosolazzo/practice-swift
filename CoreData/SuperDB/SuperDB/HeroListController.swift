5b62950a2c886ded1566d1cbbc71c504b7480a1e
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-31 19:54:29 +0200

    Saving the tab bar index

--- CoreData/SuperDB/SuperDB/HeroListController.swift
+++ CoreData/SuperDB/SuperDB/HeroListController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class HeroListController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class HeroListController: UIViewController, UITableViewDataSource,
+            UITableViewDelegate, UITabBarDelegate {
 
     @IBOutlet weak var heroTableView: UITableView!
     @IBOutlet weak var heroTabBar: UITabBar!
@@ -29,6 +30,14 @@ class HeroListController: UIViewController, UITableViewDataSource, UITableViewDe
         let item = heroTabBar.items?[selectedTab] as! UITabBarItem
         heroTabBar.selectedItem = item
     }
+    //- MARK: UITabBarDelegate
+    func tabBar(tabBar: UITabBar, didSelectItem item: UITabBarItem!) {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let items: NSArray = heroTabBar.items!
+        let tabIndex = items.indexOfObject(item)
+        defaults.setInteger(tabIndex, forKey: kSelectedTabDefaultsKey)
+    }
+    
     //- MARK: UITableViewDataSource
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
