2229fb62403c39b9b276c2d39e34c5a977a3663a
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-31 16:34:14 +0200

    Adding a table view controller

---
+++ CoreData/SuperDB/SuperDB/HeroListController.swift
@@ -0,0 +1,13 @@
+//
+//  HeroListController.swift
+//  SuperDB
+//
+//  Created by Domenico on 31/05/15.
+//  Copyright (c) 2015 Domenico. All rights reserved.
+//
+
+import Cocoa
+
+class HeroListController: UITableViewController {
+
+}
