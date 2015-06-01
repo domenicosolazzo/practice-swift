b003e89a447cc3290035bc1aa8f44fb3434fab6a
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-06-01 20:17:03 +0200

    Adding a custom table view cell

---
+++ CoreData/SuperDB/SuperDB/SuperDBEditCell.swift
@@ -0,0 +1,24 @@
+//
+//  SuperDBEditCell.swift
+//  SuperDB
+//
+//  Created by Domenico on 01/06/15.
+//  Copyright (c) 2015 Domenico. All rights reserved.
+//
+
+import UIKit
+
+class SuperDBEditCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
