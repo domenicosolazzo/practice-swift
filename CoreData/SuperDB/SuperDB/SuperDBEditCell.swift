183067e3774a6ef2f14058192b04117046164784
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-06-01 20:19:27 +0200

    Adding the ui of the cell

--- CoreData/SuperDB/SuperDB/SuperDBEditCell.swift
+++ CoreData/SuperDB/SuperDB/SuperDBEditCell.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class SuperDBEditCell: UITableViewCell {
 
+    var label: UILabel!
+    var textField: UITextField!
+    let kLabelTextColor = UIColor(red: 0.321569, green: 0.4, blue: 0.568627, alpha: 1)
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -19,6 +23,31 @@ class SuperDBEditCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        self.selectionStyle = .None
+        
+        self.label = UILabel(frame: CGRectMake(12, 15, 67, 15))
+        self.label.backgroundColor = UIColor.clearColor()
+        self.label.font = UIFont.boldSystemFontOfSize(UIFont.smallSystemFontSize())
+        self.label.textColor = kLabelTextColor
+        self.label.text = "label"
+        self.contentView.addSubview(self.label)
+        
+        self.textField = UITextField(frame: CGRectMake(93, 13, 170, 19))
+        self.textField.backgroundColor = UIColor.clearColor()
+        self.textField.clearButtonMode = .WhileEditing
+        self.textField.enabled = false
+        self.textField.font = UIFont.boldSystemFontOfSize(UIFont.systemFontSize())
+        self.textField.text = "Title"
+        self.contentView.addSubview(self.textField)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
     }
 
 }
