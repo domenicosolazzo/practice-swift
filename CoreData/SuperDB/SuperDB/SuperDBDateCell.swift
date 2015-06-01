8054e623764635add15c20bd79fbb5d6c14d7093
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-06-01 20:38:02 +0200

    Implementing a SuperDBEditCell for dates

---
+++ CoreData/SuperDB/SuperDB/SuperDBDateCell.swift
@@ -0,0 +1,58 @@
+//
+//  SuperDBDateCell.swift
+//  SuperDB
+//
+//  Created by Domenico on 01/06/15.
+//  Copyright (c) 2015 Domenico. All rights reserved.
+//
+
+import UIKit
+
+let __dateFormatter = NSDateFormatter()
+
+class SuperDBDateCell: SuperDBEditCell {
+
+    private var datePicker: UIDatePicker!
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        __dateFormatter.dateStyle = .MediumStyle
+        
+        self.textField.clearButtonMode = .Never
+        self.datePicker = UIDatePicker(frame: CGRectZero)
+        self.datePicker.datePickerMode = .Date
+        self.datePicker.addTarget(self, action: "datePickerChanged:", forControlEvents: .ValueChanged)
+        self.textField.inputView = self.datePicker
+    }
+    
+    //MARK: - SuperDBEditCell Overrides
+    override var value:AnyObject! {
+        get{
+            if self.textField.text == nil || count(self.textField.text) == 0 {
+                return nil
+            } else {
+                return self.datePicker.date as NSDate!
+            }
+        }
+        set{
+            if let _value = newValue as? NSDate {
+                self.datePicker.date = _value
+                self.textField.text = __dateFormatter.stringFromDate(_value)
+            } else {
+                self.textField.text = nil
+            }
+        }
+    }
+    
+    //MARK: (Private) Instance Methods
+    @IBAction func datePickerChanged(sender: AnyObject){
+        var date = self.datePicker.date
+        self.value = date
+        self.textField.text = __dateFormatter.stringFromDate(date)
+    }
+}
