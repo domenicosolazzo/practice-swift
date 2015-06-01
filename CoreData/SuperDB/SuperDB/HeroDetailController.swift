193dd8f44f0dee6b6cbd3d176318f6d4029de04f
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-06-01 18:54:57 +0200

    Adding HeroDetailController

---
+++ CoreData/SuperDB/SuperDB/HeroDetailController.swift
@@ -0,0 +1,14 @@
+//
+//  HeroDetailController.swift
+//  SuperDB
+//
+//  Created by Domenico on 01/06/15.
+//  License MIT
+//
+
+import UIKit
+import CoreData
+
+class HeroDetailController: UITableViewController {
+
+}
