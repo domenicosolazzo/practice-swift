aef477118973b0b1485d7ff53a1ba6f1c226f2c8
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-06-02 08:08:29 +0200

    Awake from Insert: Method is called after a new object is inserted in the context

--- CoreData/SuperDB/SuperDB/Hero.swift
+++ CoreData/SuperDB/SuperDB/Hero.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import CoreData
+import UIKit
 
 let kHeroValidationDomain = "com.domenicosolazzo.superDB.HeroValidationDomain"
 let kHeroValidationBirthday = 1000
@@ -21,5 +22,10 @@ class Hero: NSManagedObject {
     @NSManaged var name: String
     @NSManaged var secretIdentity: String
     @NSManaged var sex: String
+    
+    override func awakeFromInsert() {
+        super.awakeFromInsert()
+        self.favoriteColor = UIColor(red:1, green:1, blue:1, alpha:1)
+    }
 
 }
