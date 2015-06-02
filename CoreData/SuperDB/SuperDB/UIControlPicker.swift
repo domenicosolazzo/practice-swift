b4c9a91f78c800ec5ab0e67e62a9c1c207f54fdf
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-06-02 19:11:33 +0200

    New control picker

---
+++ CoreData/SuperDB/SuperDB/UIControlPicker.swift
@@ -0,0 +1,15 @@
+//
+//  UIControlPicker.swift
+//  SuperDB
+//
+//  Created by Domenico on 02/06/15.
+//  Copyright (c) 2015 Domenico. All rights reserved.
+//
+
+import UIKit
+
+class UIControlPicker: UIControl {
+
+    var _color: UIColor!
+
+}
