4cbf41cb18e4fbc4d3fe21ffcde3e5663a9e7a82
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-06-02 19:18:35 +0200

    Initi method

--- CoreData/SuperDB/SuperDB/SuperDBColorCell.swift
+++ CoreData/SuperDB/SuperDB/SuperDBColorCell.swift
@@ -16,5 +16,13 @@ class SuperDBColorCell: SuperDBEditCell {
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        self.colorPicker = UIColorPicker(frame: CGRectMake(0, 0, 320, 216))
+        self.colorPicker.addTarget(self, action: "colorPickerChanged:", forControlEvents: .ValueChanged)
+        self.textField.inputView = self.colorPicker
+    }
 
 }
