a2afc58fa4ebbbb1d9253ed70e5384d0d8ffeaf0
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-06-02 07:31:08 +0200

    Adding migration options for the data model

--- CoreData/SuperDB/SuperDB/AppDelegate.swift
+++ CoreData/SuperDB/SuperDB/AppDelegate.swift
@@ -65,7 +65,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("SuperDB.sqlite")
         var error: NSError? = nil
         var failureReason = "There was an error creating or loading the application's saved data."
-        if coordinator!.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil, error: &error) == nil {
+        // Migration options
+        var options = [
+            NSMigratePersistentStoresAutomaticallyOption:true,
+            NSInferMappingModelAutomaticallyOption:true
+        ]
+        if coordinator!.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: options, error: &error) == nil {
             coordinator = nil
             // Report any error we got.
             var dict = [String: AnyObject]()
