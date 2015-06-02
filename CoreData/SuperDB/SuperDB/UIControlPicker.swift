8534ab0e03e72194021cdbcf7797c555a97006fc
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-06-02 19:12:17 +0200

    Initialisation method

--- CoreData/SuperDB/SuperDB/UIControlPicker.swift
+++ CoreData/SuperDB/SuperDB/UIControlPicker.swift
@@ -16,4 +16,41 @@ class UIControlPicker: UIControl {
     private var _blueSlider: UISlider!
     private var _alphaSlider: UISlider!
 
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        labelWithFrame(CGRectMake(20, 40, 60, 24), text: "Red")
+        labelWithFrame(CGRectMake(20, 80, 60, 24), text: "Green")
+        labelWithFrame(CGRectMake(20, 120, 60, 24), text: "Blue")
+        labelWithFrame(CGRectMake(20, 160, 60, 24), text: "Alpha")
+        
+        let theFunc = "sliderChanged:"
+        self._redSlider = createSliderWithAction(CGRectMake(100, 40, 190, 24), function: theFunc)
+        self._greenSlider = createSliderWithAction(CGRectMake(100, 80, 190, 24), function: theFunc)
+        self._blueSlider = createSliderWithAction(CGRectMake(100, 120, 190, 24), function: theFunc)
+        self._alphaSlider = createSliderWithAction(CGRectMake(100, 160, 190, 24), function: theFunc)
+    }
+    
+    private func labelWithFrame(frame: CGRect, text: String){
+        var label = UILabel(frame: frame)
+        label.userInteractionEnabled = false
+        label.backgroundColor = UIColor.clearColor()
+        label.font = UIFont.boldSystemFontOfSize(UIFont.systemFontSize())
+        label.textAlignment = NSTextAlignment.Right
+        label.textColor = UIColor.darkTextColor()
+        label.text = text
+        self.addSubview(label)
+    }
+    
+    func createSliderWithAction(frame: CGRect, function: String)->UISlider{
+        var _slider = UISlider(frame: frame)
+        _slider.addTarget(self, action: Selector(function), forControlEvents: .ValueChanged)
+        self.addSubview(_slider)
+        
+        return _slider
+    }
 }
