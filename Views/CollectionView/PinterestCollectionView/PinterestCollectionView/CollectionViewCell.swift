2b901fa9efa92a0eb353175a76ea63c87abc33a6
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-06-03 18:55:59 +0200

    Collection view cell

---
+++ Views/CollectionView/PinterestCollectionView/PinterestCollectionView/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  PinterestCollectionView
+//
+//  Created by Domenico on 03/06/15.
+//  Copyright (c) 2015 Domenico. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var pinImage: UIImageView!
+}
