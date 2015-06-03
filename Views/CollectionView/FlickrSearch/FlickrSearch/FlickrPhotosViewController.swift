08f5f1e1f0a168f67d8182889c7dc39fab231da1
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-06-03 21:02:34 +0200

    Private section insets

--- Views/CollectionView/FlickrSearch/FlickrSearch/FlickrPhotosViewController.swift
+++ Views/CollectionView/FlickrSearch/FlickrSearch/FlickrPhotosViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 let reuseIdentifier = "FlickrCell"
 
 class FlickrPhotosViewController: UICollectionViewController {
-
+    private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
