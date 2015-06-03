933c5ab66a33e45937c0f9ec248af3ab32fb37f7
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-06-03 22:40:52 +0200

    CollectionView Cell

---
+++ Views/CollectionView/FlickrSearch/FlickrSearch/FlickrPhotoCell.swift
@@ -0,0 +1,13 @@
+//
+//  FlickrPhotoCell.swift
+//  FlickrSearch
+//
+//  Created by Domenico on 03/06/15.
+//  Copyright (c) 2015 Domenico. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+    
+}
