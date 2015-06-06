2b91931ec8a1a278a5589ceea81745b04c70566b
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-06-06 17:21:38 +0200

    Initialization of the scrollView and setting the imageView at the center of the scrollView

--- Views/ScrollViews/ScrollViews/ScrollViews/ViewController.swift
+++ Views/ScrollViews/ScrollViews/ScrollViews/ViewController.swift
@@ -13,5 +13,57 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     @IBOutlet var scrollView: UIScrollView!
     var imageView: UIImageView!
     
+    override func viewDidLoad() {
+        // Create a new image
+        let image = UIImage(named: "photo1.png")!
+        imageView = UIImageView(image: image)
+        imageView.frame = CGRect(origin: CGPoint(x: 0, y: 0), size:image.size)
+        scrollView.addSubview(imageView)
+        
+        // Tell the scroll view the size of the content contained within it, so that it knows how far it can scroll horizontally and vertically
+        scrollView.contentSize = image.size
+        
+        // Setting up a gesture recognizer for the double-tap to zoom in. Zoom gestures is already included in the scrollView.
+        var doubleTapRecognizer = UITapGestureRecognizer(target: self, action: "scrollViewDoubleTapped:")
+        doubleTapRecognizer.numberOfTapsRequired = 2
+        doubleTapRecognizer.numberOfTouchesRequired = 1
+        scrollView.addGestureRecognizer(doubleTapRecognizer)
+        
+        // Minimum zoom scale for the scroll view
+        let scrollViewFrame = scrollView.frame
+        let scaleWidth = scrollViewFrame.size.width / scrollView.contentSize.width
+        let scaleHeight = scrollViewFrame.size.height / scrollView.contentSize.height
+        let minScale = min(scaleWidth, scaleHeight);
+        scrollView.minimumZoomScale = minScale;
+        
+        scrollView.maximumZoomScale = 1.0
+        scrollView.zoomScale = minScale;
+        
+        // Center the image within the scroll view
+        centerScrollViewContents()
+    }
+    
+    func centerScrollViewContents() {
+        /**
+        Slight annoyance with UIScrollView: if the scroll view content size is smaller than its bounds, then it sits at the top-left rather than in the center.
+        */
+        let boundsSize = scrollView.bounds.size
+        var contentsFrame = imageView.frame
+        
+        if contentsFrame.size.width < boundsSize.width {
+            contentsFrame.origin.x = (boundsSize.width - contentsFrame.size.width) / 2.0
+        } else {
+            contentsFrame.origin.x = 0.0
+        }
+        
+        if contentsFrame.size.height < boundsSize.height {
+            contentsFrame.origin.y = (boundsSize.height - contentsFrame.size.height) / 2.0
+        } else {
+            contentsFrame.origin.y = 0.0
+        }
+        
+        imageView.frame = contentsFrame
+    }
+    
 }
 
