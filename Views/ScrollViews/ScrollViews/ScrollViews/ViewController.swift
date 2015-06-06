9d4512a6f7287107e51525ae24ac6cd5b492e09e
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-06-06 17:05:09 +0200

    Conforming to UIScrollViewDelegate and adding an IBOutlet for the ScrollView

--- Views/ScrollViews/ScrollViews/ScrollViews/ViewController.swift
+++ Views/ScrollViews/ScrollViews/ScrollViews/ViewController.swift
@@ -8,18 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    @IBOutlet var scrollView: UIScrollView!
+    
 }
 
