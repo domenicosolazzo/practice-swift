a63eec7fad71578b79baa80d40128b71b0ab77d8
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-06-06 17:44:31 +0200

    Adding new PagedScrollViewController; Conforming to UIScrollViewDelegate

---
+++ Views/ScrollViews/ScrollViews/ScrollViews/PagedScrollViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PagedScrollViewController.swift
+//  ScrollViews
+//
+//  Created by Domenico on 06/06/15.
+//  Copyright (c) 2015 Domenico. All rights reserved.
+//
+
+import UIKit
+
+class PagedScrollViewController: UIViewController, UIScrollViewDelegate {
+
+
+}
