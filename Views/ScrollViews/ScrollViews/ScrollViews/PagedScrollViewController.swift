b23eba2cbab890737fc4d20b2f4962d0d9b0a6c7
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-06-06 17:51:47 +0200

    Some comments

--- Views/ScrollViews/ScrollViews/ScrollViews/PagedScrollViewController.swift
+++ Views/ScrollViews/ScrollViews/ScrollViews/PagedScrollViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class PagedScrollViewController: UIViewController, UIScrollViewDelegate {
     @IBOutlet var scrollView: UIScrollView!
     @IBOutlet var pageControl: UIPageControl!
+    // It will hold all the images to display. 1 for page
     var pageImages: [UIImage] = []
+    // It will hold instances of UIImageView to display each image on its respective page
     var pageViews: [UIImageView?] = []
 }
