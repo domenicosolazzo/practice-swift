774b63a507faad71b14030a9783e8a6cf4b87af5
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-06-06 19:58:14 +0200

    Initialization code

--- Views/PageViewController/PageViewControllerTutorial/PageViewControllerTutorial/PageContentViewController.swift
+++ Views/PageViewController/PageViewControllerTutorial/PageViewControllerTutorial/PageContentViewController.swift
@@ -16,4 +16,15 @@ class PageContentViewController: UIViewController {
     var pageIndex: Int?
     var titleText : String!
     var imageName : String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.bkImageView.image = UIImage(named: imageName)
+        self.heading.text = self.titleText
+        self.heading.alpha = 0.1
+        UIView.animateWithDuration(1.0, animations: { () -> Void in
+            self.heading.alpha = 1.0
+        })
+        
+    }
 }
