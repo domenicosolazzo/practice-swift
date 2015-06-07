9051f7dd7165129c19a5c88032eb5421a857ee44
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-06-07 13:20:25 +0200

    Adding a PlaceTableViewController and PlaceTableViewCell

---
+++ Views/TableViews/UITableViewCell Customization/UITableViewCell Customization/PlacesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PlacesTableViewCell.swift
+//  UITableViewCell Customization
+//
+//  Created by Domenico on 07/06/15.
+//  Copyright (c) 2015 Domenico. All rights reserved.
+//
+
+import UIKit
+
+class PlacesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
