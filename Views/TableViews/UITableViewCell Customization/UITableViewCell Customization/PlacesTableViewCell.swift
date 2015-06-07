6511998b4631921eac114bd4c316fe658e5a1450
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-06-07 13:36:30 +0200

    IBOutlet for the imageView and the label

--- Views/TableViews/UITableViewCell Customization/UITableViewCell Customization/PlacesTableViewCell.swift
+++ Views/TableViews/UITableViewCell Customization/UITableViewCell Customization/PlacesTableViewCell.swift
@@ -10,15 +10,8 @@ import UIKit
 
 class PlacesTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+    @IBOutlet weak var cellLabel: UILabel!
+    @IBOutlet weak var cellImageView: UIImageView!
+    
 
 }
