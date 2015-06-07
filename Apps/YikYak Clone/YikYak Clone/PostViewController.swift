301c41bce6e070cffa8f31db8839ccd83e42ccb3
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-06-07 18:22:56 +0200

    Adding two bar button items

---
+++ Apps/YikYak Clone/YikYak Clone/PostViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PostViewController.swift
+//  YikYak Clone
+//
+//  Created by Domenico on 07/06/15.
+//  Copyright (c) 2015 Domenico. All rights reserved.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+}
