8b859dda1d488abd861c45dc50302f8af79d35d9
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-06-07 11:40:32 +0200

    Adding the IBOutlet for the table view and the search bar

--- Views/SearchBar/SearchBar/SearchBar/ViewController.swift
+++ Views/SearchBar/SearchBar/SearchBar/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
