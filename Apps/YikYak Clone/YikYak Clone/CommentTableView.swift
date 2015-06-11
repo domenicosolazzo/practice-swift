9294f652fa4743e225dae6441779051068631429
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-06-11 17:45:11 +0200

    Adding a TableView and TableViewCell

---
+++ Apps/YikYak Clone/YikYak Clone/CommentTableView.swift
@@ -0,0 +1,13 @@
+//
+//  CommentTableView.swift
+//  YikYak Clone
+//
+//  Created by Domenico on 11/06/15.
+//  Copyright (c) 2015 Domenico. All rights reserved.
+//
+
+import Cocoa
+
+class CommentTableView: UITableView {
+
+}
