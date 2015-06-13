acc892e0527afc8005d63fe2e863ec23e939d9b1
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-06-13 10:32:23 +0200

    New project: UIKit Dynamics - Gravity

---
+++ Views/Dynamics/Gravity/Gravity/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Gravity
+//
+//  Created by Domenico on 13/06/15.
+//  Copyright (c) 2015 Domenico. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
