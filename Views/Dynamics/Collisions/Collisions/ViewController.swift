6b0a3adada26f09aacfd4af051e3b89f2ffbc214
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-06-13 11:11:48 +0200

    New project: Collisions

---
+++ Views/Dynamics/Collisions/Collisions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Collisions
+//
+//  Created by Domenico on 13/06/15.
+//  Copyright (c) 2015 Domenico. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
