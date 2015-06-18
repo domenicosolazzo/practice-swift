004ba2ba70044580db37e69e413e89c0235e681d
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-06-18 19:26:20 +0200

    Podfile

---
+++ ResearchKit/Camelot/Camelot/Camelot-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Camelot-Bridging-Header.h
+//  Camelot
+//
+//  Created by Domenico on 18/06/15.
+//  Copyright (c) 2015 Domenico. All rights reserved.
+//
+
+#ifndef Camelot_Camelot_Bridging_Header_h
+#define Camelot_Camelot_Bridging_Header_h
+
+
+#endif
