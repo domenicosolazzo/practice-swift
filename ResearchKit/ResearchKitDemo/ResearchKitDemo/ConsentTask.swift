c2851270dca758ea0c938ee91b58557e5cb37ef8
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-06-18 22:54:31 +0200

    Adding a consent task

---
+++ ResearchKit/ResearchKitDemo/ResearchKitDemo/ConsentTask.swift
@@ -0,0 +1,20 @@
+//
+//  ConsentTask.swift
+//  ResearchKitDemo
+//
+//  Created by Domenico on 18/06/15.
+//  Copyright (c) 2015 Domenico. All rights reserved.
+//
+
+import ResearchKit
+
+public var ConsentTask:ORKOrderedTask{
+    var steps = [ORKStep]()
+    
+    // Visual Step
+    
+    // Review step
+    
+    return ORKOrderedTask(identifier: "ConsentTask", steps: steps)
+
+}
