e1a8b539217646425d6c459a56acc0e799ca1e2c
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-06-19 07:08:57 +0200

    Some comments

--- ResearchKit/ResearchKitDemo/ResearchKitDemo/SurveyTask.swift
+++ ResearchKit/ResearchKitDemo/ResearchKitDemo/SurveyTask.swift
@@ -10,6 +10,15 @@ import ResearchKit
 
 public var SurveyTask: ORKOrderedTask{
     var steps = [ORKStep]()
+    //TODO: add instructions step
+    
+    //TODO: add name question
+    
+    //TODO: add 'what is your quest' question
+    
+    //TODO: add color question step
+    
+    //TODO: add summary step
     
     
     return ORKOrderedTask(identifier: "SurveyTask", steps: steps)
