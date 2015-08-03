ac1f2aec592ec45a7db965e367bcbbcc7b56095e
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-08-03 12:04:14 +0200

    Big O Notation playground

---
+++ playgrounds/BigONotation.playground/Contents.swift
@@ -0,0 +1,7 @@
+/** 
+    Big O Notation
+    Source: http://waynewbishop.com/swift/big-o-notation
+*/
+
+//a simple array of sorted integers
+let numberList : Array<Int> = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
