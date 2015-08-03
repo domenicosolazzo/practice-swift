db2a812dec459e9c14cbd16882d684bc1bcf8d77
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-08-03 12:06:54 +0200

    Implementing Linear Search

--- playgrounds/BigONotation.playground/Contents.swift
+++ playgrounds/BigONotation.playground/Contents.swift
@@ -5,3 +5,21 @@
 
 //a simple array of sorted integers
 let numberList : Array<Int> = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// Linear Search O(N)
+// Brute force approach
+func linearSearch(key: Int) {
+    
+    //check all possible values until we find a match
+    for number in numberList {
+        if (number == key) {
+            let results = "value \(key) found.."
+            break
+        }
+    }
+}
+
+linearSearch(10);
+
+
+
