f172819849389203317caeec4b6c1792f073810d
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-08-03 12:29:43 +0200

    Implementing Bubble Sort

--- playgrounds/Sorting.playground/Contents.swift
+++ playgrounds/Sorting.playground/Contents.swift
@@ -41,3 +41,33 @@ insertionSort()
 
 numberList = [8, 2, 10, 9, 11, 1, 7, 3, 4]
 count = 0
+
+// Bubble Sort
+// Rank items from the lowest to highest by swapping groups of two items from left to right.
+func bubbleSort() {
+    
+    var x, y, z, passes, key : Int
+    
+    //track collection iterations
+    for x in 0..<numberList.count {
+        count++
+        println("Count is \(count)")
+        passes = (numberList.count - 1) - x;
+        
+        //use shorthand "half-open" range operator
+        for y in 0..<passes {
+            key = numberList[y]
+            
+            //compare and rank values
+            if (key > numberList[y + 1]) {
+                z = numberList[y + 1]
+                numberList[y + 1] = key
+                numberList[y] = z
+            }
+            
+        } //end for
+        
+    } //end for
+}
+
+bubbleSort()
