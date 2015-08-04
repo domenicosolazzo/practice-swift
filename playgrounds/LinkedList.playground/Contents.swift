35389d8cac6c63563edc52f383d4f297e9965ce2
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-08-04 19:34:01 +0200

    LinkedList class structure

--- playgrounds/LinkedList.playground/Contents.swift
+++ playgrounds/LinkedList.playground/Contents.swift
@@ -9,3 +9,31 @@ class LLNode<T> {
     var next: LLNode?
     var previous: LLNode?
 }
+
+public class LinkedList<T: Equatable>{
+
+    // create a new LLNode instance
+    private var head: LLNode<T> = LLNode<T>()
+    
+    // The number of linked items
+    var count: Int{
+        get{
+            return 0
+        }
+    }
+    
+    // Append a new item to a linked list
+    func addLink(key:T){
+    
+    }
+    
+    // Remove a link at a specific index
+    func removeLinkAtIndex(index: Int){
+    
+    }
+    
+    // Print all keys for the class
+    func printAllKeys(){
+    
+    }
+}
