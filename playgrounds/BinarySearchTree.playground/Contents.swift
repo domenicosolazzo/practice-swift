488028c695ea48207a776698a586d0432475002e
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-08-05 12:43:33 +0200

    Adding a new item in the binary search tree

--- playgrounds/BinarySearchTree.playground/Contents.swift
+++ playgrounds/BinarySearchTree.playground/Contents.swift
@@ -16,4 +16,42 @@ public class AVLTree<T: Comparable> {
     init(){
     
     }
+    
+    // Add item based on its value
+    func addNode(key: T){
+        
+        // Check for the head node
+        if (self.key == nil) {
+            self.key = key
+            return
+        }
+        
+        // Check the left side of the tree
+        if (key < self.key) {
+            
+            if (self.left != nil) {
+                left!.addNode(key)
+            }else{
+                
+                // Create a new left node
+                var leftChild: AVLTree = AVLTree()
+                leftChild.key = key
+                self.left = leftChild
+            
+            }
+        }
+        
+        // Check the right side of the tree
+        if (key > self.key){
+            if (self.right != nil){
+                right!.addNode(key)
+            }else{
+            
+                // Create a new right node
+                var rightChild: AVLTree = AVLTree()
+                rightChild.key = key
+                self.right = rightChild
+            }
+        }
+    }
 }
