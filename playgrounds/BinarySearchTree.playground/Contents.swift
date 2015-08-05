0671da0829b8189d1108acae6b86d9ea65cfcbcf
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-08-05 12:39:16 +0200

    Generic Binary Search Tree

--- playgrounds/BinarySearchTree.playground/Contents.swift
+++ playgrounds/BinarySearchTree.playground/Contents.swift
@@ -5,3 +5,15 @@ import Foundation
 
 // A simple array of unsorted integers
 let numberList: Array<Int> = [8, 2, 10, 9, 11, 1, 7]
+
+// Generic binary search tree
+public class AVLTree<T: Comparable> {
+    
+    var key: T?
+    var left: AVLTree?
+    var right: AVLTree?
+    
+    init(){
+    
+    }
+}
