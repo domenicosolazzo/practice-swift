ef3070bb1ae352e8a59bdb2a111e43d02ee0144d
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-08-06 18:35:09 +0200

    Add a word

--- playgrounds/Trie.playground/Contents.swift
+++ playgrounds/Trie.playground/Contents.swift
@@ -16,3 +16,53 @@ public class TrieNode{
         self.level = 0
     }
 }
+
+// Trie implementation
+public class Trie {
+    private var root: TrieNode!
+    
+    init(){
+        root = TrieNode()
+    }
+    
+    // Build a recursive tree of dictionary content
+    func addWord(keyword: String){
+        
+        if (keyword.length == 0){
+            return;
+        }
+        
+        var current: TrieNode = root
+        var searchKey: String!
+        
+        while(keyword.length != current.level){
+            var childToUse: TrieNode!
+            var searchKey = keyword.substringToIndex(current.level + 1)
+            
+            for child in current.children {
+                if (child.key == searchKey){
+                    childToUse = child
+                    break
+                }
+            }
+            
+            // Create a new node
+            if (childToUse == nil){
+                childToUse = TrieNode()
+                childToUse.key = searchKey
+                childToUse.level = current.level + 1
+                current.children.append(childToUse)
+            }
+            
+            current = childToUse
+            
+            // Add final end of word check
+            if (keyword.length == current.level) {
+                current.isFinal = true
+                print("end of the word reached")
+                return;
+            }
+        }
+        
+    }
+}
