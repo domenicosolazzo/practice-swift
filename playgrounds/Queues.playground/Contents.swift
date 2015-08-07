c580fb2c5624276d06e4e9548c3b48b8544e5560
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-08-07 10:40:31 +0200

    Enqueue a new value

--- playgrounds/Queues.playground/Contents.swift
+++ playgrounds/Queues.playground/Contents.swift
@@ -7,5 +7,32 @@ public class QNode<T> {
 }
 
 public class Queue<T> {
+    private var top: QNode<T>! = QNode<T>()
     
+    // Enqueue the specified object
+    func enQueue(var key: T){
+        
+        // Check for the instance
+        if (top == nil) {
+            top = QNode<T>()
+        }
+        
+        // establish the top node
+        if (top.key == nil) {
+            top.key = key
+            return
+        }
+        
+        var childToUse: QNode<T> = QNode<T>()
+        var current: QNode = top
+        
+        // cycle through the list of items to get to the end.
+        while (current.next != nil) {
+            current = current.next!
+        }
+        
+        // Append a new item
+        childToUse.key = key
+        current.next = childToUse
+    }
 }
