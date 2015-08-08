225306166c54d0ccf8569550e4b5ae052ff52fa0
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-08-08 14:14:13 +0200

    Vertex data structure

--- playgrounds/Graphs.playground/Contents.swift
+++ playgrounds/Graphs.playground/Contents.swift
@@ -1,5 +1,16 @@
 // Graphs
 // Source: 
 
+// Basic vertex data structure
+public class Vertex {
+    var key: String?
+    var neighbors: Array<Edge>()
+    
+    init(){
+        self.neighbors = Array<Edge>()
+    }
+    
+}
+
 
 
