f7a5b14ad753c8092be790e6f98b25e4bc639c2a
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-10-14 18:38:58 +0200

    Adding an enumeration with the Errors

--- playgrounds/TryCatch.playground/Contents.swift
+++ playgrounds/TryCatch.playground/Contents.swift
@@ -3,3 +3,10 @@
 import UIKit
 
 var str = "Hello, Try / Catch"
+
+// Adding an enumeration with the Errors
+enum MyError: ErrorType {
+    case UserError
+    case NetworkError
+    case DiscoveryError
+}
