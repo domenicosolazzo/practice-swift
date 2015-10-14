87cedd62acea4b7e0e7b05a8c21f197aa1980596
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-10-14 18:43:02 +0200

    Adding a new function with the throws keyword

--- playgrounds/TryCatch.playground/Contents.swift
+++ playgrounds/TryCatch.playground/Contents.swift
@@ -10,3 +10,13 @@ enum MyError: ErrorType {
     case NetworkError
     case DiscoveryError
 }
+// Adding a fake function
+// Throws is a simple keyword that you add to your method to tell Swift it might fail
+func doStuff() throws -> String{
+    print("Do stuff1")
+    print("Do stuff2")
+    
+    throw MyError.NetworkError
+    
+    return "Some return value"
+}
