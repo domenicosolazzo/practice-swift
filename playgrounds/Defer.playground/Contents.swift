f4585ff322d4424e1d04142bcda2e471d771fd0f
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-10-18 09:39:53 +0200

    ExampExample without defer

--- playgrounds/Defer.playground/Contents.swift
+++ playgrounds/Defer.playground/Contents.swift
@@ -2,4 +2,27 @@
 
 import UIKit
 
+// This is how you would write the code before. If the guard statement was failing, 
+// the file would be open
+/**
+func writeLog() {
+    let file = openFile()
+    
+    let hardwareStatus = fetchHardwareStatus()
+    guard hardwareStatus != "disaster" else { return }
+    file.write(hardwareStatus)
+    
+    let softwareStatus = fetchSoftwareStatus()
+    guard softwareStatus != "disaster" else { return }
+    file.write(softwareStatus)
+    
+    let networkStatus = fetchNetworkStatus()
+    guard neworkStatus != "disaster" else { return }
+    file.write(networkStatus)
+    
+    closeFile(file)
+}
+*/
+
+
 
