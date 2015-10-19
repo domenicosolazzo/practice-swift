e8bb7940322c894e0de5f5d10afc7bfa70ba7fff
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-10-19 17:20:04 +0200

    How to checheck availability before Swift 2

---
+++ playgrounds/AvailabilityChecking.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Availability Checking
+// Describing how to use the new availability checking feature in iOS9
+
+import UIKit
+
+/**
+ * How to check an OS version before Swift 2
+ *
+if NSProcessInfo().isOperatingSystemAtLeastVersion(NSOperatingSystemVersion(majorVersion: 9, minorVersion: 0, patchVersion: 0)) {
+    print("Create the stack view!")
+}
+*/
+
+
