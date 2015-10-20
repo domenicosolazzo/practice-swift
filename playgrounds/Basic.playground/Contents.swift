a48c670aeb6071e0303aad144eb51afbc5d7225e
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-10-20 20:00:53 +0200

    Describing variables and constants in Swift

--- playgrounds/Basic.playground/Contents.swift
+++ playgrounds/Basic.playground/Contents.swift
@@ -12,3 +12,14 @@ var myFloat: Float = 2.0
 var myBool: Bool = true
 var myString: String = "My string"
 
+/**
+ * Swift use variables and constants for storing value and referring to them by name
+ * It uses the 'var' keyword followed from a name to define a variable.
+ * It uses the 'let' keyword followed from a name to define a constant.
+ * A constant value cannot be changed after it has been defined.
+**/
+var myVariable = "This is a variable containing a String"
+myVariable = "Changing this variable"
+
+let myConstant = "This is a constant and you cannot change this value"
+// myConstant = "error" <= It would give an error
