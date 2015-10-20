9cf66ebfae19d8deb0701c85c665a765ebf42304
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-10-20 19:45:52 +0200

    Fundamental types in Swift

--- playgrounds/Basic.playground/Contents.swift
+++ playgrounds/Basic.playground/Contents.swift
@@ -1,4 +1,13 @@
 //: Basic about Swift
 // Swift is the new programming language for iOS, OS X and watchOS
 
-import UIKit
+/**
+ * Swift provides all fundamental C and Objective-C types like Int for Integers,
+ * Double and Float for floating-point values, Bool for Boolean values, and
+ * String for textual data.
+ **/
+let myInt: Int = 3
+let myDouble: Double = 2.12
+let myFloat: Float = 2.0
+let myBool: Bool = true
+let myString: String = "My string"
