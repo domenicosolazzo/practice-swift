1987e16d9f8a69531c8b6f08a290c5927d294237
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-10-21 17:44:07 +0200

    About Integer bounds

--- playgrounds/Basic2.playground/Contents.swift
+++ playgrounds/Basic2.playground/Contents.swift
@@ -15,6 +15,14 @@ Swift provides both formats in 8, 16, 32 and 64 bit.
 The name convention uses UInt8 for an 8-bit unsigned integer, Int32 for a signed 32-bit integer and so on.
 **/
 
+/**
+==== Integer Bounds ====
+You can access the minimum and maximum values of each integer type with its min and max properties
+**/
+// Min value is equal to 0 and type UInt8
+let minValue = UInt8.min
 
+// Max value is equal to 255 and type UInt8
+let maxValue = UInt8.max
 
 
