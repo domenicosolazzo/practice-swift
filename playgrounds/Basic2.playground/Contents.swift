b5a2123987047efc435575607d4d4a660e2cf218
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-10-21 17:42:38 +0200

    Basic2: More basics about Swift

---
+++ playgrounds/Basic2.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Basic about Swift
+// Described in this playground
+// - Integers
+
+/**
+ * =================================
+ * =========== Integers ============
+ * =================================
+**/
+
+/**
+Integers are whole numbers with no fractional component. They can be signed or unsigned.
+Swift provides both formats in 8, 16, 32 and 64 bit.
+
+The name convention uses UInt8 for an 8-bit unsigned integer, Int32 for a signed 32-bit integer and so on.
+**/
+
+
+
+
