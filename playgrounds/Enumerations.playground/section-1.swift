a0ee0662341e9d716339096d22abc4e4c036e836
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-10-27 06:57:34 +0100

    Enumeration syntax

--- playgrounds/Enumerations.playground/section-1.swift
+++ playgrounds/Enumerations.playground/section-1.swift
@@ -11,14 +11,33 @@ the enumeration's value.
 */
 import Cocoa
 
+/**
+========================================
+======== Enumeration Syntax ============
+========================================
+**/
+/**
+Enumerations are created using the enum keyword and
+place their entire definition within a pair of braces.
 
+The values are called 'members values'.
+Unline C, Swift enumerations members are not assigned a default integer value when
+they are created. They are fully-fledged values on their own.
+
+The case keyword indicates that a new line of member values is
+about to be defined.
+**/
 enum Planet{
-    case Mercury, Venus, Earth, Mars,
+    case Mercury, Venus, Earth, Mars, // Multiple member values can appear separated by comma
         Jupiter, Saturn, Uranus, Neptune
     
 }
 
 var earth = Planet.Earth
+/**
+You can drop the Type in the following assignment because it is already known by Swift
+**/
+earth = .Mercury
 
 enum Barcode{
     case UPCA(Int, Int, Int)
