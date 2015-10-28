7e38f5e5a9c3a5330ff59f34bf698faae9a6544e
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-10-28 16:18:53 +0100

    Update to Swift 2.0

--- Views/TableViews/Populate TableViews/Populate TableViews/ViewController.swift
+++ Views/TableViews/Populate TableViews/Populate TableViews/ViewController.swift
@@ -23,7 +23,7 @@ class ViewController: UIViewController, UITableViewDataSource {
             
             // It takes an object that conform to UITableViewDataSource
             theTableView.dataSource = self
-            theTableView.autoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight
+            theTableView.autoresizingMask = [UIViewAutoresizing.FlexibleWidth, UIViewAutoresizing.FlexibleHeight]
             
             // Add the table view as subview
             view.addSubview(theTableView)
@@ -51,7 +51,7 @@ class ViewController: UIViewController, UITableViewDataSource {
     
     // Return each cell
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("identifier", forIndexPath: indexPath) as! UITableViewCell
+        let cell = tableView.dequeueReusableCellWithIdentifier("identifier", forIndexPath: indexPath) 
         
         cell.textLabel!.text = "Section \(indexPath.section) -> Row \(indexPath.row)"
         return cell
