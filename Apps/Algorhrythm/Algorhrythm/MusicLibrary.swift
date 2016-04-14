ad8b4fd2886deeea8b6c8ed523a986874eec7b50
Domenico Solazzo <solazzo.domenico@gmail.com>
2016-04-14 17:42:58 +0200

    Music Library

---
+++ Apps/Algorhrythm/Algorhrythm/MusicLibrary.swift
@@ -0,0 +1,63 @@
+//
+//  MusicLibrary.swift
+//  Algorhrythm
+//
+//  Created by Domenico on 14/04/16.
+//  Copyright © 2016 Domenico Solazzo. All rights reserved.
+//
+
+import Foundation
+
+struct MusicLibrary {
+    
+    let library = [
+        [
+            "title": "Rise and Shine",
+            "description": "Get your morning going by singing along to these classic tracks as you hit the shower bright and early!",
+            "icon": "coffee.pdf",
+            "largeIcon": "coffee_large.pdf",
+            "backgroundColor": ["red": 255, "green": 204, "blue": 51, "alpha": 1.0],
+            "artists": ["American Authors", "Vacationer", "Matt and Kim", "MGMT", "Echosmith", "Tokyo Police Club", "La Femme"]
+        ],
+        [
+            "title": "Runner's Rampage",
+            "description": "Hit the track hard and get in beast mode with everything from dance tracks to classic hip hop. All the right fuel to motivate you to push your limits.",
+            "icon": "running.pdf",
+            "largeIcon": "running_large.pdf",
+            "backgroundColor": ["red": 255, "green": 102, "blue": 51, "alpha": 1.0],
+            "artists": ["Avicii", "Calvin Harris", "Pitbull", "Iggy Azalea", "Rita Ora", "Drake", "Lil Wayne"]
+        ],
+        [
+            "title": "Joy Ride",
+            "description": "Let this eclectic playlist take you wherever your heart desires. Cruise along in style to these energetic beats.",
+            "icon": "helmet.pdf",
+            "largeIcon": "helmet_large.pdf",
+            "backgroundColor": ["red": 153, "green": 102, "blue": 204, "alpha": 1.0],
+            "artists": ["Afrojack", "Kid Cudi", "Daft Punk", "Icona Pop", "Gesaffelstien", "Roksnoix", "deadmau5"]
+        ],
+        [
+            "title": "In The Zone",
+            "description": "Keep calm and focus. Shut out the noise around you and grind away with some mind sharpening instrumental tunes.",
+            "icon": "laptop.pdf",
+            "largeIcon": "laptop_large.pdf",
+            "backgroundColor": ["red": 51, "green": 153, "blue": 204, "alpha": 1.0],
+            "artists": ["Dr. Dre", "Snoop Dogg", "Com Truise", "D12", "Flying Lotus", "Kanye West", "Rundfunk"]
+        ],
+        [
+            "title": "Button Masher",
+            "description": "Turn up the speakers and get out of my way! The ultimate gaming playlist to get you hyped up and ready for the crazy fun that’s about to happen.",
+            "icon": "joystick.pdf",
+            "largeIcon": "joystick_large.pdf",
+            "backgroundColor": ["red": 51, "green": 204, "blue": 102, "alpha": 1.0],
+            "artists": ["Skrillex", "The Game", "2 Chainz", "Jay Z", "T.I.", "Rihanna", "Eminem"]
+        ],
+        [
+            "title": "Futbal Remix",
+            "description": "There’s nothing like the world’s game. Kick around the field with this eclectic playlist from around the world. Futbal for life.",
+            "icon": "ball.pdf",
+            "largeIcon": "ball_large.pdf",
+            "backgroundColor": ["red": 255, "green": 102, "blue": 153, "alpha": 1.0],
+            "artists": ["Shakira", "Santana", "Wyclef Jean", "Aloe Blacc", "Pitbull", "Enrique Iglesias", "Ricky Martin"]
+        ]
+    ]
+}
