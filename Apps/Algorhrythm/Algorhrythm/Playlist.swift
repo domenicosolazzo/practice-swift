20cd206d746415692eda8a655f2c0d3429f596f9
Domenico Solazzo <solazzo.domenico@gmail.com>
2016-04-14 17:43:40 +0200

    Adding Playlist struct

---
+++ Apps/Algorhrythm/Algorhrythm/Playlist.swift
@@ -0,0 +1,50 @@
+//
+//  Playlist.swift
+//  Algorhrythm
+//
+//  Created by Domenico on 14/04/16.
+//  Copyright © 2016 Domenico Solazzo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Playlist {
+    
+    var title: String?
+    var description: String?
+    var icon: UIImage?
+    var largeIcon: UIImage?
+    var artists: [String] = []
+    var backgroundColor: UIColor = UIColor.clearColor()
+    
+    init(index: Int) {
+        let musicLibrary = MusicLibrary().library
+        let playlistDictionary = musicLibrary[index]
+        
+        title = playlistDictionary["title"] as! String!
+        description = playlistDictionary["description"] as! String!
+        
+        let iconName = playlistDictionary["icon"] as! String!
+        icon = UIImage(named: iconName)
+        
+        let largeIconName = playlistDictionary["largeIcon"] as! String!
+        largeIcon = UIImage(named: largeIconName)
+        
+        artists += playlistDictionary["artists"] as! [String]
+        
+        let colorsDictionary = playlistDictionary["backgroundColor"] as! [String:CGFloat]
+        backgroundColor = rgbColorFromDictionary(colorsDictionary)
+        
+    }
+    
+    func rgbColorFromDictionary(colorDictionary: [String:CGFloat]) -> UIColor {
+        let red = colorDictionary["red"]!
+        let green = colorDictionary["green"]!
+        let blue = colorDictionary["blue"]!
+        let alpha = colorDictionary["alpha"]!
+        
+        return UIColor(red: red/255.0, green: green/255.0, blue: blue/255.0, alpha: alpha)
+    }
+    
+}
