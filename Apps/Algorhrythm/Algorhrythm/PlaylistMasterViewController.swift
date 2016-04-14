f83dc8a99c70dc4c5b3abf38023408743fe94ef9
Domenico Solazzo <solazzo.domenico@gmail.com>
2016-04-14 19:15:08 +0200

    Removed button; added gesture recognizer

--- Apps/Algorhrythm/Algorhrythm/PlaylistMasterViewController.swift
+++ Apps/Algorhrythm/Algorhrythm/PlaylistMasterViewController.swift
@@ -10,14 +10,12 @@ import UIKit
 
 class PlaylistMasterViewController: UIViewController {
     
-    @IBOutlet weak var aButton: UIButton!
     @IBOutlet weak var playlistImageView0: UIImageView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        aButton.setTitle("Press me!", forState: .Normal)
         let playlist = Playlist(index: 0)
         playlistImageView0.image = playlist.icon
         playlistImageView0.backgroundColor = playlist.backgroundColor
