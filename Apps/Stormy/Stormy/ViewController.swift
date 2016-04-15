1687f01594944584e3c202a93228cb5b57737b4d
Domenico Solazzo <solazzo.domenico@gmail.com>
2016-04-15 18:03:28 +0200

    Setting the data to the CurrentWeather struct

--- Apps/Stormy/Stormy/ViewController.swift
+++ Apps/Stormy/Stormy/ViewController.swift
@@ -12,7 +12,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if let plistPath = NSBundle.mainBundle().pathForResource("CurrentWeather", ofType: "plist"),
+            let weatherDictionary = NSDictionary(contentsOfFile: plistPath),
+            let currentWeatherDictionary = weatherDictionary["currently"] as? [String:AnyObject]
+        {
+            let currentWeather = CurrentWeather(weatherDictionary:currentWeatherDictionary)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
