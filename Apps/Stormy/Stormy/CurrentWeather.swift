6b586ef48be5a8b23c92f87622b5b45a94f62723
Domenico Solazzo <solazzo.domenico@gmail.com>
2016-04-16 16:44:21 +0200

    Enum for the icons

--- Apps/Stormy/Stormy/CurrentWeather.swift
+++ Apps/Stormy/Stormy/CurrentWeather.swift
@@ -8,6 +8,19 @@
 
 import Foundation
 
+enum Icon: String {
+    case ClearDay = "clear-day"
+    case ClearNight = "clear-night"
+    case Rain = "rain"
+    case Snow = "snow"
+    case Sleet = "sleet"
+    case Wind = "wind"
+    case Fog = "fog"
+    case Cloudy = "cloudy"
+    case PartlyCloudyDay = "partly-cloudy-day"
+    case PartlyCloudyNight = "partly-cloudy-night"
+}
+
 struct CurrentWeather{
     let temperature:Int?
     let humidity:Int?
