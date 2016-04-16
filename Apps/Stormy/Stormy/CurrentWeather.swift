ed78e7b3fb2c78aa1694d39750a857a2e5546039
Domenico Solazzo <solazzo.domenico@gmail.com>
2016-04-16 14:24:46 +0200

    Handling nil values in CurrentWeather

--- Apps/Stormy/Stormy/CurrentWeather.swift
+++ Apps/Stormy/Stormy/CurrentWeather.swift
@@ -9,17 +9,25 @@
 import Foundation
 
 struct CurrentWeather{
-    let temperature:Int
-    let humidity:Int
-    let precipProbability:Int
-    let summary:String
+    let temperature:Int?
+    let humidity:Int?
+    let precipProbability:Int?
+    let summary:String?
     
     init(weatherDictionary:[String:AnyObject]){
-        temperature = weatherDictionary["temperature"] as! Int
-        let humidityFloat = weatherDictionary["humidity"] as! Double
-        humidity = Int(humidityFloat * 100)
-        let precipProbabilityFloat = weatherDictionary["precipProbability"] as! Double
-        precipProbability = Int(precipProbabilityFloat * 100)
-        summary = weatherDictionary["summary"] as! String
+        temperature = weatherDictionary["temperature"] as? Int
+        if let humidityFloat = weatherDictionary["humidity"] as? Double {
+            humidity = Int(humidityFloat * 100)
+        }else{
+            humidity = nil
+        }
+        
+        
+        if let precipProbabilityFloat = weatherDictionary["precipProbability"] as? Double{
+            precipProbability = Int(precipProbabilityFloat * 100)
+        }else{
+            precipProbability = nil
+        }
+        summary = weatherDictionary["summary"] as? String
     }
 }
