82525cb15da2dcecc2e23d8a359de7d12867a64b
Domenico Solazzo <solazzo.domenico@gmail.com>
2016-04-21 22:35:46 +0200

    Main browser controller

---
+++ Apps/Browser/Browser/BrowserViewController.swift
@@ -0,0 +1,13 @@
+//
+//  BrowserViewController.swift
+//  Browser
+//
+//  Created by Domenico on 21/04/16.
+//  Copyright © 2016 Domenico Solazzo. All rights reserved.
+//
+
+import UIKit
+
+class BrowserViewController: UIViewController {
+    
+}
