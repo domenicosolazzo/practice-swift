09cbf39704c8627065374715862b87d6db978b08
Domenico Solazzo <solazzo.domenico@gmail.com>
2016-04-21 22:57:09 +0200

    Initialize method

--- Apps/Browser/Browser/BrowserViewController.swift
+++ Apps/Browser/Browser/BrowserViewController.swift
@@ -14,7 +14,18 @@ class BrowserViewController: UIViewController {
     @IBOutlet weak var backButton: UIToolbar!
     @IBOutlet weak var forwardButton: UIToolbar!
     
-    var url: String = "http://www.google.com"
+    var siteUrl: String = "http://www.google.com"
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.initialize()
+    }
+    
+    func initialize(){
+        let url = NSURL(string: siteUrl)
+        let request = NSURLRequest(URL: url!)
+        webView.loadRequest(request)
+    }
     
     @IBAction func goBack(sender: UIBarButtonItem) {
         if(webView.canGoBack){
