420b48a1b17733a608bd159fc2722838446feab8
Domenico Solazzo <solazzo.domenico@gmail.com>
2016-04-28 21:19:08 +0200

    New audio example

---
+++ Audio/AudioExample/AudioExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AudioExample
+//
+//  Created by Domenico on 28/04/16.
+//  Copyright © 2016 Domenico Solazzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
