a366cc016c9a181ad36b62f39511c21848ac1a4c
Domenico Solazzo <solazzo.domenico@gmail.com>
2016-05-05 11:14:17 +0200

    Playground for NSFileManager

---
+++ playgrounds/NSFileManager.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// NSBundle
+var bundle = NSBundle.mainBundle()
+var resourcePath = bundle.resourcePath
+var fm = NSFileManager.defaultManager()
+
+
+// NSFileManager
+try{
+    var files = try fm.contentsOfDirectoryAtPath(resourcePath!)
+}catch{
+    print("Error")
+}
+
