bab6253c1c559aecf8cc5105a22afe51eb8a120b
domenicosolazzo <solazzo.domenico@gmail.com>
2016-11-01 21:06:59 +0100

    Migrating to Swift 3

--- Gesture/TapTaps/TapTaps/ViewController.swift
+++ Gesture/TapTaps/TapTaps/ViewController.swift
@@ -18,57 +18,53 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let singleTap = UITapGestureRecognizer(target: self, action: "singleTap")
+        let singleTap = UITapGestureRecognizer(target: self, action: #selector(ViewController.singleTap))
         singleTap.numberOfTapsRequired = 1
         singleTap.numberOfTouchesRequired = 1
         view.addGestureRecognizer(singleTap)
         
-        let doubleTap = UITapGestureRecognizer(target: self, action: "doubleTap")
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(ViewController.doubleTap))
         doubleTap.numberOfTapsRequired = 2
         doubleTap.numberOfTouchesRequired = 1
         view.addGestureRecognizer(doubleTap)
         // Important line for avoiding singleTap twice
-        singleTap.requireGestureRecognizerToFail(doubleTap)
+        singleTap.require(toFail: doubleTap)
         
-        let tripleTap = UITapGestureRecognizer(target: self, action: "tripleTap")
+        let tripleTap = UITapGestureRecognizer(target: self, action: #selector(ViewController.tripleTap))
         tripleTap.numberOfTapsRequired = 3
         tripleTap.numberOfTouchesRequired = 1
         view.addGestureRecognizer(tripleTap)
-        doubleTap.requireGestureRecognizerToFail(tripleTap)
+        doubleTap.require(toFail: tripleTap)
         
-        let quadrupleTap = UITapGestureRecognizer(target: self, action: "quadrupleTap")
+        let quadrupleTap = UITapGestureRecognizer(target: self, action: #selector(ViewController.quadrupleTap))
         quadrupleTap.numberOfTapsRequired = 4
         quadrupleTap.numberOfTouchesRequired = 1
         view.addGestureRecognizer(quadrupleTap)
-        tripleTap.requireGestureRecognizerToFail(quadrupleTap)
+        tripleTap.require(toFail: quadrupleTap)
     }
 
     func singleTap() {
         singleLabel.text = "Single Tap Detected"
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(2 * NSEC_PER_SEC)),
-            dispatch_get_main_queue(),
-            { self.singleLabel.text = "" })
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + Double(Int64(2 * NSEC_PER_SEC)) / Double(NSEC_PER_SEC),
+            execute: { self.singleLabel.text = "" })
     }
     
     func doubleTap() {
         doubleLabel.text = "Double Tap Detected"
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(2 * NSEC_PER_SEC)),
-            dispatch_get_main_queue(),
-            { self.doubleLabel.text = "" })
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + Double(Int64(2 * NSEC_PER_SEC)) / Double(NSEC_PER_SEC),
+            execute: { self.doubleLabel.text = "" })
     }
     
     func tripleTap() {
         tripleLabel.text = "Triple Tap Detected"
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(2 * NSEC_PER_SEC)),
-            dispatch_get_main_queue(),
-            { self.tripleLabel.text = "" })
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + Double(Int64(2 * NSEC_PER_SEC)) / Double(NSEC_PER_SEC),
+            execute: { self.tripleLabel.text = "" })
     }
     
     func quadrupleTap() {
         quadrupleLabel.text = "Quadruple Tap Detected"
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(2 * NSEC_PER_SEC)),
-            dispatch_get_main_queue(),
-            { self.quadrupleLabel.text = "" })
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + Double(Int64(2 * NSEC_PER_SEC)) / Double(NSEC_PER_SEC),
+            execute: { self.quadrupleLabel.text = "" })
     }
 
 
