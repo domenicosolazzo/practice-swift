33c22b5e66f4555991cd77e0db8c9b84c5d4c359
domenicosolazzo <solazzo.domenico@gmail.com>
2016-11-01 19:21:18 +0100

    Migrating to Swift 3

--- File Management/Deleting File and Folders/Deleting File and Folders/AppDelegate.swift
+++ File Management/Deleting File and Folders/Deleting File and Folders/AppDelegate.swift
@@ -15,83 +15,75 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func createAndDelete(){
         
-        let fileManager = NSFileManager()
+        let fileManager = FileManager()
         
-        func createFolderAtPath(path: String){
+        func createFolderAtPath(_ path: String){
             
-            var error:NSError?
             
-            if fileManager.createDirectoryAtPath(path,
+            do{
+                try fileManager.createDirectory(atPath: path,
                                                  withIntermediateDirectories:true,
-                                                 attributes: nil) == false && error != nil{
-                print("Failed to create folder at \(path), error = \(error!)")
+                                                 attributes: nil)
+                            
+            }catch{
+                print("Failed to create folder at \(path), error = \(error)")
+
             }
             
         }
         
         /* Creates 5 .txt files in the given folder, named 1.txt, 2.txt, etc */
-        func createFilesInFolder(folder: String){
+        func createFilesInFolder(_ folder: String){
             
             for counter in 1...5{
                 let fileName = NSString(format: "%lu.txt", counter)
-                let path = (folder as NSString).stringByAppendingPathComponent(String(fileName))
+                let path = (folder as NSString).appendingPathComponent(String(fileName))
                 let fileContents = "Some text"
-                var error:NSError?
-                if fileContents.writeToFile(path, atomically: true, encoding: NSStringEncoding){
-                    if let theError = error{
-                        print("Failed to save the file at path \(path)" +
-                            " with error = \(theError)")
-                    }
+                do{
+                    try fileContents.write(toFile: path, atomically: true, encoding: String.Encoding.utf8)
+                }catch{
+                    print("Failed to save the file at path \(path)")
                 }
             }
             
         }
         
         /* Enumerates all files/folders at a given path */
-        func enumerateFilesInFolder(folder: String){
+        func enumerateFilesInFolder(_ folder: String){
             
-            let error:NSError?
-            let contents = try! fileManager.contentsOfDirectoryAtPath(
-                folder)
-            
-            if let theError = error{
-                print("An error occurred \(theError)")
-            } else if contents.count == 0{
-                print("No content was found")
-            } else {
-                print("Contents of path \(folder) = \(contents)")
+            do{
+                _ = try fileManager.contentsOfDirectory(
+                atPath: folder)
+            }catch{
+                print("error")
             }
-            
         }
         
         /* Deletes all files/folders in a given path */
-        func deleteFilesInFolder(folder: String){
+        func deleteFilesInFolder(_ folder: String){
             
-            let error:NSError?
-            let contents = (try! fileManager.contentsOfDirectoryAtPath(folder)) 
-            
-            if let theError = error{
-                print("An error occurred = \(theError)")
-            } else {
+            do{
+                let contents = (try fileManager.contentsOfDirectory(atPath: folder))
                 for fileName in contents{
-                    let filePath = (folder as NSString).stringByAppendingPathComponent(fileName)
+                    let filePath = (folder as NSString).appendingPathComponent(fileName)
                     do {
-                        try fileManager.removeItemAtPath(filePath)
+                        try fileManager.removeItem(atPath: filePath)
                         print("Successfully removed item at path \(filePath)")
                     } catch _ {
                         print("Failed to remove item at path \(filePath)")
                     }
                 }
+            }catch{
+                print("error")
             }
-            
         }
         
         /* Deletes a folder with a given path */
-        func deleteFolderAtPath(path: String){
+        func deleteFolderAtPath(_ path: String){
             
             var error:NSError?
             do {
-                try fileManager.removeItemAtPath(path)
+                try fileManager.removeItem(atPath: path)
                 print("Successfully deleted the path \(path)")
             } catch let error1 as NSError {
                 error = error1
@@ -102,7 +94,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             
         }
         
-        let txtFolder = (NSTemporaryDirectory() as NSString).stringByAppendingPathComponent("txt")
+        let txtFolder = (NSTemporaryDirectory() as NSString).appendingPathComponent("txt")
         
         createFolderAtPath(txtFolder)
         createFilesInFolder(txtFolder)
@@ -113,7 +105,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
     }
     
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         self.createAndDelete()
         return true
     }
--- File Management/Deleting File and Folders/Deleting File and FoldersTests/Deleting_File_and_FoldersTests.swift
+++ File Management/Deleting File and Folders/Deleting File and FoldersTests/Deleting_File_and_FoldersTests.swift
@@ -28,7 +28,7 @@ class Deleting_File_and_FoldersTests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock() {
+        self.measure() {
             // Put the code you want to measure the time of here.
         }
     }
