0bc4c165e9d20f3217cc7a2f24117f3109141684
domenicosolazzo <solazzo.domenico@gmail.com>
2016-11-01 21:03:28 +0100

    Migrating to Swift 3

--- Gesture/PinchMe/PinchMe/ViewController.swift
+++ Gesture/PinchMe/PinchMe/ViewController.swift
@@ -10,57 +10,57 @@ import UIKit
 
 class ViewController: UIViewController,UIGestureRecognizerDelegate {
 
-    private var imageView:UIImageView!
-    private var scale:CGFloat = 1
-    private var previousScale:CGFloat = 1
-    private var rotation:CGFloat = 0
-    private var previousRotation:CGFloat = 0
+    fileprivate var imageView:UIImageView!
+    fileprivate var scale:CGFloat = 1
+    fileprivate var previousScale:CGFloat = 1
+    fileprivate var rotation:CGFloat = 0
+    fileprivate var previousRotation:CGFloat = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         let image = UIImage(named: "yosemite-meadows")
         imageView = UIImageView(image: image)
-        imageView.userInteractionEnabled = true
+        imageView.isUserInteractionEnabled = true
         imageView.center = view.center
         view.addSubview(imageView)
         
-        let pinchGesture = UIPinchGestureRecognizer(target: self, action: "doPinch:")
+        let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(ViewController.doPinch(_:)))
         pinchGesture.delegate = self
         imageView.addGestureRecognizer(pinchGesture)
         
-        let rotationGesture = UIRotationGestureRecognizer(target: self, action: "doRotate:")
+        let rotationGesture = UIRotationGestureRecognizer(target: self, action: #selector(ViewController.doRotate(_:)))
         rotationGesture.delegate = self
         imageView.addGestureRecognizer(rotationGesture)
     }
 
     // It will allow both gesture recognizer to work together
-    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer,
-        shouldRecognizeSimultaneouslyWithGestureRecognizer
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer,
+        shouldRecognizeSimultaneouslyWith
         otherGestureRecognizer: UIGestureRecognizer) -> Bool {
             return true
     }
     
     // Helper method
     func transformImageView() {
-        var t = CGAffineTransformMakeScale(scale * previousScale, scale * previousScale)
-        t = CGAffineTransformRotate(t, rotation + previousRotation)
+        var t = CGAffineTransform(scaleX: scale * previousScale, y: scale * previousScale)
+        t = t.rotated(by: rotation + previousRotation)
         imageView.transform = t
     }
     
-    func doPinch(gesture:UIPinchGestureRecognizer) {
+    func doPinch(_ gesture:UIPinchGestureRecognizer) {
         scale = gesture.scale
         transformImageView()
-        if gesture.state == .Ended {
+        if gesture.state == .ended {
             previousScale = scale * previousScale
             scale = 1
         }
     }
     
-    func doRotate(gesture:UIRotationGestureRecognizer) {
+    func doRotate(_ gesture:UIRotationGestureRecognizer) {
         rotation = gesture.rotation
         transformImageView()
-        if gesture.state == .Ended {
+        if gesture.state == .ended {
             previousRotation = rotation + previousRotation
             rotation = 0
         }
