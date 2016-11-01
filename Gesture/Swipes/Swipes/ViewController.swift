ed5f286904ea2bcb35e2cc7c85db870421689e89
domenicosolazzo <solazzo.domenico@gmail.com>
2016-11-01 21:05:44 +0100

    Migrating to Swift 3

--- Gesture/Swipes/Swipes/ViewController.swift
+++ Gesture/Swipes/Swipes/ViewController.swift
@@ -12,9 +12,9 @@ class ViewController: UIViewController {
 
     @IBOutlet var label:UILabel!
     // First touch position
-    private var gestureStartPoint:CGPoint!
-    private let minimumGestureLength = Float(25.0)
-    private let maximumVariance = Float(5)
+    fileprivate var gestureStartPoint:CGPoint!
+    fileprivate let minimumGestureLength = Float(25.0)
+    fileprivate let maximumVariance = Float(5)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,28 +26,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
-        let touch = touches.first as! UITouch
-        gestureStartPoint = touch.locationInView(self.view)
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! as UITouch
+        gestureStartPoint = touch.location(in: self.view)
     }
     
-    override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
-        let touch = touches.first as! UITouch
-        let currentPosition = touch.locationInView(self.view)
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! as UITouch
+        let currentPosition = touch.location(in: self.view)
         
         let deltaX = fabsf(Float(gestureStartPoint.x - currentPosition.x))
         let deltaY = fabsf(Float(gestureStartPoint.y - currentPosition.y))
         
         if deltaX >= minimumGestureLength && deltaY <= maximumVariance {
             label.text = "Horizontal swipe detected"
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(2 * NSEC_PER_SEC)),
-                dispatch_get_main_queue(),
-                { self.label.text = "" })
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + Double(Int64(2 * NSEC_PER_SEC)) / Double(NSEC_PER_SEC),
+                execute: { self.label.text = "" })
         } else if deltaY >= minimumGestureLength && deltaX <= maximumVariance {
             label.text = "Vertical swipe detected"
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(2 * NSEC_PER_SEC)),
-                dispatch_get_main_queue(),
-                { self.label.text = "" })
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + Double(Int64(2 * NSEC_PER_SEC)) / Double(NSEC_PER_SEC),
+                execute: { self.label.text = "" })
         }
     }
 
