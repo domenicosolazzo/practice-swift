2eeadff20e42b93c9e981f3ab65e265fc3c297b7
domenicosolazzo <solazzo.domenico@gmail.com>
2016-11-13 18:31:49 +0100

    Migrating to Swift3...

--- Settings/Bridge Control/Bridge ControlTests/Bridge_ControlTests.swift
+++ Settings/Bridge Control/Bridge ControlTests/Bridge_ControlTests.swift
@@ -28,7 +28,7 @@ class Bridge_ControlTests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock() {
+        self.measure() {
             // Put the code you want to measure the time of here.
         }
     }
