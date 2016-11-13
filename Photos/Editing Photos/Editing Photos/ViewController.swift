c5ce58ccdc5f8f021eff27c475f66711b576e780
domenicosolazzo <solazzo.domenico@gmail.com>
2016-11-13 18:29:18 +0100

    Migrating to Swift3...

--- Photos/Editing Photos/Editing Photos/ViewController.swift
+++ Photos/Editing Photos/Editing Photos/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
 
     /* These two values are our way of telling the Photos framework about
     the identifier of the changes that we are going to make to the photo */
-    let editFormatIdentifier = NSBundle.mainBundle().bundleIdentifier;
+    let editFormatIdentifier = Bundle.main.bundleIdentifier;
     /* Just an application-specific editing version */
     let editFormatVersion = "0.1"
     /* This is our filter name. We will use this for our Core Image filter */
@@ -27,9 +27,9 @@ class ViewController: UIViewController {
         PHPhotoLibrary.requestAuthorization{
             [weak self](status: PHAuthorizationStatus) in
             
-            dispatch_async(dispatch_get_main_queue()){
+            DispatchQueue.main.async{
                 switch status{
-                case .Authorized:
+                case .authorized:
                     self!.retrieveNewestImage()
                 default:
                     self!.displayAlertWithTitle("Access",
@@ -41,12 +41,12 @@ class ViewController: UIViewController {
     
     //- MARK: Helpers
     // Fetch NSData from an image. It uses OpenGL
-    func dataFromCIImage(image:CIImage) -> NSData{
-        let glContext = EAGLContext(API: EAGLRenderingAPI.OpenGLES2)
-        let context = CIContext(EAGLContext: glContext)
-        let imageRef = context.createCGImage(image, fromRect: image.extent())
-        let image = UIImage(CGImage: imageRef, scale: 1.0, orientation: .Up)
-        return UIImageJPEGRepresentation(image, 1.0)
+    func dataFromCIImage(_ image:CIImage) -> Data{
+        let glContext = EAGLContext(api: EAGLRenderingAPI.openGLES2)
+        let context = CIContext(eaglContext: glContext!)
+        let imageRef = context.createCGImage(image, from: image.extent)
+        let image = UIImage(cgImage: imageRef!, scale: 1.0, orientation: .up)
+        return UIImageJPEGRepresentation(image, 1.0)!
     }
     
     /* After this method retrieves the newest image from the user's assets
@@ -59,23 +59,23 @@ class ViewController: UIViewController {
             Then get an object of type PHFetchResult that will contain
             all of our image assets 
         */
-        let assetResults = PHAsset.fetchAssetsWithMediaType(PHAssetMediaType.Image, options: options)
+        let assetResults = PHAsset.fetchAssets(with: PHAssetMediaType.image, options: options)
         
         if assetResults == nil{
-            println("Found no results")
+            print("Found no results")
             return
         } else{
-            println("Found \(assetResults.count) results")
+            print("Found \(assetResults.count) results")
         }
         
-        let imageManager = PHCachingImageManager()
+        _ = PHCachingImageManager()
         
         if let asset = assetResults[0] as? PHAsset{
             editAsset(asset)
         }
     }
     
-    func editAsset(asset: PHAsset){
+    func editAsset(_ asset: PHAsset){
         
         /* Can we handle previous edits on this asset? */
         let requestOptions = PHContentEditingInputRequestOptions()
@@ -91,50 +91,55 @@ class ViewController: UIViewController {
         }
         
         /* Now ask the system if we are allowed to edit the given asset */
-        asset.requestContentEditingInputWithOptions(requestOptions,
-            completionHandler: {[weak self](input: PHContentEditingInput!,
-                info: [NSObject : AnyObject]!) in
+        asset.requestContentEditingInput(with: requestOptions,
+            completionHandler: {[weak self](input: PHContentEditingInput?,
+                info: [AnyHashable: Any]
+                ) in
                 
                 /* Get the required information from the asset */
-                let url = input.fullSizeImageURL
-                let orientation = input.fullSizeImageOrientation
+                let url = input?.fullSizeImageURL
+                let orientation = input?.fullSizeImageOrientation
                 
                 /* Retrieve an instance of CIImage to apply our filter to */
                 let inputImage =
-                CIImage(contentsOfURL: url,
-                    options: nil).imageByApplyingOrientation(orientation)
+                CIImage(contentsOf: url!,
+                    options: nil)?.applyingOrientation(orientation!)
                 
                 /* Apply the filter to our image */
                 let filter = CIFilter(name: self!.filterName)
-                filter.setDefaults()
-                filter.setValue(inputImage, forKey: kCIInputImageKey)
-                let outputImage = filter.outputImage
+                filter?.setDefaults()
+                filter?.setValue(inputImage, forKey: kCIInputImageKey)
+                let outputImage = filter?.outputImage
                 
                 /* Get the data of our edited image */
-                let editedImageData = self!.dataFromCIImage(outputImage)
+                let editedImageData = self!.dataFromCIImage(outputImage!)
                 
                 /* The results of editing our image are encapsulated here */
-                let output = PHContentEditingOutput(contentEditingInput: input)
+                let output = PHContentEditingOutput(contentEditingInput: input!)
                 /* Here we are saving our edited image to the URL that is dictated
                 by the content editing output class */
-                editedImageData.writeToURL(output.renderedContentURL,
-                    atomically: true)
+                do{
+                    try editedImageData.write(to:output.renderedContentURL,
+                                    options:Data.WritingOptions.atomic)
+                }catch{
+                    print("Error writing image data")
+                }
                 output.adjustmentData =
-                    PHAdjustmentData(formatIdentifier: self!.editFormatIdentifier,
+                    PHAdjustmentData(formatIdentifier: self!.editFormatIdentifier!,
                         formatVersion: self!.editFormatVersion,
-                        data: self!.filterName.dataUsingEncoding(NSUTF8StringEncoding,
-                            allowLossyConversion: false))
+                        data: self!.filterName.data(using: String.Encoding.utf8,
+                            allowLossyConversion: false)!)
                 
                 /* Now perform our changes */
-                PHPhotoLibrary.sharedPhotoLibrary().performChanges({
+                PHPhotoLibrary.shared().performChanges({
                     /* This is the change object and its output is the output object
                     that we created previously */
-                    let change = PHAssetChangeRequest(forAsset: asset)
+                    let change = PHAssetChangeRequest(for: asset)
                     change.contentEditingOutput = output
-                    }, completionHandler: {[weak self] (success: Bool, error: NSError!) in
+                    }, completionHandler: {[weak self] (success: Bool?, error: Error?) in
                         
                         self!.performOnMainThread{
-                            if success{
+                            if success!{
                                 self!.displayAlertWithTitle("Succeeded",
                                     message: "Successfully edited the image")
                             } else {
@@ -154,22 +159,22 @@ class ViewController: UIViewController {
         A little handy method that allows us to perform a block object
         on the main thread
     */
-    func performOnMainThread(block: dispatch_block_t){
-        dispatch_async(dispatch_get_main_queue(), block)
+    func performOnMainThread(_ block: @escaping ()->()){
+        DispatchQueue.main.async(execute: block)
     }
     
     
     /* Just a little method to help us display alert dialogs to the user */
-    func displayAlertWithTitle(title: String, message: String){
+    func displayAlertWithTitle(_ title: String, message: String){
         let controller = UIAlertController(title: title,
             message: message,
-            preferredStyle: .Alert)
+            preferredStyle: .alert)
         
         controller.addAction(UIAlertAction(title: "OK",
-            style: .Default,
+            style: .default,
             handler: nil))
         
-        presentViewController(controller, animated: true, completion: nil)
+        present(controller, animated: true, completion: nil)
         
     }
 }
