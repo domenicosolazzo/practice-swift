1982d9d15b883ce77e0c52688abbfb8b80f1fd32
domenicosolazzo <solazzo.domenico@gmail.com>
2016-11-13 18:31:00 +0100

    Migrating to Swift3...

--- Security/Authenticating the User with Touch ID/Authenticating the User with Touch ID/ViewController.swift
+++ Security/Authenticating the User with Touch ID/Authenticating the User with Touch ID/ViewController.swift
@@ -13,29 +13,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var checkButton: UIButton!
     @IBOutlet weak var useButton: UIButton!
 
-    @IBAction func checkAvailability(sender: UIButton) {
+    @IBAction func checkAvailability(_ sender: UIButton) {
         // Handle for using the TouchID API
         let context = LAContext()
         var error: NSError?
         
         let isTouchIDAvailable = context.canEvaluatePolicy(
-            LAPolicy.DeviceOwnerAuthenticationWithBiometrics,
+            LAPolicy.deviceOwnerAuthenticationWithBiometrics,
             error: &error)
         
-        useButton.enabled = isTouchIDAvailable
+        useButton.isEnabled = isTouchIDAvailable
         
         if isTouchIDAvailable == false{
             displayAlertWithTitle("TouchID error", message: "Sorry! TouchID is not available");
         }
     }
-    @IBAction func useTouchID(sender: UIButton) {
+    @IBAction func useTouchID(_ sender: UIButton) {
         let context = LAContext()
         var _: NSError
         
         let reason = "Please authenticate with TouchID"
         
         context.evaluatePolicy(
-            LAPolicy.DeviceOwnerAuthenticationWithBiometrics,
+            LAPolicy.deviceOwnerAuthenticationWithBiometrics,
             localizedReason: reason) {[weak self](success:Bool, error:NSError?) -> Void in
                 let title = "Touch ID Result";
                 var message = "You have been authenticated"
@@ -50,22 +50,22 @@ class ViewController: UIViewController {
                 self!.displayAlertWithTitle(title, message: message);
                 
                 
-        }
+        } as! (Bool, Error?) -> Void as! (Bool, Error?) -> Void as! (Bool, Error?) -> Void as! (Bool, Error?) -> Void as! (Bool, Error?) -> Void as! (Bool, Error?) -> Void as! (Bool, Error?) -> Void
     }
     
-    func displayAlertWithTitle(title: String, message: String){
+    func displayAlertWithTitle(_ title: String, message: String){
         let alertController = UIAlertController(
             title: title,
             message: message,
-            preferredStyle: UIAlertControllerStyle.Alert)
+            preferredStyle: UIAlertControllerStyle.alert)
         alertController.addAction(
             UIAlertAction(
                 title: "OK",
-                style: UIAlertActionStyle.Default,
+                style: UIAlertActionStyle.default,
                 handler: nil
             )
         )
-        self.presentViewController(alertController, animated: true, completion: nil)
+        self.present(alertController, animated: true, completion: nil)
     }
 }
 
--- Security/Authenticating the User with Touch ID/Authenticating the User with Touch IDTests/Authenticating_the_User_with_Touch_IDTests.swift
+++ Security/Authenticating the User with Touch ID/Authenticating the User with Touch IDTests/Authenticating_the_User_with_Touch_IDTests.swift
@@ -28,7 +28,7 @@ class Authenticating_the_User_with_Touch_IDTests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock() {
+        self.measure() {
             // Put the code you want to measure the time of here.
         }
     }
