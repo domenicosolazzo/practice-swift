76263f535aa41e0c823521234181fec68d52ab88
domenicosolazzo <solazzo.domenico@gmail.com>
2016-11-13 17:36:47 +0100

    Migrating to Swift3...

--- Multitasking/Grouping Tasks together/Grouping Tasks together/ViewController.swift
+++ Multitasking/Grouping Tasks together/Grouping Tasks together/ViewController.swift
@@ -12,51 +12,51 @@ class ViewController: UIViewController {
 
     func reloadTableView(){
         /* Reload the table view here */
-        println(__FUNCTION__)
+        print(#function)
     }
     
     func reloadScrollView(){
         /* Do the work here */
-        println(__FUNCTION__)
+        print(#function)
     }
     
     func reloadImageView(){
         /* Reload the image view here */
-        println(__FUNCTION__)
+        print(#function)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let taskGroup = dispatch_group_create()
-        let mainQueue = dispatch_get_main_queue()
+        let taskGroup = DispatchGroup()
+        let mainQueue = DispatchQueue.main
         
         // Reload the table view in the main queue
-        dispatch_group_async(taskGroup, mainQueue, {[weak self] in
+        mainQueue.async(group: taskGroup, execute: {[weak self] in
             self!.reloadTableView()
         })
         
         // Reload the scrollView in the main queue
-        dispatch_group_async(taskGroup, mainQueue, {[weak self] in
+        mainQueue.async(group: taskGroup, execute: {[weak self] in
             self!.reloadScrollView()
         })
 
         // Reload the ImageView in the main queue
-        dispatch_group_async(taskGroup, mainQueue, {[weak self] in
+        mainQueue.async(group: taskGroup, execute: {[weak self] in
             self!.reloadImageView()
         })
         
         /* When we are done, dispatch the following block */
-        dispatch_group_notify(taskGroup, mainQueue, {[weak self] in
+        taskGroup.notify(queue: mainQueue, execute: {[weak self] in
             // Do some processing here
             
-            let alertController = UIAlertController(title: "Task group", message: "We have finished all the operations in background", preferredStyle: UIAlertControllerStyle.Alert)
+            let alertController = UIAlertController(title: "Task group", message: "We have finished all the operations in background", preferredStyle: UIAlertControllerStyle.alert)
             
             alertController.addAction(
-                UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil)
+                UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil)
             )
             
-            self!.presentViewController(alertController, animated: true, completion: nil)
+            self!.present(alertController, animated: true, completion: nil)
         })
     }
 }
