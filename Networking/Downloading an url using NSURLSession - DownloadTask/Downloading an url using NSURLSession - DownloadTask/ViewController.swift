95b41526c291e1311414cfae942f9a89df134799
domenicosolazzo <solazzo.domenico@gmail.com>
2016-11-13 17:54:43 +0100

    Migrating to Swift3...

--- Networking/Downloading an url using NSURLSession - DownloadTask/Downloading an url using NSURLSession - DownloadTask/ViewController.swift
+++ Networking/Downloading an url using NSURLSession - DownloadTask/Downloading an url using NSURLSession - DownloadTask/ViewController.swift
@@ -8,74 +8,72 @@
 
 import UIKit
 
-class ViewController: UIViewController, NSURLSessionDelegate {
+class ViewController: UIViewController, URLSessionDelegate {
     
     // Session object
-    var session: NSURLSession!
+    var session: URLSession!
     
     required init(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
+        super.init(coder: aDecoder)!
         
         // Configuration object
-        let configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let configuration = URLSessionConfiguration.default
         configuration.timeoutIntervalForRequest = 15
         
-        session = NSURLSession(configuration: configuration, delegate: self, delegateQueue: nil)
+        session = URLSession(configuration: configuration, delegate: self, delegateQueue: nil)
     }
 
-    override func viewDidAppear(animated: Bool) {
+    override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
         var urlString = "http://goo.gl/mf9xj3"
-        var url = NSURL(string: urlString)
+        var url = URL(string: urlString)
         
-        var task = session.downloadTaskWithURL(url!, completionHandler: {[weak self]
-            (url:NSURL!, response:NSURLResponse!, error:NSError!) in
+        var task = session.downloadTask(with: url!, completionHandler: {[weak self]
+            (url:URL!, response:URLResponse!, error:NSError!) in
             if error == nil{
                 
                 // NSFileManager
-                let manager = NSFileManager()
+                let manager = FileManager()
                 
                 /* Get the path to the caches folder */
-                var error: NSError?
-                var destinationPath = manager.URLForDirectory(
-                    NSSearchPathDirectory.CachesDirectory,
-                    inDomain: NSSearchPathDomainMask.UserDomainMask,
-                    appropriateForURL: url,
-                    create: true,
-                    error: &error
-                )!
+                var destinationPath = try manager.url(
+                    for: FileManager.SearchPathDirectory.cachesDirectory,
+                    in: FileManager.SearchPathDomainMask.userDomainMask,
+                    appropriateFor: url,
+                    create: true
+                )
                 
                 // Extract the name of the downloaded file
-                let componentsOfUrl = url.absoluteString!.componentsSeparatedByString("/")
+                let componentsOfUrl = url.absoluteString.componentsSeparated(by: "/")
                 let fileNameOfUrl = componentsOfUrl[componentsOfUrl.count - 1]
                 
                 // Append the name of the file to the destination folder
-                destinationPath = destinationPath.URLByAppendingPathComponent(fileNameOfUrl)
+                destinationPath = destinationPath.appendingPathComponent(fileNameOfUrl)
                 
                 // Move the file over
-                manager.moveItemAtURL(url, toURL: destinationPath, error: nil)
+                try manager.moveItem(at: url, to: destinationPath)
                 
                 // Message
                 let message = "Saved the downloaded data to \(destinationPath)"
                 self!.showAlertWithTitle("Saved", message: message)
                 
             }else{
-                println("Error: \(error)")
+                print("Error: \(error)")
                 self!.showAlertWithTitle("Error", message: "Could not download the data! Error: \(error)")
             }
-        })
+        } as! (URL?, URLResponse?, Error?) -> Void)
         
         // Start the task
         task.resume()
     }
     
     //- MARK: Helper methods
-    func showAlertWithTitle(title: String, message:String){
-        let controller = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+    func showAlertWithTitle(_ title: String, message:String){
+        let controller = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
         
-        controller.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
-        presentViewController(controller, animated: true, completion: nil)
+        controller.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        present(controller, animated: true, completion: nil)
     }
 }
 
--- Networking/Downloading an url using NSURLSession - DownloadTask/Downloading an url using NSURLSession - DownloadTaskTests/Downloading_an_url_using_NSURLSession___DownloadTaskTests.swift
+++ Networking/Downloading an url using NSURLSession - DownloadTask/Downloading an url using NSURLSession - DownloadTaskTests/Downloading_an_url_using_NSURLSession___DownloadTaskTests.swift
@@ -28,7 +28,7 @@ class Downloading_an_url_using_NSURLSession___DownloadTaskTests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock() {
+        self.measure() {
             // Put the code you want to measure the time of here.
         }
     }
