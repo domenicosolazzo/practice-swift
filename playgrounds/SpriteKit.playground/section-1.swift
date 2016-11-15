306aa2c1c7a11c8b07209af1c65914acde5d4954
domenicosolazzo <solazzo.domenico@gmail.com>
2016-11-15 05:41:57 +0100

    Migrating to Swift 3

--- playgrounds/SpriteKit.playground/section-1.swift
+++ playgrounds/SpriteKit.playground/section-1.swift
@@ -9,23 +9,23 @@ class MyScene:SKScene{
     var _previousTime = 0.0
     var _deltaTime = 0.0
     
-    override func didMoveToView(view: SKView!) {
-        var sprite = SKSpriteNode(color:NSColor.greenColor(), size: CGSize(width:50.0, height:50.0))
+    override func didMove(to view: SKView) {
+        let sprite = SKSpriteNode(color:NSColor.green, size: CGSize(width:50.0, height:50.0))
         sprite.name = "block"
         sprite.position = CGPoint(x:50.0, y:50.0)
         self.addChild(sprite)
     }
     
-    override func update(currentTime: NSTimeInterval) {
+    override func update(_ currentTime: TimeInterval) {
         if _previousTime == 0.0 { _previousTime = currentTime}
         // Delta
         _deltaTime = currentTime - _previousTime
         
         _previousTime = currentTime
         // Retrieve the sprite by name
-        var block = self.childNodeWithName("block")
+        var block = self.childNode(withName: "block")
         // Update block position
-        block.position = CGPoint(x: block.position.x + (20.0 * _deltaTime), y: block.position.y + (20.0 * _deltaTime))
+        block?.position = CGPoint(x: Double(block?.position.x) + Double((20.0 * _deltaTime)), y: Double(block.position.y) + Double((20.0 * _deltaTime)))
     }
     
     
@@ -33,7 +33,7 @@ class MyScene:SKScene{
 
 // Create the view
 var view = SKView(frame:NSRect(x:0, y:0, width:1024, height:768))
-XCPShowView("view", view)
+XCPShowView(identifier: "view", view: view)
 
 // Create a new scene
 var scene = MyScene(size: CGSize(width:1024, height:768))
