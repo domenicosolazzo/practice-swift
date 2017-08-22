cb8775bf4ba0d337f79a3d3836f41332e68c32a9
Domenico Solazzo <solazzo.domenico@gmail.com>
2017-08-22 22:39:59 +0200

    New function for generating a plane

--- AR/ARPlaneDetection/ARPlaneDetection/NodeGenerator.swift
+++ AR/ARPlaneDetection/ARPlaneDetection/NodeGenerator.swift
@@ -11,5 +11,20 @@ import SceneKit
 import ARKit
 
 struct NodeGenerator {
-
+    
+    static func generatePlaneFrom(planeAnchor: ARPlaneAnchor, physics: Bool, hidden: Bool) -> SCNNode {
+        let plane = self.plane(from: planeAnchor, hidden: hidden)
+        
+        let planeNode = SCNNode(geometry: plane)
+        planeNode.position = self.position(from: planeAnchor)
+        
+        if physics {
+            let body = SCNPhysicsBody(type: .kinematic, shape: SCNPhysicsShape(geometry: plane, options: nil))
+            body.restitution = 0.0
+            body.friction = 1.0
+            planeNode.physicsBody = body
+        }
+        
+        return planeNode
+    }
 }
