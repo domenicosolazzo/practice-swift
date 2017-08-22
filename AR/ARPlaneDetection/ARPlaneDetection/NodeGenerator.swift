465bd480a40bba62a121058d62ecb2284c4b54eb
Domenico Solazzo <solazzo.domenico@gmail.com>
2017-08-22 22:42:51 +0200

    Adding last methods and clean up....

--- AR/ARPlaneDetection/ARPlaneDetection/NodeGenerator.swift
+++ AR/ARPlaneDetection/ARPlaneDetection/NodeGenerator.swift
@@ -27,4 +27,27 @@ struct NodeGenerator {
         
         return planeNode
     }
+    
+    static func update(planeNode: SCNNode, from planeAnchor: ARPlaneAnchor, hidden: Bool) {
+        planeNode.geometry = self.plane(from: planeAnchor, hidden: hidden)
+        planeNode.position = self.position(from: planeAnchor)
+    }
+    
+    static func update(planeNode: SCNNode, hidden: Bool) {
+        planeNode.geometry?.materials.first?.diffuse.contents = hidden ? UIColor(white: 1, alpha: 0) : UIColor(red: 0, green: 0, blue: 1, alpha: 0.5)
+    }
+    
+    private static func plane(from planeAnchor: ARPlaneAnchor, hidden: Bool) -> SCNGeometry {
+        let plane = SCNBox(width: CGFloat(planeAnchor.extent.x), height: 0.005, length: CGFloat(planeAnchor.extent.z), chamferRadius: 0)
+        
+        let color = SCNMaterial()
+        color.diffuse.contents = hidden ? UIColor(white: 1, alpha: 0) : UIColor(red: 0, green: 0, blue: 1, alpha: 0.5)
+        plane.materials = [color]
+        
+        return plane
+    }
+    
+    private static func position(from planeAnchor: ARPlaneAnchor) -> SCNVector3 {
+        return SCNVector3Make(planeAnchor.center.x, -0.005, planeAnchor.center.z)
+    }
 }
