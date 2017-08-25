92d78a516b7591c44926796838603af6fba22a51
Domenico Solazzo <solazzo.domenico@gmail.com>
2017-08-25 15:03:37 +0200

    First commit

---
+++ Vision/VisionFaceLandmarks/VisionFaceLandmarks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VisionFaceLandmarks
+//
+//  Created by Domenico Solazzo on 8/25/17.
+//  Copyright © 2017 Domenico Solazzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
